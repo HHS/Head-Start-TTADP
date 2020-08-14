a73112d305af96c4615b68e35ab6e4008a256cc6
Josh Salisbury <josh@adhocteam.us>
2020-08-14 12:51:33 -0500

    Update hello world text

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -14,14 +14,9 @@ function App() {
           {' '}
           and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          Hello World!
+        </p>
       </header>
     </div>
   );
