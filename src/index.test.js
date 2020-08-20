de65fab05c7ad8adbd1c22c5bb70b6a7373773b8
Josh Salisbury <josh@adhocteam.us>
2020-08-20 16:50:20 -0500

    cleanup yarn scripts (#8)
    * Clarify package.json scripts
    
    The backend `test` and `lint` commands now only test/lint a single
    componenet. This should help clarify how to run tests or the linter
    only the frontend or backend. Add readme for the frontend.
    
    * Unit test result files go to the correct dir
    
    * Remove unneeded env variables from compose file
    
    * Remove the audit command
    
    * Add cross-env as a dev package
    
    This package allows cross platform setting of environment variables
    
    * Update readme
    
    Add section instructing to not switch between docker and host yarn
    commands without installing proper dependencies. Add section that offers
    help running docker on windows
    
    * Remove build files, add to gitignore
    
    * Clarify and update .env. Rename test
    
     * .env handling updated. .env is now .gitignored and the old .env
    renamed to .env.example. Should make understanding it's purpose a little
    easier. Added more to the comment explaining the .env files purpose
     * Test name now easier to understand for non-tech people
    
    * Format lint:CI to console and file
    
    Co-authored-by: Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>

--- src/index.test.js
+++ src/index.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from './app';
 
 describe('Root', () => {
-  test('Returns a 200', async () => {
+  test('Successfully returns the page', async () => {
     const response = await request(app).get('/');
     expect(response.status).toBe(200);
   });
