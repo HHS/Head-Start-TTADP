14a0b80c8e5c5cc4d274ca97a52dd5a3baa8a2cf
Josh Salisbury <josh@adhocteam.us>
2020-10-27 16:45:17 -0500

    Add cucumber test

---
+++ cucumber/features/steps/activityReportSteps.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const {
+  Given, Then, When,
+} = require('@cucumber/cucumber');
+const assert = require('assert');
+const scope = require('../support/scope');
+
+Given('I am on the activity reports page', async () => {
+  const page = scope.context.currentPage;
+  const selector = 'a[href$="activity-reports"]';
+  await Promise.all([
+    page.waitForNavigation(),
+    page.click(selector),
+  ]);
+});
+
+When('I select {string}', async (inputLabel) => {
+  const page = scope.context.currentPage;
+  const selector = `//label[text()='${inputLabel}']`;
+  const element = await page.$x(selector);
+  await element[0].click();
+});
+
+Then('I see {string} as an option in the {string} dropdown', async (expectedOption, dropdownLabel) => {
+  const page = scope.context.currentPage;
+  const selector = `//label[text()='${dropdownLabel}']/../select`;
+  const el = await page.$x(selector);
+  const selected = await el[0].$x(`//option[text()='${expectedOption}']`);
+
+  assert(selected.length === 1);
+  assert(selected !== null || selected !== undefined);
+});
