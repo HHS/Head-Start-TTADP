47298d8490e3d10d2d30871c5e7b1d7a817ce93f
kryswisnaskas <krystyna.wisnaskas@gmail.com>
2020-11-06 12:19:12 -0500

    Update routes

--- src/app.js
+++ src/app.js
@@ -10,13 +10,12 @@ import path from 'path';
 import join from 'url-join';
 
 import logger, { requestLogger } from './logger';
-import authMiddleware, { hsesAuth, login } from './middleware/authMiddleware';
+import authMiddleware, { hsesAuth } from './middleware/authMiddleware';
+import { loginPath } from './routes/apiDirectory';
 
 const app = express();
-const router = express.Router();
 const MemoryStore = memorystore(session);
 const oauth2CallbackPath = '/oauth2-client/login/oauth2/code/';
-const loginPath = '/login';
 
 app.use(requestLogger);
 app.use(express.json());
@@ -43,29 +42,7 @@ if (process.env.NODE_ENV === 'production') {
 authMiddleware.unless = unless;
 // TODO: update unless to replace `oauth1CallbackPath with `join('/api', oauth2CallbackPath)`
 // once our oauth callback has been updated
-app.use(authMiddleware.unless({ path: [oauth2CallbackPath, join('/api', loginPath), '/api/user'] }));
-
-// router.get('/hello', (req, res) => {
-//   logger.info('Hello from ttadp');
-//   res.send('Hello from ttadp');
-// });
-
-// router.post('/hello', (req, res) => {
-//   logger.info('Hello from ttadp');
-//   res.send('Hello from ttadp');
-// });
-
-// router.get('/user', (req, res) => {
-//   const { userId, role, name } = req.session;
-//   res.send({ userId, role, name });
-// });
-
-router.get('/logout', (req, res) => {
-  req.session.destroy();
-  res.sendStatus(204);
-});
-
-// router.get(loginPath, login);
+app.use(authMiddleware.unless({ path: [oauth2CallbackPath, join('/api', loginPath)] }));
 
 // TODO: change `app.get...` with `router.get...` once our oauth callback has been updated
 app.get(oauth2CallbackPath, async (req, res) => {
@@ -92,6 +69,5 @@ app.get(oauth2CallbackPath, async (req, res) => {
 });
 
 app.use('/api', require('./routes/apiDirectory').default);
-// app.use('/api', router);
 
 module.exports = app;
