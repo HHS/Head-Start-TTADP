d1d3a3bdc0de838848d05d442cdf3ca2e7fd0bf4
Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>
2020-12-21 17:56:40 -0600

    database_url

--- config/config.js
+++ config/config.js
@@ -17,10 +17,6 @@ module.exports = {
     logging: false,
   },
   production: {
-    username: process.env.POSTGRES_USERNAME,
-    password: process.env.POSTGRES_PASSWORD,
-    database: process.env.POSTGRES_DB,
-    host: process.env.POSTGRES_HOST,
-    dialect: 'postgres',
+    use_env_variable: process.env.DATABASE_URL
   },
 };
