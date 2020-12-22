2761340b80355677caef62b02dddc70da6e79c2e
Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>
2020-12-22 15:42:47 -0600

    use variable name

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], { dialect: 'postgres' });
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
