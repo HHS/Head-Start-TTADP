2c38854f32347207534401038b87446ba0cc1a19
Sarah-Jaine Szekeresh <sarah-jaine@adhocteam.us>
2020-12-22 14:26:56 -0600

    without config options

--- src/models/index.js
+++ src/models/index.js
@@ -10,7 +10,7 @@ const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable]);
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
