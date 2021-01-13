6881b4e142b51ace58aac8960cbbdfe8f9ed3eef
Chuck McAndrew <chuck.mcandrew@adhocteam.us>
2021-01-13 14:15:34 -0500

    add POST /files endpoint to upload files to s3

--- src/routes/apiDirectory.js
+++ src/routes/apiDirectory.js
@@ -5,6 +5,7 @@ import join from 'url-join';
 import authMiddleware, { login } from '../middleware/authMiddleware';
 import handleErrors from '../lib/apiErrorHandler';
 import adminRouter from './user';
+import filesRouter from './files';
 import activityReportsRouter from './activityReports';
 import { userById } from './admin/user';
 
@@ -18,7 +19,7 @@ router.use(authMiddleware.unless({ path: [join('/api', loginPath)] }));
 
 router.use('/admin/users', adminRouter);
 router.use('/activity-reports', activityReportsRouter);
-
+router.use('/files', filesRouter)
 router.use('/hello', (req, res) => {
   res.send('Hello from ttadp');
 });
