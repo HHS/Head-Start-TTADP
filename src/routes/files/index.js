a41b773356b03038bbad53e0fe0422ee8262e795
Chuck McAndrew <chuck.mcandrew@adhocteam.us>
2021-01-13 14:15:05 -0500

    Add handler for file upload endpoint

---
+++ src/routes/files/index.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import uploadHandler from './handlers'
+
+const router = express.Router();
+
+/**
+ * API for file uploads
+ */
+
+router.post('/', uploadHandler);
+
+export default router;
