d2cff7f284136170b356cb3468cc24d90d636791
Chuck McAndrew <chuck.mcandrew@adhocteam.us>
2021-01-19 10:52:46 -0500

    update create-files migration

--- src/migrations/20210114193123-create-file.js
+++ src/migrations/20210114193123-create-file.js
@@ -7,14 +7,26 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER,
       },
+      activityReportId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references: {
+          model: {
+            tableName: 'ActivityReports',
+          },
+        },
+      },
       originalFileName: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       key: {
         type: Sequelize.STRING,
+        allowNull: false,
       },
       status: {
         type: Sequelize.ENUM('UPLOADING', 'UPLOADED', 'UPLOAD_FAILED', 'SCANNING', 'APPROVED', 'REJECTED'),
+        allowNull: false,
       },
       createdAt: {
         allowNull: false,
