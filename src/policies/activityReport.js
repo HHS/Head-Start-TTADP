418df050af91bfbf0bf3f52fdfd0736691e543ae
Josh Salisbury <josh@adhocteam.us>
2021-01-25 09:58:34 -0600

    Update documentation

--- src/policies/activityReport.js
+++ src/policies/activityReport.js
@@ -1,3 +1,11 @@
+/*
+  This class handles authorization for Activity Reports. In express handlers this
+  object should be constructed with the current user and the activity report to be
+  updated/retrieved for update/get or the new report for create. The permissions
+  of the user are checked against the report and simple questions about the actions
+  the user can take on that report are answered, mainly can the user create, update
+  or get the report.
+*/
 import _ from 'lodash';
 import SCOPES from '../middleware/scopeConstants';
 
