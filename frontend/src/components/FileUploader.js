84a97a6bb2498ef1cfc59d6475f8aacc8ccff7e5
Chuck McAndrew <chuck.mcandrew@adhocteam.us>
2021-01-28 16:52:33 -0500

    fmt

--- frontend/src/components/FileUploader.js
+++ frontend/src/components/FileUploader.js
@@ -8,11 +8,9 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useDropzone } from 'react-dropzone';
-import * as FS from 'fs';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { Tag, Button, Grid } from '@trussworks/react-uswds';
+import {  Button } from '@trussworks/react-uswds';
 import { uploadFile } from '../fetchers/File';
 
 import './FileUploader.css';
@@ -23,57 +21,31 @@ function Dropzone(props) {
     let attachmentType;
     if (id === 'attachments') {
       attachmentType = 'ATTACHMENT';
-    } else if (id === "other-resources") {
+    } else if (id === 'other-resources') {
       attachmentType = 'RESOURCE';
     }
     e.forEach( async (file) => {
-    try {
-      const data = new FormData()
-      data.append("reportId", reportId)
-      data.append("attachmentType", attachmentType)
-      data.append("file", file)
-      await uploadFile(data)
-    } catch (error) {
-      console.log(error)
-    }
-
+      try {
+        const data = new FormData();
+        data.append('reportId', reportId);
+        data.append('attachmentType', attachmentType);
+        data.append('file', file)
+        await uploadFile(data)
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.log(error);
+      }
     });
-
-    console.log(e)
     onChange(e);
   };
   const { getRootProps, getInputProps } = useDropzone({ onDrop });
-
-  // I tried moving these styles to a css file and applying a class to the container
-  // and span. The styles were not being applied, it seems like the Dropzone library
-  // is messing with the styles somewhere
-  const containerStyle = {
-    maxWidth: '21rem',
-    height: '8rem',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderStyle: 'dashed',
-    borderWidth: '0.125rem',
-    borderColor: '#979797',
-  };
-
-  const textStyle = {
-    textAlign: 'left',
-    fontSize: '1.25rem',
-    backgroundColor: "#0166AB",
-    color: "white", 
-    padding: ".5rem",
-    borderRadius: "5px",
-  };
-
 
   return (
     <div
       {...getRootProps()}
     >
       <input {...getInputProps()} />
-      <button className="usa-button">
+      <button className='usa-button'>
         Browse files
       </button>
     </div>
@@ -82,35 +54,37 @@ function Dropzone(props) {
 
 Dropzone.propTypes = {
   onChange: PropTypes.func.isRequired,
+  reportId: PropTypes.any.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 const FileTable = ({onFileRemoved, files}) => {
   let msg 
   if (files.length === 0) {
     msg = (
-      <p className="files-table--empty">No files uploaded</p>
+      <p className='files-table--empty'>No files uploaded</p>
     )
   }
   return (
-    <div className="files-table--container margin-top-2">
-      <table row gap className="files-table">
-        <thead className="files-table--thead" bgcolor="#F8F8F8">
-        <th width="50%">
+    <div className='files-table--container margin-top-2'>
+      <table row gap className='files-table'>
+        <thead className='files-table--thead' bgcolor='#F8F8F8'>
+        <th width='50%'>
           Name
         </th>
-        <th width="20%">
+        <th width='20%'>
           Size
         </th>
-        <th width="20%">
+        <th width='20%'>
           Status
         </th>
-        <th width="10%">
+        <th width='10%'>
         </th>
         </thead>
         <tbody>
         {files.map((file, index) => (
         <tr>
-            <td className="files-table--file-name">
+            <td className='files-table--file-name'>
               {file.name}
             </td>
             <td>
@@ -121,14 +95,14 @@ const FileTable = ({onFileRemoved, files}) => {
             </td>
           <td>
               <Button
-                role="button"
-                className="smart-hub--file-tag-button"
+                role='button'
+                className='smart-hub--file-tag-button'
                 unstyled
-                aria-label="remove file"
+                aria-label='remove file'
                 onClick={() => { onFileRemoved(index); }}
               >
-                <span className="fa-stack fa-sm">
-                  <FontAwesomeIcon className="fa-stack-1x" color="black" icon={faTrash} />
+                <span className='fa-stack fa-sm'>
+                  <FontAwesomeIcon className='fa-stack-1x' color='black' icon={faTrash} />
                 </span>
               </Button>
           </td>
