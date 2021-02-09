b29ea71aacc38cc3f1dd4b00abee2435fd201ca3
Chuck McAndrew <chuck.mcandrew@adhocteam.us>
2021-02-09 13:22:40 -0500

    fmt

--- frontend/src/components/FileUploader.js
+++ frontend/src/components/FileUploader.js
@@ -101,7 +101,6 @@ const FileTable = ({ onFileRemoved, files }) => (
           <tr key={file.key} id={`files-table-row-${index}`}>
             <td className="files-table--file-name">
               {file.originalFileName}
-              {console.log(file.url)}
             </td>
             <td>
               {`${(file.fileSize / 1000).toFixed(1)} KB`}
