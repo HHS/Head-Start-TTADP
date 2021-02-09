1d08788738b2fecacc1c710d8dc33f94b5707251
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-02-09 12:18:39 -0500

    Display lastLogin on admin user details

--- frontend/src/pages/Admin/UserInfo.js
+++ frontend/src/pages/Admin/UserInfo.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import {
   Label, TextInput, Grid, Fieldset,
 } from '@trussworks/react-uswds';
+import moment from 'moment';
 
 import RegionDropdown from '../../components/RegionDropdown';
 import JobTitleDropdown from '../../components/JobTitleDropdown';
@@ -12,12 +13,16 @@ import JobTitleDropdown from '../../components/JobTitleDropdown';
  * editing of basic user information.
  */
 function UserInfo({ user, onUserChange }) {
+  let { lastLogin } = user;
+  if (lastLogin && lastLogin !== '') {
+    lastLogin = moment(lastLogin).format('lll Z');
+  }
   return (
     <Fieldset className="margin-bottom-2" legend="User Info">
       <Grid row gap>
         <Grid col={12}>
           <Label htmlFor="input-email-name">Email</Label>
-          <TextInput disabled id="input-email-name" type="text" name="email" value={user.email || ''} onChange={onUserChange} />
+          <TextInput readOnly disabled id="input-email-name" type="text" name="email" value={user.email || ''} />
         </Grid>
         <Grid col={12}>
           <Label htmlFor="input-full-name">Full Name</Label>
@@ -30,6 +35,12 @@ function UserInfo({ user, onUserChange }) {
         </Grid>
         <Grid col={6}>
           <JobTitleDropdown id="role" name="role" value={user.role || undefined} onChange={onUserChange} />
+        </Grid>
+      </Grid>
+      <Grid row gap>
+        <Grid col={12}>
+          <Label htmlFor="input-last-login">Last Login</Label>
+          <TextInput readOnly disabled id="input-last-login" type="text" name="lastLogin" value={lastLogin} />
         </Grid>
       </Grid>
     </Fieldset>
