4c849e0a51344abc82794aadc78310566c01b172
Chuck McAndrew <chuck.mcandrew@adhocteam.us>
2021-02-15 15:04:00 -0500

    add retry and backoff logic to file scanning job

--- src/services/queue.js
+++ src/services/queue.js
@@ -6,5 +6,11 @@ const { REDIS_HOST, REDIS_PASS } = process.env;
 const scanQueue = new Queue('scan', `redis://${REDIS_HOST}:${REDIS_PORT}`, { redis: { password: REDIS_PASS } });
 
 export default async function addToScanQueue(fileKey) {
-  await scanQueue.add(fileKey);
+  const retries = process.env.FILE_SCAN_RETRIES || 5;
+  const delay = process.env.FILE_SCAN_BACKOFF_DELAY || 5000;
+  const backOffOpts = {
+    type: 'exponential',
+    delay,
+  };
+  await scanQueue.add(fileKey, { attempts: retries, backoff: backOffOpts });
 }
