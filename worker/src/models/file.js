998b6ebeff32786a71adaa29efe6943154985169
Chuck McAndrew <chuck.mcandrew@adhocteam.us>
2021-02-15 15:35:54 -0500

    restructure worker

---
+++ worker/src/models/file.js
@@ -0,0 +1,49 @@
+import { Model } from 'sequelize';
+
+module.exports = (sequelize, DataTypes) => {
+  class File extends Model {
+    static associate(models) {
+      File.belongsTo(models.ActivityReport, { foreignKey: 'activityReportId' });
+    }
+  }
+  File.init({
+    activityReportId: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+    },
+    originalFileName: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    key: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.ENUM(
+        'UPLOADING',
+        'UPLOADED',
+        'UPLOAD_FAILED',
+        'QUEUEING_FAILED',
+        'SCANNING_QUEUED',
+        'SCANNING',
+        'APPROVED',
+        'REJECTED',
+      ),
+      allowNull: false,
+    },
+    attachmentType: {
+      type: DataTypes.ENUM('ATTACHMENT', 'RESOURCE'),
+      allowNull: false,
+    },
+    // File size in bytes
+    fileSize: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  }, {
+    sequelize,
+    modelName: 'File',
+  });
+  return File;
+};
