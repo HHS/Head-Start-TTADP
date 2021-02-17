8f7a8415ad7075ef9cb7f9d7a0564d384f11b978
Daniel Cloud <daniel.cloud@adhocteam.us>
2021-02-17 12:04:47 -0500

    Update SiteNav with accessibility fixes: color contrast, landmark roles for sections

--- frontend/src/components/SiteNav.js
+++ frontend/src/components/SiteNav.js
@@ -11,8 +11,7 @@ const navLinkClasses = [
   'padding-y-1',
   'margin-y-1',
   'font-ui-xs',
-  'text-base-light',
-  'text-base-light',
+  'text-base-lightest',
   'text-no-underline',
   'smart-hub-cursor-pointer',
   'hover:text-white',
@@ -49,31 +48,33 @@ const SiteNav = ({
       {authenticated && (
         <>
           <div className="width-full">
-            <div className="margin-x-2 border-bottom smart-hub-border-darkblue padding-y-2">
+            <div role="complementary" className="margin-x-2 border-bottom smart-hub-border-darkblue padding-y-2">
               <p className="text-bold margin-y-105">{ user.name }</p>
               <p className="font-sans-3xs margin-y-105">{ user.email }</p>
             </div>
           </div>
-          <nav className="width-full margin-bottom-2">
-            <ul className="add-list-reset">
-              <li>
-                <Link
-                  className="display-block border-left-05 border-white text-white text-bold text-no-underline padding-2 margin-y-2"
-                  to="/activity-reports"
-                >
-                  <span className="padding-right-105">
-                    <FontAwesomeIcon color="white" icon={faChartBar} />
-                  </span>
-                  Activity Reports
-                </Link>
-              </li>
-            </ul>
+          <nav>
+            <div className="width-full margin-bottom-2">
+              <ul className="add-list-reset">
+                <li>
+                  <Link
+                    className="display-block border-left-05 border-white text-white text-bold text-no-underline padding-2 margin-y-2"
+                    to="/activity-reports"
+                  >
+                    <span className="padding-right-105">
+                      <FontAwesomeIcon color="white" icon={faChartBar} />
+                    </span>
+                    Activity Reports
+                  </Link>
+                </li>
+              </ul>
+            </div>
+            <div className="width-full position-absolute bottom-0 padding-bottom-6">
+              <ul className="add-list-reset margin-x-2 padding-top-2 text-base-lightest border-top smart-hub-border-darkblue">
+                {items.map((item, i) => (<li key={`smart-hub-nav__item-${i}`}>{item}</li>))}
+              </ul>
+            </div>
           </nav>
-          <div className="width-full position-absolute bottom-0 padding-bottom-6">
-            <ul className="add-list-reset margin-x-2 padding-top-2 text-base-lighter border-top smart-hub-border-darkblue">
-              {items.map((item, i) => (<li key={`smart-hub-nav__item-${i}`}>{item}</li>))}
-            </ul>
-          </div>
         </>
       )}
     </div>
