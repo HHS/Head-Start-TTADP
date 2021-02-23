4ed635c934707d7ed9755c4ec15468aae0cadc74
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2021-02-23 17:16:24 -0500

    fmt

--- frontend/src/components/__tests__/FileUploader.js
+++ frontend/src/components/__tests__/FileUploader.js
@@ -66,11 +66,11 @@ describe('FileUploader', () => {
 
   it('files can be removed', () => {
     const mockOnChange = jest.fn();
-    render(<FileUploader reportId="new" id="attachment" onChange={mockOnChange} files={[{id: 1, originalFileName: 'fileOne'}, {id: 2, originalFileName: 'fileTwo'}]} />);
+    render(<FileUploader reportId="new" id="attachment" onChange={mockOnChange} files={[{ id: 1, originalFileName: 'fileOne' }, { id: 2, originalFileName: 'fileTwo' }]} />);
     const fileTwo = screen.getByText('fileTwo');
     fireEvent.click(fileTwo.parentNode.lastChild.firstChild);
 
-    expect(mockOnChange).toHaveBeenCalledWith([{id: 1, originalFileName: 'fileOne'}]);
+    expect(mockOnChange).toHaveBeenCalledWith([{ id: 1, originalFileName: 'fileOne' }]);
   });
   describe('getStatus tests', () => {
     it('returns the correct statuses', () => {
--- frontend/src/fetchers/__tests__/File.js
+++ frontend/src/fetchers/__tests__/File.js
@@ -6,7 +6,7 @@ const fileApiUrl = join('/', 'api', 'files');
 const fakeFile = new File(['testing'], 'test.txt');
 
 describe('File fetcher', () => {
-  beforeEach(()=>fetchMock.reset())
+  beforeEach(() => fetchMock.reset());
   it('test that the file gets uploaded', async () => {
     fetchMock.postOnce(fileApiUrl, 200);
     const res = await uploadFile(fakeFile);
@@ -17,12 +17,12 @@ describe('File fetcher', () => {
     const res = await deleteFile(1);
     expect(res.status).toBe(200);
   });
-  it('Throws an error if the response status isn\'t 200', async () => {
+  it('file upload throws an error if the response status isn\'t 200', async () => {
     fetchMock.postOnce(fileApiUrl, 500);
-    await expect(uploadFile(fakeFile)).rejects.toThrow()
-  })
-  it('Throws an error if the response status isn\'t 200', async () => {
-    fetchMock.deleteOnce(join(fileApiUrl,'1'), 500);
-    await expect(deleteFile(fakeFile)).rejects.toThrow()
-  })
+    await expect(uploadFile(fakeFile)).rejects.toThrow();
+  });
+  it('file delete throws an error if the response status isn\'t 200', async () => {
+    fetchMock.deleteOnce(join(fileApiUrl, '1'), 500);
+    await expect(deleteFile(fakeFile)).rejects.toThrow();
+  });
 });
