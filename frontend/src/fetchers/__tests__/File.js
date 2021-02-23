84c971756b0c3e53c79fbb8ff47c6b4d4d16f911
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2021-02-23 16:16:48 -0500

    add tests

--- frontend/src/fetchers/__tests__/File.js
+++ frontend/src/fetchers/__tests__/File.js
@@ -1,14 +1,28 @@
 import fetchMock from 'fetch-mock';
 import join from 'url-join';
-import uploadFile from '../File';
+import uploadFile, { deleteFile } from '../File';
 
-const activityReportUrl = join('/', 'api', 'files');
+const fileApiUrl = join('/', 'api', 'files');
 const fakeFile = new File(['testing'], 'test.txt');
 
 describe('File fetcher', () => {
+  beforeEach(()=>fetchMock.reset())
   it('test that the file gets uploaded', async () => {
-    fetchMock.mock(activityReportUrl, 200);
+    fetchMock.postOnce(fileApiUrl, 200);
     const res = await uploadFile(fakeFile);
     expect(res.status).toBe(200);
   });
+  it('test that the file gets deleted', async () => {
+    fetchMock.deleteOnce(join(fileApiUrl, '1'), 200);
+    const res = await deleteFile(1);
+    expect(res.status).toBe(200);
+  });
+  it('Throws an error if the response status isn\'t 200', async () => {
+    fetchMock.postOnce(fileApiUrl, 500);
+    await expect(uploadFile(fakeFile)).rejects.toThrow()
+  })
+  it('Throws an error if the response status isn\'t 200', async () => {
+    fetchMock.deleteOnce(join(fileApiUrl,'1'), 500);
+    await expect(deleteFile(fakeFile)).rejects.toThrow()
+  })
 });
