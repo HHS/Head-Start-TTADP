2b3288aad061ff1df178c94d4780dee1b444ec33
Josh Salisbury <josh@adhocteam.us>
2021-03-03 15:33:37 -0600

    Add additional fetcher test

--- frontend/src/fetchers/__tests__/activityReports.js
+++ frontend/src/fetchers/__tests__/activityReports.js
@@ -1,7 +1,7 @@
 import join from 'url-join';
 import fetchMock from 'fetch-mock';
 
-import { submitReport, saveReport, reviewReport } from '../activityReports';
+import { submitReport, saveReport, reviewReport, resetToDraft } from '../activityReports';
 
 describe('activityReports fetcher', () => {
   afterEach(() => fetchMock.restore());
@@ -20,6 +20,15 @@ describe('activityReports fetcher', () => {
       const report = { id: 1 };
       fetchMock.put(join('api', 'activity-reports', '1'), report);
       const savedReport = await saveReport(1, report);
+      expect(savedReport).toEqual(report);
+    });
+  });
+
+  describe('resetToDraft', () => {
+    it('calls reset and returns the result', async () => {
+      const report = { id: 1 };
+      fetchMock.put(join('api', 'activity-reports', '1', 'reset'), report);
+      const savedReport = await resetToDraft(1);
       expect(savedReport).toEqual(report);
     });
   });
