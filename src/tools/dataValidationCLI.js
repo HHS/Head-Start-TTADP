285a381ebb5dc610fca80bc49205a0957e497f29
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-03-15 11:40:38 -0400

    Add prod running instructions, will also add to wiki

--- src/tools/dataValidationCLI.js
+++ src/tools/dataValidationCLI.js
@@ -4,6 +4,8 @@ import { auditLogger } from '../logger';
 /**
  * dataValidationCLI runs basic queries against the DB to verify that the db state
  * is as we expect following import or restore operations.
+ *
+ * To run: `cf run-task tta-smarthub-prod --command "yarn db:validation"`
  */
 
 dataValidation().catch((e) => {
