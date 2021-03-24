ce3be5f1027c9bb3854f581c020c22b5a75da12a
Ryan Ahearn <ryan.ahearn@gsa.gov>
2021-03-24 11:11:28 -0400

    Linter fixes

--- src/migrations/20210323195745-fix-legacy-report-region-id.js
+++ src/migrations/20210323195745-fix-legacy-report-region-id.js
@@ -1,6 +1,6 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => (
-    queryInterface.sequelize.query('UPDATE "ActivityReports" SET "legacyId" = regexp_replace("legacyId", \'R(\d)-\', \'R0\1-\') WHERE "legacyId" ~ \'R\d-\'')
+  up: (queryInterface) => (
+    queryInterface.sequelize.query('UPDATE "ActivityReports" SET "legacyId" = regexp_replace("legacyId", \'R(\\d)-\', \'R0\\1-\') WHERE "legacyId" ~ \'R\\d-\'')
   ),
   down: async () => {
     /**
