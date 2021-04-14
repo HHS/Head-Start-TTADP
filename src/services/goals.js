2546e7bfa753decfcc99568624bb92ab724ec71a
Chuck McAndrew <6248903+dcmcand@users.noreply.github.com>
2021-04-14 15:14:55 -0400

    fix goals.js

--- src/services/goals.js
+++ src/services/goals.js
@@ -84,12 +84,13 @@ async function removeUnusedObjectivesGoalsFromReport(reportId, currentGoals, tra
 
   const goalIdsToRemove = previousGoalIds.filter((id) => !currentGoalIds.includes(id));
   const objectiveIdsToRemove = previousObjectiveIds
-    .filter(async (id) => {
-      const notCurrent = !currentObjectiveIds.includes(id);
-      const activityReportObjectives = await ActivityReportObjective.findAll({ where: { id } });
-      const lastInstance = activityReportObjectives.length <= 1;
-      return notCurrent && lastInstance;
-    });
+    .filter((id) => !currentObjectiveIds.includes(id));
+    // .filter(async (id) => {
+    //   const notCurrent = !currentObjectiveIds.includes(id);
+    //   const activityReportObjectives = await ActivityReportObjective.findAll({ where: { id } });
+    //   const lastInstance = activityReportObjectives.length <= 1;
+    //   return notCurrent && lastInstance;
+    // });
 
   await removeActivityReportObjectivesFromReport(reportId, transaction);
   await removeObjectives(objectiveIdsToRemove, transaction);
