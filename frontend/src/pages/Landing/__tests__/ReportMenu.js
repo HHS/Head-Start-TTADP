adbfc4b728225c427920800b6f023199df9425f9
Daniel Cloud <daniel.cloud@adhocteam.us>
2021-05-14 17:19:16 -0400

    [TTAHUB-110]: Add new tests to ReportMenu.js for keyboard/focus event handlers

--- frontend/src/pages/Landing/__tests__/ReportMenu.js
+++ frontend/src/pages/Landing/__tests__/ReportMenu.js
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import ReportMenu from '../ReportMenu';
@@ -47,5 +47,54 @@ describe('ReportMenu', () => {
       userEvent.click(exportButton);
       expect(onExport).toHaveBeenCalled();
     });
+  });
+
+  it('closes when focus moves away from the menu', async () => {
+    let report;
+    let menu;
+
+    render(<RenderReportMenu />);
+    // first, open the menu
+    const button = await screen.findByRole('menuitem');
+    userEvent.click(button);
+    report = await screen.findByText('Reports');
+    expect(report).toHaveClass('smart-hub--menu-button__open');
+    menu = screen.queryByRole('menu');
+    expect(menu).toBeInTheDocument();
+
+    // Focus on the menu button should close the menu
+    button.focus();
+    report = await screen.findByText('Reports');
+    expect(report).not.toHaveClass('smart-hub--menu-button__open');
+    menu = screen.queryByRole('menu');
+    expect(menu).not.toBeInTheDocument();
+  });
+
+  it('closes when the Escape key is pressed', async () => {
+    let report;
+    let menu;
+
+    render(<RenderReportMenu />);
+    // first, open the menu
+    const button = await screen.findByRole('menuitem');
+    userEvent.click(button);
+    report = await screen.findByText('Reports');
+    expect(report).toHaveClass('smart-hub--menu-button__open');
+    menu = screen.queryByRole('menu');
+    expect(menu).toBeInTheDocument();
+
+    // Keypress with the keys other than 'Escape' should NOT close the menu
+    fireEvent.keyDown(document.activeElement || document.body, { key: ' ' });
+    report = await screen.findByText('Reports');
+    expect(report).toHaveClass('smart-hub--menu-button__open');
+    menu = screen.queryByRole('menu');
+    expect(menu).toBeInTheDocument();
+
+    // Keypress with the escape key should close the menu
+    fireEvent.keyDown(document.activeElement || document.body, { key: 'Escape' });
+    report = await screen.findByText('Reports');
+    expect(report).not.toHaveClass('smart-hub--menu-button__open');
+    menu = screen.queryByRole('menu');
+    expect(menu).not.toBeInTheDocument();
   });
 });
