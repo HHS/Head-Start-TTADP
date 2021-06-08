b6e1573f17bb65cd70542fdfb26c98243a656896
Adam Levin <adam.levin@adhocteam.us>
2021-06-08 16:25:19 -0400

    TTAHUB-105: Bring goal branch coverage to 100%.

--- frontend/src/pages/ActivityReport/Pages/components/__tests__/Goal.js
+++ frontend/src/pages/ActivityReport/Pages/components/__tests__/Goal.js
@@ -140,6 +140,25 @@ describe('Goal', () => {
       }]);
     });
 
+    it('cant be removed', async () => {
+      const onUpdate = jest.fn();
+      const objectives = [
+        {
+          id: 'a', title: 'first', ttaProvided: '<p>This is the TTA Desc</p>', status: 'Not Started',
+        },
+      ];
+      render(<RenderGoal onUpdateObjectives={onUpdate} name="test goal" objectives={objectives} />);
+
+      const optionsObjBtn = screen.getByRole('button', { name: /edit or delete objective 1 on goal 1/i });
+      fireEvent.click(optionsObjBtn);
+
+      const deleteObjBtn = await screen.findByRole('button', { name: 'Delete' });
+      fireEvent.click(deleteObjBtn);
+
+      expect(objectives.length)
+      expect(onUpdate).not.toHaveBeenCalledWith(objectives[0]);
+    });
+
     it('can be updated', async () => {
       const onUpdate = jest.fn();
       const objectives = [{
