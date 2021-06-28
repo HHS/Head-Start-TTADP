8de1c13008f937a413b43b92affb2d1d5897e669
Adam Levin <adam.levin@adhocteam.us>
2021-06-28 15:10:42 -0400

    fix axe

--- frontend/src/components/SiteNav.js
+++ frontend/src/components/SiteNav.js
@@ -50,48 +50,49 @@ const SiteNav = ({
   const items = admin ? navItems.concat(adminNavItem) : navItems;
 
   return (
-    <div>
+    <>
       <Helmet>
         <script src="https://touchpoints.app.cloud.gov/touchpoints/7d519b5e.js" async />
       </Helmet>
-
-      <div className="position-relative z-top">
-        <button id="tp-ar-landing-survey" className="usa-button position-fixed  bottom-2 right-1 display-block" aria-label="Please leave feedback" type="button">Please leave feedback</button>
+      <div>
+        <div className="position-relative z-top">
+          <button id="tp-ar-landing-survey" className="usa-button position-fixed  bottom-2 right-1 display-block" aria-label="Please leave feedback" type="button">Please leave feedback</button>
+        </div>
+        <div className="smart-hub-sitenav pin-y position-fixed z-0 padding-top-9 font-ui text-white smart-hub-bg-blue width-15 tablet:width-card desktop:width-card-lg no-print">
+          {authenticated && (
+            <>
+              <div className="width-full smart-hub-sitenav-separator--after">
+                <div role="complementary" className="padding-2 smart-hub-sitenav-word-wrap--break">
+                  <p className="text-bold margin-top-5">{user.name}</p>
+                  <p className="font-sans-3xs margin-bottom-5">{user.email}</p>
+                </div>
+              </div>
+              <nav>
+                <div className="width-full margin-bottom-2 margin-top-6">
+                  <ul className="add-list-reset">
+                    <li>
+                      <NavLink
+                        to="/activity-reports"
+                      >
+                        <span className="padding-right-105">
+                          <FontAwesomeIcon color="white" icon={faChartBar} />
+                        </span>
+                        Activity Reports
+                      </NavLink>
+                    </li>
+                  </ul>
+                </div>
+                <div className="width-full position-absolute bottom-0 padding-bottom-5 smart-hub-sitenav-separator--before opacity-70">
+                  <ul className="add-list-reset padding-top-5 text-base-lightest">
+                    {items.map((item, i) => (<li key={`smart-hub-nav__item-${i}`}>{item}</li>))}
+                  </ul>
+                </div>
+              </nav>
+            </>
+          )}
+        </div>
       </div>
-      <div className="smart-hub-sitenav pin-y position-fixed z-0 padding-top-9 font-ui text-white smart-hub-bg-blue width-15 tablet:width-card desktop:width-card-lg no-print">
-        {authenticated && (
-        <>
-          <div className="width-full smart-hub-sitenav-separator--after">
-            <div role="complementary" className="padding-2 smart-hub-sitenav-word-wrap--break">
-              <p className="text-bold margin-top-5">{user.name}</p>
-              <p className="font-sans-3xs margin-bottom-5">{user.email}</p>
-            </div>
-          </div>
-          <nav>
-            <div className="width-full margin-bottom-2 margin-top-6">
-              <ul className="add-list-reset">
-                <li>
-                  <NavLink
-                    to="/activity-reports"
-                  >
-                    <span className="padding-right-105">
-                      <FontAwesomeIcon color="white" icon={faChartBar} />
-                    </span>
-                    Activity Reports
-                  </NavLink>
-                </li>
-              </ul>
-            </div>
-            <div className="width-full position-absolute bottom-0 padding-bottom-5 smart-hub-sitenav-separator--before opacity-70">
-              <ul className="add-list-reset padding-top-5 text-base-lightest">
-                {items.map((item, i) => (<li key={`smart-hub-nav__item-${i}`}>{item}</li>))}
-              </ul>
-            </div>
-          </nav>
-        </>
-        )}
-      </div>
-    </div>
+    </>
   );
 };
 SiteNav.displayName = 'SiteNav';
