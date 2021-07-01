83be4a04e2cf4b37a2497765839020eeb609ed6e
Adam Levin <adam.levin@adhocteam.us>
2021-07-01 12:42:39 -0400

    Set script to defer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -144,7 +144,7 @@ function App() {
   return (
     <>
       <Helmet titleTemplate="%s - TTA Smart Hub" defaultTitle="TTA Smart Hub">
-        <script src="https://touchpoints.app.cloud.gov/touchpoints/7d519b5e.js" async />
+        <script src="https://touchpoints.app.cloud.gov/touchpoints/7d519b5e.js" defer />
         <meta charSet="utf-8" />
       </Helmet>
       <BrowserRouter>
