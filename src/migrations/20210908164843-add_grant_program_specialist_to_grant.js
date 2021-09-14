82836f5de4c8e9815050375740cca047ce86cc54
Josh Salisbury <josh@adhocteam.us>
2021-09-14 09:37:15 -0500

    Migration returns transaction

--- src/migrations/20210908164843-add_grant_program_specialist_to_grant.js
+++ src/migrations/20210908164843-add_grant_program_specialist_to_grant.js
@@ -1,34 +1,32 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    queryInterface.sequelize.transaction(
-      async (transaction) => {
-        await queryInterface.addColumn(
-          'Grants',
-          'programSpecialistName',
-          { type: Sequelize.DataTypes.STRING },
-          { transaction },
-        );
-        await queryInterface.addColumn(
-          'Grants',
-          'programSpecialistEmail',
-          { type: Sequelize.DataTypes.STRING },
-          { transaction },
-        );
-        await queryInterface.addColumn(
-          'Grants',
-          'grantSpecialistName',
-          { type: Sequelize.DataTypes.STRING },
-          { transaction },
-        );
-        await queryInterface.addColumn(
-          'Grants',
-          'grantSpecialistEmail',
-          { type: Sequelize.DataTypes.STRING },
-          { transaction },
-        );
-      },
-    );
-  },
+  up: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
+    async (transaction) => {
+      await queryInterface.addColumn(
+        'Grants',
+        'programSpecialistName',
+        { type: Sequelize.DataTypes.STRING },
+        { transaction },
+      );
+      await queryInterface.addColumn(
+        'Grants',
+        'programSpecialistEmail',
+        { type: Sequelize.DataTypes.STRING },
+        { transaction },
+      );
+      await queryInterface.addColumn(
+        'Grants',
+        'grantSpecialistName',
+        { type: Sequelize.DataTypes.STRING },
+        { transaction },
+      );
+      await queryInterface.addColumn(
+        'Grants',
+        'grantSpecialistEmail',
+        { type: Sequelize.DataTypes.STRING },
+        { transaction },
+      );
+    },
+  ),
 
   down: async (queryInterface) => {
     await queryInterface.removeColumn('Grants', 'programSpecialistName');
