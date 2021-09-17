9010f82758ac23e205db951296150d5ac2011889
Matt Bevilacqua <thewatermethod@gmail.com>
2021-09-17 12:28:27 -0400

    add unit test

--- src/services/grantee.test.js
+++ src/services/grantee.test.js
@@ -1,5 +1,5 @@
-import db, { Grantee } from '../models';
-import { allGrantees } from './grantee';
+import db, { Grantee, Grant } from '../models';
+import { allGrantees, granteesByNameAndRegion } from './grantee';
 
 describe('Grantee DB service', () => {
   afterEach(() => {
@@ -40,6 +40,90 @@ describe('Grantee DB service', () => {
       expect(foundIds).toContain(60);
       expect(foundIds).toContain(61);
       expect(foundIds).toContain(62);
+    });
+  });
+
+  describe('granteesByNameAndRegion', () => {
+    const grantees = [
+      {
+        id: 63,
+        name: 'Apple',
+      },
+      {
+        id: 64,
+        name: 'Orange',
+      },
+      {
+        id: 65,
+        name: 'Banana',
+      },
+    ];
+
+    const grants = [
+      {
+        id: 50,
+        granteeId: 63,
+        regionId: 1,
+        number: '12345',
+      },
+      {
+        id: 51,
+        granteeId: 63,
+        regionId: 1,
+        number: '12346',
+      },
+      {
+        id: 52,
+        granteeId: 64,
+        regionId: 1,
+        number: '55557',
+      },
+      {
+        id: 53,
+        granteeId: 64,
+        regionId: 1,
+        number: '55558',
+      },
+      {
+        id: 54,
+        granteeId: 65,
+        regionId: 1,
+        number: '12349',
+      },
+      {
+        id: 55,
+        granteeId: 65,
+        regionId: 2,
+        number: '12350',
+      },
+    ];
+
+    beforeEach(async () => {
+      await Promise.all(grantees.map((g) => Grantee.create(g)));
+      await Promise.all(grants.map((g) => Grant.create(g)));
+    });
+
+    afterEach(async () => {
+      await Grant.destroy({ where: { granteeId: grantees.map((g) => g.id) } });
+      await Grantee.destroy({ where: { id: grantees.map((g) => g.id) } });
+    });
+
+    it('finds based on grantee name', async () => {
+      const foundGrantees = await granteesByNameAndRegion('apple', 1);
+      expect(foundGrantees.length).toBe(1);
+      expect(foundGrantees.map((g) => g.id)).toContain(63);
+    });
+
+    it('finds based on grantee id', async () => {
+      const foundGrantees = await granteesByNameAndRegion('5555', 1);
+      expect(foundGrantees.length).toBe(1);
+      expect(foundGrantees.map((g) => g.id)).toContain(64);
+    });
+
+    it('finds based on region', async () => {
+      const foundGrantees = await granteesByNameAndRegion('banana', 2);
+      expect(foundGrantees.length).toBe(1);
+      expect(foundGrantees.map((g) => g.id)).toContain(65);
     });
   });
 });
