fa9668c39b347f0f1dac83e63f85bf8dbc16e7f5
Matt Bevilacqua <thewatermethod@gmail.com>
2021-09-23 13:20:05 -0400

    add granteeId to scopes

--- src/scopes/activityReport/granteeId.js
+++ src/scopes/activityReport/granteeId.js
@@ -1,11 +1,10 @@
-import { Op } from 'sequelize';
+import { DECIMAL_BASE } from '../../constants';
 import { sequelize } from '../../models';
 
-export default function withGranteeId(granteeId) {
-  return sequelize.where(
-    sequelize.col('"Grantees".id FROM "Grantees" INNER JOIN "ActivityRecipients" ON "ActivityReport"."id" = "ActivityRecipients"."activityReportId" JOIN "Grants" ON "Grants"."id" = "ActivityRecipients"."grantId" AND "Grantees"."id" = "Grants"."granteeId" GROUP BY "ActivityRecipients"."activityReportId"'),
-    {
-      [Op.eq]: granteeId,
-    },
-  );
+const grantee = '(SELECT UNNEST(ARRAY_AGG("Grantee"."id")) FROM "Grantees" AS "Grantee" INNER JOIN "ActivityRecipients" ON "ActivityReport"."id" = "ActivityRecipients"."activityReportId" JOIN "Grants" ON "Grants"."id" = "ActivityRecipients"."grantId" AND "Grantee"."id" = "Grants"."granteeId" GROUP BY "ActivityReport"."id")';
+
+export default function withGranteeId(id) {
+  const [g] = id; // we're only parsing the first one right now
+  const granteeId = sequelize.escape(parseInt(g, DECIMAL_BASE));
+  return sequelize.literal(`${granteeId} = ANY ${grantee}`);
 }
