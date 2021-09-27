d32aa404d21c8c3c7fbfceb1155517a4e028721e
Matt Bevilacqua <thewatermethod@gmail.com>
2021-09-27 15:36:14 -0400

    defeated, we add no an eslint ignore

--- frontend/src/pages/GranteeSearch/index.js
+++ frontend/src/pages/GranteeSearch/index.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable react/jsx-no-bind */
 import React, { useEffect, useState, useRef } from 'react';
 import PropTypes from 'prop-types';
 import { Helmet } from 'react-helmet';
@@ -131,7 +131,7 @@ function GranteeSearch({ user }) {
                 <div className="margin-right-2">
                   <RegionalSelect
                     regions={regions}
-                    // onApply={onApplyRegion}
+                    onApply={onApplyRegion}
                     hasCentralOffice={hasCentralOffice}
                     appliedRegion={appliedRegion}
                   />
@@ -147,7 +147,7 @@ function GranteeSearch({ user }) {
           </form>
         </Grid>
         <main>
-          {/* <GranteeResults
+          <GranteeResults
             region={appliedRegion}
             grantees={results}
             loading={loading}
@@ -158,7 +158,7 @@ function GranteeSearch({ user }) {
             handlePageChange={handlePageChange}
             requestSort={requestSort}
             sortConfig={sortConfig}
-          /> */}
+          />
         </main>
       </div>
     </>
