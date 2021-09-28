4847b1cea1076942b25d36018ae9581522b84819
Matt Bevilacqua <thewatermethod@gmail.com>
2021-09-28 12:46:55 -0400

    checkpoint

--- frontend/src/pages/Landing/Components/TooltipWithEllipsis.js
+++ frontend/src/pages/Landing/Components/TooltipWithEllipsis.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEllipsisH } from '@fortawesome/free-solid-svg-icons';
@@ -6,6 +6,26 @@ import { Tag } from '@trussworks/react-uswds';
 import './TooltipWithEllipsis.css';
 
 export default function TooltipWithEllipsis({ collection, limit }) {
+  const [showTooltip, setShowTooltip] = useState(false);
+  // const [tooltipPosition, setTooltipPosition] = useState({
+  //   left: 0,
+  //   top: 0,
+  // });
+
+  const button = useRef();
+
+  useEffect(() => {
+    // if (button.current) {
+    //   const rect = button.current.getBoundingClientRect();
+    //   const { left, top, width } = rect;
+    //   setTooltipPosition({ left: left + width, top });
+    // }
+  }, []);
+
+  if (collection.length === 0) {
+    return null;
+  }
+
   const tooltip = (collection || []).reduce(
     (result, member) => `${result + member}\n`,
     '',
@@ -20,12 +40,15 @@ export default function TooltipWithEllipsis({ collection, limit }) {
     </Tag>
   ));
 
+  console.log(showTooltip);
+  const cssClasses = showTooltip ? 'smart-hub--tooltip-with-ellipsis show-tooltip' : 'smart-hub--tooltip-with-ellipsis';
+
   return (
-    <span className="smart-hub--tooltip-with-ellipsis">
+    <span className={cssClasses}>
       <span className="smart-hub--ellipsis">
         {tags}
       </span>
-      <button type="button" className="usa-button usa-button--unstyled">
+      <button type="button" className="usa-button usa-button--unstyled" ref={button} onClick={() => setShowTooltip(true)}>
         <FontAwesomeIcon icon={faEllipsisH} color="black" />
         <span className="usa-tooltip__body usa-tooltip__body--right" role="tooltip">{tooltip}</span>
       </button>
