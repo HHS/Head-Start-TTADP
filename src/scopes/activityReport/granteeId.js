34dfe04c77844769ffe44f1164c96b91184bf455
Matt Bevilacqua <thewatermethod@gmail.com>
2021-09-29 16:39:30 -0400

    leverage existing filter code

--- src/scopes/activityReport/granteeId.js
+++ src/scopes/activityReport/granteeId.js
@@ -1,14 +1,10 @@
 import { Op } from 'sequelize';
-import { sequelize } from '../../models';
+import { filterAssociation } from './utils';
 
 export default function withGranteeId(id) {
-  function reportInSubQuery(baseQuery, searchTerms, operator) {
-    return searchTerms.map((term) => sequelize.literal(`"ActivityReport"."id" ${operator} (${baseQuery} = ${sequelize.escape(term)})`));
-  }
-
   const query = 'SELECT "ActivityRecipients"."activityReportId" FROM "Grantees" INNER JOIN "Grants" ON "Grants"."granteeId" = "Grantees"."id" INNER JOIN "ActivityRecipients" ON "ActivityRecipients"."grantId" = "Grants"."id" WHERE "Grantees".id';
 
   return {
-    [Op.and]: reportInSubQuery(query, id, 'IN'),
+    [Op.and]: filterAssociation(query, id, false, '='),
   };
 }
