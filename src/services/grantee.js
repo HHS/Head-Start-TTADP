6875e537cf829fd7954369edd88fc957f8ba2679
Matt Bevilacqua <thewatermethod@gmail.com>
2021-10-18 10:43:10 -0400

    refactor granteeById to use program table

--- src/services/grantee.js
+++ src/services/grantee.js
@@ -1,6 +1,6 @@
 import { Op } from 'sequelize';
 import {
-  Grant, Grantee, ActivityReport, ActivityRecipient,
+  Grant, Grantee, Program,
 } from '../models';
 import orderGranteesBy from '../lib/orderGranteesBy';
 import { GRANTEES_PER_PAGE } from '../constants';
@@ -18,14 +18,14 @@ export async function allGrantees() {
 }
 
 export async function granteeById(granteeId, grantScopes) {
-  const grantee = await Grantee.findOne({
+  return Grantee.findOne({
     attributes: ['id', 'name'],
     where: {
       id: granteeId,
     },
     include: [
       {
-        attributes: ['id'],
+        attributes: ['id', 'number', 'regionId', 'status', 'startDate', 'endDate', 'programSpecialistName', 'granteeId'],
         model: Grant,
         as: 'grants',
         where: {
@@ -33,87 +33,19 @@ export async function granteeById(granteeId, grantScopes) {
             grantScopes,
           ],
         },
+        include: [
+          {
+            model: Program,
+            attributes: ['name'],
+            as: 'programs',
+          },
+        ],
       },
     ],
     order: [
       [{ model: Grant, as: 'grants' }, 'endDate', 'DESC'], [{ model: Grant, as: 'grants' }, 'number', 'ASC'],
     ],
   });
-
-  if (!grantee) {
-    return null;
-  }
-
-  if (grantee.grants.length < 1) {
-    return {
-      name: grantee.name,
-      grants: [],
-    };
-  }
-
-  const grantIds = [...new Set(grantee.grants.map((grant) => grant.id))];
-
-  const recipients = await ActivityRecipient.findAll({
-    attributes: ['grantId'],
-    include: [
-      {
-        model: Grant,
-        attributes: ['id', 'number', 'regionId', 'status', 'startDate', 'endDate', 'programSpecialistName', 'granteeId'],
-        as: 'grant',
-        where: {
-          id: grantIds,
-        },
-      },
-      {
-        model: ActivityReport,
-        attributes: ['programTypes'],
-        as: 'ActivityReport',
-      },
-    ],
-  });
-
-  const grants = new Map();
-
-  recipients.forEach((recipient) => {
-    if (grants.has(recipient.grantId)) {
-      const grant = grants.get(recipient.grantId);
-      const programTypes = Array.from(
-        new Set(
-          [...recipient.ActivityReport.programTypes, ...grant.programTypes],
-        ),
-      );
-
-      grants.set(recipient.grantId, { ...grant, programTypes });
-    } else {
-      const {
-        id,
-        number,
-        regionId,
-        status,
-        startDate,
-        endDate,
-        programSpecialistName,
-      } = recipient.grant;
-
-      const grant = {
-        id,
-        number,
-        regionId,
-        status,
-        startDate,
-        endDate,
-        programSpecialistName,
-        programTypes: recipient.ActivityReport.programTypes,
-      };
-
-      grants.set(recipient.grantId, grant);
-    }
-  });
-
-  return {
-    name: grantee.name,
-    grants: Array.from(grants.values()),
-  };
 }
 
 /**
