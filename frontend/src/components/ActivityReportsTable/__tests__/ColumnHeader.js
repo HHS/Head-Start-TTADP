58703c616330fe13e1cc49f0db5aceaa0bd2de34
Matt Bevilacqua <thewatermethod@gmail.com>
2021-11-11 15:31:27 -0500

    add test to column header

--- frontend/src/components/ActivityReportsTable/__tests__/ColumnHeader.js
+++ frontend/src/components/ActivityReportsTable/__tests__/ColumnHeader.js
@@ -1,36 +1,50 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import {
-  render, screen,
+  render, screen, act,
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ColumnHeader from '../ColumnHeader';
 
 describe('ActivityReportsTable ColumnHeader', () => {
+  const renderColumnHeader = (onUpdateSort = jest.fn(), sortDirection = 'asc') => {
+    const name = 'fanciest shoes';
+    render(
+      <div>
+        <table>
+          <thead>
+            <tr>
+              <ColumnHeader
+                onUpdateSort={onUpdateSort}
+                displayName={name}
+                name="shoes"
+                sortBy="shoes"
+                sortDirection={sortDirection}
+              />
+            </tr>
+          </thead>
+        </table>
+      </div>,
+    );
+  };
+
   it('renders and calls on update sort', async () => {
     const onUpdateSort = jest.fn();
-    const name = 'fanciest shoes';
-    const sortDirection = 'asc';
-
-    render(
-      <table>
-        <thead>
-          <tr>
-            <ColumnHeader
-              onUpdateSort={onUpdateSort}
-              displayName={name}
-              name="shoes"
-              sortBy="shoes"
-              sortDirection={sortDirection}
-            />
-          </tr>
-        </thead>
-      </table>,
-    );
+    renderColumnHeader(onUpdateSort);
 
     const shoes = await screen.findByText('fanciest shoes');
 
-    userEvent.click(shoes);
+    await act(async () => userEvent.click(shoes));
     expect(onUpdateSort).toHaveBeenCalledWith('shoes');
+  });
+
+  it('sorts on keypress', async () => {
+    const onUpdateSort = jest.fn();
+    renderColumnHeader(onUpdateSort, 'desc');
+
+    const shoes = await screen.findByText('fanciest shoes');
+
+    await act(async () => userEvent.type(shoes, '{enter}'));
+    expect(onUpdateSort).toHaveBeenCalledTimes(2);
   });
 });
