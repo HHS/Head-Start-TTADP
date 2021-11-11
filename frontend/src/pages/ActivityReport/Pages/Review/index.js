3d48d9a8326f74d0a2684ccc453a566fb7539db7
Adam Levin <adam.levin@adhocteam.us>
2021-11-11 16:25:33 -0500

    see if remvoing display none fixes issue

--- frontend/src/pages/ActivityReport/Pages/Review/index.js
+++ frontend/src/pages/ActivityReport/Pages/Review/index.js
@@ -88,8 +88,8 @@ const ReviewSubmit = ({
             onSaveForm={onSaveForm}
           >
             <>
-              <h2 className="display-none">test2</h2>
-              <h3 className="display-none">test3</h3>
+              <h2>test2</h2>
+              <h3>test3</h3>
               <Accordion bordered={false} items={items} />
             </>
           </Submitter>
@@ -105,8 +105,8 @@ const ReviewSubmit = ({
             isPendingApprover={isPendingApprover}
           >
             <>
-              <h2 className="display-none">test2</h2>
-              <h3 className="display-none">test3</h3>
+              <h2>test2</h2>
+              <h3>test3</h3>
               <Accordion bordered={false} items={items} />
             </>
           </Approver>
