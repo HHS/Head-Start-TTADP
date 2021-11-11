1058919dd6e173947d55376f4bcba8060e05b308
Matt Bevilacqua <thewatermethod@gmail.com>
2021-11-11 16:22:11 -0500

    cleanup test

--- frontend/src/pages/GranteeRecord/pages/__tests__/TTAHistory.js
+++ frontend/src/pages/GranteeRecord/pages/__tests__/TTAHistory.js
@@ -1,7 +1,6 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import { render, screen, act } from '@testing-library/react';
-import fetchMock from 'fetch-mock';
 import { Router } from 'react-router';
 import { createMemoryHistory } from 'history';
 import TTAHistory from '../TTAHistory';
@@ -9,15 +8,6 @@ import TTAHistory from '../TTAHistory';
 const memoryHistory = createMemoryHistory();
 
 describe('Grantee Record - TTA History', () => {
-  const overviewResponse = {
-    numReports: '1', numGrants: '1', inPerson: '0', sumDuration: '1.0', numParticipants: '1',
-  };
-
-  const tableResponse = {
-    count: 0,
-    rows: [],
-  };
-
   const renderTTAHistory = () => {
     render(
       <Router history={memoryHistory}>
@@ -25,17 +15,6 @@ describe('Grantee Record - TTA History', () => {
       </Router>,
     );
   };
-
-  beforeEach(async () => {
-    const overviewUrl = '/api/widgets/overview?region.in[]=400&granteeId.in[]=100&modelType.is=grant';
-    const tableUrl = '/api/activity-reports?sortBy=updatedAt&sortDir=desc&offset=0&limit=10&region.in[]=400&region.in[]=401&granteeId.in[]=100&modelType.is=grant';
-    fetchMock.get(overviewUrl, overviewResponse);
-    fetchMock.get(tableUrl, tableResponse);
-  });
-
-  afterEach(() => {
-    fetchMock.restore();
-  });
 
   it('renders the TTA History page appropriately', async () => {
     act(() => renderTTAHistory());
