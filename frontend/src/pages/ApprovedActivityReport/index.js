d42f2ab4deababc709d93d8c239e295d226cdb18
Matt Bevilacqua <thewatermethod@gmail.com>
2021-11-11 13:02:16 -0500

    add target populations to ar

--- frontend/src/pages/ApprovedActivityReport/index.js
+++ frontend/src/pages/ApprovedActivityReport/index.js
@@ -125,6 +125,7 @@ export default function ApprovedActivityReport({ match, user }) {
   const [participantCount, setParticipantCount] = useState('');
   const [reasons, setReasons] = useState('');
   const [programType, setProgramType] = useState('');
+  const [targetPopulations, setTargetPopulations] = useState('');
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
   const [duration, setDuration] = useState('');
@@ -176,6 +177,7 @@ export default function ApprovedActivityReport({ match, user }) {
       setDisplayId(report.displayId);
       setCreator(report.author.fullName);
       setCollaborators(report.collaborators);
+      setTargetPopulations(report.targetPopulations.map((population) => population).join(', '));
 
       // Approvers.
       const approversNames = report.approvers.map((a) => a.User.fullName);
@@ -386,6 +388,7 @@ export default function ApprovedActivityReport({ match, user }) {
               recipientType,
               'Reason',
               'Program Type',
+              'Target populations',
               'Start date',
               'End date',
               'Topics',
@@ -402,6 +405,7 @@ export default function ApprovedActivityReport({ match, user }) {
               recipients,
               reasons,
               programType,
+              targetPopulations,
               startDate,
               endDate,
               topics,
