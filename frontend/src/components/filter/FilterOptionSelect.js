7d43bca1f104ec3e620cfbbc490694ef05c50b08
Matt Bevilacqua <thewatermethod@gmail.com>
2021-12-09 13:13:38 -0500

    improve styles

--- frontend/src/components/filter/FilterOptionSelect.js
+++ frontend/src/components/filter/FilterOptionSelect.js
@@ -7,6 +7,61 @@ export default function FilterOptionSelect({
   labelText,
   options,
 }) {
+  // useEffect(() => {
+  //   /**
+  //    * unfortunately, given our support for ie11, we can't
+  //    * upgrade to react-select v5, which support a spellcheck
+  //    * attribute. Here is an awkward solution I've concocted
+  //    * in it's stead.
+  //    */
+  //   const input = document.querySelector(`#${inputId}`);
+  //   if (input) {
+  //     input.setAttribute('spellcheck', 'true');
+  //   }
+  // });
+
+  const styles = {
+    container: (provided, state) => {
+      // To match the focus indicator provided by uswds
+      const outline = state.isFocused ? '0.25rem solid #2491ff;' : '';
+      return {
+        ...provided,
+        outline,
+      };
+    },
+    groupHeading: (provided) => ({
+      ...provided,
+      fontWeight: 'bold',
+      fontFamily: 'SourceSansPro',
+      textTransform: 'capitalize',
+      fontSize: '14px',
+      color: '#21272d',
+      lineHeight: '22px',
+    }),
+    control: (provided, state) => ({
+      ...provided,
+      border: 'none',
+      // Match uswds disabled style
+      opacity: state.isDisabled ? '0.7' : '1',
+      position: 'absolute',
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0,
+    }),
+    indicatorsContainer: (provided) => ({
+      ...provided,
+      display: 'inline',
+      // The arrow dropdown icon is too far to the right, this pushes it back to the left
+      marginRight: '4px',
+    }),
+    indicatorSeparator: () => ({ display: 'none' }),
+    menu: (provided) => ({
+      ...provided,
+      zIndex: 2,
+    }),
+  };
+
   const onChange = (selected) => {
     onApply(selected.map((selection) => selection.label));
   };
@@ -16,6 +71,11 @@ export default function FilterOptionSelect({
       label={labelText}
       onChange={onChange}
       options={options}
+      styles={styles}
+      components={{
+        DropdownIndicator: null,
+      }}
+      className="usa-select"
       isMulti
     />
   );
