93130c24aacf0719a86c848f3ea22dbcc795b20c
Adam Levin <adam.levin@adhocteam.us>
2021-12-13 12:17:33 -0500

    added scope and test for created at

--- src/scopes/activityReport/index.test.js
+++ src/scopes/activityReport/index.test.js
@@ -1377,4 +1377,65 @@ describe('filtersToScopes', () => {
         .toEqual(expect.arrayContaining([reportOne.id, reportTwo.id]));
     });
   });
+
+  describe('createDate', () => {
+    let firstReport;
+    let secondReport;
+    let thirdReport;
+    let fourthReport;
+    let possibleIds;
+
+    beforeAll(async () => {
+      firstReport = await ActivityReport.create({ ...draftReport, createdAt: '2019-01-01T21:00:57.149Z' });
+      secondReport = await ActivityReport.create({ ...draftReport, createdAt: '2020-02-01T21:11:57.149Z' });
+      thirdReport = await ActivityReport.create({ ...draftReport, createdAt: '2021-01-01T21:14:57.149Z' });
+      fourthReport = await ActivityReport.create({ ...draftReport, createdAt: '2023-01-01T21:15:57.149Z' });
+      possibleIds = [
+        firstReport.id,
+        secondReport.id,
+        thirdReport.id,
+        fourthReport.id,
+        globallyExcludedReport.id,
+      ];
+    });
+
+    afterAll(async () => {
+      await ActivityReport.destroy({
+        where: { id: [firstReport.id, secondReport.id, thirdReport.id, fourthReport.id] },
+      });
+    });
+
+    it('before returns reports with create dates before the given date', async () => {
+      const filters = { 'createDate.bef': '2020/12/31' };
+      const scope = filtersToScopes(filters);
+      const found = await ActivityReport.findAll({
+        where: { [Op.and]: [scope, { id: possibleIds }] },
+      });
+      expect(found.length).toBe(2);
+      expect(found.map((f) => f.id))
+        .toEqual(expect.arrayContaining([firstReport.id, secondReport.id]));
+    });
+
+    it('after returns reports with create dates before the given date', async () => {
+      const filters = { 'createDate.aft': '2021/06/06' };
+      const scope = filtersToScopes(filters);
+      const found = await ActivityReport.findAll({
+        where: { [Op.and]: [scope, { id: possibleIds }] },
+      });
+      expect(found.length).toBe(2);
+      expect(found.map((f) => f.id))
+        .toEqual(expect.arrayContaining([fourthReport.id]));
+    });
+
+    it('within returns reports with create dates between the two dates', async () => {
+      const filters = { 'createDate.win': '2020/01/01-2021/06/06' };
+      const scope = filtersToScopes(filters);
+      const found = await ActivityReport.findAll({
+        where: { [Op.and]: [scope, { id: possibleIds }] },
+      });
+      expect(found.length).toBe(2);
+      expect(found.map((f) => f.id))
+        .toEqual(expect.arrayContaining([secondReport.id, thirdReport.id]));
+    });
+  });
 });
