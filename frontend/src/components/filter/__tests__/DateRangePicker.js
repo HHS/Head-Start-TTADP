1b66a148933c154d903d7d32848627b615f61a2a
Matt Bevilacqua <thewatermethod@gmail.com>
2021-12-27 15:31:29 -0500

    fix errors with date range picker

--- frontend/src/components/filter/__tests__/DateRangePicker.js
+++ frontend/src/components/filter/__tests__/DateRangePicker.js
@@ -9,7 +9,7 @@ import userEvent from '@testing-library/user-event';
 import DateRangePicker from '../DateRangePicker';
 
 describe('DateRangePicker', () => {
-  const renderDateRangePicker = (query, onApply = jest.fn()) => {
+  const renderDateRangePicker = (query = '2021/11/03-2021/11/10', onApply = jest.fn()) => {
     render(
       <>
         <DateRangePicker
@@ -45,6 +45,7 @@ describe('DateRangePicker', () => {
     userEvent.click(dumb);
 
     error = await screen.findByText(/please enter a date range between 09\/01\/2020 and today, in the format mm\/dd\/yyyy/i);
+
     expect(error).toBeVisible();
 
     userEvent.clear(endDate);
@@ -57,22 +58,16 @@ describe('DateRangePicker', () => {
   });
 
   it('adjusts the deltas', async () => {
-    renderDateRangePicker('2021/11/03-2021/11/10');
+    renderDateRangePicker();
     const toggle = await screen.findByRole('button', { name: /change custom date range/i });
     userEvent.click(toggle);
+    const [sdCalendar] = await screen.findAllByRole('button', { name: /toggle calendar/i });
+    userEvent.click(sdCalendar);
 
-    const startDate = await screen.findByRole('textbox', { name: /start date/i });
-    // userEvent.clear(startDate);
-    // userEvent.type(startDate, '11/03/2021');
+    const newStart = await screen.findByRole('button', { name: /11 november 2021 thursday/i });
+    act(() => userEvent.click(newStart));
 
     const endDate = await screen.findByRole('textbox', { name: /end date/i });
-    // userEvent.clear(endDate);
-    // userEvent.type(endDate, '11/10/2021');
-
-    userEvent.clear(startDate);
-    act(() => userEvent.type(startDate, '11/11/2021'));
-    userEvent.tab();
-    userEvent.tab();
     expect(endDate.value).toBe('11/18/2021');
   });
 });
