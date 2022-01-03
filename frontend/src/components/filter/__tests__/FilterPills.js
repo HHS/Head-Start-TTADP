a6b9ccc42a9bfba66b34eb0c4edb3c902976942e
Matt Bevilacqua <thewatermethod@gmail.com>
2022-01-03 15:02:06 -0500

    patch up unit test

--- frontend/src/components/filter/__tests__/FilterPills.js
+++ frontend/src/components/filter/__tests__/FilterPills.js
@@ -26,12 +26,16 @@ describe('Filter Pills', () => {
         topic: 'role',
         condition: 'Contains',
         query: [],
+        displayQuery: (q) => q,
+        display: 'Specialist role',
       },
       {
         id: '2',
         topic: 'startDate',
         condition: 'Is within',
         query: '2021/10/01-2021/10/31',
+        displayQuery: (q) => q,
+        display: 'Start date',
       }];
 
       renderFilterMenu(filters);
