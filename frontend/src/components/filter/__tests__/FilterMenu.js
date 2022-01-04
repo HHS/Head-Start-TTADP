8c74090ff15f10fe2806931ea424d83ed202bbc4
Matt Bevilacqua <thewatermethod@gmail.com>
2022-01-04 09:25:53 -0500

    Revert "fix filter menu ui tests"
    This reverts commit 020566c30f6699c163a32800734ab0a5d68f26e9.

--- frontend/src/components/filter/__tests__/FilterMenu.js
+++ frontend/src/components/filter/__tests__/FilterMenu.js
@@ -7,18 +7,6 @@ import {
 } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import FilterMenu from '../FilterMenu';
-import {
-  START_DATE_FILTER,
-  GRANT_NUMBER_FILTER,
-  PROGRAM_SPECIALIST_FILTER,
-  PROGRAM_TYPE_FILTER,
-  REASON_FILTER,
-  RECIPIENT_NAME_FILTER,
-  REGION_FILTER,
-  ROLE_FILTER,
-  TARGET_POPULATION_FILTER,
-  TOPICS_FILTER,
-} from '../constants';
 
 describe('Filter Menu', () => {
   afterAll(() => {
@@ -31,18 +19,6 @@ describe('Filter Menu', () => {
         <h1>Filter menu</h1>
         <div>
           <FilterMenu
-            allowedFilters={[
-              START_DATE_FILTER,
-              GRANT_NUMBER_FILTER,
-              PROGRAM_SPECIALIST_FILTER,
-              PROGRAM_TYPE_FILTER,
-              REASON_FILTER,
-              RECIPIENT_NAME_FILTER,
-              REGION_FILTER,
-              ROLE_FILTER,
-              TARGET_POPULATION_FILTER,
-              TOPICS_FILTER,
-            ]}
             filters={filters}
             onApplyFilters={onApplyFilters}
             applyButtonAria="apply test filters"
@@ -272,12 +248,10 @@ describe('Filter Menu', () => {
   it('validates input and sets focus', async () => {
     const filters = [
       {
-        id: 'filter-3',
-        display: '',
-        conditions: [],
-        topic: 'programSpecialist',
-        query: '',
-        condition: 'Contains',
+        id: 'filter1234',
+        topic: 'startDate',
+        condition: 'Is after',
+        query: '2021/10/31',
       },
     ];
 
