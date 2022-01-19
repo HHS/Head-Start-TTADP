77bac9229faea68bc175f2a402f411d2db0de169
Matt Bevilacqua <thewatermethod@gmail.com>
2022-01-19 13:27:42 -0500

    add filter panel to frontend

---
+++ frontend/src/components/filter/FilterStatus.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import FilterSelect from './FilterSelect';
+
+const options = [
+  {
+    label: 'Needs status',
+    value: 'Needs status',
+  },
+  {
+    label: 'Draft',
+    value: 'Draft',
+  },
+  {
+    label: 'Not started',
+    value: 'Not Started',
+  },
+  {
+    label: 'In progress',
+    value: 'In Progress',
+  },
+  {
+    label: 'Closed',
+    value: 'Closed',
+  },
+  {
+    label: 'Ceased/suspended',
+    value: 'Ceased/suspended',
+  },
+];
+
+export default function FilterStatus({
+  onApply,
+  inputId,
+  query,
+}) {
+  const onApplyClick = (selected) => {
+    onApply(selected);
+  };
+
+  return (
+    <FilterSelect
+      onApply={onApplyClick}
+      inputId={inputId}
+      labelText="Select status to filter by"
+      options={options}
+      selectedValues={query}
+    />
+  );
+}
+
+FilterStatus.propTypes = {
+  inputId: PropTypes.string.isRequired,
+  onApply: PropTypes.func.isRequired,
+  query: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.string),
+    PropTypes.string,
+  ]).isRequired,
+};
--- frontend/src/pages/RecipientRecord/pages/GoalsObjectives.js
+++ frontend/src/pages/RecipientRecord/pages/GoalsObjectives.js
@@ -1,7 +1,46 @@
 import React from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import useUrlFilters from '../../../hooks/useUrlFilters';
+import FilterPanel from '../../../components/filter/FilterPanel';
+import { formatDateRange } from '../../../components/DateRangeSelect';
+
+const yearToDate = formatDateRange({ yearToDate: true, forDateTime: true });
 
 export default function GoalsObjectives() {
+  const [filters, setFilters] = useUrlFilters([{
+    id: uuidv4(),
+    topic: 'createDate',
+    condition: 'Is within',
+    query: yearToDate,
+  }]);
+
+  const onRemoveFilter = (id) => {
+    const newFilters = [...filters];
+    const index = newFilters.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      newFilters.splice(index, 1);
+      setFilters(newFilters);
+    }
+  };
+
   return (
-    <div />
+    <div className="margin-x-2 maxw-widescreen">
+      <div className="display-flex flex-wrap margin-bottom-2" data-testid="filter-panel">
+        <FilterPanel
+          onRemoveFilter={onRemoveFilter}
+          onApplyFilters={setFilters}
+          allowedFilters={
+          [
+            'status',
+            'createDate',
+            'topic',
+            'reason',
+          ]
+        }
+          applyButtonAria="Apply filters to goals"
+          filters={filters}
+        />
+      </div>
+    </div>
   );
 }
