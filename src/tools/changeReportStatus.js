b97b3d7e53ac5d6099a23ebcb299a22eb5ae028d
GarrettEHill <garretthill@gmail.com>
2022-01-26 11:04:25 -0800

    Move transaction outside of the for loop

--- src/tools/changeReportStatus.js
+++ src/tools/changeReportStatus.js
@@ -14,24 +14,25 @@ export default async function changeReportStatus(ids, status) {
   const idsArray = ids.split(',');
 
   const promises = [];
+  await sequelize.transaction(async (transaction) => {
+    for await (const id of idsArray) {
+      const report = await ActivityReport.unscoped().findOne({ where: { id } });
 
-  for await (const id of idsArray) {
-    const report = await ActivityReport.unscoped().findOne({ where: { id } });
-
-    if (report) {
-      auditLogger.info(`Changing status of report: ${id} to ${status}`);
-      promises.push(
-        await sequelize.transaction(async (transaction) => report.update(
-          {
-            submissionStatus: status,
-          },
-          transaction,
-        )),
-      );
-    } else {
-      auditLogger.info(`Couldn't find any reports with the id: ${id}`);
+      if (report) {
+        auditLogger.info(`Changing status of report: ${id} to ${status}`);
+        promises.push(
+          await report.update(
+            {
+              submissionStatus: status,
+            },
+            transaction,
+          ),
+        );
+      } else {
+        auditLogger.info(`Couldn't find any reports with the id: ${id}`);
+      }
     }
-  }
+  });
 
   return Promise.all(promises);
 }
