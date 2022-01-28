d179dd1325c3a711c572e16ec14ddb036097a19d
Matt Bevilacqua <thewatermethod@gmail.com>
2022-01-28 15:35:49 -0500

    show imported data

--- frontend/src/pages/LegacyReport/EditMode.js
+++ frontend/src/pages/LegacyReport/EditMode.js
@@ -44,10 +44,13 @@ export default function EditMode({ report }) {
 
   const [showSuccess, setShowSuccess] = useState(false);
   const [showError, setShowError] = useState(false);
+  const [showImported, setShowImported] = useState(true);
   const [startDate, setStartDate] = useState(report.startDate);
   const [endDate, setEndDate] = useState(report.endDate);
   const [targetPopulations, setTargetPopulations] = useState(report.targetPopulations);
   const [reason, setReason] = useState(report.reason);
+
+  const toggleShowImported = () => setShowImported(!showImported);
 
   const onSubmit = async (e) => {
     e.preventDefault();
@@ -86,7 +89,7 @@ export default function EditMode({ report }) {
       </Alert>
       )}
 
-      <Form onSubmit={onSubmit}>
+      <Form onSubmit={onSubmit} className="margin-bottom-2">
         <Label htmlFor="startDate">Start Date</Label>
         <TextInput id="startDate" value={startDate} onChange={(e) => setStartDate(e.target.value)} />
 
@@ -107,6 +110,23 @@ export default function EditMode({ report }) {
 
         <Button type="submit">Submit edits</Button>
       </Form>
+
+      <Button type="button" outline onClick={toggleShowImported}>
+        {showImported ? 'Hide' : 'Show' }
+        {' '}
+        imported data
+      </Button>
+      {showImported
+      && (
+      <pre style={{
+        whiteSpace: 'pre-wrap',
+        maxWidth: '100%',
+      }}
+      >
+        {JSON.stringify(report.imported, null, 2)}
+      </pre>
+      )}
+
     </>
   );
 }
@@ -118,5 +138,6 @@ EditMode.propTypes = {
     endDate: PropTypes.string,
     targetPopulations: PropTypes.arrayOf(PropTypes.string),
     reason: PropTypes.arrayOf(PropTypes.string),
+    imported: PropTypes.string,
   }).isRequired,
 };
