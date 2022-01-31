03b964d1d203634f1d13330305ce6b12a60f8b1d
GarrettEHill <garretthill@gmail.com>
2022-01-31 10:31:56 -0800

    keep test changes just in case

--- src/models/tests/auditModelGenerator.test.js
+++ src/models/tests/auditModelGenerator.test.js
@@ -1,4 +1,5 @@
 import db, { User, ZALUser } from '..';
+import { auditLogger } from '../../logger';
 
 describe('Audit models', () => {
   afterAll(async () => {
@@ -6,50 +7,74 @@ describe('Audit models', () => {
   });
 
   describe('audit user model', () => {
-    let transaction;
-
     describe('default scope', () => {
-      const users = [
-        {
-          id: 60,
-          name: 'd',
-        },
-        {
-          id: 61,
-          name: 'c',
-        },
-        {
-          id: 62,
-          name: 'a',
-        },
-        {
-          id: 63,
-          name: 'b',
-        },
-      ];
+      let t;
+      let userIds = [];
+      let userIdNames = [];
 
       beforeEach(async () => {
-        transaction = await db.sequelize.transaction();
-        await Promise.all(
-          users.map((u) => User.create({
-            id: u.id,
-            name: u.name,
-            hsesUsername: u.id,
-            hsesUserId: u.id,
-          })),
+        t = await db.sequelize.transaction();
+
+        const descriptor = await db.sequelize.queryInterface.sequelize.query(
+          `SELECT
+            set_config('audit.auditDescriptor', '${expect.getState().currentTestName}', TRUE) as "auditDescriptor";`,
+          { transaction: t },
         );
+        auditLogger.info(descriptor);
+
+        const users = await Promise.all([
+          await User.create({
+            name: 'aa',
+            email: 'aa@aa.com',
+            hsesUserId: 11111,
+            hsesUsername: 'aa',
+          }, { transaction: t })
+            .catch(() => console.error(users)), // eslint-disable-line no-console
+          await User.create({
+            name: 'bb',
+            email: 'bb@bb.com',
+            hsesUserId: 22222,
+            hsesUsername: 'bb',
+          }, { transaction: t })
+            .catch(() => console.error(users)), // eslint-disable-line no-console
+          await User.create({
+            name: 'cc',
+            email: 'cc@cc.com',
+            hsesUserId: 33333,
+            hsesUsername: 'cc',
+          }, { transaction: t })
+            .catch(() => console.error(users)), // eslint-disable-line no-console
+          await User.create({
+            name: 'dd',
+            email: 'dd@dd.com',
+            hsesUserId: 44444,
+            hsesUsername: 'dd',
+          }, { transaction: t })
+            .catch(() => console.error(users)), // eslint-disable-line no-console
+        ]);
+
+        userIds = users.map((u) => u.id)
+          .catch(() => console.error(users)); // eslint-disable-line no-console
+
+        userIdNames = users.map((u) => ({ id: u.id, name: u.name }))
+          .catch(() => console.error(users)); // eslint-disable-line no-console
       });
 
       afterEach(async () => {
-        transaction.rollback();
-        // await User.destroy({ where: { id: ids } });
+        if (t) {
+          // t.rollback();
+          t.commit();
+        }
       });
 
       it('Added users in audit record', async () => {
         const auditUsers = await ZALUser.findAll({
-          where: { data_id: users.map((u) => u.id) },
-          transaction,
+          where: { data_id: userIds },
+          transaction: t,
         });
+
+        auditLogger.info(auditUsers);
+
         const dmlType = auditUsers.map((au) => au.dml_type);
         dmlType.sort();
         expect(dmlType).toEqual(['INSERT', 'INSERT', 'INSERT', 'INSERT']);
@@ -59,22 +84,26 @@ describe('Audit models', () => {
           name: au.new_row_data.name,
         }));
         data.sort((a, b) => ((a.id > b.id) ? 1 : -1));
-        expect(data).toEqual(users);
+        expect(data).toEqual(userIdNames);
       });
 
       it('Modified users in audit record', async () => {
-        await User.update(
-          { name: 'z', email: 'z@z.com' },
+        const updates = await User.update(
+          { name: 'zz', email: 'zz@zz.com' },
           {
-            where: { id: users[0].id },
-            transaction,
+            where: { id: userIds[0] },
+            transaction: t,
           },
-        );
+        ).catch((err) => console.error(err)); // eslint-disable-line no-console
+
+        auditLogger.info(updates);
 
         const auditUsers = await ZALUser.findAll({
-          where: { data_id: users[0].id, dml_type: 'UPDATE' },
-          transaction,
+          where: { data_id: userIds[0], dml_type: 'UPDATE' },
+          transaction: t,
         });
+
+        auditLogger.info(auditUsers);
 
         const data = auditUsers.map((au) => ({
           id: parseInt(au.data_id, 10),
@@ -83,7 +112,7 @@ describe('Audit models', () => {
         }));
         data.sort((a, b) => ((a.id > b.id) ? 1 : -1));
 
-        const modified = [{ id: users[0].id, oldName: users[0].name, newName: 'z' }];
+        const modified = [{ id: userIdNames[0].id, oldName: userIdNames[0].name, newName: 'z' }];
 
         expect(data).toEqual(modified);
       });
