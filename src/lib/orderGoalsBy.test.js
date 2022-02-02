48e25352563141d42d60753f356ebf5fd22622e7
Adam Levin <adam.levin@adhocteam.us>
2022-02-02 09:24:43 -0500

    fix BE test

--- src/lib/orderGoalsBy.test.js
+++ src/lib/orderGoalsBy.test.js
@@ -1,12 +1,14 @@
 import orderGoalsBy from './orderGoalsBy';
+import { sequelize } from '../models';
 
 describe('orderGoalsBy', () => {
   it('returns the correct values', () => {
     const one = orderGoalsBy('goalStatus', 'asc');
-    expect(one).toStrictEqual([[
-      'status',
-      'asc',
-    ]]);
+    expect(one).toStrictEqual([
+      [
+        sequelize.literal('status_sort asc'),
+      ],
+    ]);
 
     const two = orderGoalsBy('createdOn', 'desc');
     expect(two).toStrictEqual([[
