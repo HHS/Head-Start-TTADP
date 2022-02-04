01e6700af22b16164dc3ca20b681853d9699f03f
Matt Bevilacqua <thewatermethod@gmail.com>
2022-02-04 10:41:33 -0500

    fix migration to reference correct column

--- src/migrations/20220204151917-remove-old-manager.js
+++ src/migrations/20220204151917-remove-old-manager.js
@@ -1,10 +1,10 @@
 module.exports = {
   up: async (queryInterface) => Promise.all([
     queryInterface.removeColumn('ActivityRepors', 'oldMangagerNotes'),
-    queryInterface.removeColumn('ActivityReports', 'oldManagerId'),
+    queryInterface.removeColumn('ActivityReports', 'oldApprovingManagerId'),
   ]),
   down: async (queryInterface, Sequelize) => Promise.all([
     queryInterface.addColumn('ActivityRepors', 'oldMangagerNotes', { type: Sequelize.DataTypes.TEXT }),
-    queryInterface.addColumn('ActivityReports', 'oldManagerId', { type: Sequelize.DataTypes.INTEGER }),
+    queryInterface.addColumn('ActivityReports', 'oldApprovingManagerId', { type: Sequelize.DataTypes.INTEGER }),
   ]),
 };
