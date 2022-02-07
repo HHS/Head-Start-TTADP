78812bc844c944b587d74825a95a318a614736e8
Adam Levin <adam.levin@adhocteam.us>
2022-02-07 15:14:57 -0500

    rearrange tests

--- src/services/goals.test.js
+++ src/services/goals.test.js
@@ -191,6 +191,34 @@ describe('Goals DB service', () => {
   });
 });
 
+describe('Change Goal Status', () => {
+  let goal;
+  beforeAll(async () => {
+    // Create Goal.
+    goal = await Goal.create({
+      name: 'Goal with Objectives',
+      status: 'Not Started',
+      timeframe: '12 months',
+      isFromSmartsheetTtaPlan: false,
+      createdAt: new Date('2021-01-02'),
+    });
+  });
+  afterAll(async () => {
+    // Cleanup Goal.
+    await Goal.destroy({
+      where: {
+        id: goal.id,
+      },
+    });
+    await sequelize.close();
+  });
+  it('Updates goal status', async () => {
+    const newStatus = 'In Progress';
+    const updatedGoal = await updateGoalStatusById(goal.id.toString(), newStatus);
+    expect(updatedGoal.status).toEqual(newStatus);
+  });
+});
+
 describe('goalsForGrants', () => {
   beforeAll(async () => {
     jest.resetAllMocks();
@@ -230,35 +258,5 @@ describe('goalsForGrants', () => {
       ],
       order: ['createdAt'],
     });
-  });
-});
-
-describe('Change Goal Status', () => {
-  const goalId = 598745;
-  beforeAll(async () => {
-    // Create Goal.
-    await Promise.all([Goal.create({
-      id: goalId,
-      name: 'Goal with Objectives',
-      status: 'Not Started',
-      timeframe: '12 months',
-      isFromSmartsheetTtaPlan: false,
-      createdAt: new Date('2021-01-02'),
-    })]);
-  });
-
-  afterAll(async () => {
-    // Cleanup Goal.
-    await Goal.destroy({
-      where: {
-        id: goalId,
-      },
-    });
-    await sequelize.close();
-  });
-  it('Updates goal status', async () => {
-    const newStatus = 'In Progress';
-    const updatedGoal = await updateGoalStatusById(goalId.toString(), newStatus);
-    expect(updatedGoal.status).toEqual(newStatus);
   });
 });
