4f12495987e54647c5dbe04c1e39511a3b1ad474
GarrettEHill <garretthill@gmail.com>
2022-02-11 08:58:14 -0800

    Update 20220211000000-add-checksum-metadata-to-file.js

--- src/migrations/20220211000000-add-checksum-metadata-to-file.js
+++ src/migrations/20220211000000-add-checksum-metadata-to-file.js
@@ -1,11 +1,11 @@
 /* eslint-disable max-len */
 module.exports = {
   up: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction(async (transaction) => Promise.all([
-    queryInterface.addColumn('file', 'checksums', { type: Sequelize.DataTypes.JSON, allowNull: true }, { transaction }),
-    queryInterface.addColumn('file', 'metadata', { type: Sequelize.DataTypes.JSON, allowNull: true }, { transaction }),
+    queryInterface.addColumn('files', 'checksums', { type: Sequelize.DataTypes.JSON, allowNull: true }, { transaction }),
+    queryInterface.addColumn('files', 'metadata', { type: Sequelize.DataTypes.JSON, allowNull: true }, { transaction }),
   ])),
   down: async (queryInterface) => queryInterface.sequelize.transaction(async (transaction) => Promise.all([
-    queryInterface.removeColumn('file', 'checksums', { transaction }),
-    queryInterface.removeColumn('file', 'metadata', { transaction }),
+    queryInterface.removeColumn('files', 'checksums', { transaction }),
+    queryInterface.removeColumn('files', 'metadata', { transaction }),
   ])),
 };
