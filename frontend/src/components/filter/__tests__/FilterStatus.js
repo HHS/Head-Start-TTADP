89f10f5c6e8f6b1d5972f08171b083e06b84a626
Matt Bevilacqua <thewatermethod@gmail.com>
2022-02-14 12:29:04 -0500

    add test for status filter (just for test coverage)

---
+++ frontend/src/components/filter/__tests__/FilterStatus.js
@@ -0,0 +1,30 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+import selectEvent from 'react-select-event';
+import FilterStatus from '../FilterStatus';
+
+const { findByText } = screen;
+
+describe('FilterStatus', () => {
+  const renderStatusSelect = (onApply) => (
+    render(
+      <FilterStatus
+        onApply={onApply}
+        inputId="curly"
+        query={[]}
+      />,
+    ));
+
+  it('calls the onapply handler', async () => {
+    const onApply = jest.fn();
+    renderStatusSelect(onApply);
+
+    const select = await findByText(/select status to filter by/i);
+    await selectEvent.select(select, ['Needs status']);
+    expect(onApply).toHaveBeenCalled();
+  });
+});
