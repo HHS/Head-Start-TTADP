7eca291f7ae5804e7eacb1c1c40fb4413f9eccd2
Adam Levin <adam.levin@adhocteam.us>
2022-02-16 15:02:20 -0500

    BE changes for other entities scope

--- src/scopes/activityReport/index.test.js
+++ src/scopes/activityReport/index.test.js
@@ -1508,4 +1508,98 @@ describe('filtersToScopes', () => {
         .toEqual(expect.arrayContaining([excludedReport.id, globallyExcludedReport.id]));
     });
   });
+
+  describe('only other entities', () => {
+    let reportIncluded1;
+    let reportExcluded;
+    let reportIncluded2;
+
+    let otherEntityIncluded1;
+    let otherEntityIncluded2;
+    let otherEntityExcluded;
+
+    let activityRecipientIncluded1;
+    let activityRecipientIncluded2;
+    let activityRecipientExcluded;
+
+    let possibleIds;
+
+    beforeAll(async () => {
+      otherEntityIncluded1 = await OtherEntity.create({ id: 25458, name: 'Head Start Collaboration Office' });
+      otherEntityExcluded = await OtherEntity.create({ id: 25459, name: 'QRIS System' });
+      otherEntityIncluded2 = await OtherEntity.create({ id: 25460, name: 'State CCR&R' });
+
+      reportIncluded1 = await ActivityReport.create(
+        { userId: mockUser.id, ...draftReport },
+      );
+      reportIncluded2 = await ActivityReport.create(
+        { userId: mockUser.id, ...draftReport },
+      );
+      reportExcluded = await ActivityReport.create(
+        { userId: mockUser.id, ...draftReport },
+      );
+
+      activityRecipientIncluded1 = await ActivityRecipient.create({
+        activityReportId: reportIncluded1.id,
+        otherEntityId: otherEntityIncluded1.id,
+      });
+      activityRecipientExcluded = await ActivityRecipient.create({
+        activityReportId: reportExcluded.id,
+        otherEntityId: otherEntityExcluded.id,
+      });
+      activityRecipientIncluded2 = await ActivityRecipient.create({
+        activityReportId: reportIncluded2.id,
+        otherEntityId: otherEntityIncluded2.id,
+      });
+
+      possibleIds = [
+        reportIncluded1.id,
+        reportIncluded2.id,
+        reportExcluded.id,
+        globallyExcludedReport.id,
+      ];
+    });
+
+    afterAll(async () => {
+      await ActivityRecipient.destroy({
+        where: {
+          id: [
+            activityRecipientIncluded1.id,
+            activityRecipientIncluded2.id,
+            activityRecipientExcluded.id,
+          ],
+        },
+      });
+      await ActivityReport.destroy({
+        where: { id: [reportIncluded1.id, reportIncluded2.id, reportExcluded.id] },
+      });
+      await OtherEntity.destroy({
+        where: { id: [otherEntityIncluded1.id, otherEntityIncluded2.id, otherEntityExcluded.id] },
+      });
+
+      await db.sequelize.close();
+    });
+
+    it('includes other entities', async () => {
+      const filters = { 'otherEntities.in': ['Head Start Collaboration Office', 'State CCR&R'] };
+      const { activityReport: scope } = filtersToScopes(filters);
+      const found = await ActivityReport.findAll({
+        where: { [Op.and]: [scope, { id: possibleIds }] },
+      });
+      expect(found.length).toBe(2);
+      expect(found.map((f) => f.id))
+        .toEqual(expect.arrayContaining([reportIncluded1.id, reportIncluded2.id]));
+    });
+
+    it('excludes other entities', async () => {
+      const filters = { 'otherEntities.nin': ['Head Start Collaboration Office', 'State CCR&R'] };
+      const { activityReport: scope } = filtersToScopes(filters);
+      const found = await ActivityReport.findAll({
+        where: { [Op.and]: [scope, { id: possibleIds }] },
+      });
+      expect(found.length).toBe(2);
+      expect(found.map((f) => f.id))
+        .toEqual(expect.arrayContaining([reportExcluded.id, globallyExcludedReport.id]));
+    });
+  });
 });
