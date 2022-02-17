f1150d683816817841fdd72046aefa206937d379
Matt Bevilacqua <thewatermethod@gmail.com>
2022-02-17 09:25:12 -0500

    fix test I must have broken

--- frontend/src/pages/RecipientRecord/pages/__tests__/GoalsObjectives.js
+++ frontend/src/pages/RecipientRecord/pages/__tests__/GoalsObjectives.js
@@ -81,7 +81,7 @@ describe('Goals and Objectives', () => {
     fetchMock.get(goalsUrl, { count: 1, goalRows: goals });
 
     // Filters Status.
-    const filterStatusUrl = '/api/recipient/401/region/1/goals?sortBy=goalStatus&sortDir=asc&offset=0&limit=5&status.in[]=Not%20Started';
+    const filterStatusUrl = '/api/recipient/401/region/1/goals?sortBy=goalStatus&sortDir=asc&offset=0&limit=5&status.in[]=Not%20started';
     fetchMock.get(filterStatusUrl, { count: 1, goalRows: filterStatusGoals });
 
     // No Filters.
@@ -95,7 +95,7 @@ describe('Goals and Objectives', () => {
     const goalStatusGraph = `/api/widgets/goalStatusGraph?createDate.win=${yearToDate}&region.in[]=1&recipientId.ctn[]=401`;
     fetchMock.get(goalStatusGraph, statusRes);
 
-    const goalStatusGraphWStatus = '/api/widgets/goalStatusGraph?status.in[]=Not%20Started&region.in[]=1&recipientId.ctn[]=401';
+    const goalStatusGraphWStatus = '/api/widgets/goalStatusGraph?status.in[]=Not%20started&region.in[]=1&recipientId.ctn[]=401';
     fetchMock.get(goalStatusGraphWStatus, statusRes);
 
     const goalStatusGraphUnfiltered = '/api/widgets/goalStatusGraph?region.in[]=1&recipientId.ctn[]=401';
@@ -135,7 +135,7 @@ describe('Goals and Objectives', () => {
     userEvent.selectOptions(await screen.findByRole('combobox', { name: 'condition' }), 'Is');
 
     const statusSelect = await screen.findByLabelText(/select status to filter by/i);
-    await selectEvent.select(statusSelect, ['Not Started']);
+    await selectEvent.select(statusSelect, ['Not started']);
 
     const apply = await screen.findByRole('button', { name: /apply filters to goals/i });
     userEvent.click(apply);
