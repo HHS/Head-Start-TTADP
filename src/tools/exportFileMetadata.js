6f4a3eb603305002f1a303b87a1214c0563a36f7
GarrettEHill <garretthill@gmail.com>
2022-02-22 15:05:42 -0800

    Limit parallel processing to 5

--- src/tools/exportFileMetadata.js
+++ src/tools/exportFileMetadata.js
@@ -1,3 +1,4 @@
+import async from 'async';
 import temp from 'temp';
 import generateMetadataFromFile from '../lib/fileProcessing';
 import { downloadFile } from '../lib/s3';
@@ -5,36 +6,45 @@ import { sequelize, File } from '../models';
 import { updateMetadata } from '../services/files';
 import { auditLogger } from '../logger';
 
-const exportFileMetadata = () => sequelize.transaction(async (transaction) => {
+const processFile = async (file) => {
+  let stream;
+  let hasFile;
   try {
-    const files = File.findAll({ where: { metadata: null }, transaction });
-    files.map(async (file) => {
-      let stream;
-      let hasFile;
-      try {
-        auditLogger.info(JSON.stringify(file));
-        temp.track(); // Automatically track and cleanup files at exit
-        stream = temp.createWriteStream();
-        hasFile = stream.write(await downloadFile(files.key));
-      } catch (err) {
-        auditLogger.error(JSON.stringify(err));
-      }
-      if (hasFile) {
-        try {
-          auditLogger.info(hasFile);
-          auditLogger.info(stream.path);
-          await updateMetadata(file.id, generateMetadataFromFile(stream.path));
-          stream.end();
-        } catch (err) {
-          auditLogger.error(JSON.stringify(err));
-          throw (err);
-        }
-      }
-    });
+    auditLogger.info(JSON.stringify(file));
+    temp.track(); // Automatically track and cleanup files at exit
+    stream = temp.createWriteStream();
+    hasFile = stream.write(await downloadFile(file.key));
   } catch (err) {
     auditLogger.error(JSON.stringify(err));
-    throw (err);
   }
-});
+  if (hasFile) {
+    try {
+      auditLogger.info(hasFile);
+      auditLogger.info(stream.path);
+      await updateMetadata(file.id, generateMetadataFromFile(stream.path));
+      stream.end();
+    } catch (err) {
+      auditLogger.error(JSON.stringify(err));
+      throw (err);
+    }
+  }
+};
+
+const exportFileMetadata = async () => {
+  await sequelize.transaction(async (transaction) => {
+    try {
+      const files = await File.findAll({ where: { metadata: null }, transaction });
+      async.forEachLimit(
+        files,
+        5,
+        processFile,
+        (err) => { if (err) auditLogger.error(JSON.stringify(err)); },
+      );
+    } catch (err) {
+      auditLogger.error(JSON.stringify(err));
+      throw (err);
+    }
+  });
+};
 
 export default exportFileMetadata;
