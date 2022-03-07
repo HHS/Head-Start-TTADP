e57de4181fb1a0e349fedd57284940dea55cb16e
Josh Salisbury <josh@adhocteam.us>
2022-03-07 16:01:58 -0600

    Loader shown when downloading reports

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -4,14 +4,14 @@ import Spinner from './Spinner';
 
 import './Loader.css';
 
-function Loader({ loading, loadingLabel }) {
+function Loader({ loading, loadingLabel, text }) {
   return (
     <>
       {loading && (
         <div role="status" aria-live="polite" className="overlay" aria-label={loadingLabel}>
           <div className="overlay-spinner">
             <Spinner />
-            Loading Data
+            { text }
           </div>
         </div>
       )}
@@ -22,6 +22,11 @@ function Loader({ loading, loadingLabel }) {
 Loader.propTypes = {
   loading: PropTypes.bool.isRequired,
   loadingLabel: PropTypes.string.isRequired,
+  text: PropTypes.string,
+};
+
+Loader.defaultProps = {
+  text: 'Loading data',
 };
 
 export default Loader;
