5dc0a5aa21e9a874d352d10bcbb3e2b8cc2ebfee
GarrettEHill <garretthill@gmail.com>
2022-03-08 12:27:13 -0800

    changed jest-environment to node for file tests

--- src/lib/fileProcessing.test.js
+++ src/lib/fileProcessing.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment node
+ */
 import {
   spinUpTool,
   isToolUp,
--- src/routes/files/handlers.test.js
+++ src/routes/files/handlers.test.js
@@ -1,3 +1,6 @@
+/**
+ * @jest-environment node
+ */
 import { validate } from 'uuid';
 import db, {
   File,
