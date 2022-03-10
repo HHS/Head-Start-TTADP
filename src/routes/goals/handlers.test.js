bade4df302fd7e5d90981f488d441bd47e4e0216
Matt Bevilacqua <thewatermethod@gmail.com>
2022-03-10 09:27:11 -0500

    add handlers test

--- src/routes/goals/handlers.test.js
+++ src/routes/goals/handlers.test.js
@@ -1,25 +1,124 @@
 import { INTERNAL_SERVER_ERROR, NOT_FOUND } from 'http-codes';
-import { changeGoalStatus } from './handlers';
-import { updateGoalStatusById } from '../../services/goals';
+import { changeGoalStatus, createGoals } from './handlers';
+import { updateGoalStatusById, createOrUpdateGoals } from '../../services/goals';
+import { userById } from '../../services/users';
+import SCOPES from '../../middleware/scopeConstants';
+
+jest.mock('../../services/users', () => ({
+  userById: jest.fn(),
+}));
 
 jest.mock('../../services/goals', () => ({
   updateGoalStatusById: jest.fn(),
+  createOrUpdateGoals: jest.fn(),
 }));
 
 jest.mock('../../services/accessValidation');
 
+const mockResponse = {
+  attachment: jest.fn(),
+  json: jest.fn(),
+  send: jest.fn(),
+  sendStatus: jest.fn(),
+  status: jest.fn(() => ({
+    end: jest.fn(),
+  })),
+};
+
+describe('createGoals', () => {
+  afterAll(async () => {
+    jest.clearAllMocks();
+  });
+
+  it('checks permissions', async () => {
+    const req = {
+      body: {
+        goals: [
+          { regionId: 2 },
+        ],
+      },
+      session: {
+        userId: 1,
+      },
+    };
+
+    userById.mockResolvedValueOnce({
+      permissions: [
+        {
+          regionId: 2,
+          scopeId: SCOPES.READ_REPORTS,
+        },
+      ],
+    });
+
+    // createOrUpdateGoals.mockResolvedValueOnce();
+
+    await createGoals(req, mockResponse);
+
+    expect(mockResponse.sendStatus).toHaveBeenCalledWith(401);
+  });
+
+  it('handles success', async () => {
+    const req = {
+      body: {
+        goals: [
+          { regionId: 2 },
+        ],
+      },
+      session: {
+        userId: 1,
+      },
+    };
+
+    userById.mockResolvedValueOnce({
+      permissions: [
+        {
+          regionId: 2,
+          scopeId: SCOPES.READ_WRITE_REPORTS,
+        },
+      ],
+    });
+
+    createOrUpdateGoals.mockResolvedValueOnce({});
+    await createGoals(req, mockResponse);
+
+    expect(mockResponse.json).toHaveBeenCalledWith({});
+  });
+
+  it('handles failures', async () => {
+    const req = {
+      body: {
+        goals: [
+          { regionId: 2 },
+        ],
+      },
+      session: {
+        userId: 1,
+      },
+    };
+
+    userById.mockResolvedValueOnce({
+      permissions: [
+        {
+          regionId: 2,
+          scopeId: SCOPES.READ_WRITE_REPORTS,
+        },
+      ],
+    });
+
+    createOrUpdateGoals.mockImplementationOnce(() => {
+      throw new Error();
+    });
+
+    await createGoals(req, mockResponse);
+
+    expect(mockResponse.status).toHaveBeenCalledWith(INTERNAL_SERVER_ERROR);
+  });
+});
+
 describe('changeGoalStatus', () => {
   const goalWhere = { name: 'My updated goal' };
 
-  const mockResponse = {
-    attachment: jest.fn(),
-    json: jest.fn(),
-    send: jest.fn(),
-    sendStatus: jest.fn(),
-    status: jest.fn(() => ({
-      end: jest.fn(),
-    })),
-  };
   it('updates status goal by id', async () => {
     const req = {
       params: {
