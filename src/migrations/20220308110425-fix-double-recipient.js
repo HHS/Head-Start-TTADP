f6cf74caa395db4d27a2d0ca63b6c7a4c2936492
unknown <krystyna@adhocteam.us>
2022-03-10 10:25:19 -0500

    Fix lint errors

--- src/migrations/20220308110425-fix-double-recipient.js
+++ src/migrations/20220308110425-fix-double-recipient.js
@@ -1,33 +1,27 @@
 /* eslint-disable max-len */
 
 module.exports = {
-  up: async (queryInterface) => {
-
-    return queryInterface.sequelize.transaction(async (transaction) => {
+  up: async (queryInterface) => queryInterface.sequelize.transaction(async (transaction) => {
     /*
       * Temporary recipient merge after a better solution is found.
       * Ideally, this would be fixed in HSES (TTAHUB-705).
       * Folds grant with an id 9957 under recipient 7782.
       * Deletes recipient 5.
       */
-      await queryInterface.sequelize.query('UPDATE "Grants" SET "recipientId" = 7782 WHERE "id" = 9957;', { transaction });
-      await queryInterface.sequelize.query('UPDATE "GrantGoals" SET "recipientId" = 7782 WHERE "id" = 9957;', { transaction });
-      await queryInterface.sequelize.query('DELETE FROM "Recipients" WHERE "id" = 5;', { transaction });
-    });
-  },
+    await queryInterface.sequelize.query('UPDATE "Grants" SET "recipientId" = 7782 WHERE "id" = 9957;', { transaction });
+    await queryInterface.sequelize.query('UPDATE "GrantGoals" SET "recipientId" = 7782 WHERE "id" = 9957;', { transaction });
+    await queryInterface.sequelize.query('DELETE FROM "Recipients" WHERE "id" = 5;', { transaction });
+  }),
 
-  down: async (queryInterface) => {
-
-    return queryInterface.sequelize.transaction(async (transaction) => {
+  down: async (queryInterface) => queryInterface.sequelize.transaction(async (transaction) => {
     /*
       * Undo the above actions.
       */
 
-    const query = `INSERT INTO "Recipients"(id, "name", "createdAt", "updatedAt", "recipientType") VALUES (5, 'Arkansas Department of Human Services', '2021-03-16T01:34:16.558Z', '2021-03-16T01:34:16.558Z', 'Government Agency (Non-CAA)');`
+    const query = 'INSERT INTO "Recipients"(id, "name", "createdAt", "updatedAt", "recipientType") VALUES (5, \'Arkansas Department of Human Services\', \'2021-03-16T01:34:16.558Z\', \'2021-03-16T01:34:16.558Z\', \'Government Agency (Non-CAA)\');';
 
     await queryInterface.sequelize.query(query, { transaction });
     await queryInterface.sequelize.query('UPDATE "Grants" SET "recipientId" = 5 WHERE "id" = 9957;', { transaction });
     await queryInterface.sequelize.query('UPDATE "GrantGoals" SET "recipientId" = 5 WHERE "id" = 9957;', { transaction });
-    });
-  },
+  }),
 };
