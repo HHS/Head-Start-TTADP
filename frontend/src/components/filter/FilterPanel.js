2476b3713773fd1343864e51f9da8ccb4228a68a
Adam Levin <adam.levin@adhocteam.us>
2022-03-22 15:50:24 -0400

    code cleanup WIP

--- frontend/src/components/filter/FilterPanel.js
+++ frontend/src/components/filter/FilterPanel.js
@@ -22,52 +22,20 @@ export default function FilterPanel({
     setFiltersToShow(hideRegionFilters ? filters.filter((f) => f.topic !== 'region') : filters);
   }, [hideRegionFilters, filters]);
 
-  /* DEV NOTE:
-     Right now we are using the filter panel to intercept any filter changes.
-     If the default behavior is to hide region filters but display them in the URL,
-     we intercept and handle these cases in 'onApply' and 'onRemoveFilter'.
-     If we are NOT hiding region filters we do the same behavior as before.
-     This code is being handled here for reusability in other filter locations.
-  */
   const onApply = (items) => {
-    if (hideRegionFiltersByDefault) {
-      // If we are currently hiding the region filters.
-      if (hideRegionFilters) {
-        // Check if user added region filter.
-        const regionFilters = items.find((f) => f.topic === 'region');
-        if (!regionFilters) {
-          // If user didn't add a region filter add existing region filters from url.
-          onApplyFilters(items, true);
-        } else {
-          // User added a region filter.
-          setHideRegionFilters(false);
-          onApplyFilters(items, false);
-        }
-      } else {
-        // We are not hiding the region filters.
-        const regionFilter = items.find((f) => f.topic === 'region');
-        onApplyFilters(items, !regionFilter);
-        setHideRegionFilters(!regionFilter);
-      }
-    } else {
-      // Handle apply normally.
-      onApplyFilters(items, false);
-    }
+    // Check for region filter.
+    const regionFilter = items.find((f) => f.topic === 'region');
+    onApplyFilters(items, !regionFilter);
+    setHideRegionFilters(!regionFilter);
   };
 
   const onRemoveFilterPill = (id) => {
-    if (hideRegionFiltersByDefault) {
-      // Check if pill being removed is a region.
-      const pillToRemove = filters.find((f) => f.id === id);
-      const addBackAndHideDefaultRegions = pillToRemove && pillToRemove.topic === 'region';
-      // Remove from parent add back default regions.
-      onRemoveFilter(id, addBackAndHideDefaultRegions);
-      if (addBackAndHideDefaultRegions && !hideRegionFilters) {
-        setHideRegionFilters(true);
-      }
-    } else {
-      // Handle onRemove normally.
-      onRemoveFilter(id, false);
+    // Check if pill being removed is a region filter.
+    const pillToRemove = filters.find((f) => f.id === id);
+    const isRegionFilter = pillToRemove && pillToRemove.topic === 'region';
+    onRemoveFilter(id, isRegionFilter);
+    if (isRegionFilter) {
+      setHideRegionFilters(true);
     }
   };
 
