1d8a32d34d98f7478dbe0be55f3c3433172c9196
Matt Bevilacqua <thewatermethod@gmail.com>
2022-03-23 09:39:40 -0400

    fix hasty test

--- frontend/src/fetchers/__tests__/topics.js
+++ frontend/src/fetchers/__tests__/topics.js
@@ -5,8 +5,9 @@ describe('topics fetcher', () => {
   beforeEach(() => fetchMock.reset());
 
   it('fetches topics', async () => {
-    fetchMock.get('/api/topic', 200);
+    fetchMock.get('/api/topic', []);
     await getTopics();
+
     expect(fetchMock.called()).toBeTruthy();
   });
 });
