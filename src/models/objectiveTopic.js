3a9fa9464ef7ba832596bd30ce37a41c16ced05a
Matt Bevilacqua <thewatermethod@gmail.com>
2022-04-06 10:03:05 -0400

    fixed association with topics

--- src/models/objectiveTopic.js
+++ src/models/objectiveTopic.js
@@ -10,7 +10,9 @@ const {
    */
 module.exports = (sequelize, DataTypes) => {
   class ObjectiveTopic extends Model {
-    static associate() {
+    static associate(models) {
+      ObjectiveTopic.belongsTo(models.Objective, { foreignKey: 'objectiveId', onDelete: 'cascade', as: 'objective' });
+      ObjectiveTopic.belongsTo(models.Topic, { foreignKey: 'topicId', onDelete: 'cascade', as: 'topic' });
     }
   }
   ObjectiveTopic.init({
