3d853d620cbe048f8a64b356403198095a83875f
Matt Bevilacqua <thewatermethod@gmail.com>
2022-04-11 15:48:33 -0400

    approvers still start on review

--- frontend/src/pages/ActivityReport/index.js
+++ frontend/src/pages/ActivityReport/index.js
@@ -295,6 +295,12 @@ function ActivityReport({
     );
   }
 
+  if (!currentPage && editable && isPendingApprover) {
+    return (
+      <Redirect push to={`/activity-reports/${activityReportId}/review`} />
+    );
+  }
+
   if (!currentPage) {
     return (
       <Redirect push to={`/activity-reports/${activityReportId}/activity-summary`} />
