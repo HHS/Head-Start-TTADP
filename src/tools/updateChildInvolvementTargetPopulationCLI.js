803188ea2736e7d58e6f0b5e303074f73262ee43
Adam Levin <adam.levin@adhocteam.us>
2022-04-11 10:16:36 -0400

    remove run scripts

--- src/tools/updateChildInvolvementTargetPopulationCLI.js
+++
@@ -1,7 +0,0 @@
-import updateChildInvolvementTargetPopulation from './updateChildInvolvementTargetPopulation';
-import { auditLogger } from '../logger';
-
-updateChildInvolvementTargetPopulation().catch((e) => {
-  auditLogger.error(e);
-  process.exit(1);
-});
--- src/tools/updateParticipantsListCLI.js
+++
@@ -1,7 +0,0 @@
-import updateParticipantsList from './updateParticipantsList';
-import { auditLogger } from '../logger';
-
-updateParticipantsList().catch((e) => {
-  auditLogger.error(e);
-  process.exit(1);
-});
