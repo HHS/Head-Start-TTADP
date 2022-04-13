1f5de0019562ab1e850aba41eb4b7f873e3b9c30
Adam Levin <adam.levin@adhocteam.us>
2022-04-13 09:24:30 -0400

    wip

--- frontend/src/pages/ActivityReport/Pages/supportingAttachments.js
+++ frontend/src/pages/ActivityReport/Pages/supportingAttachments.js
@@ -5,16 +5,9 @@ import { Helmet } from 'react-helmet';
 import {
   Fieldset, Label,
 } from '@trussworks/react-uswds';
-
-import { isUndefined } from 'lodash';
 import Section from './Review/ReviewSection';
-import ReviewItem from './Review/ReviewItem';
 import FileReviewItem from './Review/FileReviewItem';
-import MultiSelect from '../../../components/MultiSelect';
 import FileUploader from '../../../components/FileUploader';
-import FormItem from '../../../components/FormItem';
-import ResourceSelector from './components/ResourceSelector';
-import { TOPICS as topics } from '../../../Constants';
 import { reportIsEditable } from '../../../utils';
 
 const SupportingAttachments = ({
@@ -27,61 +20,21 @@ const SupportingAttachments = ({
       <Helmet>
         <title>Supporting attachments</title>
       </Helmet>
-      <Fieldset className="smart-hub--report-legend margin-top-4" legend="Topics Covered">
-        <div id="topics-covered" />
-        <div className="margin-top-4">
-          <FormItem
-            label="Topic(s) covered. You may choose more than one."
-            name="topics"
-          >
-            <MultiSelect
-              name="topics"
-              label="Topic(s) covered. You may choose more than one."
-              control={control}
-              placeholder="Select a topic..."
-              required="Please select at least one topic"
-              options={
-              topics.map((topic) => ({ value: topic, label: topic }))
-            }
-            />
-          </FormItem>
-        </div>
-      </Fieldset>
-      <Fieldset className="smart-hub--report-legend margin-top-4" legend="OHS / ECLKC resources">
-        <div id="ECLKCResources" />
-        <div className="margin-top-4">
-          <Label>
-            Resources from OHS / ECLKC
-            <br />
-            Enter the URL for OHS resource(s) used.
-            {' '}
-            {/* eslint-disable-next-line react/jsx-no-target-blank */}
-            <a target="_blank" rel="noopener" href="https://eclkc.ohs.acf.hhs.gov/">https://eclkc.ohs.acf.hhs.gov/</a>
-            <ResourceSelector
-              name="ECLKCResourcesUsed"
-              ariaName="ECLKC Resources"
-            />
-          </Label>
-        </div>
-      </Fieldset>
-      <Fieldset className="smart-hub--report-legend margin-top-4" legend="Non-ECLKC resources">
-        <div id="nonECLKCResources" />
-        <div className="margin-top-4">
-          <Label>
-            For non-ECLKC resources enter URL.
-            <br />
-            If no URL is available, upload document in next section.
-            <ResourceSelector
-              name="nonECLKCResourcesUsed"
-              ariaName="non-ECLKC Resources"
-            />
-          </Label>
-        </div>
-      </Fieldset>
-      <Fieldset legend="Supporting attachments" className="smart-hub--report-legend margin-top-4">
+      <Fieldset className="smart-hub--report-legend margin-top-4">
         <div id="attachments" />
-        <Label htmlFor="attachments">Upload resources not available online, agenda, service plans, sign-in sheets, etc.</Label>
-        <span className="usa-hint">Max size: 30 MB | File types: images, .pdf, .docx, .xlsx, .pptx, .doc, .xls, .ppt, .zip, .txt, .csv</span>
+        <Label className="margin-top-0" htmlFor="attachments">
+          Upload any relevant attachments, such as:
+          <ul className="margin-top-0 padding-left-4">
+            <li>meetings agendas</li>
+            <li>services plans</li>
+            <li>sign-in or attendance sheets</li>
+            <li>other items not available online</li>
+          </ul>
+        </Label>
+
+        <span className="usa-hint font-sans-3xs">File types accepted:</span>
+        <br />
+        <span className="usa-hint font-sans-3xs">images, .pdf, .docx, .xlsx, .pptx, .doc, .xls, .ppt, .zip, .txt, .csv (max size 30 MB)</span>
         <Controller
           name="attachments"
           defaultValue={[]}
@@ -102,11 +55,8 @@ SupportingAttachments.propTypes = {
 const ReviewSection = () => {
   const { watch } = useFormContext();
   const {
-    nonECLKCResources,
-    ECLKCResources,
     attachments,
     calculatedStatus,
-    topics: formTopics,
   } = watch();
 
   const hasAttachments = attachments && attachments.length > 0;
@@ -114,48 +64,6 @@ const ReviewSection = () => {
 
   return (
     <>
-      <Section
-        hidePrint={isUndefined(formTopics)}
-        key="Topics covered"
-        basePath="supporting-attachments"
-        anchor="supporting-attachments"
-        title="Topics covered"
-        canEdit={canEdit}
-      >
-        <ReviewItem
-          label="Topics"
-          name="topics"
-          sortValues
-        />
-      </Section>
-      <Section
-        hidePrint={isUndefined(ECLKCResources)}
-        key="OHS / ECLKC resources"
-        basePath="supporting-attachments"
-        anchor="ECLKCResources"
-        title="OHS / ECLKC resources"
-        canEdit={canEdit}
-      >
-        <ReviewItem
-          label="ECLKC resources"
-          name="ECLKCResourcesUsed"
-          path="value"
-        />
-      </Section>
-      <Section
-        hidePrint={isUndefined(nonECLKCResources)}
-        key="Non-ECLKC resources"
-        basePath="supporting-attachments"
-        anchor="nonECLKCResources"
-        title="Non-ECLKC resources"
-        canEdit={canEdit}
-      >
-        <ReviewItem
-          label="Non-ECLKC resources"
-          name="nonECLKCResourcesUsed"
-          path="value"
-        />
-      </Section>
       <Section
         hidePrint={!hasAttachments}
         key="Attachments"
@@ -183,7 +91,7 @@ export default {
   path: 'supporting-attachments',
   reviewSection: () => <ReviewSection />,
   review: false,
-  render: (additionalData, formData, reportId) => (
+  render: (reportId) => (
     <SupportingAttachments
       reportId={reportId}
     />
