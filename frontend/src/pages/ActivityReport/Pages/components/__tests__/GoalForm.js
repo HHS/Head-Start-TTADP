c9ae91e90f51b9643b0e4ab6d484bce83e0babd2
Matt Bevilacqua <thewatermethod@gmail.com>
2022-04-25 15:33:14 -0400

    add some new tests

---
+++ frontend/src/pages/ActivityReport/Pages/components/__tests__/GoalForm.js
@@ -0,0 +1,75 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import '@testing-library/jest-dom';
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+import React from 'react';
+import PropTypes from 'prop-types';
+import fetchMock from 'fetch-mock';
+import { FormProvider, useForm } from 'react-hook-form/dist/index.ie11';
+import { newGoal } from '../GoalPicker';
+import GoalForm from '../GoalForm';
+
+describe('GoalForm', () => {
+  const Form = ({ id }) => {
+    const goal = { ...newGoal(), id };
+
+    const hookForm = useForm({
+      mode: 'onChange',
+      defaultValues: {
+        collaborators: [],
+        author: {
+          role: 'Human spoon',
+        },
+      },
+    });
+
+    return (
+      <FormProvider {...hookForm}>
+        <GoalForm
+          goal={goal}
+          topicOptions={[{ label: 'Coaching', value: 1 }]}
+        />
+      </FormProvider>
+    );
+  };
+
+  Form.propTypes = {
+    id: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.number,
+    ]).isRequired,
+  };
+
+  const renderGoalForm = (id) => {
+    render(<Form id={id} />);
+  };
+
+  beforeEach(async () => fetchMock.restore());
+
+  it('fetches data for existing goals', async () => {
+    const goalId = 123;
+    fetchMock.get(`/api/goals/${goalId}`, {
+      endDate: '',
+      status: '',
+      value: goalId,
+      label: 'Test',
+      id: goalId,
+      name: 'Test',
+      objectives: [],
+    });
+
+    renderGoalForm(goalId);
+    expect(fetchMock.called()).toBe(true);
+  });
+
+  it('displays new goals properly', async () => {
+    const goalId = 'new';
+    renderGoalForm(goalId);
+    expect(fetchMock.called()).toBe(false);
+
+    const endDate = await screen.findByText(/estimated close date/i);
+    expect(endDate).toBeVisible();
+  });
+});
