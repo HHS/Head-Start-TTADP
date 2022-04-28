f4f31997b8036ebf623aaffe7902f77b81ee4822
Matt Bevilacqua <thewatermethod@gmail.com>
2022-04-28 10:58:31 -0400

    update and simplify config

--- frontend/src/components/SocketProvider.js
+++ frontend/src/components/SocketProvider.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 
 export const SocketContext = createContext();
 
-const WS_URL = `wss://${process.env.REACT_APP_BACKEND_PROXY}`;
+const WS_URL = process.env.REACT_APP_WEBSOCKET_URL;
+
 export default function SocketProvider({ children }) {
   const [store, setStore] = useState(null);
 
--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -5,7 +5,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = (app) => {
   app.use('/api',
     createProxyMiddleware({
-      target: process.env.REACT_APP_BACKEND_PROXY,
+      target: process.env.BACKEND_PROXY,
       changeOrigin: true,
     }));
 };
