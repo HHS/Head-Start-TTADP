54295589c972c04d62cf75df64461d01864943e4
Adam Levin <adam.levin@adhocteam.us>
2022-05-02 11:01:18 -0400

    added tests

--- frontend/src/pages/ActivityReport/Pages/__tests__/nextSteps.js
+++ frontend/src/pages/ActivityReport/Pages/__tests__/nextSteps.js
@@ -2,7 +2,7 @@
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
 import {
-  render, screen,
+  render, screen, waitFor, fireEvent,
 } from '@testing-library/react';
 import React from 'react';
 import { FormProvider, useForm } from 'react-hook-form/dist/index.ie11';
@@ -46,12 +46,27 @@ describe('next steps', () => {
     expect(await screen.findByText(/specialist's next steps/i)).toBeVisible();
     expect(await screen.findByText(/what have you agreed to do next\?/i)).toBeVisible();
     expect(await screen.findByTestId(SPECIALIST_INPUT)).toBeVisible();
-    expect(await screen.findByTestId(SPECIALIST_BUTTON)).toBeVisible();
+    expect(screen.queryByText(SPECIALIST_BUTTON)).toBeNull();
 
     expect(await screen.findByText(/recipient's next steps/i)).toBeVisible();
     expect(await screen.findByText(/what has the recipient agreed to do next\?/i)).toBeVisible();
     expect(await screen.findByTestId(RECIPIENT_INPUT)).toBeVisible();
-    expect(await screen.findByTestId(RECIPIENT_BUTTON)).toBeVisible();
+    expect(screen.queryByText(RECIPIENT_BUTTON)).toBeNull();
+  });
+
+  it('displays user can add new steps', async () => {
+    // When a user is on the next steps page
+    renderNextSteps([{ note: 'First Specialist Step', id: 1 }], [{ note: 'First Recipient Step', id: 2 }]);
+
+    // Add new steps.
+    const newStepButtons = screen.queryAllByRole('button', { name: /add next step/i });
+    expect(newStepButtons.length).toBe(2);
+    userEvent.click(newStepButtons[0]);
+    userEvent.click(newStepButtons[1]);
+
+    // Verify new steps are created.
+    const newSteps = screen.queryAllByRole('textbox', { name: /next step 2/i });
+    expect(newSteps.length).toBe(2);
   });
 
   it('can add and delete an entry for specialist', async () => {
@@ -93,5 +108,23 @@ describe('next steps', () => {
     // Then we see there is no more input box
     const input = screen.queryByText(/next step 3/i);
     expect(input).toBeNull();
+  });
+
+  it('can change step for specialist', async () => {
+    renderNextSteps(
+      [{ note: 'Step 1', id: 1 }],
+    );
+    const stepText = await screen.findByRole('textbox', { name: /next step 1/i });
+    fireEvent.change(stepText, { target: { value: 'This is my changed step text.' } });
+    await waitFor(() => expect(stepText).toHaveValue('This is my changed step text.'));
+  });
+
+  it('can change step for recipient', async () => {
+    renderNextSteps(
+      [], [{ note: 'Step 1', id: 2 }],
+    );
+    const stepText = await screen.findByRole('textbox', { name: /next step 1/i });
+    fireEvent.change(stepText, { target: { value: 'This is my changed step text.' } });
+    await waitFor(() => expect(stepText).toHaveValue('This is my changed step text.'));
   });
 });
