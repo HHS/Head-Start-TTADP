08865053401da2479df565696839194700e1e8ec
Matt Bevilacqua <thewatermethod@gmail.com>
2022-05-09 14:21:34 -0400

    trying a different approach

--- frontend/src/components/filter/FilterSelectContainer.js
+++ frontend/src/components/filter/FilterSelectContainer.js
@@ -1,167 +1,7 @@
 /* eslint-disable react/jsx-props-no-spreading */
 /* eslint-disable react/prop-types */
 import React from 'react';
-import { ClassNames } from '@emotion/react';
 // import PropTypes from 'prop-types';
-
-const Svg = ({ size, ...props }) => (
-  <svg
-    height={size}
-    width={size}
-    viewBox="0 0 20 20"
-    aria-hidden="true"
-    focusable="false"
-    css={{
-      display: 'inline-block',
-      fill: 'currentColor',
-      lineHeight: 1,
-      stroke: 'currentColor',
-      strokeWidth: 0,
-    }}
-    {...props}
-  />
-);
-
-export const CrossIcon = (props) => (
-  <Svg size={20} {...props}>
-    <path d="M14.348 14.849c-0.469 0.469-1.229 0.469-1.697 0l-2.651-3.030-2.651 3.029c-0.469 0.469-1.229 0.469-1.697 0-0.469-0.469-0.469-1.229 0-1.697l2.758-3.15-2.759-3.152c-0.469-0.469-0.469-1.228 0-1.697s1.228-0.469 1.697 0l2.652 3.031 2.651-3.031c0.469-0.469 1.228-0.469 1.697 0s0.469 1.229 0 1.697l-2.758 3.152 2.758 3.15c0.469 0.469 0.469 1.229 0 1.698z" />
-  </Svg>
-);
-
-export const multiValueCSS = ({
-  theme: { spacing, borderRadius, colors },
-}) => ({
-  label: 'multiValue',
-  backgroundColor: colors.neutral10,
-  borderRadius: borderRadius / 2,
-  display: 'flex',
-  margin: spacing.baseUnit / 2,
-  minWidth: 0, // resolves flex/text-overflow bug
-});
-
-export const multiValueLabelCSS = ({
-  theme: { borderRadius, colors },
-  cropWithEllipsis,
-}) => ({
-  borderRadius: borderRadius / 2,
-  color: colors.neutral80,
-  fontSize: '85%',
-  overflow: 'hidden',
-  padding: 3,
-  paddingLeft: 6,
-  textOverflow: cropWithEllipsis ? 'ellipsis' : null,
-  whiteSpace: 'nowrap',
-});
-
-export const multiValueRemoveCSS = ({
-  theme: { spacing, borderRadius, colors },
-  isFocused,
-}) => ({
-  alignItems: 'center',
-  borderRadius: borderRadius / 2,
-  backgroundColor: isFocused && colors.dangerLight,
-  display: 'flex',
-  paddingLeft: spacing.baseUnit,
-  paddingRight: spacing.baseUnit,
-  ':hover': {
-    backgroundColor: colors.dangerLight,
-    color: colors.danger,
-  },
-});
-
-export const MultiValueGeneric = ({
-  children,
-  innerProps,
-}) => <div {...innerProps}>{children}</div>;
-
-export const MultiValueContainer = MultiValueGeneric;
-export const MultiValueLabel = MultiValueGeneric;
-
-export function MultiValueRemove({
-  children,
-  innerProps,
-}) {
-  return <div {...innerProps}>{children || <CrossIcon size={14} />}</div>;
-}
-
-const MultiValue = (props) => {
-  const {
-    children,
-    className,
-    components,
-    cx,
-    data,
-    getStyles,
-    innerProps,
-    isDisabled,
-    removeProps,
-    selectProps,
-  } = props;
-
-  const { Container, Label, Remove } = components;
-
-  return (
-    <ClassNames>
-      {({ css, cx: emotionCx }) => (
-        <Container
-          data={data}
-          innerProps={{
-            className: emotionCx(
-              css(getStyles('multiValue', props)),
-              cx(
-                {
-                  'multi-value': true,
-                  'multi-value--is-disabled': isDisabled,
-                },
-                className,
-              ),
-            ),
-            ...innerProps,
-          }}
-          selectProps={selectProps}
-        >
-          <Label
-            data={data}
-            innerProps={{
-              className: emotionCx(
-                css(getStyles('multiValueLabel', props)),
-                cx(
-                  {
-                    'multi-value__label': true,
-                  },
-                  className,
-                ),
-              ),
-            }}
-            selectProps={selectProps}
-          >
-            {children}
-          </Label>
-          <Remove
-            data={data}
-            innerProps={{
-              className: emotionCx(
-                css(getStyles('multiValueRemove', props)),
-                cx(
-                  {
-                    'multi-value__remove': true,
-                  },
-                  className,
-                ),
-              ),
-              ...removeProps,
-            }}
-            selectProps={selectProps}
-          />
-        </Container>
-      )}
-    </ClassNames>
-  );
-};
-
-MultiValue.defaultProps = {
-  cropWithEllipsis: true,
-};
 
 const FilterSelectContainer = (props) => {
   const {
@@ -173,6 +13,7 @@ const FilterSelectContainer = (props) => {
     isDisabled,
     getValue,
     isRtl,
+    selectProps,
   } = props;
 
   const styles = getStyles('container', props);
@@ -181,6 +22,8 @@ const FilterSelectContainer = (props) => {
   const showTruncated = currentSelected.length > 1;
 
   if (showTruncated) {
+    // console.log(currentSelected);
+
     return (
       <div
         style={styles}
@@ -193,7 +36,29 @@ const FilterSelectContainer = (props) => {
         )}
         {...innerProps}
       >
-        {currentSelected.map(({ label }) => <span>{label}</span>)}
+        {currentSelected.map((selection, selectionIndex) => {
+          if (selectionIndex === 0) {
+            const label = selection.label.length > 18
+              ? `${selection.label.slice(0, 10)}...${selection.label.slice(-3)}` : selection.label;
+            return (
+              <span {...props} selectProps={selectProps} key={selection.value}>{label}</span>
+            );
+          }
+
+          if (selectionIndex === 1) {
+            return (
+              <span key={selection.value}>
+                +
+                {' '}
+                {currentSelected.length - 1}
+                {' '}
+                more tags
+              </span>
+            );
+          }
+
+          return null;
+        })}
       </div>
     );
   }
