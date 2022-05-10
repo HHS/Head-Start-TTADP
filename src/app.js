eebfc0b4454254c96eaa1e99d8ff17c7820f6283
Matt Bevilacqua <thewatermethod@gmail.com>
2022-05-10 15:40:10 -0400

    add new url to allowed origins

--- src/app.js
+++ src/app.js
@@ -28,7 +28,7 @@ app.use(helmet({
       'form-action': ["'self'"],
       scriptSrc: ["'self'", 'https://touchpoints.app.cloud.gov/touchpoints/7d519b5e.js'],
       imgSrc: ["'self'", 'data:', 'https://touchpoints.app.cloud.gov'],
-      defaultSrc: ["'self'", 'https://touchpoints.app.cloud.gov/touchpoints/7d519b5e/submissions.json', 'wss://tta-smarthub-sandbox.app.cloud.gov'],
+      defaultSrc: ["'self'", 'https://touchpoints.app.cloud.gov/touchpoints/7d519b5e/submissions.json', 'wss://tta-smarthub-sandbox.app.cloud.gov', 'wss://tta-smarthub-dev.app.cloud.gov'],
     },
   },
 }));
