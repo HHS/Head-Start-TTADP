cdb7713cfc8826d74f50ed53e50e3655b46ec5ae
Matt Bevilacqua <thewatermethod@gmail.com>
2022-05-17 12:53:29 -0400

    uncomment tests

--- frontend/src/pages/ActivityReport/__tests__/index.js
+++ frontend/src/pages/ActivityReport/__tests__/index.js
@@ -1,5 +1,3 @@
-/* eslint-disable jest/no-commented-out-tests */
-/* eslint-disable jest/no-disabled-tests */
 import '@testing-library/jest-dom';
 import reactSelectEvent from 'react-select-event';
 import {
@@ -196,19 +194,19 @@ describe('ActivityReport', () => {
     });
   });
 
-  // describe('updatePage', () => {
-  //   it('navigates to the correct page', async () => {
-  //     fetchMock.post('/api/activity-reports', { id: 1 });
-  //     renderActivityReport('new');
-  //     const button = await screen.findByRole(
-  //       'button', { name: 'Topics and resources Not Started' },
-  //     );
-  //     userEvent.click(button);
-  //     await waitFor(() => expect(history.location.pathname).toEqual(
-  //       '/activity-reports/1/topics-resources',
-  //     ));
-  //   });
-  // });
+  describe('updatePage', () => {
+    it('navigates to the correct page', async () => {
+      fetchMock.post('/api/activity-reports', { id: 1 });
+      renderActivityReport('new');
+      const button = await screen.findByRole(
+        'button', { name: 'Topics and resources Not Started' },
+      );
+      userEvent.click(button);
+      await waitFor(() => expect(history.location.pathname).toEqual(
+        '/activity-reports/1/topics-resources',
+      ));
+    });
+  });
 
   describe('onSave', () => {
     it('calls "report create"', async () => {
@@ -279,16 +277,16 @@ describe('ActivityReport', () => {
       expect(await screen.findByText(/creator:/i)).toBeVisible();
     });
 
-    // it('calls "report update"', async () => {
-    //   fetchMock.get('/api/activity-reports/1', formData());
-    //   fetchMock.put('/api/activity-reports/1', {});
-    //   renderActivityReport(1);
-    // const button = await screen.findByRole(
-    //   'button', { name: 'Topics and resources In Progress' }
-    // );
-    //   userEvent.click(button);
-    //   await waitFor(() => expect(fetchMock.called('/api/activity-reports/1')).toBeTruthy());
-    // });
+    it('calls "report update"', async () => {
+      fetchMock.get('/api/activity-reports/1', formData());
+      fetchMock.put('/api/activity-reports/1', {});
+      renderActivityReport(1);
+      const button = await screen.findByRole(
+        'button', { name: 'Topics and resources In Progress' },
+      );
+      userEvent.click(button);
+      await waitFor(() => expect(fetchMock.called('/api/activity-reports/1')).toBeTruthy());
+    });
 
     it('automatically sets creator role on existing report', async () => {
       const data = formData();
