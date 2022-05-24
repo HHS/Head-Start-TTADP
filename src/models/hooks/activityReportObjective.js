cc7c9d9c1e05c4ecdb8d0fa2719a8d9f3de7565d
GarrettEHill <garretthill@gmail.com>
2022-05-24 13:37:01 -0700

    Remove automatic status change for objectives
    Per conversation with Rachel, the user must control the status of the objectives manually.

--- src/models/hooks/activityReportObjective.js
+++ src/models/hooks/activityReportObjective.js
@@ -1,36 +1,21 @@
 // const { Op } = require('sequelize');
 // import { auditLogger } from '../../logger';
 
-const automaticStatusChangeOnUse = async (sequelize, instance, options) => {
-  const objective = await sequelize.models.Objective.findOne(
-    {
-      where: {
-        id: instance.objectiveId,
-        status: ['Draft', 'Not Started', 'Suspended'],
-      },
-      transaction: options.transaction,
-    },
-  );
-  if (objective) {
-    objective.status = 'In Progress';
-    return objective.save();
-  }
-};
-
+// eslint-disable-next-line no-unused-vars
 const afterCreate = async (sequelize, instance, options) => {
-  await automaticStatusChangeOnUse(sequelize, instance, options);
-};
 
+};
+// eslint-disable-next-line no-unused-vars
 const afterUpdate = async (sequelize, instance, options) => {
-  await automaticStatusChangeOnUse(sequelize, instance, options);
-};
 
+};
+// eslint-disable-next-line no-unused-vars
 const afterUpsert = async (sequelize, instance, options) => {
-  await automaticStatusChangeOnUse(sequelize, instance, options);
+
 };
 
 export {
-  automaticStatusChangeOnUse,
+
   afterCreate,
   afterUpdate,
   afterUpsert,
