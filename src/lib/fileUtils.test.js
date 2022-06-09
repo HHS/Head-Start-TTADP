882e49614d5e6ea3ca2228ef79891526c3896a8f
GarrettEHill <garretthill@gmail.com>
2022-06-09 08:45:02 -0700

    Fixing file utils test

--- src/lib/fileUtils.test.js
+++ src/lib/fileUtils.test.js
@@ -2,7 +2,7 @@ import { fileHash } from './fileUtils';
 
 describe('fileUtils', () => {
   it('fileHash', () => {
-    expect(fileHash('./src/lib/fileUtils.js'))
+    expect(fileHash('./src/lib/fileUtils.test.hash'))
       .toStrictEqual('ce8e0d68940617ff72d4655f78452abaa2c7ca1126986f1670b625550804807e');
     expect(fileHash('bad path')).toStrictEqual(null);
     expect(fileHash('')).toStrictEqual(null);
