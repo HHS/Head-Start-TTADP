56a2f6e634468ca7d45711f47537e43adaee99c0
Matt Bevilacqua <thewatermethod@gmail.com>
2022-06-10 11:46:14 -0400

    face->palm

--- src/lib/fileUtils.test.js
+++ src/lib/fileUtils.test.js
@@ -3,7 +3,7 @@ import { fileHash } from './fileUtils';
 describe('fileUtils', () => {
   it('fileHash', () => {
     expect(fileHash('./src/lib/fileUtils.test.hash'))
-      .toStrictEqual('ce8e0d68940617ff72d4655f78452abaa2c7ca1126986f1670b625550804807egenerateFullName');
+      .toStrictEqual('ce8e0d68940617ff72d4655f78452abaa2c7ca1126986f1670b625550804807e');
     expect(fileHash('bad path')).toStrictEqual(null);
     expect(fileHash('')).toStrictEqual(null);
     expect(fileHash(null)).toStrictEqual(null);
