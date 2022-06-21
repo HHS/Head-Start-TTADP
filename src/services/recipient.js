33bde5484bf7f502dc8e67f7fd5b7cb361070601
Adam Levin <adam.levin@adhocteam.us>
2022-06-21 12:24:15 -0400

    update options passed to scopes filter for perf

--- src/services/recipient.js
+++ src/services/recipient.js
@@ -202,7 +202,9 @@ export async function getGoalsByActivityRecipient(
   },
 ) {
   // Scopes.
-  const { goal: scopes } = filtersToScopes(filters, 'goal');
+  // const { goal: scopes } = filtersToScopes(filters, 'goal');
+  const { goal: scopes } = filtersToScopes(filters, { goal: { recipientId } });
+  // , { grant: { subset: true } }
 
   // Paging.
   const limitNum = parseInt(limit, 10);
