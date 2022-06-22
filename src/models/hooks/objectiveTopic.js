bd300c55b80f3f0c207994523339776843433bf2
Josh Salisbury <josh@adhocteam.us>
2022-06-22 11:50:14 -0500

    Fix some tests

--- src/models/hooks/objectiveTopic.js
+++ src/models/hooks/objectiveTopic.js
@@ -15,7 +15,8 @@ const propagateCreateToTemplate = async (sequelize, instance, options) => {
     ],
     transaction: options.transaction,
   });
-  if (objective.objectiveTemplate.creationMethod === AUTOMATIC_CREATION) { // 'Automatic'
+  if (objective
+    && objective.objectiveTemplate.creationMethod === AUTOMATIC_CREATION) { // 'Automatic'
     const ott = await sequelize.models.ObjectiveTemplateTopic.findOrCreate({
       where: {
         objectiveTemplateId: objective.objectiveTemplateId,
