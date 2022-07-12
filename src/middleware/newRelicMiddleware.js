2a4f8101a0222974c098b1bf0e20bb59863c3661
GarrettEHill <garretthill@gmail.com>
2022-07-11 21:55:55 -0700

    Revert new relic change as it did nothing

--- src/middleware/newRelicMiddleware.js
+++ src/middleware/newRelicMiddleware.js
@@ -2,14 +2,6 @@
 // desired results. These middleware functions were created for cases
 // where we want to separate routes that were automatically grouped.
 
-const fs = require('fs');
-
-let fileContent = fs.readFileSync('./node_modules/newrelic/index.js', { encoding: 'utf-8' });
-
-fileContent = fileContent.replace('lib/config\'', 'lib/config/index\'');
-
-fs.writeFileSync('./node_modules/newrelic/index.js', fileContent, { encoding: 'utf-8', flag: 'w' });
-
 const nr = require('newrelic');
 
 /**
