3d54926f3ab1eb459db320852568ed54819ef7ed
GarrettEHill <garretthill@gmail.com>
2022-07-15 14:36:07 -0700

    Use env to determine db name

--- src/lib/dbMaintenance.js
+++ src/lib/dbMaintenance.js
@@ -1,7 +1,11 @@
 import { QueryTypes } from 'sequelize';
+import config from '../../config/config';
 import { sequelize } from '../models';
+
+const env = process.env.NODE_ENV || 'development';
+const dbName = config[env].database;
 
 export default async function dbMaintenance() {
   await sequelize.query('VACUUM FULL;', { type: QueryTypes.RAW });
-  await sequelize.query('REINDEX DATABASE ttasmarthub;', { type: QueryTypes.RAW });
+  await sequelize.query(`REINDEX DATABASE ${dbName};`, { type: QueryTypes.RAW });
 }
