275acf88416d109c1729803a1466b6af8175b0ff
GarrettEHill <garretthill@gmail.com>
2022-07-15 11:41:29 -0700

    change to QueryTypes.RAW

--- src/lib/dbMaintenance.js
+++ src/lib/dbMaintenance.js
@@ -1,6 +1,7 @@
+import { QueryTypes } from 'sequelize';
 import { sequelize } from '../models';
 
 export default async function dbMaintenance() {
-  await sequelize.query('VACUUM FULL;');
-  await sequelize.query('REINDEX DATABASE;');
+  await sequelize.query('VACUUM FULL;', { type: QueryTypes.RAW });
+  await sequelize.query('REINDEX DATABASE;', { type: QueryTypes.RAW });
 }
