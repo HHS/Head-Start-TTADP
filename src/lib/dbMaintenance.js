f80a6b6aaae6b554dccdbdd878f0dcbf805dfa38
GarrettEHill <garretthill@gmail.com>
2022-07-16 01:23:48 -0700

    Adding additional flags

--- src/lib/dbMaintenance.js
+++ src/lib/dbMaintenance.js
@@ -1,11 +1,26 @@
 import { QueryTypes } from 'sequelize';
 import config from '../../config/config';
 import { sequelize } from '../models';
+import { auditLogger } from '../logger';
 
 const env = process.env.NODE_ENV || 'development';
 const dbName = config[env].database;
 
 export default async function dbMaintenance() {
-  await sequelize.query('VACUUM FULL;', { type: QueryTypes.RAW });
-  await sequelize.query(`REINDEX DATABASE ${dbName};`, { type: QueryTypes.RAW });
+  try {
+    await sequelize.query('VACUUM FULL;', {
+      type: QueryTypes.RAW,
+      raw: true,
+      rawErrors: true,
+      logging: auditLogger.error,
+    });
+    await sequelize.query(`REINDEX DATABASE ${dbName};`, {
+      type: QueryTypes.RAW,
+      raw: true,
+      rawErrors: true,
+      logging: auditLogger.error,
+    });
+  } catch (err) {
+    auditLogger.error(JSON.stringify(err));
+  }
 }
