41700a046d1de44df2cd7128ba1f047e698331ca
Adam Levin <adam.levin@adhocteam.us>
2022-07-18 11:15:45 -0400

    remove unwanted code

--- frontend/src/components/GoalsTable/GoalsTableHeader.js
+++ frontend/src/components/GoalsTable/GoalsTableHeader.js
@@ -38,7 +38,7 @@ export default function GoalsTableHeader({
   const { user } = useContext(UserContext);
   const hasButtonPermissions = canEditOrCreateGoals(user, parseInt(regionId, DECIMAL_BASE));
 
-  const showAddNewButton = hasActiveGrants && hasButtonPermissions && false;
+  const showAddNewButton = hasActiveGrants && hasButtonPermissions;
 
   const onPrint = () => {
     history.push(`/recipient-tta-records/${recipientId}/region/${regionId}/goals-objectives/print`, {
