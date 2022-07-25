6ff63c6fa5e400025ff646097b599b09dc69c848
Krys Wisnaskas <krystyna@adhocteam.us>
2022-07-25 18:17:25 -0400

    Add a migration to remove extra data

---
+++ src/migrations/20220725000000-remove-extra-resources-data.js
@@ -0,0 +1,28 @@
+module.exports = {
+  up: async (queryInterface) => queryInterface.sequelize.transaction(
+    async (transaction) => {
+      const loggedUser = '0';
+      const sessionSig = __filename;
+      const auditDescriptor = 'RUN MIGRATIONS';
+      await queryInterface.sequelize.query(
+        `SELECT
+            set_config('audit.loggedUser', '${loggedUser}', TRUE) as "loggedUser",
+            set_config('audit.transactionId', NULL, TRUE) as "transactionId",
+            set_config('audit.sessionSig', '${sessionSig}', TRUE) as "sessionSig",
+            set_config('audit.auditDescriptor', '${auditDescriptor}', TRUE) as "auditDescriptor";`,
+        { transaction },
+      );
+      await queryInterface.sequelize.query(
+        `UPDATE "ActivityReports" SET "nonECLKCResourcesUsed" =
+          (select array_remove("nonECLKCResourcesUsed", '{"value":""}'))
+          where  '{"value":""}' = ANY ("nonECLKCResourcesUsed");`,
+        { transaction });
+      await queryInterface.sequelize.query(
+        `UPDATE "ActivityReports" SET "ECLKCResourcesUsed" =
+            (select array_remove("ECLKCResourcesUsed", '{"value":""}'))
+            where  '{"value":""}' = ANY ("ECLKCResourcesUsed");`,
+        { transaction });
+    },
+  ),
+  down: async () => { },
+};
