51dbba9e1018b220c53e12753dcb7f98a3d54102
Matt Bevilacqua <thewatermethod@gmail.com>
2022-08-04 13:12:58 -0400

    test new validations

--- frontend/src/pages/ActivityReport/Pages/activitySummary.js
+++ frontend/src/pages/ActivityReport/Pages/activitySummary.js
@@ -557,6 +557,77 @@ const ReviewSection = () => {
   );
 };
 
+export const isPageComplete = (formData, formState) => {
+  const { isValid } = formState;
+  if (isValid) {
+    return true;
+  }
+
+  const {
+    // strings
+    activityRecipientType,
+    requester,
+    deliveryMethod,
+    virtualDeliveryType,
+
+    // arrays
+    activityRecipients,
+    targetPopulations: targetPopulationsArray,
+    reason,
+    ttaType,
+    participants,
+
+    // numbers
+    duration,
+    numberOfParticipants,
+
+    // dates
+    startDate,
+    endDate,
+  } = formData;
+
+  const stringsToValidate = [
+    activityRecipientType,
+    requester,
+    deliveryMethod,
+  ];
+
+  if (!stringsToValidate.every((str) => str)) {
+    return false;
+  }
+
+  const arraysToValidate = [
+    activityRecipients,
+    targetPopulationsArray,
+    reason,
+    ttaType,
+    participants,
+  ];
+
+  if (!arraysToValidate.every((arr) => arr.length)) {
+    return false;
+  }
+
+  const numbersToValidate = [
+    duration,
+    numberOfParticipants,
+  ];
+
+  if (!numbersToValidate.every((num) => num && Number.isNaN(num) === false)) {
+    return false;
+  }
+
+  if (![startDate, endDate].every((date) => moment(date, 'MM/DD/YYYY').isValid())) {
+    return false;
+  }
+
+  if (deliveryMethod === 'virtual' && !virtualDeliveryType) {
+    return false;
+  }
+
+  return true;
+};
+
 export default {
   position: 1,
   label: 'Activity summary',
@@ -572,71 +643,5 @@ export default {
       />
     );
   },
-  isPageComplete: (formData, formState) => {
-    const { isValid } = formState;
-    if (isValid) {
-      return true;
-    }
-
-    const {
-      // strings
-      activityRecipientType,
-      requester,
-      deliveryMethod,
-      virtualDeliveryType,
-
-      // arrays
-      activityRecipients,
-      targetPopulations: targetPopulationsArray,
-      reason,
-      ttaType,
-      participants,
-
-      // numbers
-      duration,
-      numberOfParticipants,
-
-      // dates
-      startDate,
-      endDate,
-    } = formData;
-
-    const stringsToValidate = [
-      activityRecipientType,
-      requester,
-      deliveryMethod,
-      virtualDeliveryType,
-    ];
-
-    if (!stringsToValidate.every((str) => str)) {
-      return false;
-    }
-
-    const arraysToValidate = [
-      activityRecipients,
-      targetPopulationsArray,
-      reason,
-      ttaType,
-      participants,
-    ];
-
-    if (!arraysToValidate.every((arr) => arr.length)) {
-      return false;
-    }
-
-    const numbersToValidate = [
-      duration,
-      numberOfParticipants,
-    ];
-
-    if (!numbersToValidate.every((num) => num && Number.isNaN(num) === false)) {
-      return false;
-    }
-
-    if (![startDate, endDate].every((date) => moment(date, 'MM/DD/YYYY').isValid())) {
-      return false;
-    }
-
-    return true;
-  },
+  isPageComplete,
 };
