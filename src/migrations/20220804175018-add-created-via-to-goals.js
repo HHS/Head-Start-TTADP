b6827f1eec8c6aad52e40db2ee15c90b329fa571
Matt Bevilacqua <thewatermethod@gmail.com>
2022-08-05 09:06:38 -0400

    disable audit logging in migration

--- src/migrations/20220804175018-add-created-via-to-goals.js
+++ src/migrations/20220804175018-add-created-via-to-goals.js
@@ -3,6 +3,18 @@ module.exports = {
   up: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction(async (transaction) => {
     const createdViaSources = ['imported', 'activityReport', 'rtr'];
 
+    const loggedUser = '0';
+    const sessionSig = __filename;
+    const auditDescriptor = 'RUN MIGRATIONS';
+    await queryInterface.sequelize.query(
+      `SELECT
+          set_config('audit.loggedUser', '${loggedUser}', TRUE) as "loggedUser",
+          set_config('audit.transactionId', NULL, TRUE) as "transactionId",
+          set_config('audit.sessionSig', '${sessionSig}', TRUE) as "sessionSig",
+          set_config('audit.auditDescriptor', '${auditDescriptor}', TRUE) as "auditDescriptor";`,
+      { transaction },
+    );
+
     await queryInterface.addColumn(
       'Goals',
       'createdVia',
