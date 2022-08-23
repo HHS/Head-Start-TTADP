7e81fee01e0fea89ba930ce80d81bce133dc913e
nvms <pyersjonathan@gmail.com>
2022-08-23 10:30:15 -0600

    Create `UserValidationStatus` model.

---
+++ src/models/userValidationStatus.js
@@ -0,0 +1,26 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class UserValidationStatus extends Model {
+    static associate(models) {
+      UserValidationStatus.belongsTo(models.User, { foreignKey: 'userId' });
+    }
+  }
+
+  UserValidationStatus.init(
+    {
+      userId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: { model: { tableName: 'users' }, key: 'id' },
+        onUpdate: 'CASCADE',
+      },
+      token: { type: DataTypes.STRING, allowNull: false },
+      type: { type: DataTypes.STRING, allowNull: false },
+      validatedAt: { allowNull: true, type: DataTypes.DATE },
+    },
+    { sequelize, modelName: 'UserValidationStatus' },
+  );
+
+  return UserValidationStatus;
+};
