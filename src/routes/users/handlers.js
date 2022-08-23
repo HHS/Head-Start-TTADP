69954228de9ce682b508a14d04df15586e0a8f4c
nvms <pyersjonathan@gmail.com>
2022-08-23 15:01:52 -0600

    Add verification routes, email templates, etc.

--- src/routes/users/handlers.js
+++ src/routes/users/handlers.js
@@ -4,6 +4,8 @@ import { userById, usersWithPermissions } from '../../services/users';
 import handleErrors from '../../lib/apiErrorHandler';
 import { DECIMAL_BASE } from '../../constants';
 import { statesByGrantRegion } from '../../services/grant';
+import { createAndStoreVerificationToken, validateVerificationToken } from '../../services/token';
+import { sendEmailVerificationRequestWithToken } from '../../lib/mailer';
 
 export async function getPossibleCollaborators(req, res) {
   try {
@@ -28,6 +30,38 @@ export async function getPossibleStateCodes(req, res) {
     const regions = user.permissions.map((permission) => permission.regionId);
     const stateCodes = await statesByGrantRegion(regions);
     res.json(stateCodes);
+  } catch (error) {
+    await handleErrors(req, res, error, { namespace: 'SERVICE:USER' });
+  }
+}
+
+export async function requestVerificationEmail(req, res) {
+  try {
+    const user = await userById(req.session.userId);
+    const token = await createAndStoreVerificationToken(req.session.userId, 'email');
+
+    await sendEmailVerificationRequestWithToken(user, token);
+    res.sendStatus(200);
+  } catch (error) {
+    await handleErrors(req, res, error, { namespace: 'SERVICE:USER' });
+  }
+}
+
+export async function verifyEmailToken(req, res) {
+  const { token } = req.params;
+
+  if (!token || !req.session.userId) {
+    res.sendStatus(400);
+    return;
+  }
+
+  try {
+    const validated = await validateVerificationToken(req.session.userId, token, 'email');
+    if (!validated) {
+      res.sendStatus(403);
+      return;
+    }
+    res.sendStatus(200);
   } catch (error) {
     await handleErrors(req, res, error, { namespace: 'SERVICE:USER' });
   }
