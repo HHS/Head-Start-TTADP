0815a408b181eeaeb48e05eb5e34299fc092b424
Matt Bevilacqua <thewatermethod@gmail.com>
2022-08-30 14:48:10 -0400

    Remove logs from file

--- frontend/src/components/FileUploader/ObjectiveFileUploader.js
+++ frontend/src/components/FileUploader/ObjectiveFileUploader.js
@@ -33,13 +33,9 @@ const ObjectiveFileUploader = ({
   };
 
   const handleDrop = async (e) => {
-    console.log({ e });
-
     const newFiles = await Promise.all(
       e.map((file) => upload(file, objective, setError, index)),
     );
-
-    console.log({ newFiles });
 
     let objectives;
     let setObjectives;
