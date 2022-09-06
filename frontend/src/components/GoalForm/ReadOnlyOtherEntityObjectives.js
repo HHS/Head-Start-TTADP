86c260187eb0e8cdc1f3b675f093e537e68155d5
Adam Levin <adam.levin@adhocteam.us>
2022-09-06 09:51:37 -0400

    more tests

--- frontend/src/components/GoalForm/ReadOnlyOtherEntityObjectives.js
+++ frontend/src/components/GoalForm/ReadOnlyOtherEntityObjectives.js
@@ -10,6 +10,7 @@ export default function ReadOnlyOtherEntityObjectives({
   objectives,
 }) {
   let menuItems;
+  console.log('Hide Edit Value: ', hideEdit);
   if (!hideEdit) {
     menuItems = [
       {
--- frontend/src/components/GoalForm/__tests__/ReadOnlyOtherEntityObjectives.js
+++ frontend/src/components/GoalForm/__tests__/ReadOnlyOtherEntityObjectives.js
@@ -1,8 +1,9 @@
 import '@testing-library/jest-dom';
 import React from 'react';
 import {
-  render, screen,
+  render, screen, act,
 } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import ReadOnlyOtherEntityObjectives from '../ReadOnlyOtherEntityObjectives';
 
 const createdObjectives = [{
@@ -28,16 +29,16 @@ const createdObjectives = [{
 
 // eslint-disable-next-line react/prop-types
 const RenderReadOnlyObjective = (onEdit = jest.fn(), hideEdit = false) => (
-  <ReadOnlyOtherEntityObjectives
+  render(<ReadOnlyOtherEntityObjectives
     onEdit={onEdit}
     objectives={createdObjectives}
     hideEdit={hideEdit}
-  />
+  />)
 );
 
 describe('ReadOnlyObjective', () => {
   it('can render with objectives', async () => {
-    render(<RenderReadOnlyObjective />);
+    RenderReadOnlyObjective();
     expect(screen.queryAllByRole('heading', { name: /objective summary/i }).length).toBe(2);
     expect(await screen.findByText('Sample Objective 1')).toBeVisible();
     expect(await screen.findByText('test1.txt')).toBeVisible();
@@ -48,7 +49,17 @@ describe('ReadOnlyObjective', () => {
   });
 
   it('hides the context menu', async () => {
-    render(<RenderReadOnlyObjective hideEdit />);
+    render(jest.fn(), true);
     expect(screen.queryAllByRole('button', { name: /actions for objectives/i }).length).toBe(0);
+  });
+
+  it('edits the objective', async () => {
+    const editObjective = jest.fn();
+    RenderReadOnlyObjective(editObjective);
+    const context = await screen.findByRole('button', { name: /actions for objectives/i });
+    userEvent.click(context);
+    const [button] = await screen.findAllByRole('button', { name: 'Edit' });
+    act(() => userEvent.click(button));
+    expect(editObjective).toHaveBeenCalled();
   });
 });
