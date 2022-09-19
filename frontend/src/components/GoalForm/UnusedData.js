11a29de118c4403858ff4f48e645e475df64789a
Matt Bevilacqua <thewatermethod@gmail.com>
2022-09-19 13:52:14 -0400

    properly style unused data

--- frontend/src/components/GoalForm/UnusedData.js
+++ frontend/src/components/GoalForm/UnusedData.js
@@ -2,11 +2,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBan } from '@fortawesome/free-solid-svg-icons';
+import colors from '../../colors';
+import './UnusedData.scss';
 
 export default function UnusedData({ key, value }) {
   return (
-    <li key={key}>
-      <FontAwesomeIcon icon={faBan} />
+    <li key={key} className="ttahub-objective-list-item--unused-data">
+      <FontAwesomeIcon icon={faBan} color={colors.baseDark} className="padding-right-1" />
       {value}
     </li>
   );
