f06795a24a8d673748caf3946186c9fa0b8312bf
Krys Wisnaskas <krystyna@adhocteam.us>
2022-09-23 15:37:27 -0400

    Fix incorrect arguments

--- src/app.js
+++ src/app.js
@@ -119,8 +119,8 @@ const runDailyEmailJob = () => {
     try {
       await collaboratorDigest(EMAIL_DIGEST_FREQ.DAILY, DIGEST_SUBJECT_FREQ.DAILY);
       await changesRequestedDigest(EMAIL_DIGEST_FREQ.DAILY, DIGEST_SUBJECT_FREQ.DAILY);
-      await submittedDigest(EMAIL_DIGEST_FREQ.DAILY.DIGEST_SUBJECT_FREQ.DAILY);
-      await approvedDigest(EMAIL_DIGEST_FREQ.DAILY.DIGEST_SUBJECT_FREQ.DAILY);
+      await submittedDigest(EMAIL_DIGEST_FREQ.DAILY, DIGEST_SUBJECT_FREQ.DAILY);
+      await approvedDigest(EMAIL_DIGEST_FREQ.DAILY, DIGEST_SUBJECT_FREQ.DAILY);
     } catch (error) {
       auditLogger.error(`Error processing Daily Email Digest job: ${error}`);
       logger.error(`Daily Email Digest Error: ${error.stack}`);
