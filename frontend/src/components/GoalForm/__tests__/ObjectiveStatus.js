fb086c6a81aebbeab9c0ab169875cc25779c25f3
Matt Bevilacqua <thewatermethod@gmail.com>
2022-09-23 09:21:21 -0400

    add status tests

--- frontend/src/components/GoalForm/__tests__/ObjectiveStatus.js
+++ frontend/src/components/GoalForm/__tests__/ObjectiveStatus.js
@@ -7,7 +7,7 @@ import {
 import ObjectiveStatus from '../ObjectiveStatus';
 
 describe('ObjectiveStatus', () => {
-  it('shows the read only view', async () => {
+  it('shows the dropdown', async () => {
     const onChangeStatus = jest.fn();
 
     render(<ObjectiveStatus
@@ -21,7 +21,47 @@ describe('ObjectiveStatus', () => {
     const dropdown = await screen.findByLabelText('Objective status');
     expect(dropdown).toBeVisible();
 
+    const options = screen.getAllByRole('option');
+    expect(options).toHaveLength(3);
+
     userEvent.selectOptions(dropdown, 'Completed');
     expect(onChangeStatus).toHaveBeenCalledWith('Completed');
+  });
+
+  it('shows the correct options for completed', async () => {
+    const onChangeStatus = jest.fn();
+
+    render(<ObjectiveStatus
+      status="Completed"
+      goalStatus="In Progress"
+      onChangeStatus={onChangeStatus}
+      inputName="objective-status"
+      isOnReport={false}
+    />);
+
+    const dropdown = await screen.findByLabelText('Objective status');
+    expect(dropdown).toBeVisible();
+
+    const options = screen.getAllByRole('option');
+    expect(options).toHaveLength(2);
+  });
+
+  it('shows the read only view when the goal is closed', async () => {
+    const onChangeStatus = jest.fn();
+
+    render(<ObjectiveStatus
+      status="Completed"
+      goalStatus="Closed"
+      onChangeStatus={onChangeStatus}
+      inputName="objective-status"
+      isOnReport={false}
+    />);
+
+    const label = await screen.findByText('Objective status');
+
+    expect(label).toBeVisible();
+    expect(label.tagName).toEqual('P');
+
+    expect(document.querySelector('select')).toBe(null);
   });
 });
