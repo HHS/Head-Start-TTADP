aa934c65b617c86acdf4381b9f028974a7b810a2
Krys Wisnaskas <krystyna@adhocteam.us>
2022-09-26 12:22:54 -0400

    Remove tmp schedule

--- src/app.js
+++ src/app.js
@@ -92,13 +92,10 @@ if (process.env.NODE_ENV === 'production') {
 // Set timing parameters.
 // Run at 4 am ET
 const schedule = '0 4 * * *';
-// const dailyEmailDigestSchedule = '*/10 * * * * *';
 // Run daily at 4 pm
-// const dailySched = '0 16 * * *';
-// const dailySched = '*/10 * * * * *';
-const tmpSched = '0 * * * *'; // every hour
+const dailySched = '0 16 * * *';
 // Run at 4 pm every Friday
-// const weeklySched = '0 16 * * 5';
+const weeklySched = '0 16 * * 5';
 // Run at 4 pm on the last of the month
 const monthlySched = '0 16 30 * *';
 const timezone = 'America/New_York';
@@ -165,9 +162,9 @@ const runMonthlyEmailJob = () => {
 if (process.env.CF_INSTANCE_INDEX === '0' && process.env.NODE_ENV === 'production') {
   const job = new CronJob(schedule, () => runJob(), null, true, timezone);
   job.start();
-  const dailyJob = new CronJob(tmpSched, () => runDailyEmailJob(), null, true, timezone);
+  const dailyJob = new CronJob(dailySched, () => runDailyEmailJob(), null, true, timezone);
   dailyJob.start();
-  const weeklyJob = new CronJob(tmpSched, () => runWeeklyEmailJob(), null, true, timezone);
+  const weeklyJob = new CronJob(weeklySched, () => runWeeklyEmailJob(), null, true, timezone);
   weeklyJob.start();
   const monthlyJob = new CronJob(monthlySched, () => runMonthlyEmailJob(), null, true, timezone);
   monthlyJob.start();
