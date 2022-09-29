95d3a8977dd3d1118c349fcf5a521ae67fa51e51
nvms <pyersjonathan@gmail.com>
2022-09-29 16:20:03 -0700

    Fix imports.

--- frontend/src/components/__tests__/Logout.js
+++ frontend/src/components/__tests__/Logout.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render } from '@testing-library/react';
 import UserContext from '../../UserContext';
 import Logout from '../../pages/Logout';
--- frontend/src/pages/Logout/index.js
+++ frontend/src/pages/Logout/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext } from 'react';
+import { useEffect, useContext } from 'react';
 import UserContext from '../../UserContext';
 
 function Logout() {
