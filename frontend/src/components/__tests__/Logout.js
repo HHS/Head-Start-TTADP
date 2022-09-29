43f2a8e2bf25fd584245db71dedc32d34224294a
nvms <pyersjonathan@gmail.com>
2022-09-29 15:02:33 -0700

    Add test.

---
+++ frontend/src/components/__tests__/Logout.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { render } from '@testing-library/react';
+import UserContext from '../../UserContext';
+import Logout from '../../pages/Logout';
+
+describe('Logout', () => {
+  const logout = jest.fn();
+
+  const renderLogout = () => {
+    render(
+      <UserContext.Provider value={{ logout }}>
+        <Logout />
+      </UserContext.Provider>,
+    );
+  };
+
+  it('renders the logout page and calls logout from context', () => {
+    renderLogout();
+    expect(logout).toHaveBeenCalled();
+  });
+});
