96d90b8549d292fa2f55e25d75552d8304e0ea4a
nvms <pyersjonathan@gmail.com>
2022-09-29 16:07:26 -0700

    Remove unused.

--- frontend/src/components/__tests__/Logout.js
+++ frontend/src/components/__tests__/Logout.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import '@testing-library/jest-dom';
 import { render } from '@testing-library/react';
 import UserContext from '../../UserContext';
 import Logout from '../../pages/Logout';
