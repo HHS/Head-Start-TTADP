f97cec356724e5554c56a000a8588da688ddc33a
Matt Bevilacqua <thewatermethod@gmail.com>
2022-10-17 13:53:46 -0400

    add new url input component

---
+++ frontend/src/components/URLInput.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { TextInput } from '@trussworks/react-uswds';
+
+export default function URLInput({
+  onChange, onBlur, id, value, disabled,
+}) {
+  return (
+    <TextInput
+      id={id}
+      onBlur={onBlur}
+      type="url"
+      placeholder="https://"
+      onChange={onChange}
+      value={value}
+      disabled={disabled}
+      pattern="^(?:https?:\/\/)(?:[\w]+\.)(?:\.?[\w]{2,})"
+    />
+  );
+}
+
+URLInput.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  onBlur: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  disabled: PropTypes.bool,
+};
+
+URLInput.defaultProps = {
+  disabled: false,
+};
