a0244583624ab802c8ca42be0515798b6c363d3c
GarrettEHill <garretthill@gmail.com>
2022-10-20 08:39:34 -0700

    Add a global try catch within the transaction with rollback

--- src/routes/transactionWrapper.js
+++ src/routes/transactionWrapper.js
@@ -1,9 +1,22 @@
 import { sequelize } from '../models';
+import handleErrors from '../lib/apiErrorHandler';
+
+const namespace = 'SERVICE:WRAPPER';
+
+const logContext = {
+  namespace,
+};
 
 export default function transactionWrapper(originalFunction) {
   return async function wrapper(req, res, next) {
-    return sequelize.transaction(async () => {
-      const result = await originalFunction(req, res, next);
+    return sequelize.transaction(async (t) => {
+      let result;
+      try {
+        result = await originalFunction(req, res, next);
+      } catch (err) {
+        t.rollback();
+        await handleErrors(req, res, err, logContext);
+      }
       return result;
     });
   };
