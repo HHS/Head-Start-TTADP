12089695606b48eef525f8858eb02ad1177fdff8
Matt Bevilacqua <thewatermethod@gmail.com>
2022-11-02 16:03:40 -0400

    format SQL for readability

--- src/scopes/goals/topics.js
+++ src/scopes/goals/topics.js
@@ -3,9 +3,20 @@ import { sequelize } from '../../models';
 
 const topicFilter = (topics, options) => {
   const useRecipient = options && options.recipientId;
-  return `(SELECT DISTINCT "Goal".id FROM "ActivityReports" ar INNER JOIN "ActivityReportGoals" arg ON ar.id = arg."activityReportId" INNER JOIN "Goals" "Goal" ON arg."goalId" = "Goal".id INNER JOIN "Grants" gr ON "Goal"."grantId" = gr."id" WHERE ${useRecipient ? `gr."recipientId" = ${sequelize.escape(options.recipientId)} AND ` : ''} ar."topics" && (ARRAY[${topics.map((t) => sequelize.escape(t)).join(',')}::varchar])
+  return `(
+          SELECT DISTINCT "Goal".id FROM "ActivityReports" ar 
+            INNER JOIN "ActivityReportGoals" arg ON ar.id = arg."activityReportId" 
+            INNER JOIN "Goals" "Goal" ON arg."goalId" = "Goal".id 
+            INNER JOIN "Grants" gr ON "Goal"."grantId" = gr."id" 
+            WHERE ${useRecipient ? `gr."recipientId" = ${sequelize.escape(options.recipientId)} AND ` : ''} 
+            ar."topics" && (ARRAY[${topics.map((t) => sequelize.escape(t)).join(',')}::varchar])
           UNION ALL
-          SELECT DISTINCT "Goal"."id" FROM "Objectives" "Objectives" INNER JOIN "ObjectiveTopics" "ObjectiveTopics" ON "Objectives"."id" = "ObjectiveTopics"."objectiveId" INNER JOIN "Topics" "Topics" ON "ObjectiveTopics"."topicId" = "Topics"."id" INNER JOIN "Goals" "Goal" ON "Objectives"."goalId" = "Goal"."id" WHERE "Topics"."name" IN (${topics.map((t) => sequelize.escape(t)).join(',')}))`;
+          SELECT DISTINCT "Goal"."id" FROM "Objectives" "Objectives" 
+            INNER JOIN "ObjectiveTopics" "ObjectiveTopics" ON "Objectives"."id" = "ObjectiveTopics"."objectiveId" 
+            INNER JOIN "Topics" "Topics" ON "ObjectiveTopics"."topicId" = "Topics"."id" 
+            INNER JOIN "Goals" "Goal" ON "Objectives"."goalId" = "Goal"."id" 
+            WHERE "Topics"."name" IN (${topics.map((t) => sequelize.escape(t)).join(',')})
+        )`;
 };
 
 export function withTopics(topics, options) {
