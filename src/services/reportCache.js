c1d298c2e6e0c3420212bce081563b00a6e55294
Adam Levin <adam.levin@adhocteam.us>
2022-11-06 16:49:50 -0500

    fix status save

--- src/services/reportCache.js
+++ src/services/reportCache.js
@@ -54,7 +54,7 @@ const cacheTopics = async (activityReportObjectiveId, topics = []) => Promise.al
 
 const cacheObjectiveMetadata = async (objective, reportId, metadata) => {
   const {
-    files, resources, topics, ttaProvided, status,
+    files, resources, topics, ttaProvided,
   } = metadata;
   const objectiveId = objective.id;
   const [aro] = await ActivityReportObjective.findOrCreate({
@@ -67,7 +67,7 @@ const cacheObjectiveMetadata = async (objective, reportId, metadata) => {
   return Promise.all([
     await ActivityReportObjective.update({
       title: objective.title,
-      status,
+      status: objective.status,
       ttaProvided,
     }, {
       where: { id: activityReportObjectiveId },
