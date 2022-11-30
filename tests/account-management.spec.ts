c15a8d6e89ef60ede23aaf4ba03d3eadc596b8f2
nvms <pyersjonathan@gmail.com>
2022-11-30 10:51:51 -0700

    gate this test

--- tests/account-management.spec.ts
+++ tests/account-management.spec.ts
@@ -1,29 +1,32 @@
-// import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-// test.describe('Account Management', () => {
-//   /**
-//    * There are apparently known problems with trying to send emails from containers in CircleCI.
-//    * See: https://support.circleci.com/hc/en-us/articles/360007444314-Troubleshooting-sending-email-s-from-a-container
-//    */
-//   test('can verify email address', async ({ page }) => {
-//     await page.goto('/');
-//     await page.getByTestId('header-avatar').click();
-//     await page.getByRole('link', { name: 'Account Management' }).click();
+test.describe('Account Management', () => {
+  /**
+   * There are apparently known problems with trying to send emails from containers in CircleCI.
+   * See: https://support.circleci.com/hc/en-us/articles/360007444314-Troubleshooting-sending-email-s-from-a-container
+   * I think this test still provides value, so we just skip it if we're running in CI.
+   */
+  if (process.env.CI) return;
 
-//     await page.getByTestId('send-verification-email-button').click();
-//     await page.keyboard.press('Enter');
-//     await page.getByText('Verification email sent');
+  test('can verify email address', async ({ page }) => {
+    await page.goto('/');
+    await page.getByTestId('header-avatar').click();
+    await page.getByRole('link', { name: 'Account Management' }).click();
 
-//     const page1 = await page.context().newPage();
-//     await page1.goto('http://localhost:1080/');
-//     await page1.locator('#messages > table > tbody > tr:nth-child(1)').click();
-//     await page1.waitForTimeout(1_000);
+    await page.getByTestId('send-verification-email-button').click();
+    await page.keyboard.press('Enter');
+    await page.getByText('Verification email sent');
 
-//     const [page2] = await Promise.all([
-//       page1.waitForEvent('popup'),
-//       page1.frameLocator('iframe').getByRole('link', { name: /http:\/\/localhost:3000\/account\/verify*/ }).click()
-//     ]);
+    const page1 = await page.context().newPage();
+    await page1.goto('http://localhost:1080/');
+    await page1.locator('#messages > table > tbody > tr:nth-child(1)').click();
+    await page1.waitForTimeout(1_000);
 
-//     expect(await page2.getByText('Your email has been verified!')).toBeTruthy();
-//   });
-// });
+    const [page2] = await Promise.all([
+      page1.waitForEvent('popup'),
+      page1.frameLocator('iframe').getByRole('link', { name: /http:\/\/localhost:3000\/account\/verify*/ }).click()
+    ]);
+
+    expect(await page2.getByText('Your email has been verified!')).toBeTruthy();
+  });
+});
