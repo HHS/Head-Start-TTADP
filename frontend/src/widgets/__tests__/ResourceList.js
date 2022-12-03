2363d8a1d6b2c3fdda57f5dc5b4b3205a554a247
GarrettEHill <garretthill@gmail.com>
2022-12-02 23:18:34 -0800

    Update ResourceList.js

--- frontend/src/widgets/__tests__/ResourceList.js
+++ frontend/src/widgets/__tests__/ResourceList.js
@@ -16,45 +16,50 @@ describe('Resource List Widget', () => {
     renderResourceList(data);
 
     expect(screen.getByText(/resources in activity reports/i)).toBeInTheDocument();
-    expect(screen.getByRole('columnheader', { name: /resource/i })).toBeInTheDocument(2);
+    expect(screen.getByRole('columnheader', { name: /resource/i })).toBeInTheDocument(3);
     expect(screen.getByRole('columnheader', { name: /number of activities/i })).toBeInTheDocument();
+    expect(screen.getByRole('columnheader', { name: /number of recipients/i })).toBeInTheDocument();
   });
 
   it('renders correctly with data', async () => {
     const data = [
-      { name: 'Resource one', count: 4 },
-      { name: 'Resource two', count: 2 },
+      { name: 'Resource one', reportCount: 4, recipientCount: 2 },
+      { name: 'Resource two', reportCount: 2, recipientCount: 2 },
     ];
     renderResourceList(data);
 
     expect(screen.getByText(/resources in activity reports/i)).toBeInTheDocument();
-    expect(screen.getByRole('columnheader', { name: /resource/i })).toBeInTheDocument(2);
+    expect(screen.getByRole('columnheader', { name: /resource/i })).toBeInTheDocument(3);
     expect(screen.getByRole('columnheader', { name: /number of activities/i })).toBeInTheDocument();
+    expect(screen.getByRole('columnheader', { name: /number of recipients/i })).toBeInTheDocument();
     expect(screen.getByRole('cell', { name: /resource one/i })).toBeInTheDocument();
     expect(screen.getByRole('cell', { name: /4/i })).toBeInTheDocument();
+    expect(screen.getByRole('cell', { name: /2/i })).toBeInTheDocument();
     expect(screen.getByRole('cell', { name: /resource two/i })).toBeInTheDocument();
+    expect(screen.getByRole('cell', { name: /2/i })).toBeInTheDocument();
     expect(screen.getByRole('cell', { name: /2/i })).toBeInTheDocument();
   });
 
   it('renders large resource and count', async () => {
     const data = [
-      { name: 'resource one', count: 10 },
-      { name: 'resource two', count: 9 },
-      { name: 'resource three', count: 8 },
-      { name: 'resource four', count: 7 },
-      { name: 'resource five', count: 6 },
-      { name: 'resource six', count: 5 },
-      { name: 'resource seven', count: 4 },
-      { name: 'resource 8', count: 3 },
-      { name: 'resource 9', count: 2 },
-      { name: 'resource 10 is a very very very long resource and should not cut off the text', count: '999,999' },
+      { name: 'resource one', reportCount: 10, recipientCount: 1 },
+      { name: 'resource two', reportCount: 9, recipientCount: 2 },
+      { name: 'resource three', reportCount: 8, recipientCount: 3 },
+      { name: 'resource four', reportCount: 7, recipientCount: 4 },
+      { name: 'resource five', reportCount: 6, recipientCount: 5 },
+      { name: 'resource six', reportCount: 5, recipientCount: 6 },
+      { name: 'resource seven', reportCount: 4, recipientCount: 7 },
+      { name: 'resource 8', reportCount: 3, recipientCount: 8 },
+      { name: 'resource 9', reportCount: 2, recipientCount: 9 },
+      { name: 'resource 10 is a very very very long resource and should not cut off the text', reportCount: '999,999', recipientCount: '888,888' },
     ];
     renderResourceList(data);
 
     expect(screen.getByText(/resources in activity reports/i)).toBeInTheDocument();
-    expect(screen.getByRole('columnheader', { name: /resource/i })).toBeInTheDocument(2);
+    expect(screen.getByRole('columnheader', { name: /resource/i })).toBeInTheDocument(3);
     expect(screen.getByRole('columnheader', { name: /number of activities/i })).toBeInTheDocument();
     expect(screen.getByRole('cell', { name: /resource 10 is a very very very long resource and should not cut off the text/i })).toBeInTheDocument();
     expect(screen.getByRole('cell', { name: /999,999/i })).toBeInTheDocument();
+    expect(screen.getByRole('cell', { name: /888,888/i })).toBeInTheDocument();
   });
 });
