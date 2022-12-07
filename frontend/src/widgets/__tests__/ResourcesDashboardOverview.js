334a52fb391368b17e9643b94a0f1215b096f3c7
GarrettEHill <garretthill@gmail.com>
2022-12-07 00:05:04 -0800

    Update ResourcesDashboardOverview.js

--- frontend/src/widgets/__tests__/ResourcesDashboardOverview.js
+++ frontend/src/widgets/__tests__/ResourcesDashboardOverview.js
@@ -110,18 +110,18 @@ describe('Resource Dashboard Overview Widget', () => {
 
     renderResourcesDashboardOverview({ data });
 
-    expect(screen.getByText(/^[ \t]*Recipient rec'd resources\r?\n?[ \t]*220 of 231/i)).toBeInTheDocument();
-    expect(screen.getByText(/^[ \t]*Recipient rec'd ECLKC resources\r?\n?[ \t]*219 of 231/i)).toBeInTheDocument();
-    expect(screen.getByText(/^[ \t]*Recipient rec'd non-ECLKC resources\r?\n?[ \t]*83 of 231/i)).toBeInTheDocument();
-    expect(screen.getByText(/^[ \t]*Recipient rec'd no resources\r?\n?[ \t]*11 of 231/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*Recipient rec'd resources\r?\n?[ \t]*1 of 231/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*Recipient rec'd ECLKC resources\r?\n?[ \t]*1 of 231/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*Recipient rec'd non-ECLKC resources\r?\n?[ \t]*1 of 231/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*Recipient rec'd no resources\r?\n?[ \t]*1 of 231/i)).toBeInTheDocument();
 
-    expect(screen.getByText(/^[ \t]*Report include resources\r?\n?[ \t]*661 of 1,721/i)).toBeInTheDocument();
-    expect(screen.getByText(/^[ \t]*Report include ECLKC resources\r?\n?[ \t]*634 of 1,721/i)).toBeInTheDocument();
-    expect(screen.getByText(/^[ \t]*Report include non-ECLKC resources\r?\n?[ \t]*101 of 1,721/i)).toBeInTheDocument();
-    expect(screen.getByText(/^[ \t]*Report include no resources\r?\n?[ \t]*1,060 of 1,721/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*Report include resources\r?\n?[ \t]*1 of 1,721/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*Report include ECLKC resources\r?\n?[ \t]*1 of 1,721/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*Report include non-ECLKC resources\r?\n?[ \t]*1 of 1,721/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*Report include no resources\r?\n?[ \t]*1 of 1,721/i)).toBeInTheDocument();
 
-    expect(screen.getByText(/^[ \t]*ECLKC Resource\r?\n?[ \t]*500 of 606/i)).toBeInTheDocument();
-    expect(screen.getByText(/^[ \t]*Non-ECLKC Resource\r?\n?[ \t]*106 of 606/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*ECLKC Resource\r?\n?[ \t]*1 of 606/i)).toBeInTheDocument();
+    expect(screen.getByText(/^[ \t]*Non-ECLKC Resource\r?\n?[ \t]*1 of 606/i)).toBeInTheDocument();
   });
 
   it('renders loading when loading', async () => {
