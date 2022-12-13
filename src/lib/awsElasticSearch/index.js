f43c8484988f8a55fff3ca8f13ebc88b3c35218c
Adam Levin <adam.levin@adhocteam.us>
2022-12-13 17:32:40 -0500

    https

--- src/lib/awsElasticSearch/index.js
+++ src/lib/awsElasticSearch/index.js
@@ -63,13 +63,15 @@ const createAwsConnector = (credentials, region) => {
 const getClient = async () => new Client({
   ...createAwsConnector(
     {
-      node: 'http://opensearch-node1:9200',
+      node: 'https://opensearch-node1:9200',
+      //node: 'http://localhost:9200',
       accessKeyId: 'admin',
       secretAccessKey: 'admin',
     },
     'us-gov-west-1',
   ),
-  node: 'http://opensearch-node1:9200',
+  node: 'https://opensearch-node1:9200',
+  //node: 'http://localhost:9200',
 });
 /*
   Create an index that can have searchable documents assigned.
