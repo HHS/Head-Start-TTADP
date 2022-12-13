53b994a3e8384e9da2a28a01ec0520f6005b5400
Adam Levin <adam.levin@adhocteam.us>
2022-12-13 11:57:38 -0500

    put back old code

--- src/lib/awsElasticSearch/index.js
+++ src/lib/awsElasticSearch/index.js
@@ -38,7 +38,7 @@ const generateEsConfig = () => {
 };
 
 const {
-  // uri,
+  uri,
   access_key,
   secret_key,
 } = generateEsConfig();
@@ -68,8 +68,9 @@ const getClient = async () => new Client({
     },
     'us-gov-west-1',
   ),
-  node: 'http://admin:admin@localhost:9200',
-  cloud: undefined,
+  node: uri,
+  // node: 'http://admin:admin@localhost:9200',
+  // cloud: undefined,
 });
 /*
   Create an index that can have searchable documents assigned.
