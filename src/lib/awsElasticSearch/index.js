140fbf5a727217e538d65e412487a7f92ce8138c
Adam Levin <adam.levin@adhocteam.us>
2022-12-14 15:04:54 -0500

    change container name

--- src/lib/awsElasticSearch/index.js
+++ src/lib/awsElasticSearch/index.js
@@ -63,15 +63,15 @@ const createAwsConnector = (credentials, region) => {
 const getClient = async () => new Client({
   ...createAwsConnector(
     {
-      // node: 'http://opensearch-node1:9200',
-      node: 'https://localhost:9200',
+      node: 'http://opensearch-container:9200',
+      // node: 'https://localhost:9200',
       accessKeyId: 'admin',
       secretAccessKey: 'admin',
     },
     'us-gov-west-1',
   ),
-  // node: 'http://opensearch-node1:9200',
-  node: 'https://localhost:9200',
+  node: 'http://opensearch-container:9200',
+  // node: 'https://localhost:9200',
 });
 /*
   Create an index that can have searchable documents assigned.
