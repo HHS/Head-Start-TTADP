c6a7ab256cea6c5ea742e575b37e68730d0c954d
Adam Levin <adam.levin@adhocteam.us>
2022-12-16 16:38:44 -0500

    update to sequelize per Garrett

--- src/tools/createAwsElasticSearchIndexes.js
+++ src/tools/createAwsElasticSearchIndexes.js
@@ -1,11 +1,19 @@
 /* eslint-disable no-restricted-syntax */
+import { Op } from 'sequelize';
 import moment from 'moment';
 import { auditLogger, logger } from '../logger';
 import {
-  getClient, deleteIndex, createIndex, bulkIndex,
+  getClient,
+  deleteIndex,
+  createIndex,
+  bulkIndex,
 } from '../lib/awsElasticSearch/index';
 import {
   sequelize,
+  ActivityReportGoal,
+  ActivityReport,
+  NextStep,
+  ActivityReportObjective,
 } from '../models';
 import { AWS_ELASTIC_SEARCH_INDEXES } from '../constants';
 
@@ -33,107 +41,71 @@ export default async function createAwsElasticSearchIndexes() {
 
     // Get activity reports.
     const startGettingReports = moment();
-
-    /* We need to use raw sql here otherwise we timeout and run out of memory. */
-    const reportsSql = `
-    -- Create AR temp table.
-    SELECT
-    ar."id",
-    ar."context",
-    ar."startDate",
-    ar."endDate"
-    INTO TEMP TABLE temp_ars
-    FROM "ActivityReports" ar
-    WHERE ar."calculatedStatus" = 'approved'
-    ORDER BY ar."id" DESC;
-
-    -- Recipient Next Steps.
-    SELECT
-    rns."activityReportId",
-    rns."note"
-    INTO TEMP TABLE temp_recipient_next_steps
-    FROM "NextSteps" rns
-    WHERE rns."noteType" ='RECIPIENT'
-    AND rns."activityReportId" IN (SELECT id FROM temp_ars)
-    GROUP BY rns."activityReportId", rns."note"
-    ORDER BY rns."activityReportId";
-
-    -- Specialist Next Steps.
-    SELECT
-    rns."activityReportId",
-    rns."note"
-    INTO TEMP TABLE temp_specialist_next_steps
-    FROM "NextSteps" rns
-    WHERE rns."noteType" ='SPECIALIST'
-    AND rns."activityReportId" IN (SELECT id FROM temp_ars)
-    GROUP BY rns."activityReportId", rns."note"
-    ORDER BY rns."activityReportId";
-
-    -- Goals.
-    SELECT
-    arg."activityReportId",
-    arg."name"
-    INTO TEMP TABLE temp_goals
-    FROM "ActivityReportGoals" arg
-    WHERE arg."activityReportId" IN (SELECT id FROM temp_ars)
-    GROUP BY arg."activityReportId", arg."name"
-    ORDER BY arg."activityReportId";
-
-    -- Objectives.
-    SELECT
-    aro."activityReportId",
-    aro."title",
-    aro."ttaProvided"
-    INTO TEMP TABLE temp_objectives
-    FROM "ActivityReportObjectives" aro
-    WHERE aro."activityReportId" IN (SELECT id FROM temp_ars)
-    GROUP BY aro."activityReportId", aro."title", aro."ttaProvided"
-    ORDER BY aro."activityReportId";`;
-
     // Query DB.
     let reportsToIndex;
     let recipientNextStepsToIndex;
     let specialistNextStepsToIndex;
     let goalsToIndex;
     let objectivesToIndex;
+
     await sequelize.transaction(async (transaction) => {
-      // Create Temp Tables.
-      await sequelize.query(
-        reportsSql,
-        { transaction },
-      );
-
       // Reports.
-      reportsToIndex = await sequelize.query(
-        'SELECT * FROM temp_ars;',
-        { transaction },
-      );
+      reportsToIndex = await ActivityReport.findAll({
+        attributes: ['id', 'context', 'startDate', 'endDate'],
+        where: { calculatedStatus: 'approved' },
+        order: [['id', 'ASC']],
+        raw: true,
+        transaction,
+      });
 
+      const reportIds = reportsToIndex.map((report) => report.id);
       // Recipient Steps.
-      recipientNextStepsToIndex = await sequelize.query(
-        'SELECT * FROM temp_recipient_next_steps;',
-        { transaction },
-      );
-
+      recipientNextStepsToIndex = await NextStep.findAll({
+        attributes: ['activityReportId', 'note'],
+        where: {
+          noteType: 'RECIPIENT',
+          activityReportId: { [Op.in]: reportIds },
+        },
+        group: ['activityReportId', 'note'],
+        order: [['activityReportId', 'ASC']],
+        raw: true,
+        transaction,
+      });
       // Specialist Steps.
-      specialistNextStepsToIndex = await sequelize.query(
-        'SELECT * FROM temp_specialist_next_steps;',
-        { transaction },
-      );
-
+      specialistNextStepsToIndex = await NextStep.findAll({
+        attributes: ['activityReportId', 'note'],
+        where: {
+          noteType: 'SPECIALIST',
+          activityReportId: { [Op.in]: reportIds },
+        },
+        group: ['activityReportId', 'note'],
+        order: [['activityReportId', 'ASC']],
+        raw: true,
+        transaction,
+      });
       // Goals.
-      goalsToIndex = await sequelize.query(
-        'SELECT * FROM temp_goals;',
-        { transaction },
-      );
-
+      goalsToIndex = await ActivityReportGoal.findAll({
+        attributes: ['activityReportId', 'name'],
+        where: {
+          activityReportId: { [Op.in]: reportIds },
+        },
+        group: ['activityReportId', 'name'],
+        order: [['activityReportId', 'ASC']],
+        raw: true,
+        transaction,
+      });
       // objectives.
-      objectivesToIndex = await sequelize.query(
-        'SELECT * FROM temp_objectives;',
-        { transaction },
-      );
+      objectivesToIndex = await ActivityReportObjective.findAll({
+        attributes: ['activityReportId', 'title', 'ttaProvided'],
+        where: {
+          activityReportId: { [Op.in]: reportIds },
+        },
+        group: ['activityReportId', 'title', 'ttaProvided'],
+        order: [['activityReportId', 'ASC']],
+        raw: true,
+        transaction,
+      });
     });
-
     const finishGettingReports = moment();
     if (!reportsToIndex.length) {
       logger.info('Search Index Job Info: No reports found to index.');
@@ -145,8 +117,8 @@ export default async function createAwsElasticSearchIndexes() {
     // Build Documents Object Json.
     const startCreatingBulk = moment();
     const documents = [];
-    for (let i = 0; i < reportsToIndex[0].length; i += 1) {
-      const ar = reportsToIndex[0][i];
+    for (let i = 0; i < reportsToIndex.length; i += 1) {
+      const ar = reportsToIndex[i];
       documents.push({
         create: {
           _index: AWS_ELASTIC_SEARCH_INDEXES.ACTIVITY_REPORTS,
@@ -155,14 +127,14 @@ export default async function createAwsElasticSearchIndexes() {
       });
 
       // Get relevant AR records.
-      const arRecipientSteps = recipientNextStepsToIndex[0].filter(
+      const arRecipientSteps = recipientNextStepsToIndex.filter(
         (r) => r.activityReportId === ar.id,
       );
-      const arSpecialistSteps = specialistNextStepsToIndex[0].filter(
+      const arSpecialistSteps = specialistNextStepsToIndex.filter(
         (s) => s.activityReportId === ar.id,
       );
-      const arGoalsSteps = goalsToIndex[0].filter((g) => g.activityReportId === ar.id);
-      const arObjectivesSteps = objectivesToIndex[0].filter((o) => o.activityReportId === ar.id);
+      const arGoalsSteps = goalsToIndex.filter((g) => g.activityReportId === ar.id);
+      const arObjectivesSteps = objectivesToIndex.filter((o) => o.activityReportId === ar.id);
 
       // Add to bulk update.
       documents.push({
@@ -182,14 +154,14 @@ export default async function createAwsElasticSearchIndexes() {
     const startBulkImport = moment();
     await bulkIndex(documents, AWS_ELASTIC_SEARCH_INDEXES.ACTIVITY_REPORTS, client);
     const finishBulkImport = moment();
-    logger.info(`Search Index Job Info: ...Finished indexing of ${reportsToIndex[0].length} reports`);
+    logger.info(`Search Index Job Info: ...Finished indexing of ${reportsToIndex.length} reports`);
 
     const finishTotalTime = moment();
 
     // Log times.
     logger.info(`
     - Create AWS Elasticsearch Indexes -
-    | Total Reports Indexed (#): ${reportsToIndex[0].length} |
+    | Total Reports Indexed (#): ${reportsToIndex.length} |
     | Total Time: ${finishTotalTime.diff(startTotalTime) / 1000}sec |
     | Clean Index: ${finishCleaningIndex.diff(startCleaningIndex) / 1000}sec |
     | Create Index: ${finishCreatingIndex.diff(startCreatingIndex) / 1000}sec |
