a177afce913f075af29688701ad1c3e57529f636
Matt Bevilacqua <thewatermethod@gmail.com>
2022-12-19 09:05:10 -0500

    more test debugging

--- frontend/src/hooks/useSocket.js
+++ frontend/src/hooks/useSocket.js
@@ -26,31 +26,27 @@ export default function useSocket(user) {
 
   useEffect(() => {
     if (!WS_URL || !socketPath) {
-      console.log('no socket path or url', WS_URL, socketPath);
       return;
     }
 
     // if we've already created a socket for the current path, return
     if (socket.current && path === socket.current.url) {
-      console.log('socket already exists', path, socket.current);
       return;
     }
 
     if (socket.current && socket.current.readyState === socket.current.OPEN) {
       socket.current.close();
       clearStore();
-      console.log('closing socket', path, socket.current);
     }
 
     const s = new WebSocket(path);
-
-    console.log('creating new socket', path, s);
 
     // we don't want to send bufferdata, but json
     s.binaryType = 'arraybuffer';
 
     // opening a new socket will clear the store
     s.addEventListener('open', () => {
+      console.log('socket opened', s, user);
       clearStore();
 
       if (user) {
