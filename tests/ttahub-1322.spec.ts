ba96d5fabf56a2b5095e5203f950951e7af30a58
GarrettEHill <garretthill@gmail.com>
2023-01-06 12:31:45 -0800

    update to use test data

--- tests/ttahub-1322.spec.ts
+++ tests/ttahub-1322.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
 test('rtr goals and objectives has the correct title', async ({ page }) => {
-  await page.goto('http://localhost:3000/recipient-tta-records/376/region/1/goals-objectives');
-  expect(await page.title()).toBe('Goals and Objectives - Action for Boston Community Development, Inc. - TTA Hub');
+  await page.goto('http://localhost:3000/recipient-tta-records/11/region/1/goals-objectives');
+  expect(await page.title()).toBe('Goals and Objectives - Agency 2 in region 1, Inc. - TTA Hub');
 });
