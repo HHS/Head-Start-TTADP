12e20c9c3350c71c7102f0354c719cf1da855318
Jonathan Pyers <pyersjonathan@gmail.com>
2023-01-24 14:13:14 -0700

    add test

---
+++ frontend/src/components/__tests__/Drawer.js
@@ -0,0 +1,98 @@
+import '@testing-library/jest-dom';
+import React from 'react';
+import { render, screen, act } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import Drawer from '../Drawer';
+
+describe('Drawer', () => {
+  // Create a ref for the trigger element which opens the drawer:
+  const triggerRef = React.createRef();
+  const clickTargetRef = React.createRef();
+
+  const renderDrawer = () => (
+    <div>
+      <div
+        ref={clickTargetRef}
+      >
+        click target
+      </div>
+      <div>
+        <button
+          type="button"
+          ref={triggerRef}
+          onClick={() => {}}
+        >
+          Open
+        </button>
+      </div>
+      <Drawer
+        title="Hello"
+        triggerRef={triggerRef}
+      >
+        Content
+      </Drawer>
+    </div>
+  );
+
+  it('should open the drawer when the trigger is clicked', async () => {
+    render(renderDrawer());
+
+    expect(screen.queryByText('Content')).not.toBeInTheDocument();
+
+    act(() => {
+      const button = screen.getByRole('button', { name: 'Open' });
+      userEvent.click(button);
+    });
+
+    expect(screen.getByText('Content')).toBeInTheDocument();
+  });
+
+  it('should close when the escape key is pressed', async () => {
+    render(renderDrawer());
+
+    act(() => {
+      const button = screen.getByRole('button', { name: 'Open' });
+      userEvent.click(button);
+    });
+
+    expect(screen.getByText('Content')).toBeInTheDocument();
+
+    act(() => {
+      userEvent.keyboard('{esc}');
+    });
+
+    expect(screen.queryByText('Content')).not.toBeInTheDocument();
+  });
+
+  it('should automatically focus the close button', async () => {
+    render(renderDrawer());
+
+    act(() => {
+      const button = screen.getByRole('button', { name: 'Open' });
+      userEvent.click(button);
+    });
+
+    expect(screen.getByText('Content')).toBeInTheDocument();
+
+    expect(screen.getByRole('button', { name: 'Close' })).toHaveFocus();
+  });
+
+  it('clicking outside of the drawer should close the drawer', async () => {
+    render(renderDrawer());
+
+    act(() => {
+      const button = screen.getByRole('button', { name: 'Open' });
+      userEvent.click(button);
+    });
+
+    expect(screen.getByText('Content')).toBeInTheDocument();
+
+    act(() => {
+      const clickTarget = clickTargetRef.current;
+      userEvent.click(clickTarget);
+    });
+
+    expect(screen.queryByText('Content')).not.toBeInTheDocument();
+  });
+});
