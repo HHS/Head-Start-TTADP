0ef9b17f6fc9313f5f65c6fd45e9c2b98e9654ef
Matt Bevilacqua <thewatermethod@gmail.com>
2023-01-24 17:19:11 -0500

    add missing tools test

---
+++ src/tools/createAwsElasticSearchIndexes.additional.test.js
@@ -0,0 +1,46 @@
+/* eslint-disable dot-notation */
+import db, {
+  ActivityReport,
+} from '../models';
+import createAwsElasticSearchIndexes from './createAwsElasticSearchIndexes';
+import { auditLogger, logger } from '../logger';
+
+jest.mock('../lib/awsElasticSearch/index', () => ({
+  getClient: jest.fn(() => ({})),
+  deleteIndex: jest.fn(),
+  createIndex: jest.fn(),
+}));
+
+jest.mock('bull');
+
+describe('Create AWS Elastic Search Indexes', () => {
+  describe('error states', () => {
+    afterEach(() => {
+      jest.clearAllMocks();
+    });
+
+    afterAll(async () => {
+      await db.sequelize.close();
+    });
+
+    it('no reports to index', async () => {
+      ActivityReport.findAll = jest.fn().mockResolvedValueOnce([]);
+      jest.spyOn(logger, 'info');
+
+      // Create Indexes.
+      await createAwsElasticSearchIndexes();
+      expect(logger.info).toHaveBeenCalledWith('Search Index Job Info: No reports found to index.');
+    });
+
+    it('search index job error', async () => {
+      ActivityReport.findAll = jest.fn().mockRejectedValueOnce(new Error('test error'));
+
+      jest.spyOn(auditLogger, 'error');
+
+      // Create Indexes.
+      await createAwsElasticSearchIndexes();
+
+      expect(auditLogger.error).toHaveBeenCalledWith('Search Index Job Error: Error: test error');
+    });
+  });
+});
