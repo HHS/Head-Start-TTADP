6532787eab4d4ba8d933851a37d489c7b2add267
Jonathan Pyers <pyersjonathan@gmail.com>
2023-01-24 13:33:14 -0700

    use `faPenCircle` instead of `faPencilAlt`

--- frontend/src/components/GoalCards/icons.js
+++ frontend/src/components/GoalCards/icons.js
@@ -4,17 +4,17 @@ import {
   faClock,
   faCheckCircle,
   faExclamationCircle,
-  faPencilAlt,
   faMinusCircle,
   faTimesCircle,
 } from '@fortawesome/free-solid-svg-icons';
+import { faPenCircle } from '@fortawesome/pro-regular-svg-icons';
 import colors from '../../colors';
 
 const InProgress = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.ttahubMediumBlue} icon={faClock} />;
 const Closed = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.successDarker} icon={faCheckCircle} />;
 const NotStarted = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.ttahubOrange} icon={faMinusCircle} />;
 const NoStatus = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.baseLighter} icon={faExclamationCircle} />;
-const Draft = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.baseDarkest} icon={faPencilAlt} />;
+const Draft = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.baseDarkest} icon={faPenCircle} />;
 const Ceased = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.errorDark} icon={faTimesCircle} />;
 
 export {
