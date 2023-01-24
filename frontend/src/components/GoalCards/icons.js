04946e4a55aca0e424816a8dafbc8b0e3feabb35
Jonathan Pyers <pyersjonathan@gmail.com>
2023-01-24 14:29:09 -0700

    use solid variant

--- frontend/src/components/GoalCards/icons.js
+++ frontend/src/components/GoalCards/icons.js
@@ -7,7 +7,7 @@ import {
   faMinusCircle,
   faTimesCircle,
 } from '@fortawesome/free-solid-svg-icons';
-import { faPenCircle } from '@fortawesome/pro-regular-svg-icons';
+import { faPenCircle } from '@fortawesome/pro-solid-svg-icons';
 import colors from '../../colors';
 
 const InProgress = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.ttahubMediumBlue} icon={faClock} />;
