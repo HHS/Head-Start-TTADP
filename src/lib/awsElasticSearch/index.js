6093db35414947a989d28d59116bc1b88674c94d
Adam Levin <adam.levin@adhocteam.us>
2023-02-06 09:56:39 -0500

    rollback number of search results

--- src/lib/awsElasticSearch/index.js
+++ src/lib/awsElasticSearch/index.js
@@ -142,80 +142,33 @@ const bulkIndex = async (documents, indexName, passedClient) => {
     throw error;
   }
 };
+
 /*
   Search index documents.
-  Note: Right now we use search with search_after.
-  This allows us to return all results in batches of 10k.
 */
-const search = async (indexName, fields, query, passedClient, overrideBatchSize) => {
+const search = async (indexName, fields, query, passedClient) => {
   try {
     // Initialize the client.
     const client = passedClient || await getClient();
 
-    // Total hits.
-    let totalHits = [];
-
-    // Loop vars.
-    const maxLoopIterations = 9;
-    let retrieveAgain = true;
-    const batchSize = overrideBatchSize || 10000; // Default batch size to 10k.
-    let loopIterations = 0;
-    let res;
-    let searchAfter;
-
     // Create search body.
-    let body = {
-      size: batchSize,
+    const body = {
+      size: 2000,
       query: {
-        query_string: {
-          query: `*${query}*`,
+        multi_match: {
+          query,
           fields,
         },
       },
-      sort: [
-        {
-          id: {
-            order: 'asc', // necessary for batch processing.
-          },
-        },
-      ],
     };
 
-    while (retrieveAgain && loopIterations <= maxLoopIterations) {
-      // Search an index.
-      res = await client.search({
-        index: indexName,
-        body,
-      });
-
-      // Get hits.
-      const hits = res.body.hits.hits || res.body.hits;
-
-      // Check if these are new results.
-      if (hits && hits.length > 0) {
-        // Append new hits.
-        totalHits = [...totalHits, ...hits];
-
-        // Get search_after from last hit.
-        const lastHit = hits.pop();
-
-        // Set search_after.
-        searchAfter = lastHit.sort;
-
-        // Update search_after.
-        body = { ...body, search_after: searchAfter };
-
-        // Increase loop count.
-        loopIterations += 1;
-
-        // If we don't have a sort after (undefined) stop looping.
-        retrieveAgain = searchAfter;
-      } else {
-        retrieveAgain = false;
-      }
-    }
+    // Search an index.
+    const res = await client.search({
+      index: indexName,
+      body,
+    });
     logger.info(`AWS OpenSearch: Successfully searched the index ${indexName} using query '${query}`);
-    return { hits: totalHits };
+    return res.body.hits;
   } catch (error) {
     auditLogger.error(`AWS OpenSearch Error: Unable to search the index '${indexName}' with query '${query}': ${error.message}`);
     throw error;
