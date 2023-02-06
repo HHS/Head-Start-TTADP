f7e03f150e9209041e4e6e1a36304bdd94a1609e
Matt Bevilacqua <thewatermethod@gmail.com>
2023-02-06 17:24:48 -0500

    try this

--- tests/activity-report.spec.ts
+++ tests/activity-report.spec.ts
@@ -479,7 +479,7 @@ test.describe("Activity Report", () => {
   
     // confirm goal is in RTR
     await expect(page.getByText('This is a goal for multiple grants')).toBeVisible();
-    await expect(page.getByRole('heading', { name: /Goal G-(\d), G-(\d)RTTAPA/i }).last()).toBeVisible();
+    await expect(page.getByRole('heading', { name: /Goal G-(\d), G-(\d)RTTAPA/i })).toBeVisible();
   
     // navigate to the AR page
     await page.getByRole('link', { name: 'Activity Reports' }).click();
