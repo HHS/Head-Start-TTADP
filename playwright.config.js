fbdba2528190cd5c451b8a06fe10c7f4354a073b
Matt Bevilacqua <thewatermethod@gmail.com>
2023-02-07 10:17:04 -0500

    some cleanup of debugging

--- playwright.config.js
+++ playwright.config.js
@@ -8,13 +8,13 @@ import { devices } from '@playwright/test';
 const config = {
   testDir: './tests',
   outputDir: './playwright/test-results',
-  workers: 3,
+  workers: 3, // this was originally set to 3, but changed to 2 when I saw data collisions, but tests have been improved since then so I think it's safe to go back to 3
   expect: {
     timeout: 20000,
   },
   fullyParallel: true,
   reporter: [['html', { outputFolder: './playwright/report', open: 'never' }]],
-  timeout: 600000,
+  timeout: 300000,
   globalTimeout: 900000,
   globalSetup: './tests/init/globalSetup.ts',
   projects: [
