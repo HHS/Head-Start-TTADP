53b49fd761a1b0d3a03d899c08fac659398b741b
Matt Bevilacqua <thewatermethod@gmail.com>
2023-02-10 14:59:59 -0500

    fix bug w/datavalue ref

--- src/services/goals.js
+++ src/services/goals.js
@@ -714,7 +714,11 @@ export async function goalByIdAndRecipient(id, recipientId) {
       topics: objective.objectiveTopics
         .map((objectiveTopic) => ({
           ...objectiveTopic.dataValues,
-          ...objectiveTopic.topic.dataValues,
+          ...(
+            objectiveTopic.topic && objectiveTopic.topic.dataValues
+              ? objectiveTopic.topic.dataValues
+              : []
+          ),
         }))
         .map((o) => ({ ...o, topic: undefined })),
       files: objective.objectiveFiles
@@ -741,7 +745,11 @@ export async function goalsByIdAndRecipient(ids, recipientId) {
             .map((objectiveTopic) => {
               const ot = {
                 ...objectiveTopic.dataValues,
-                ...objectiveTopic.topic.dataValues,
+                ...(
+                  objectiveTopic.topic && objectiveTopic.topic.dataValues
+                    ? objectiveTopic.topic.dataValues
+                    : []
+                ),
               };
               delete ot.topic;
               return ot;
