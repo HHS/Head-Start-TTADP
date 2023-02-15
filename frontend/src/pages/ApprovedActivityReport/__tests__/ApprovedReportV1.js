62b1ce0584aaa8680864a4538e36bc7ac5739dfd
Adam Levin <adam.levin@adhocteam.us>
2023-02-15 09:26:51 -0500

    more coverage

--- frontend/src/pages/ApprovedActivityReport/__tests__/ApprovedReportV1.js
+++ frontend/src/pages/ApprovedActivityReport/__tests__/ApprovedReportV1.js
@@ -135,4 +135,29 @@ describe('Approved Activity Report V1 component', () => {
     expect(await screen.findByText('01/09/2023')).toBeInTheDocument();
     expect(await screen.findByText(/Topics/i)).toBeInTheDocument();
   });
+
+  it('renders different tta types', async () => {
+    // Technical assistance.
+    render(<ApprovedReportV1 data={{
+      ...report, ttaType: ['technical-assistance'],
+    }}
+    />);
+    expect(await screen.findByText('Technical Assistance, Virtual (Phone)')).toBeInTheDocument();
+
+    // Both.
+    render(<ApprovedReportV1 data={{
+      ...report, ttaType: ['technical-assistance', 'training'],
+    }}
+    />);
+    expect(await screen.findByText('Training and Technical Assistance, Virtual (Phone)')).toBeInTheDocument();
+  });
+
+  it('renders approved at', async () => {
+    render(<ApprovedReportV1 data={{
+      ...report, approvedAt: '2023-02-15',
+    }}
+    />);
+    expect(await screen.findByText(/Date approved:/i)).toBeInTheDocument();
+    expect(screen.queryAllByText('02/15/2023').length).toBe(2);
+  });
 });
