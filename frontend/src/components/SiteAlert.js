669ed276ab6a732acd091481883faf6bbb65f235
Matt Bevilacqua <thewatermethod@gmail.com>
2023-02-15 12:06:35 -0500

    fix emergency colors

--- frontend/src/components/SiteAlert.js
+++ frontend/src/components/SiteAlert.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { SiteAlert as BaseSiteAlert } from '@trussworks/react-uswds';
-import './SiteAlert.css';
+import './SiteAlert.scss';
 
 export default function SiteAlert({
   heading, children, style, variant,
