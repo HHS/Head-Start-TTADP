c7ef766b97d40a189b38b2629eb48b87b2a07dec
Jonathan Pyers <pyersjonathan@gmail.com>
2023-02-20 11:01:12 -0700

    add feature flag to user model

--- src/models/user.js
+++ src/models/user.js
@@ -2,7 +2,7 @@ const { Model } = require('sequelize');
 const isEmail = require('validator/lib/isEmail');
 const generateFullName = require('./helpers/generateFullName');
 
-const featureFlags = [];
+const featureFlags = ['regional_goal_dashboard'];
 
 export default (sequelize, DataTypes) => {
   class User extends Model {
