cc87523a7aacd236a5f33e825afb89329ade7115
Jonathan Pyers <pyersjonathan@gmail.com>
2023-02-24 14:32:13 -0700

    add handler for `TopicsTable`

--- src/widgets/regionalGoalDashboard/topicsByGoalStatus.ts
+++ src/widgets/regionalGoalDashboard/topicsByGoalStatus.ts
@@ -2,7 +2,7 @@ import { Op } from 'sequelize';
 import { TOPICS, GOAL_STATUS } from '../../constants';
 import {
   // @ts-ignore
-  Goal, Topic,
+  Goal, Topic, Objective, ObjectiveTopic,
 } from '../../models';
 
 type Tpc = typeof TOPICS[number];
@@ -15,5 +15,64 @@ type Topics = {
   }
 };
 
-export default async function topicsByGoalStatus(scopes) {
+export default async function topicsByGoalStatus(scopes): Promise<Topics> {
+  const topics = {};
+
+  // Goal -> Objective -> ObjectiveTopic -> Topic
+  // Legacy solution:
+  // Goal -> ARGoal -> AR -> AR.topics (array of enums)
+
+  type QueryResults = {
+    id: number;
+    status: Status;
+    'objectives.objectiveTopics.topic.id': number;
+    'objectives.objectiveTopics.topic.topic': Tpc;
+  };
+
+  const allTopics = await Goal.findAll({
+    attributes: ['id', 'status'],
+    where: {
+      [Op.and]: [scopes.goal],
+    },
+    include: [
+      {
+        model: Objective,
+        as: 'objectives',
+        attributes: [],
+        include: [
+          {
+            model: ObjectiveTopic,
+            as: 'objectiveTopics',
+            attributes: [],
+            include: [
+              {
+                model: Topic,
+                as: 'topic',
+                attributes: [['name', 'topic']],
+              },
+            ],
+          },
+        ],
+      },
+    ],
+    raw: true,
+  }) as QueryResults[];
+
+  let sanitized = allTopics.reduce((acc, goal) => {
+    const { status, 'objectives.objectiveTopics.topic.topic': topic } = goal;
+    if (!acc[topic]) {
+      acc[topic] = { ...Object.values(GOAL_STATUS).reduce((a, s) => ({ ...a, [s]: 0 }), {}) };
+    }
+
+    acc[topic][status] += 1;
+
+    return acc;
+  }, {});
+
+  sanitized = Object.entries(sanitized).reduce((acc, [topic, statuses]) => {
+    acc[topic].total = Object.values(statuses).reduce((a, s) => a + s, 0);
+    return acc;
+  }, sanitized);
+
+  return topics;
 }
