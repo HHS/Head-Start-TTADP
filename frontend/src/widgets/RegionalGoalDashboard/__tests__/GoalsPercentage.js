080b3ff559482e2c427f31a3521542c28723ff02
Jonathan Pyers <pyersjonathan@gmail.com>
2023-02-24 12:27:01 -0700

    add test

---
+++ frontend/src/widgets/RegionalGoalDashboard/__tests__/GoalsPercentage.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {
+  render,
+  screen,
+} from '@testing-library/react';
+import { GoalsPercentageWidget } from '../GoalsPercentage';
+
+const TEST_DATA = {
+  numerator: 100,
+  denominator: 1000,
+  percentage: 10,
+};
+
+const renderGoalsPercentage = async () => (
+  render(<GoalsPercentageWidget loading={false} data={TEST_DATA} />)
+);
+
+describe('Goals Percentage', () => {
+  it('shows the percentage', async () => {
+    renderGoalsPercentage();
+    const percentage = await screen.findByText('10%');
+    expect(percentage).toBeInTheDocument();
+  });
+
+  it('shows the numerator', async () => {
+    renderGoalsPercentage();
+    const numerator = await screen.findByText('100');
+    expect(numerator).toBeInTheDocument();
+  });
+
+  it('shows the denominator', async () => {
+    renderGoalsPercentage();
+    const denominator = await screen.findByText('1000');
+    expect(denominator).toBeInTheDocument();
+  });
+});
