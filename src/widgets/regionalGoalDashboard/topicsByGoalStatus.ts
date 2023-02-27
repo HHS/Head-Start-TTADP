5183ab55f861b8f426f14a5c447ee03761b7ac9d
Jonathan Pyers <pyersjonathan@gmail.com>
2023-02-26 21:33:17 -0700

    refine handler response

--- src/widgets/regionalGoalDashboard/topicsByGoalStatus.ts
+++ src/widgets/regionalGoalDashboard/topicsByGoalStatus.ts
@@ -9,15 +9,13 @@ type Tpc = typeof TOPICS[number];
 
 type Status = keyof typeof GOAL_STATUS;
 
-type Topics = {
+type TopicResponse = {
   [topic: string]: {
     [S in Status]: number;
-  }
+  } & { total: number };
 };
 
-export default async function topicsByGoalStatus(scopes): Promise<Topics> {
-  const topics = {};
-
+export default async function topicsByGoalStatus(scopes): Promise<TopicResponse[]> {
   // Goal -> Objective -> ObjectiveTopic -> Topic
   // Legacy solution:
   // Goal -> ARGoal -> AR -> AR.topics (array of enums)
@@ -60,11 +58,14 @@ export default async function topicsByGoalStatus(scopes): Promise<Topics> {
 
   let sanitized = allTopics.reduce((acc, goal) => {
     const { status, 'objectives.objectiveTopics.topic.topic': topic } = goal;
-    if (!acc[topic]) {
+    // TODO: topic is null for some of these - why?
+    if (topic && !acc[topic]) {
       acc[topic] = { ...Object.values(GOAL_STATUS).reduce((a, s) => ({ ...a, [s]: 0 }), {}) };
     }
 
-    acc[topic][status] += 1;
+    if (acc[topic]) {
+      acc[topic][status] += 1;
+    }
 
     return acc;
   }, {});
@@ -74,5 +75,16 @@ export default async function topicsByGoalStatus(scopes): Promise<Topics> {
     return acc;
   }, sanitized);
 
-  return topics;
+  // Format this so it's more easily digestible by the frontend
+  const response: TopicResponse[] = Object.entries(sanitized)
+    .map(([topic, statuses]) => ({ topic, statuses }))
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    .map((obj: any) => {
+      const { statuses } = obj;
+      const { total } = statuses;
+      delete statuses.total;
+      return { ...obj, total };
+    });
+
+  return response;
 }
