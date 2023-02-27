bc9cfc2e74328025c2b7995274583172aac297cf
Matt Bevilacqua <thewatermethod@gmail.com>
2023-02-27 13:09:08 -0500

    fix frontend tests

--- frontend/src/pages/Home/__tests__/Llama.js
+++ frontend/src/pages/Home/__tests__/Llama.js
@@ -1,31 +1,59 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {
+  render, screen, act, waitFor,
+} from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-// import fetchMock from 'fetch-mock';
+import fetchMock from 'fetch-mock';
 import Llama from '../Llama';
+import { SCOPE_IDS } from '../../../Constants';
 
-// const mockResponse = {
-//     userId,
-//     daysSinceJoined: '123 days',
-//     arsCreated: 3,
-//     arsCollaboratedOn: 4,
-//     ttaProvided: '10 days 5 hours',
-//     recipientsReached: 1000,
-//     grantsServed: 34,
-//     participantsReached: 2344,
-//     goalsApproved: 234,
-//     objectivesApproved: 23,
-//   }
+const mockResponse = {
+  daysSinceJoined: '123 days',
+  arsCreated: 3,
+  arsCollaboratedOn: 4,
+  ttaProvided: '10 days 5 hours',
+  recipientsReached: 1000,
+  grantsServed: 34,
+  participantsReached: 2344,
+  goalsApproved: 234,
+  objectivesApproved: 23,
+};
 
 describe('Llama', () => {
-  it('renders the llama', () => {
-    // const url = '/api/users/1/statistics';
-    // fetchMock.get(url, mockResponse);
+  afterEach(() => {
+    fetchMock.restore();
+  });
 
-    render(<Llama user={{ id: 1 }} />);
+  it('renders the llama', async () => {
+    const url = '/api/users/statistics';
+    fetchMock.get(url, mockResponse);
+
+    act(() => {
+      render(<Llama user={{ id: 1, permissions: [] }} />);
+    });
+    await waitFor(() => expect(fetchMock.called(url)).toBe(true));
     const img = screen.getByAltText('hey folks, it\'s me, llawrence the llama, and I\'m just here to tell you that you\'ve done a great job here on the ttahub');
     expect(img).toBeInTheDocument();
     userEvent.click(img.parentElement);
     expect(img).toHaveClass('the-wiggler');
+  });
+
+  it('shows the extra fields when the user can write', async () => {
+    const url = '/api/users/statistics';
+    fetchMock.get(url, mockResponse);
+
+    act(() => {
+      render(<Llama user={{
+        id: 1,
+        permissions: [
+          {
+            scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+          },
+        ],
+      }}
+      />);
+    });
+    await waitFor(() => expect(fetchMock.called(url)).toBe(true));
+    expect(screen.getByText('since you joined')).toBeInTheDocument();
   });
 });
