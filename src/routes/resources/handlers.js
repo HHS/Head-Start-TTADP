4eca53f9880922b5872fd960ab26c31c73ad4e39
GarrettEHill <garretthill@gmail.com>
2023-02-28 10:12:14 -0800

    attached to handlers

--- src/routes/resources/handlers.js
+++ src/routes/resources/handlers.js
@@ -1,52 +1,19 @@
 /* eslint-disable import/prefer-default-export */
-// import { getResourcesData } from '../../services/resources';
-
-const dummyOverview = {
-  report: {
-    numResources: '8,135',
-    num: '19,914',
-    percentResources: '40.85%',
-  },
-  resource: {
-    numEclkc: '1,819',
-    num: '2,365',
-    percentEclkc: '79.91%',
-  },
-  recipient: {
-    numResources: '248',
-  },
-  participant: {
-    numParticipants: '765',
-  },
-};
-
-const dummyUse = {
-  headers: ['Jan-22'],
-  resources: [
-    {
-      heading: 'https://test1.gov',
-      isUrl: true,
-      data: [
-        {
-          title: 'Jan-22',
-          value: '17',
-        },
-        {
-          title: 'total',
-          value: '20',
-        },
-      ],
-    },
-  ],
-};
+import filtersToScopes from '../../scopes';
+import { currentUserId } from '../../services/currentUser';
+import { setReadRegions } from '../../services/accessValidation';
+import { resourceDashboardPhase1 } from '../../services/dashboards/resource';
 
 export async function getResourcesDashboardData(req, res) {
+  const userId = await currentUserId(req, res);
+  const query = await setReadRegions(req.query, userId);
+  const scopes = await filtersToScopes(query);
   // Garrett call your service function here.
   // Build scopes from the req (see AR handler)
-  // getResourcesData()
-  const roles = {
-    resourcesDashboardOverview: dummyOverview,
-    resourcesUse: dummyUse,
+  const data = await resourceDashboardPhase1(scopes);
+  const formatedData = {
+    resourcesDashboardOverview: data.overview,
+    resourcesUse: data.use,
   };
-  res.json(roles);
+  res.json(formatedData);
 }
