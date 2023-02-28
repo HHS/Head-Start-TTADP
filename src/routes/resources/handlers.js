a80ebbdafea6fa49a291d4fbb91a73fa0c9c7ab1
GarrettEHill <garretthill@gmail.com>
2023-02-28 13:39:58 -0800

    handler

--- src/routes/resources/handlers.js
+++ src/routes/resources/handlers.js
@@ -8,12 +8,9 @@ export async function getResourcesDashboardData(req, res) {
   const userId = await currentUserId(req, res);
   const query = await setReadRegions(req.query, userId);
   const scopes = await filtersToScopes(query);
-  // Garrett call your service function here.
-  // Build scopes from the req (see AR handler)
   const data = await resourceDashboardPhase1(scopes);
-  const formatedData = {
+  res.json({
     resourcesDashboardOverview: data.overview,
     resourcesUse: data.use,
-  };
-  res.json(formatedData);
+  });
 }
--- src/routes/resources/handlers.test.js
+++ src/routes/resources/handlers.test.js
@@ -1,23 +1,23 @@
 import { getResourcesDashboardData } from './handlers';
-// import { getResourcesData } from '../../services/resources';
+import { resourceDashboardPhase1 } from '../../services/dashboards/resource';
+import { getUserReadRegions } from '../../services/accessValidation';
 
-/*
-jest.mock('../../services/resources', () => ({
-  getResourcesData: jest.fn(),
+jest.mock('../../services/dashboards/resource', () => ({
+  resourceDashboardPhase1: jest.fn(),
 }));
-*/
+jest.mock('../../services/accessValidation');
+
 describe('Resources handler', () => {
   describe('getResourcesDashboardData', () => {
     it('should return all dashboard data', async () => {
-      // Garrett Mock and assert you data here.
-      /*
-      const resourcesData = [{ resourcesDashboardOverview: {}, resourcesUse: {} }];
-      getResourcesData.mockResolvedValue(resourcesData);
+      const responseData = { overview: {}, use: {} };
+      const resourcesData = { resourcesDashboardOverview: {}, resourcesUse: {} };
+      resourceDashboardPhase1.mockResolvedValue(responseData);
+      getUserReadRegions.mockResolvedValue([1]);
+      const req = { session: { userId: 1 }, query: {} };
+      const res = { json: jest.fn() };
       await getResourcesDashboardData(req, res);
       expect(res.json).toHaveBeenCalledWith(resourcesData);
-      */
-
-      expect(true).toBe(false);
     });
   });
 });
