ee56033e32755c8420d87add90dc631816b47235
GarrettEHill <garretthill@gmail.com>
2023-03-02 13:34:16 -0800

    small refactor and added comments

--- src/lib/semaphore.js
+++ src/lib/semaphore.js
@@ -1,29 +1,38 @@
 export default class Semaphore {
-  constructor(maxResources) {
-    this.maxResources = maxResources;
-    this.usedResources = 0;
+  constructor(maxConcurrency = 1) {
+    // Max number of concurrent operations the semaphore will allow
+    this.maxConcurrency = maxConcurrency;
+
+    // Current number of concurrent operations the semaphore has processing
+    this.currentConcurrency = 0;
+
+    // Array of promises blocking holding back all instances waiting for the semaphore
     this.waiting = [];
   }
 
   async acquire() {
-    if (this.usedResources < this.maxResources) {
+    // When there is still available in the concurrency, take it
+    if (this.currentConcurrency < this.maxConcurrency) {
       // eslint-disable-next-line no-plusplus
-      this.usedResources++;
-      return Promise.resolve();
+      this.currentConcurrency++;
+    } else {
+      // When no room is available in the concurrency, push a promise into the array
+      // and wait for it to be resolved
+      await new Promise((resolve) => {
+        this.waiting.push(resolve);
+      });
     }
-
-    return new Promise((resolve) => {
-      this.waiting.push(resolve);
-    });
   }
 
   release() {
+    // When a promise is waiting, resolve the oldest one
     if (this.waiting.length > 0) {
       const resolve = this.waiting.shift();
-      resolve();
+      if (resolve) resolve();
     } else {
+      // When no more promises are waiting decrament the current concurrency
       // eslint-disable-next-line no-plusplus
-      this.usedResources--;
+      this.currentConcurrency--;
     }
   }
 }
