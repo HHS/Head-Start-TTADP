836214830874603df419762a00431c398ccf0031
Jonathan Pyers <pyersjonathan@gmail.com>
2023-03-07 08:58:30 -0700

    only approved ARs for `goalsByStatus`

--- src/widgets/regionalGoalDashboard/goalsByStatus.ts
+++ src/widgets/regionalGoalDashboard/goalsByStatus.ts
@@ -1,7 +1,13 @@
 import { Op } from 'sequelize';
+import { REPORT_STATUSES } from '../../constants';
 import db from '../../models';
 
-const { Goal, sequelize } = db;
+const {
+  Goal,
+  ActivityReportGoal,
+  ActivityReport,
+  sequelize,
+} = db;
 
 export const GOAL_STATUS = {
   NOT_STARTED: 'Not Started',
@@ -31,6 +37,28 @@ export default async function goalsByStatus(scopes) {
         scopes.goal,
       ],
     },
+    include: [
+      {
+        model: ActivityReportGoal,
+        as: 'activityReportGoals',
+        required: true,
+        attributes: [],
+        include: [
+          {
+            model: ActivityReport,
+            as: 'activityReport',
+            attributes: [],
+            where: {
+              [Op.and]: {
+                calculatedStatus: {
+                  [Op.eq]: REPORT_STATUSES.APPROVED,
+                },
+              },
+            },
+          },
+        ],
+      },
+    ],
     // BIGINT (type returned from count) gets converted to string. Explicitly set count to int
     attributes: [
       [
