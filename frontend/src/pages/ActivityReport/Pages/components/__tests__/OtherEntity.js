331d43334c5749f69101d7d8d26ec06be9a8881a
Matt Bevilacqua <thewatermethod@gmail.com>
2023-03-20 10:51:41 -0400

    update additional UI test

--- frontend/src/pages/ActivityReport/Pages/components/__tests__/OtherEntity.js
+++ frontend/src/pages/ActivityReport/Pages/components/__tests__/OtherEntity.js
@@ -1,11 +1,14 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import '@testing-library/jest-dom';
 import userEvent from '@testing-library/user-event';
-import { render, screen, waitFor } from '@testing-library/react';
+import {
+  render, screen, waitFor, act,
+} from '@testing-library/react';
 import React from 'react';
 import fetchMock from 'fetch-mock';
 import { FormProvider, useForm } from 'react-hook-form/dist/index.ie11';
 import OtherEntity from '../OtherEntity';
+import AppLoadingContext from '../../../../../AppLoadingContext';
 
 // eslint-disable-next-line react/prop-types
 const RenderOtherEntity = ({ objectivesWithoutGoals }) => {
@@ -18,7 +21,14 @@ const RenderOtherEntity = ({ objectivesWithoutGoals }) => {
 
   return (
     <FormProvider {...hookForm}>
-      <OtherEntity recipientIds={[]} onSaveDraft={jest.fn()} reportId="123" />
+      <AppLoadingContext.Provider value={{ setIsAppLoading: jest.fn }}>
+        <OtherEntity
+          recipientIds={[]}
+          onSaveDraft={jest.fn()}
+          reportId="123"
+          regionId={1}
+        />
+      </AppLoadingContext.Provider>
     </FormProvider>
   );
 };
@@ -44,6 +54,22 @@ describe('OtherEntity', () => {
   beforeEach(async () => {
     fetchMock.restore();
     fetchMock.get('/api/topic', []);
+    fetchMock.post('/api/objectives/other-entity/new', {
+      id: 1,
+      label: '',
+      activityReportObjectives: [],
+      files: [],
+      topics: [],
+      activityReports: [],
+      resources: [],
+      value: 1,
+      ids: [1],
+      recipientIds: [1],
+      isNew: false,
+      status: 'Not Started',
+      onApprovedAR: false,
+      title: '',
+    });
   });
   it('renders created objectives', async () => {
     render(<RenderOtherEntity objectivesWithoutGoals={objectives} />);
@@ -61,9 +87,13 @@ describe('OtherEntity', () => {
   it('the button adds a new objective', async () => {
     render(<RenderOtherEntity objectivesWithoutGoals={[]} />);
     const button = await screen.findByRole('button', { name: /Add new objective/i });
-    userEvent.click(button);
-    expect(screen.queryAllByText(/objective status/i).length).toBe(1);
-    userEvent.click(button);
+    act(() => {
+      userEvent.click(button);
+    });
+    await waitFor(() => expect(screen.queryAllByText(/objective status/i).length).toBe(1));
+    act(() => {
+      userEvent.click(button);
+    });
     await waitFor(() => expect(screen.queryAllByText(/objective status/i).length).toBe(2));
   });
 });
