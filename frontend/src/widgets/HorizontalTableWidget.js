2387cbf6b855acd750531302b9bd4212e9bed033
Adam Levin <adam.levin@adhocteam.us>
2023-03-21 08:54:00 -0400

    remove lint ignore

--- frontend/src/widgets/HorizontalTableWidget.js
+++ frontend/src/widgets/HorizontalTableWidget.js
@@ -1,5 +1,4 @@
 /* eslint-disable react/no-array-index-key */
-/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Table } from '@trussworks/react-uswds';
