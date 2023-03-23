35dc87ff1d494c43e6750a96f3e2545b289bbef2
Matt Bevilacqua <thewatermethod@gmail.com>
2023-03-23 10:25:26 -0400

    tweak test for services

--- src/services/rttapa.test.js
+++ src/services/rttapa.test.js
@@ -1,3 +1,4 @@
+import db from '../models';
 import {
   newRttapa,
   rttapa,
@@ -5,6 +6,13 @@ import {
 } from './rttapa';
 
 describe('rttapa service', () => {
+  let reportId;
+
+  afterAll(async () => {
+    await db.RttapaPilot.destroy({ where: { id: reportId } });
+    await db.sequelize.close();
+  });
+
   describe('newRttapa', () => {
     it('includes the correct fields', async () => {
       const report = await newRttapa(1, {
@@ -12,12 +20,16 @@ describe('rttapa service', () => {
         recipientId: 1,
         goalIds: [1, 2, 3],
         notes: '',
+        reviewDate: '2021-01-01',
       });
+
+      reportId = report.id;
 
       expect(report).toHaveProperty('regionId');
       expect(report).toHaveProperty('recipientId');
       expect(report).toHaveProperty('goals');
       expect(report).toHaveProperty('notes');
+      expect(report).toHaveProperty('reviewDate');
     });
   });
 
@@ -29,18 +41,20 @@ describe('rttapa service', () => {
       expect(report).toHaveProperty('recipientId');
       expect(report).toHaveProperty('goals');
       expect(report).toHaveProperty('notes');
+      expect(report).toHaveProperty('reviewDate');
     });
   });
 
   describe('allRttapas', () => {
     it('includes the correct fields', async () => {
-      const reports = await allRttapas(1, 1);
+      const reports = await allRttapas(1, 1, { sortBy: 'reviewDate', direction: 'desc' });
 
       reports.forEach((report) => {
         expect(report).toHaveProperty('regionId');
         expect(report).toHaveProperty('recipientId');
         expect(report).toHaveProperty('goals');
         expect(report).toHaveProperty('notes');
+        expect(report).toHaveProperty('reviewDate');
       });
     });
   });
