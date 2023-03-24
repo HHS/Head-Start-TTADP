58c4cfe856fc5c432dd1d1708508abfcf02dd61d
Matt Bevilacqua <thewatermethod@gmail.com>
2023-03-24 09:13:43 -0400

    extract query params instead of request body

--- src/routes/goalTemplates/handlers.ts
+++ src/routes/goalTemplates/handlers.ts
@@ -1,12 +1,18 @@
 /* eslint-disable import/prefer-default-export */
 import { Request, Response } from 'express';
+import { DECIMAL_BASE } from '../../constants';
 import handleErrors from '../../lib/apiErrorHandler';
 import { getCuratedTemplates } from '../../services/goalTemplates';
 
 export async function getGoalTemplates(req: Request, res: Response) {
   try {
-    const { grantIds } = req.body;
-    const templates = await getCuratedTemplates(grantIds);
+    const { grantIds } = req.params;
+
+    // ensure we only pass numbers to the service
+    const parsedGrantIds = [grantIds].flat().map((id: string) => parseInt(id, DECIMAL_BASE))
+      .filter((id: number) => !Number.isNaN(id));
+
+    const templates = await getCuratedTemplates(parsedGrantIds);
     res.json(templates);
   } catch (err) {
     await handleErrors(req, res, err, 'goalTemplates.getGoalTemplates');
