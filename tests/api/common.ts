d5f699684a1b5a3be00ff7e468549438ba6f5ed6
Jonathan Pyers <pyersjonathan@gmail.com>
2023-03-28 14:05:30 -0600

    cleanup

--- tests/api/common.ts
+++ tests/api/common.ts
@@ -1,8 +1,1 @@
 export const root = `http://localhost:8080/api`;
-
-// test('/settings/email', async ({ request }) => {
-//   const response = await request.get(`${root}/settings`);
-//   const body = await response.body();
-//   const json = JSON.parse(String(body));
-//   console.log(json);
-// });
