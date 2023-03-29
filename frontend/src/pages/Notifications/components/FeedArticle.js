7e0eaae969507d5b71330017c1c9efc45d79d90c
Matt Bevilacqua <thewatermethod@gmail.com>
2023-03-29 10:03:27 -0400

    Finish up FE tests

--- frontend/src/pages/Notifications/components/FeedArticle.js
+++ frontend/src/pages/Notifications/components/FeedArticle.js
@@ -11,7 +11,7 @@ const FeedArticle = ({
   <article className={`ttahub-feed-article ttahub-feed-article__whats-new position-relative margin-bottom-3 ${unread ? 'ttahub-feed-article__whats-new--unread' : ''}`}>
     <span className="ttahub-feed-article__whats-new--date font-body-xs padding-left-4">{published.format(FULL_DATE_FORMAT)}</span>
     <div className="ttahub-feed-article__whats-new-content position-relative padding-left-4">
-      <h4 className="usa-prose margin-0 padding-0">{title}</h4>
+      <h4 className="ttahub-feed-article__whats-new-title usa-prose margin-0 padding-0">{title}</h4>
       <ReadOnlyEditor value={content} ariaLabel={title} />
     </div>
   </article>
