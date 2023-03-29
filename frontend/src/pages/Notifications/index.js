12ace73ea706852fe2bb2cbd92ac3db5e215ce88
Matt Bevilacqua <thewatermethod@gmail.com>
2023-03-29 16:55:34 -0400

    fix top of notifications page

--- frontend/src/pages/Notifications/index.js
+++ frontend/src/pages/Notifications/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
-import { Link } from 'react-router-dom';
 import WhatsNew from './components/WhatsNew';
-import colors from '../../colors';
+import BackLink from '../../components/BackLink';
 import './Notifications.scss';
 
 export default function Notifications({ notifications }) {
@@ -14,12 +11,9 @@ export default function Notifications({ notifications }) {
   return (
     <div>
       {referrer && (
-        <>
-          <FontAwesomeIcon className="margin-right-1" data-testid="back-link-icon" color={colors.ttahubMediumBlue} icon={faArrowLeft} />
-          <Link className=" margin-bottom-2 display-inline-block" to={decodeURIComponent(referrer)}>Back</Link>
-        </>
+        <BackLink to={decodeURIComponent(referrer)}>Back</BackLink>
       )}
-      <h1 className="landing">Notifications</h1>
+      <h1 className="landing margin-top-0 margin-bottom-3">Notifications</h1>
       <WhatsNew data={notifications.whatsNew} />
     </div>
   );
