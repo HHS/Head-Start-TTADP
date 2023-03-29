283ac6296f92825e559b40e0265515fc5f25d710
Matt Bevilacqua <thewatermethod@gmail.com>
2023-03-29 12:26:38 -0400

    remove log

--- frontend/src/pages/Notifications/components/WhatsNew.js
+++ frontend/src/pages/Notifications/components/WhatsNew.js
@@ -34,10 +34,7 @@ export const formatWhatsNew = (feed) => {
   let alreadyRead = [];
 
   try {
-    console.log(window.localStorage.getItem());
-
     const storage = window.localStorage.getItem(LOCAL_STORAGE_KEY);
-    console.log('storage', storage);
     if (storage) {
       alreadyRead = JSON.parse(storage);
     }
