ea0b199dae02887f13ada2fe7f9107a3abccb98f
Adam Levin <adam.levin@adhocteam.us>
2023-03-30 17:57:09 -0400

    clean up and added counts

--- src/migrations/20230328124028-remove-orphan-goals.js
+++ src/migrations/20230328124028-remove-orphan-goals.js
@@ -23,7 +23,8 @@ module.exports = {
             page that are NOT associated with a report.
         */
         -- 1.) Create Orphan GOALS Id's Temp Table.
-        SELECT G."id" AS "goalId" INTO TEMP TEMP_ORPHAN_GOAL_IDS
+        SELECT G."id" AS "goalId"
+        INTO TEMP TEMP_ORPHAN_GOAL_IDS
         FROM "Goals" G
         WHERE COALESCE(G."createdVia",
                       'activityReport') = 'activityReport'
@@ -34,7 +35,8 @@ module.exports = {
               FROM "ActivityReportGoals");
 
         -- 2.) Create Orphan OBJECTIVE Id's Temp Table.
-        SELECT * INTO TEMP TEMP_ORPHAN_OBJECTIVE_IDS
+        SELECT *
+        INTO TEMP TEMP_ORPHAN_OBJECTIVE_IDS
         FROM
           (
             -- Orphan Objectives linked to orphan Goals.
@@ -52,22 +54,32 @@ module.exports = {
               AND COALESCE(O."createdVia", 'activityReport') = 'activityReport' ) AS TMP;
 
         -- 3.) Clean Objective Orphan Topics.
+        WITH deletedObjectiveTopics AS (
         DELETE
         FROM "ObjectiveTopics"
         WHERE "objectiveId" IN
             (SELECT "objectiveId"
-              FROM TEMP_ORPHAN_OBJECTIVE_IDS);
+              FROM TEMP_ORPHAN_OBJECTIVE_IDS)
+        RETURNING *)
+        SELECT 'Objective Topics', count(*)
+        INTO TEMP temp_results_count FROM deletedObjectiveTopics;
 
         -- 4.) Clean Objective Orphan Resources.
-        DELETE
-        FROM "ObjectiveResources"
-        WHERE "objectiveId" IN
-            (SELECT "objectiveId"
-              FROM TEMP_ORPHAN_OBJECTIVE_IDS);
+        WITH objectiveResources AS (
+          DELETE
+          FROM "ObjectiveResources"
+          WHERE "objectiveId" IN
+              (SELECT "objectiveId"
+                FROM TEMP_ORPHAN_OBJECTIVE_IDS)
+        RETURNING *)
+        INSERT INTO temp_results_count
+        SELECT 'Objective Resources', count(*)
+        temp_results_count FROM objectiveResources;
 
         -- 5.) Clean Objective Orphan Files.
         -- 5a.) Create Temp Table of Files being used by reports or non-orphan Objectives.
-        SELECT * INTO TEMP_USED_OBJECTIVE_FILES_IDS
+        SELECT *
+        INTO TEMP TEMP_USED_OBJECTIVE_FILES_IDS
         FROM
           (
             SELECT o."fileId"
@@ -82,76 +94,110 @@ module.exports = {
             SELECT DISTINCT ARF."fileId"
             FROM "ActivityReportFiles" ARF) AS TMP;
 
-        -- 5b.) Clean Objective Files.
-        DELETE
-        FROM "ObjectiveFiles"
-        WHERE "objectiveId" IN
-            (SELECT "objectiveId"
-              FROM TEMP_ORPHAN_OBJECTIVE_IDS);
+      -- 5b.) Clean Objective Files.
+      WITH objectiveFiless AS (
+          DELETE
+          FROM "ObjectiveFiles"
+          WHERE "objectiveId" IN
+              (SELECT "objectiveId"
+                FROM TEMP_ORPHAN_OBJECTIVE_IDS)
+      RETURNING *)
+      INSERT INTO temp_results_count
+      SELECT 'Objective Files', count(*)
+      FROM objectiveFiless;
 
-        -- 5c.) Clean files only used by Orphan Objectives.
-        DELETE
-        FROM "Files"
-        WHERE "id" NOT IN
-            (SELECT "fileId"
-              FROM TEMP_USED_OBJECTIVE_FILES_IDS);
+      -- 5c.) Clean files only used by Orphan Objectives.
+      WITH files AS (
+          DELETE
+          FROM "Files"
+          WHERE "id" NOT IN
+              (SELECT "fileId"
+                FROM TEMP_USED_OBJECTIVE_FILES_IDS)
+      RETURNING *)
+      INSERT INTO temp_results_count
+      SELECT 'Files', count(*)
+      FROM files;
 
-        -- 6.) Clean Objective Orphan's.
-        DELETE
-        FROM "Objectives"
-        WHERE "id" IN
-            (SELECT "objectiveId"
-              FROM TEMP_ORPHAN_OBJECTIVE_IDS);
+      -- 6.) Clean Objective Orphan's.
+      WITH objectives AS (
+          DELETE
+          FROM "Objectives"
+          WHERE "id" IN
+              (SELECT "objectiveId"
+                FROM TEMP_ORPHAN_OBJECTIVE_IDS)
+      RETURNING *)
+      INSERT INTO temp_results_count
+      SELECT 'Objectives', count(*)
+      FROM objectives;
 
-        -- 7.) Clean Goal Orphan Resources.
-        -- 7a.) Create Temp Table of Resources being used by reports or non-orphan Objectives.
-        SELECT * INTO TEMP_USED_OBJECTIVE_RESOURCE_IDS
-        FROM
-          (
-            SELECT O."resourceId"
-            FROM "ObjectiveResources" O
-            WHERE O."objectiveId" NOT IN
-                (SELECT "objectiveId"
-                  FROM TEMP_ORPHAN_OBJECTIVE_IDS)
-            UNION
-            SELECT DISTINCT ARR."resourceId"
-            FROM "ActivityReportResources" ARR
-            UNION
-            SELECT DISTINCT NSR."resourceId"
-            FROM "NextStepResources" NSR
-            UNION
-            SELECT DISTINCT OTR."resourceId"
-            FROM "ObjectiveTemplateResources" OTR
-            UNION
-            SELECT DISTINCT AROR."resourceId"
-            FROM "ActivityReportObjectiveResources" AROR) AS TMP;
+      -- 7.) Clean Goal Orphan Resources.
+      -- 7a.) Create Temp Table of Resources being used by reports or non-orphan Objectives.
+          SELECT *
+      INTO TEMP TEMP_USED_OBJECTIVE_RESOURCE_IDS
+          FROM
+            (
+              SELECT O."resourceId"
+              FROM "ObjectiveResources" O
+              WHERE O."objectiveId" NOT IN
+                  (SELECT "objectiveId"
+                    FROM TEMP_ORPHAN_OBJECTIVE_IDS)
+              UNION
+              SELECT DISTINCT ARR."resourceId"
+              FROM "ActivityReportResources" ARR
+              UNION
+              SELECT DISTINCT NSR."resourceId"
+              FROM "NextStepResources" NSR
+              UNION
+              SELECT DISTINCT OTR."resourceId"
+              FROM "ObjectiveTemplateResources" OTR
+              UNION
+              SELECT DISTINCT AROR."resourceId"
+              FROM "ActivityReportObjectiveResources" AROR) AS TMP;
 
-        -- 7b.) Clean Objective Resources.
-        DELETE
-        FROM "GoalResources"
-        WHERE "goalId" IN
-            (SELECT "goalId"
-              FROM TEMP_ORPHAN_GOAL_IDS);
+      -- 7b.) Clean Objective Resources.
+      WITH goalResources AS (
+          DELETE
+          FROM "GoalResources"
+          WHERE "goalId" IN
+              (SELECT "goalId"
+                FROM TEMP_ORPHAN_GOAL_IDS)
+      RETURNING *)
+      INSERT INTO temp_results_count
+      SELECT 'Goal Resources', count(*)
+      FROM goalResources;
 
-        -- 7c.) Clean resources only used by Orphan Objectives.
-        DELETE
-        FROM "Resources"
-        WHERE "id" NOT IN
-            (SELECT "resourceId"
-              FROM TEMP_USED_OBJECTIVE_RESOURCE_IDS);
+      -- 7c.) Clean resources only used by Orphan Objectives.
+      WITH resources AS (
+          DELETE
+          FROM "Resources"
+          WHERE "id" NOT IN
+              (SELECT "resourceId"
+                FROM TEMP_USED_OBJECTIVE_RESOURCE_IDS)
+      RETURNING *)
+      INSERT INTO temp_results_count
+      SELECT 'Resources', count(*)
+      FROM resources;
 
-        -- 8.) Clean Goal Orphan's.
-        DELETE
-        FROM "Goals"
-        WHERE "id" IN
-            (SELECT "goalId"
-              FROM TEMP_ORPHAN_GOAL_IDS);
+      -- 8.) Clean Goal Orphan's.
+      WITH goals AS (
+            DELETE
+            FROM "Goals"
+            WHERE "id" IN
+                (SELECT "goalId"
+                  FROM TEMP_ORPHAN_GOAL_IDS)
+        RETURNING *)
+        INSERT INTO temp_results_count
+        SELECT 'Goals', count(*)
+        FROM goals;
 
         -- 9.) DROP temp tables.
         DROP TABLE TEMP_ORPHAN_GOAL_IDS;
         DROP TABLE TEMP_ORPHAN_OBJECTIVE_IDS;
         DROP TABLE TEMP_USED_OBJECTIVE_FILES_IDS;
         DROP TABLE TEMP_USED_OBJECTIVE_RESOURCE_IDS;
+
+        -- 10.) Select Results.
+        SELECT * FROM temp_results_count;
         `,
         { transaction },
       );
