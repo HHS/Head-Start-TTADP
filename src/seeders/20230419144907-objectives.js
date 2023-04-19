f0124b1480a068e044d8321d493e97ee34680a1e
Jonathan Pyers <pyersjonathan@gmail.com>
2023-04-19 08:55:10 -0600

    add objective seeder

---
+++ src/seeders/20230419144907-objectives.js
@@ -0,0 +1,19 @@
+const objectives = [
+  {
+    id: 1,
+    title: 'Objective 1',
+    status: 'Draft',
+    onApprovedAR: false,
+  },
+];
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Objectives', objectives, {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Objectives', null, {});
+  }
+};
