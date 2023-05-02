0abf6a396652561937902871e457e617f14a154f
Jonathan Pyers <pyersjonathan@gmail.com>
2023-05-02 08:43:59 -0400

    add filter associations for additional resource tables

--- src/scopes/activityReport/reportText.js
+++ src/scopes/activityReport/reportText.js
@@ -33,6 +33,38 @@ INNER JOIN "Resources" "Resources"
 ON "Resources"."id" = "ActivityReportObjectiveResources"."resourceId"
 WHERE "Resources"."url"`;
 
+const activityReportResource = `
+SELECT DISTINCT
+  "ActivityReportResources"."activityReportId"
+FROM "ActivityReportResources" "ActivityReportResources"
+INNER JOIN "Resources" "Resources"
+ON "Resources"."id" = "ActivityReportResources"."resourceId"
+WHERE "Resources"."url"`;
+
+const activityReportGoalResource = `
+SELECT DISTINCT
+  "ActivityReportGoalResources"."activityReportId"
+FROM "ActivityReportGoalResources" "ActivityReportGoalResources"
+INNER JOIN "Resources" "Resources"
+ON "Resources"."id" = "ActivityReportGoalResources"."resourceId"
+WHERE "Resources"."url"`;
+
+const activityReportObjectiveResource = `
+SELECT DISTINCT
+  "ActivityReportObjectiveResources"."activityReportId"
+FROM "ActivityReportObjectiveResources" "ActivityReportObjectiveResources"
+INNER JOIN "Resources" "Resources"
+ON "Resources"."id" = "ActivityReportObjectiveResources"."resourceId"
+WHERE "Resources"."url"`;
+
+const nextStepsResource = `
+SELECT DISTINCT
+  "NextStepsResources"."activityReportId"
+FROM "NextStepsResources" "NextStepsResources"
+INNER JOIN "Resources" "Resources"
+ON "Resources"."id" = "NextStepsResources"."resourceId"
+WHERE "Resources"."url"`;
+
 export function withReportText(searchText) {
   return {
     [Op.or]: [
@@ -41,6 +73,10 @@ export function withReportText(searchText) {
       filterAssociation(objectiveTitle, searchText, false, 'ILIKE'),
       filterAssociation(objectiveTtaProvided, searchText, false, 'ILIKE'),
       filterAssociation(goalResources, searchText, false, 'ILIKE'),
+      filterAssociation(activityReportResource, searchText, false, 'ILIKE'),
+      filterAssociation(activityReportGoalResource, searchText, false, 'ILIKE'),
+      filterAssociation(activityReportObjectiveResource, searchText, false, 'ILIKE'),
+      filterAssociation(nextStepsResource, searchText, false, 'ILIKE'),
     ],
   };
 }
@@ -53,6 +89,10 @@ export function withoutReportText(searchText) {
       filterAssociation(objectiveTitle, searchText, false, 'NOT ILIKE'),
       filterAssociation(objectiveTtaProvided, searchText, false, 'NOT ILIKE'),
       filterAssociation(goalResources, searchText, false, 'NOT ILIKE'),
+      filterAssociation(activityReportResource, searchText, false, 'NOT ILIKE'),
+      filterAssociation(activityReportGoalResource, searchText, false, 'NOT ILIKE'),
+      filterAssociation(activityReportObjectiveResource, searchText, false, 'NOT ILIKE'),
+      filterAssociation(nextStepsResource, searchText, false, 'NOT ILIKE'),
     ],
   };
 }
