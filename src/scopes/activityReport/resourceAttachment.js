0edfd217ffc7058c314ce890d4d4c5a85ba90bee
Jonathan Pyers <pyersjonathan@gmail.com>
2023-05-12 11:51:07 -0400

    add objective files

--- src/scopes/activityReport/resourceAttachment.js
+++ src/scopes/activityReport/resourceAttachment.js
@@ -15,12 +15,29 @@ const activityReportFilesPosNeg = (pos = true) => {
   WHERE "Files"."originalFileName"${a}`;
 };
 
+const activityReportObjectiveFilesPosNeg = (pos = true) => {
+  const a = pos ? '' : ' IS NULL OR "Files"."originalFileName"';
+
+  return `
+  SELECT DISTINCT
+    "ActivityReports"."id"
+  FROM "ActivityReports"
+  LEFT JOIN "ActivityReportObjectives"
+  ON "ActivityReportObjectives"."activityReportId" = "ActivityReports"."id"
+  LEFT JOIN "ActivityReportObjectiveFiles"
+  ON "ActivityReportObjectiveFiles"."activityReportObjectiveId" = "ActivityReportObjectives"."id"
+  LEFT JOIN "Files"
+  ON "Files"."id" = "ActivityReportObjectiveFiles"."fileId"
+  WHERE "Files"."originalFileName"${a}`;
+};
+
 export function withResourceAttachment(query) {
   const search = [`%${query}%`];
 
   return {
     [Op.or]: [
       filterAssociation(activityReportFilesPosNeg(true), search, false, 'ILIKE'),
+      filterAssociation(activityReportObjectiveFilesPosNeg(true), search, false, 'ILIKE'),
     ],
   };
 }
@@ -31,6 +48,7 @@ export function withoutResourceAttachment(query) {
   return {
     [Op.and]: [
       filterAssociation(activityReportFilesPosNeg(false), search, false, 'NOT ILIKE'),
+      filterAssociation(activityReportObjectiveFilesPosNeg(false), search, false, 'NOT ILIKE'),
     ],
   };
 }
