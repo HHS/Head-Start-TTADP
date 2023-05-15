792c5d5bce86b6987762b708696b431c25b65511
Jonathan Pyers <pyersjonathan@gmail.com>
2023-05-15 09:26:38 -0400

    add test for resourceUrl scope

--- src/scopes/activityReport/index.test.js
+++ src/scopes/activityReport/index.test.js
@@ -11,9 +11,11 @@ import { auditLogger } from '../../logger';
 import db, {
   ActivityReport,
   ActivityReportApprover,
+  ActivityReportResource,
   ActivityRecipient,
   User,
   Recipient,
+  Resource,
   Grant,
   ActivityReportCollaborator,
   OtherEntity,
@@ -35,6 +37,7 @@ import {
   createIndex,
   addIndexDocument,
 } from '../../lib/awsElasticSearch/index';
+import { findOrCreateResources, processActivityReportForResourcesById } from '../../services/resource';
 
 const mockUser = {
   id: faker.datatype.number(),
@@ -2180,6 +2183,137 @@ describe('filtersToScopes', () => {
           excludedReport.id,
           globallyExcludedReport.id,
         ]));
+    });
+  });
+
+  describe('resourceUrl', () => {
+    let reportOne;
+    let reportTwo;
+    let reportOneWasCreated;
+    let reportTwoWasCreated;
+    let arOneResources;
+    let arTwoResources;
+
+    const reportOneUrls = [
+      'http://google.com',
+      'http://github.com',
+      'http://cloud.gov',
+      'https://adhocteam.us/',
+    ];
+
+    const reportTwoUrls = [
+      'http://www.crayola.com',
+    ];
+
+    beforeAll(async () => {
+      [reportOne, reportOneWasCreated] = await ActivityReport.findOrCreate({
+        where: {
+          id: 99_998,
+        },
+        defaults: {
+          context: '',
+          submissionStatus: REPORT_STATUSES.DRAFT,
+          calculatedStatus: REPORT_STATUSES.DRAFT,
+          numberOfParticipants: 1,
+          deliveryMethod: 'method',
+          duration: 0,
+          endDate: '2020-01-01T12:00:00Z',
+          startDate: '2020-01-01T12:00:00Z',
+          requester: 'requester',
+          regionId: 1,
+          targetPopulations: [],
+        },
+        individualHooks: true,
+        raw: true,
+      });
+      await findOrCreateResources(reportOneUrls);
+      arOneResources = await processActivityReportForResourcesById(
+        reportOne.id,
+        reportOneUrls,
+      );
+
+      [reportTwo, reportTwoWasCreated] = await ActivityReport.findOrCreate({
+        where: {
+          id: 99_999,
+        },
+        defaults: {
+          context: '',
+          submissionStatus: REPORT_STATUSES.DRAFT,
+          calculatedStatus: REPORT_STATUSES.DRAFT,
+          numberOfParticipants: 1,
+          deliveryMethod: 'method',
+          duration: 0,
+          endDate: '2020-01-01T12:00:00Z',
+          startDate: '2020-01-01T12:00:00Z',
+          requester: 'requester',
+          regionId: 1,
+          targetPopulations: [],
+        },
+        individualHooks: true,
+        raw: true,
+      });
+      await findOrCreateResources(reportTwoUrls);
+      arTwoResources = await processActivityReportForResourcesById(
+        reportTwo.id,
+        reportTwoUrls,
+      );
+    });
+
+    afterAll(async () => {
+      await ActivityReportResource.destroy({
+        where: { activityReportId: reportOne.id },
+        individualHooks: true,
+      });
+      await ActivityReportResource.destroy({
+        where: { activityReportId: reportTwo.id },
+        individualHooks: true,
+      });
+      await Resource.destroy({
+        where: { url: { [Op.in]: [...reportOneUrls, ...reportTwoUrls] } },
+        individualHooks: true,
+      });
+      if (reportOneWasCreated) {
+        await ActivityReport.destroy({
+          where: { id: reportOne.id },
+          individualHooks: true,
+        });
+      }
+      if (reportTwoWasCreated) {
+        await ActivityReport.destroy({
+          where: { id: reportTwo.id },
+          individualHooks: true,
+        });
+      }
+    });
+
+    it('returns correct resource url filter search results', async () => {
+      const filters = { 'resourceUrl.ctn': ['google'] };
+      const { activityReport: scope } = await filtersToScopes(filters);
+      const found = await ActivityReport.findAll({
+        where: {
+          [Op.and]: [
+            scope,
+            { id: [reportOne.id, reportTwo.id] },
+          ],
+        },
+      });
+      expect(found.length).toBe(1);
+      expect(found.map((f) => f.id)).toEqual(expect.arrayContaining([reportOne.id]));
+    });
+
+    it('excludes correct resource url filter search results', async () => {
+      const filters = { 'resourceUrl.nctn': ['http'] };
+      const { activityReport: scope } = await filtersToScopes(filters);
+
+      const found = await ActivityReport.findAll({
+        where: {
+          [Op.and]: [
+            scope,
+            { id: [reportOne.id, reportTwo.id] },
+          ],
+        },
+      });
+      expect(found.length).toBe(0);
     });
   });
 
