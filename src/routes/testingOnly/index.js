1f63b3b7dc1bc4e9ffd4fdf8aa478613fcc349c4
GarrettEHill <garretthill@gmail.com>
2023-05-17 06:22:37 -0700

    fix lint

--- src/routes/testingOnly/index.js
+++ src/routes/testingOnly/index.js
@@ -5,6 +5,6 @@ import testingOnly from '../../middleware/testingOnlyMiddleware';
 
 const router = express.Router();
 // must run outside of transaction wrapper as some of the action will run in a child process
-router.get('/reseed',testingOnly, reseedDB);
+router.get('/reseed', testingOnly, reseedDB);
 
 export default router;
