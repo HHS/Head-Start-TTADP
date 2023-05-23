263b5b95caba57c33625152cf130dbd4907c0f1e
Adam Levin <adam.levin@adhocteam.us>
2023-05-23 10:13:30 -0400

    event import

---
+++ src/tools/importSmartSheetEvent.js
@@ -0,0 +1,83 @@
+/* eslint-disable no-restricted-syntax */
+/* eslint-disable no-loop-func */
+import parse from 'csv-parse/lib/sync';
+import { Op } from 'sequelize';
+import { downloadFile } from '../lib/s3';
+import {
+  sequelize,
+  User,
+  EventReportPilot,
+} from '../models';
+import { logger } from '../logger';
+
+async function parseCsv(fileKey) {
+  let smartSheetEvents = {};
+  const { Body: csv } = await downloadFile(fileKey);
+  [...smartSheetEvents] = parse(csv, {
+    skipEmptyLines: true,
+    columns: true,
+  });
+  return smartSheetEvents;
+}
+
+export default async function importSmartSheetEvent(fileKey) {
+  const smartSheetEvents = await parseCsv(fileKey);
+
+  try {
+    logger.info(`>>> Starting processing of ${smartSheetEvents.length} SmartSheet Events`);
+    logger.info('\n\n\n-----File to Import123: ', smartSheetEvents);
+    for await (const smartSheetEvent of smartSheetEvents) {
+      logger.info(`Processing Event ID: ${smartSheetEvent['Event ID']} - Creator: ${smartSheetEvent.Creator}`);
+
+      // Get the region number from the Event ID.
+      const eventId = smartSheetEvent['Event ID'];
+      let regionId = eventId.split('-')[0];
+      // Remove alpha characters.
+      regionId = regionId.replace(/\D/g, '');
+
+      // Get user via creator email.
+      const creatorEmail = smartSheetEvent.Creator;
+      const creator = await User.findOne({
+        where: {
+          email: creatorEmail.toLowerCase(),
+        },
+      });
+
+      if (!creator) {
+        logger.info(`Creator Not Found: '${creatorEmail}' not found in User table. Skipping...`);
+        // eslint-disable-next-line no-continue
+        continue;
+      }
+      const ownerId = creator.id;
+
+      // Check for an existing EventReportPilot record.
+      const existingEventReportPilot = await EventReportPilot.findOne({
+        where: {
+          ownerId,
+          id: {
+            [Op.in]: sequelize.literal(
+              `(SELECT id FROM "EventReportPilots" WHERE data->>'Event ID' = '${eventId}')`,
+            ),
+          },
+        },
+      });
+      if (existingEventReportPilot) {
+        logger.info(`Event ID: '${eventId}' already exists in EventReportPilot table. Skipping...`);
+        // eslint-disable-next-line no-continue
+        continue;
+      } else {
+        await EventReportPilot.create({
+          pocId: 1,
+          collaboratorIds: [1],
+          ownerId,
+          regionId,
+          data: sequelize.cast(JSON.stringify(smartSheetEvent), 'jsonb'),
+        });
+      }
+    }
+    logger.info(`<<< Success! Finished processing of ${smartSheetEvents.length} SmartSheet Events`);
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  }
+}
