340085d55a8e81390c71ad282ada1181db7116e6
Matt Bevilacqua <thewatermethod@gmail.com>
2023-05-30 15:09:00 -0400

    add comments to widget card

--- frontend/src/components/WidgetCard.js
+++ frontend/src/components/WidgetCard.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+/**
+ * the most generic possible implementation of a card component for the widget
+ * takes in an arbitrary header, body, and footer and renders them in a "card",
+ * I.E. a rounded rectangle with a shadow
+ *
+ * the idea is that this component can be used to wrap any widget, and the margin, padding,
+ * and other styling will be consistent. Hopefully can extend existing widgets to use this
+ * and add specialized variations of this component as needed
+ *
+ * @param {props} see proptypes below
+ * @returns react component
+ */
 export default function WidgetCard({
   header,
   children,
