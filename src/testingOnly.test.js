0cd2518c9e4438859ee5b3a6fdbc68c8012e6a95
GarrettEHill <garretthill@gmail.com>
2023-06-01 00:22:39 -0700

    Create testingOnly.test.js

---
+++ src/testingOnly.test.js
@@ -0,0 +1,39 @@
+describe('testingOnly', () => {
+  test('load testingOnly', async () => {
+    jest.mock('express', () => {
+      const use = jest.fn((route, callback) => {
+        expect(route).toEqual('/testingOnly');
+        expect(callback).toBeDefined();
+      });
+
+      const listen = jest.fn((port, name, callback) => {
+        expect(port).toEqual(9999);
+        expect(name).toEqual('localhost');
+        expect(callback).toBeDefined();
+      });
+
+      const get = jest.fn(() => {});
+
+      const mockRouter = {
+        get,
+      };
+
+      function createExpressInstance() {
+        const instance = () => {};
+        instance.use = use;
+        instance.listen = listen;
+        instance.Router = jest.fn(() => mockRouter);
+        return instance;
+      }
+
+      const express = jest.fn(createExpressInstance);
+
+      express.Router = jest.fn(() => mockRouter);
+
+      return express;
+    });
+
+    // eslint-disable-next-line global-require
+    const testingOnly = require('./testingOnly');
+  });
+});
