4da9728452318b4b0982a3db2bfcb65a2aabea9a
Jonathan Pyers <pyersjonathan@gmail.com>
2023-06-01 16:08:43 -0400

    add query helper and use it

--- tests/utils/common.ts
+++ tests/utils/common.ts
@@ -5,3 +5,17 @@ export const reseed = async (request: APIRequestContext) => {
   expect(response.status()).toBe(200);
   return response;
 };
+
+export const query = async (request: APIRequestContext, command: string) => {
+  // make a post request with this command body:
+  const response = await request.post(
+    'http://localhost:9999/testingOnly/query',
+    {
+      data: {
+        command,
+      },
+    },
+  );
+  expect(response.status()).toBe(200);
+  return response;
+};
