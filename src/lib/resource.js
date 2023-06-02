7960c68f66cad0b49d77894aedb58cfc5a9ff0da
Adam Levin <adam.levin@adhocteam.us>
2023-06-02 17:12:07 -0400

    code cleanup

--- src/lib/resource.js
+++ src/lib/resource.js
@@ -4,111 +4,127 @@ import axios from 'axios';
 import { auditLogger, logger } from '../logger';
 import { Resource } from '../models';
 
+const getPageScrapeValues = async (resourceUrl, isEclkc) => {
+  // Use axios to get url info.
+  const res = await axios.get(resourceUrl, { maxRedirects: 3 });
+
+  // Scrape Title & National Centers from ECLKC resources.
+  const foundTitle = res.data.match(/<title[^>]*>([^<]+)<\/title>/);
+  // eslint-disable-next-line max-len
+
+  // Find the meta tag with name="national-centers".
+  const foundNationalCenters = isEclkc ? res.data.match(/<meta[^>]*=*"national-centers"[^>]*content="([^"]+)"[^>]*>/) : null;
+
+  // Update object to save.
+  const updateObj = {};
+
+  // If we found a title, add to update the resource.
+  if (foundTitle && foundTitle.length >= 1) {
+    // Get title.
+    const titleToUpdate = foundTitle[1].trim();
+
+    // Update URL in DB.
+    updateObj.title = titleToUpdate;
+  }
+
+  // If we found a national centers, add to update the resource.
+  if (foundNationalCenters && foundNationalCenters.length >= 1) {
+    // Get national centers.
+    const nationalCentersToUpdate = foundNationalCenters[1].trim();
+
+    // Update URL in DB.
+    updateObj.nationalCenters = nationalCentersToUpdate;
+  }
+
+  // If we have values to update then update the resource, else retry.
+  if (Object.keys(updateObj).length > 0) {
+    const updatedCnt = await Resource.update(updateObj, {
+      where: { url: resourceUrl },
+      individualHooks: false,
+    });
+
+    // If we don't update anything throw an error to retry.
+    if (updatedCnt[0] === 0) {
+      throw Error('Failed to update resource.');
+    }
+
+    return updatedCnt[0];
+  }
+
+  return 0;
+};
+
+const getMetadataValues = async (resourceId, resourceUrl, isEclkc) => {
+  // Attempt to get the resource metadata (if valid ECLKC resource).
+  // Sample: https://eclkc.ohs.acf.hhs.gov/mental-health/article/head-start-heals-campaign?_format=json
+  if (isEclkc) {
+    try {
+      const metadataUrl = `${resourceUrl}?_format=json`;
+      const res = await axios.get(metadataUrl, { maxRedirects: 3 });
+      const metadata = res.data;
+
+      // get created.
+      const { created } = metadata;
+      // get changed.
+      const { changed } = metadata;
+      // get title.
+      const { title } = metadata;
+      // get field_taxonomy_national_centers.
+      // eslint-disable-next-line max-len
+      const fieldTaxonomyNationalCenters = metadata.field_taxonomy_national_centers;
+      // get the field_taxonomy_topic.
+      const fieldTaxonomyTopic = metadata.field_taxonomy_topic;
+      // get the langcode.
+      const { langcode } = metadata;
+      // get the field_context.
+      const fieldContext = metadata.field_context;
+
+      // Create the metadata object.
+      const metadataObj = {
+        created,
+        changed,
+        title,
+        fieldTaxonomyNationalCenters,
+        fieldTaxonomyTopic,
+        langcode,
+        fieldContext,
+      };
+
+      // Update URL in DB.
+      await Resource.update({
+        metadata: metadataObj,
+        metadataUpdatedAt: new Date(),
+      }, {
+        where: { url: resourceUrl },
+        individualHooks: false,
+      });
+    } catch (error) {
+      auditLogger.error(`Resource Queue: Unable to retrieve ECLKC metadata for Resource (ID: ${resourceId} URL: ${resourceUrl}).`, error);
+    }
+  } else {
+    auditLogger.info(`Resource Queue: Warning, not a ECLKC resource SKIPPING metadata collection for: '${resourceUrl}'.`);
+  }
+};
+
 const getResourceMetaDataJob = async (job) => {
   const {
     resourceId, resourceUrl,
   } = job.data;
-  let res;
+
   try {
-    // Determine if this is an ECLKC resource.
+    // 1.) Determine if this is an ECLKC resource.
     const isEclkc = resourceUrl.includes('eclkc.ohs.acf.hhs.gov');
 
-    // Use axios to get url info.
-    res = await axios.get(resourceUrl, { maxRedirects: 3 });
-
-    // Scrape Title & National Centers from ECLKC resources.
-    const foundTitle = res.data.match(/<title[^>]*>([^<]+)<\/title>/);
-    // eslint-disable-next-line max-len
-
-    // Find the meta tag with name="national-centers".
-    const foundNationalCenters = isEclkc ? res.data.match(/<meta[^>]*=*"national-centers"[^>]*content="([^"]+)"[^>]*>/) : null;
-
-    // Update object to save.
-    const updateObj = {};
-
-    // If we found a title, add to update the resource.
-    if (foundTitle && foundTitle.length >= 1) {
-      // Get title.
-      const titleToUpdate = foundTitle[1].trim();
-
-      // Update URL in DB.
-      updateObj.title = titleToUpdate;
-    }
-
-    // If we found a national centers, add to update the resource.
-    if (foundNationalCenters && foundNationalCenters.length >= 1) {
-      // Get national centers.
-      const nationalCentersToUpdate = foundNationalCenters[1].trim();
-
-      // Update URL in DB.
-      updateObj.nationalCenters = nationalCentersToUpdate;
-    }
-
-    // If we have values to update then update the resource, else retry.
-    if (Object.keys(updateObj).length > 0) {
-      const updatedCnt = await Resource.update(updateObj, {
-        where: { url: resourceUrl },
-        individualHooks: false,
-      });
-
-      // If we don't update anything throw an error to retry.
-      if (updatedCnt[0] === 0) {
-        throw Error('Failed to update resource.');
-      }
-    } else {
+    // 2.) Scrape page data (title & resource).
+    const updated = await getPageScrapeValues(resourceUrl, isEclkc);
+    if (updated === 0) {
+      // We want to cause a retry.
       auditLogger.info(`Resource Queue: Warning, unable to retrieve resource title for resource '${resourceUrl}'.`);
       return ({ status: httpCodes.NOT_FOUND, data: { url: resourceUrl } });
     }
 
-    // Attempt to get the resource metadata (if valid ECLKC resource).
-    // Sample: https://eclkc.ohs.acf.hhs.gov/mental-health/article/head-start-heals-campaign?_format=json
-    if (isEclkc) {
-      try {
-        const metadataUrl = `${resourceUrl}?_format=json`;
-        res = await axios.get(metadataUrl, { maxRedirects: 3 });
-        const metadata = res.data;
-
-        // get created.
-        const { created } = metadata;
-        // get changed.
-        const { changed } = metadata;
-        // get title.
-        const { title } = metadata;
-        // get field_taxonomy_national_centers.
-        // eslint-disable-next-line max-len
-        const fieldTaxonomyNationalCenters = metadata.field_taxonomy_national_centers;
-        // get the field_taxonomy_topic.
-        const fieldTaxonomyTopic = metadata.field_taxonomy_topic;
-        // get the langcode.
-        const { langcode } = metadata;
-        // get the field_context.
-        const fieldContext = metadata.field_context;
-
-        // Create the metadata object.
-        const metadataObj = {
-          created,
-          changed,
-          title,
-          fieldTaxonomyNationalCenters,
-          fieldTaxonomyTopic,
-          langcode,
-          fieldContext,
-        };
-
-        // Update URL in DB.
-        await Resource.update({
-          metadata: metadataObj,
-          metadataUpdatedAt: new Date(),
-        }, {
-          where: { url: resourceUrl },
-          individualHooks: false,
-        });
-      } catch (error) {
-        auditLogger.error(`Resource Queue: Unable to retrieve ECLKC metadata for Resource (ID: ${resourceId} URL: ${resourceUrl}).`, error);
-      }
-    } else {
-      auditLogger.info(`Resource Queue: Warning, not a ECLKC resource SKIPPING metadata collection for: '${resourceUrl}'.`);
-    }
+    // 3.) Get metadata (if ECLKC resource).
+    await getMetadataValues(resourceId, resourceUrl, isEclkc);
 
     logger.info(`Resource Queue: Successfully retrieved resource metadata for resource '${resourceUrl}'`);
     return ({ status: httpCodes.OK, data: { url: resourceUrl } });
