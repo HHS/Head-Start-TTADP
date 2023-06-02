4a256d7db121ddc76df953a9f9f51d52f4f7cc73
Matt Bevilacqua <thewatermethod@gmail.com>
2023-06-02 16:26:26 -0400

    add fetcher tests

---
+++ frontend/src/fetchers/__tests__/event.js
@@ -0,0 +1,50 @@
+import fetchMock from 'fetch-mock';
+
+import { eventById, updateEvent } from '../event';
+
+describe('eventById', () => {
+  beforeEach(() => {
+    fetchMock.get('/api/event/id/1', {
+      id: 1,
+      name: 'test event',
+    });
+  });
+
+  afterEach(() => {
+    fetchMock.reset();
+  });
+
+  it('fetches data from the server with the given id', async () => {
+    const event = await eventById(1);
+
+    expect(fetchMock.called()).toBe(true);
+    expect(fetchMock.lastUrl()).toBe('/api/event/id/1');
+    expect(event).toEqual({ id: 1, name: 'test event' });
+  });
+});
+
+describe('updateEvent', () => {
+  beforeEach(() => {
+    fetchMock.put('/api/event/id/1', {
+      id: 1,
+      name: 'updated test event',
+    });
+  });
+
+  afterEach(() => {
+    fetchMock.reset();
+  });
+
+  it('updates the event on the server with the given id and data', async () => {
+    const eventData = { name: 'updated test event' };
+    const event = await updateEvent(1, eventData);
+
+    expect(fetchMock.called()).toBe(true);
+    expect(fetchMock.lastUrl()).toBe('/api/event/id/1');
+
+    const lastOptions = fetchMock.lastOptions();
+    expect(lastOptions.method).toBe('PUT');
+    expect(lastOptions.body).toBe(JSON.stringify(eventData));
+    expect(event).toEqual({ id: 1, name: 'updated test event' });
+  });
+});
