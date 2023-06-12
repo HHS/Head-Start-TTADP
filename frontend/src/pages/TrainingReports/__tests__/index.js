d083c502d06d2c43696b18b451a2392c68b345e0
Adam Levin <adam.levin@adhocteam.us>
2023-06-12 16:00:38 -0400

    FE tests

--- frontend/src/pages/TrainingReports/__tests__/index.js
+++ frontend/src/pages/TrainingReports/__tests__/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   render, screen, act, fireEvent, waitFor,
 } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import join from 'url-join';
 import { Router, MemoryRouter } from 'react-router';
 import { createMemoryHistory } from 'history';
@@ -11,6 +12,9 @@ import TrainingReports from '../index';
 import UserContext from '../../../UserContext';
 import AppLoadingContext from '../../../AppLoadingContext';
 import { EVENT_STATUS } from '../constants';
+import AriaLiveContext from '../../../AriaLiveContext';
+
+const mockAnnounce = jest.fn();
 
 const notStartedEvents = [{
   id: 1,
@@ -77,18 +81,20 @@ describe('TrainingReports', () => {
     const user = u || nonCentralOfficeUser;
     render(
       <Router history={history}>
-        <UserContext.Provider value={{ user }}>
-          <AppLoadingContext.Provider value={
+        <AriaLiveContext.Provider value={{ announce: mockAnnounce }}>
+          <UserContext.Provider value={{ user }}>
+            <AppLoadingContext.Provider value={
             {
               setIsAppLoading: jest.fn(),
               setAppLoadingText: jest.fn(),
               isAppLoading: false,
             }
           }
-          >
-            <TrainingReports match={{ params: { status: passedStatus } }} />
-          </AppLoadingContext.Provider>
-        </UserContext.Provider>
+            >
+              <TrainingReports match={{ params: { status: passedStatus } }} />
+            </AppLoadingContext.Provider>
+          </UserContext.Provider>
+        </AriaLiveContext.Provider>
       </Router>,
     );
   };
@@ -275,5 +281,67 @@ describe('TrainingReports', () => {
       expect(screen.queryByText(/you successfully tested training report R04-PD-23-1123 on today/i)).not.toBeInTheDocument();
       expect(screen.queryByLabelText(/dismiss alert/i)).not.toBeInTheDocument();
     });
+  });
+
+  // Filters.
+  it('correctly renders data based on filters', async () => {
+    const user = {
+      homeRegionId: 14,
+      permissions: [{
+        regionId: 1,
+        scopeId: SCOPE_IDS.READ_ACTIVITY_REPORTS,
+      }, {
+        regionId: 2,
+        scopeId: SCOPE_IDS.READ_ACTIVITY_REPORTS,
+      }],
+    };
+
+    // Initial Page Load.
+    // api/events/not-started?region.in[]=1&region.in[]=2
+    const initialUrl = join(eventUrl, `/${EVENT_STATUS.NOT_STARTED}?region.in[]=1&region.in[]=2`);
+    fetchMock.get(initialUrl, notStartedEvents);
+
+    // Only Region 1.
+    const region1Url = join(eventUrl, `/${EVENT_STATUS.NOT_STARTED}?region.in[]=1`);
+    fetchMock.get(region1Url, [notStartedEvents[1]]);
+
+    // Render Training Reports.
+    renderTrainingReports(user);
+
+    // Assert initial data.
+    expect(await screen.findByRole('heading', { name: /training reports - all regions/i })).toBeInTheDocument();
+    expect(await screen.findByText(/not started event 1/i)).toBeInTheDocument();
+    expect(await screen.findByText(/not started event 2/i)).toBeInTheDocument();
+
+    // Open filters menu.
+    const open = await screen.findByRole('button', { name: /open filters for this page/i });
+    act(() => userEvent.click(open));
+
+    // Change first filter to Region 1.
+    const [lastTopic] = Array.from(document.querySelectorAll('[name="topic"]')).slice(-1);
+    act(() => userEvent.selectOptions(lastTopic, 'region'));
+
+    const [lastCondition] = Array.from(document.querySelectorAll('[name="condition"]')).slice(-1);
+    act(() => userEvent.selectOptions(lastCondition, 'is'));
+
+    const select = await screen.findByRole('combobox', { name: 'Select region to filter by' });
+    act(() => userEvent.selectOptions(select, 'Region 1'));
+
+    // Apply filter menu with Region 1 filter.
+    const apply = await screen.findByRole('button', { name: /apply filters for training reports/i });
+    act(() => userEvent.click(apply));
+
+    // Verify page render after apply.
+    expect(await screen.findByText(/training reports/i)).toBeVisible();
+    expect(await screen.findByText(/not started event 2/i)).toBeInTheDocument();
+    expect(screen.queryAllByText(/not started event 1/i).length).toBe(0);
+
+    // Remove Region 1 filter pill.
+    const removeRegion = await screen.findByRole('button', { name: /this button removes the filter: region is 1/i });
+    act(() => userEvent.click(removeRegion));
+
+    expect(await screen.findByText(/training reports/i)).toBeVisible();
+    expect(await screen.findByText(/not started event 1/i)).toBeInTheDocument();
+    expect(await screen.findByText(/not started event 2/i)).toBeInTheDocument();
   });
 });
