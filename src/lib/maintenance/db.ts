871ac3980e6472c9968750c8ce4f1dc76dc0f8a3
GarrettEHill <garretthill@gmail.com>
2023-06-16 11:11:18 -0700

    first pass at clean reusable implementation

---
+++ src/lib/maintenance/db.ts
@@ -0,0 +1,146 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+const { sequelize, DBMaintenanceLog } = require('../../models');
+const db = require('../../constants');
+const { auditLogger } = require('../../logger');
+
+const { DB_MAINTENANCE_TYPE } = db;
+
+/**
+ * Runs a maintenance command on the database table and logs the activity in the maintenance log.
+ *
+ * @param {string} command - The SQL command to be executed on the table.
+ * @param {DB_MAINTENANCE_TYPE[keyof typeof DB_MAINTENANCE_TYPE]} type - The type of
+ * maintenance activity being performed.
+ * @param {*} data - Additional data related to the maintenance activity.
+ * @returns {Promise<void>} A Promise that resolves to void.
+ * @throws {Error} If an error occurs while running the maintenance command.
+ */
+const maintenanceCommand = async (
+  command: string,
+  type: typeof DB_MAINTENANCE_TYPE[keyof typeof DB_MAINTENANCE_TYPE],
+  data: any,
+): Promise<void> => {
+  // Reset the log messages and benchmark data arrays for each operation
+  const logMessages: any[] = [];
+  const logBenchmarkData: any[] = [];
+
+  // Create a maintenance log entry for this operation
+  const log = await DBMaintenanceLog.create({ type, data });
+
+  try {
+    // Run the VACUUM FULL command on the table
+    const result = await sequelize.query(command, {
+      // Log all messages from the query to the logMessages array
+      logging: (message) => {
+        logMessages.push(message);
+      },
+      // Enable benchmarking for the query
+      benchmark: true,
+      // Log all benchmark data to the logBenchmarkData array
+      benchmarkLogger: (benchmarkData) => {
+        logBenchmarkData.push(benchmarkData);
+      },
+    });
+
+    // Update the log entry to indicate that the maintenance activity has been completed.
+    const updateResult = await DBMaintenanceLog.update(
+      {
+        data: {
+          ...log.data,
+          messages: logMessages,
+          benchmarks: logBenchmarkData,
+        },
+        // Check if the log messages contain the word "successfully" to set isSuccessful accordingly
+        isSuccessful: logMessages.some((message) => message.toLowerCase().includes('successfully')),
+      },
+      { where: { id: log.id } },
+    );
+
+    if (updateResult[0] === 1) {
+      // Log successful update
+    }
+  } catch (err) {
+    // Log error message
+    auditLogger.error(`Error occurred while running maintenance command: ${err.message}`);
+
+    // Update the log entry to indicate that the maintenance activity has been completed.
+    await DBMaintenanceLog.update(
+      {
+        data: {
+          ...log.data,
+          messages: logMessages,
+          benchmarks: logBenchmarkData,
+        },
+        isSuccessful: false,
+      },
+      { where: { id: log.id } },
+    );
+  }
+};
+
+/**
+ * Executes a table maintenance command.
+ * @async
+ * @param {string} command - The command to be executed.
+ * @param {typeof DB_MAINTENANCE_TYPE[keyof typeof DB_MAINTENANCE_TYPE]} type - The type of
+ * database maintenance operation to be performed.
+ * @param {any} model - The model representing the table to be maintained.
+ * @returns {Promise<any>} A promise that resolves with the result of the maintenance operation.
+ * @throws {Error} If an error occurs during the maintenance operation.
+ */
+const tableMaintenanceCommand = async (
+  command: string, // The SQL command to execute
+  // The type of maintenance command to run
+  type: typeof DB_MAINTENANCE_TYPE[keyof typeof DB_MAINTENANCE_TYPE],
+  model: any, // The Sequelize model object for the table being maintained
+) => {
+  // Get the name of the table from the model
+  const tableName = model.getTableName();
+
+  // Execute the maintenance command with the table name and return the result
+  return maintenanceCommand(`${command} ${tableName};`, type, { table: tableName });
+};
+
+/**
+ * Vacuums a table in the database.
+ *
+ * @async
+ * @param {any} model - The model of the table to be vacuumed.
+ * @returns {Promise<void>} - A promise that resolves when the table has been vacuumed successfully.
+ * @throws {Error} - If there is an error during the table maintenance command execution.
+ */
+// This function vacuums a table in the database
+const vacuumTable = async (model: any): Promise<void> => tableMaintenanceCommand('VACUUM FULL', DB_MAINTENANCE_TYPE.VACUUM, model);
+
+/**
+ * Asynchronously vacuums all tables in the database using Sequelize ORM.
+ * @returns A promise that resolves to an array of any type, representing the result of vacuuming
+ * each table.
+ * @throws {Error} If there is an error while vacuuming a table.
+ */
+const vacuumTables = async (): Promise<any[]> => Promise.all(
+  db.sequelize.models.map(async (model: any) => vacuumTable(model)),
+);
+
+/**
+ * Asynchronously reindexes a table in the database.
+ *
+ * @param model - The model of the table to be reindexed.
+ * @returns A Promise that resolves with void when the reindexing is complete.
+ * @throws {Error} If there is an issue with the database maintenance command.
+ */
+const reindexTable = async (model: any): Promise<void> => tableMaintenanceCommand('REINDEX TABLE', DB_MAINTENANCE_TYPE.REINDEX, model);
+
+/**
+ * Asynchronously reindexes all tables in the database using Sequelize models.
+ * @returns A promise that resolves to an array of any type.
+ * @throws Throws an error if there is an issue with reindexing a table.
+ */
+const reindexTables = async (): Promise<any[]> => Promise.all(
+  db.sequelize.models.map(async (model: any) => reindexTable(model)),
+);
+
+module.exports = {
+  vacuumTable,
+  reindexTable,
+};
