7675dfcf5540e3716db72e61ba88051ccddbf7f0
Jonathan Pyers <pyersjonathan@gmail.com>
2023-06-21 09:57:15 -0400

    does this make it more readable?

--- src/scopes/activityReport/reportText.js
+++ src/scopes/activityReport/reportText.js
@@ -1,67 +1,59 @@
 import { Op } from 'sequelize';
 import { filterAssociation } from './utils';
 
-const nextStepsPosNeg = (pos = true) => {
-  const a = pos
-    ? ''
-    : `bool_or("NextSteps".note IS NULL)
-    OR`;
-
-  return `
+const selectDistinctActivityReports = (select, join, groupBy, having) => `
   SELECT DISTINCT
     "ActivityReports"."id"
   FROM "ActivityReports"
-  LEFT JOIN "NextSteps"
-  ON "NextSteps"."activityReportId" = "ActivityReports"."id"
-  GROUP BY "ActivityReports"."id"
-  HAVING ${a} LOWER(STRING_AGG("NextSteps".note, CHR(10)))`;
+  ${join}
+  GROUP BY ${groupBy}
+  HAVING ${having}`;
+
+const nextStepsPosNeg = (pos = true) => {
+  const a = pos ? '' : 'bool_or("NextSteps".note IS NULL) OR';
+
+  return selectDistinctActivityReports(
+    '',
+    'LEFT JOIN "NextSteps" ON "NextSteps"."activityReportId" = "ActivityReports"."id"',
+    '"ActivityReports"."id"',
+    `${a} LOWER(STRING_AGG("NextSteps".note, CHR(10)))`,
+  );
 };
 
 const argsPosNeg = (pos = true) => {
-  const a = pos
-    ? ''
-    : `bool_or("ActivityReportGoals".name IS NULL)
-    OR`;
+  const a = pos ? '' : 'bool_or("ActivityReportGoals".name IS NULL) OR';
 
-  return `
-  SELECT DISTINCT
-    "ActivityReports"."id"
-  FROM "ActivityReports"
-  LEFT JOIN "ActivityReportGoals"
-  ON "ActivityReportGoals"."activityReportId" = "ActivityReports"."id"
-  GROUP BY "ActivityReports"."id"
-  HAVING ${a} LOWER(STRING_AGG("ActivityReportGoals".name, CHR(10)))`;
+  return selectDistinctActivityReports(
+    '',
+    'LEFT JOIN "ActivityReportGoals" ON "ActivityReportGoals"."activityReportId" = "ActivityReports"."id"',
+    '"ActivityReports"."id"',
+    `${a} LOWER(STRING_AGG("ActivityReportGoals".name, CHR(10)))`,
+  );
 };
 
 const objectiveTitleAndTtaProvidedPosNeg = (pos = true) => {
   const a = pos
     ? ''
-    : `bool_or("ActivityReportObjectives".title IS NULL
-    OR "ActivityReportObjectives"."ttaProvided" IS NULL)
-    OR`;
+    : `bool_or("ActivityReportObjectives".title IS NULL OR
+    "ActivityReportObjectives"."ttaProvided" IS NULL) OR`;
 
-  return `
-  SELECT DISTINCT
-    "ActivityReports"."id"
-  FROM "ActivityReports"
-  LEFT JOIN "ActivityReportObjectives"
-  ON "ActivityReportObjectives"."activityReportId" = "ActivityReports"."id"
-  GROUP BY "ActivityReports"."id"
-  HAVING ${a} LOWER(STRING_AGG(concat_ws(CHR(10), "ActivityReportObjectives".title, "ActivityReportObjectives"."ttaProvided"), CHR(10)))`;
+  return selectDistinctActivityReports(
+    '',
+    'LEFT JOIN "ActivityReportObjectives" ON "ActivityReportObjectives"."activityReportId" = "ActivityReports"."id"',
+    '"ActivityReports"."id"',
+    `${a} LOWER(STRING_AGG(concat_ws(CHR(10), "ActivityReportObjectives".title, "ActivityReportObjectives"."ttaProvided"), CHR(10)))`,
+  );
 };
 
 const activityReportContextandAdditionalNotesPosNeg = (pos = true) => {
-  const a = pos
-    ? ''
-    : `bool_or("ActivityReports"."context" IS NULL)
-    OR`;
+  const a = pos ? '' : 'bool_or("ActivityReports"."context" IS NULL) OR';
 
-  return `
-  SELECT DISTINCT
-    "ActivityReports"."id"
-  FROM "ActivityReports"
-  GROUP BY "ActivityReports"."id"
-  HAVING ${a} LOWER(STRING_AGG(concat_ws(CHR(10), "ActivityReports"."context", "ActivityReports"."additionalNotes"), CHR(10)))`;
+  return selectDistinctActivityReports(
+    '',
+    '',
+    '"ActivityReports"."id"',
+    `${a} LOWER(STRING_AGG(concat_ws(CHR(10), "ActivityReports"."context", "ActivityReports"."additionalNotes"), CHR(10)))`,
+  );
 };
 
 export function withReportText(searchText) {
