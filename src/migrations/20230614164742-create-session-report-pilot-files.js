6aafe65fbe5260ad65aaa9c7db3ce2479dfab8cc
Matt Bevilacqua <thewatermethod@gmail.com>
2023-06-26 09:06:59 -0400

    add audit table migration reset

--- src/migrations/20230614164742-create-session-report-pilot-files.js
+++ src/migrations/20230614164742-create-session-report-pilot-files.js
@@ -1,6 +1,18 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
     async (transaction) => {
+      const loggedUser = '0';
+      const sessionSig = __filename;
+      const auditDescriptor = 'RUN MIGRATIONS';
+      await queryInterface.sequelize.query(
+        `SELECT
+                set_config('audit.loggedUser', '${loggedUser}', TRUE) as "loggedUser",
+                set_config('audit.transactionId', NULL, TRUE) as "transactionId",
+                set_config('audit.sessionSig', '${sessionSig}', TRUE) as "sessionSig",
+                set_config('audit.auditDescriptor', '${auditDescriptor}', TRUE) as "auditDescriptor";`,
+        { transaction },
+      );
+
       await queryInterface.createTable('SessionReportPilotFiles', {
         id: {
           allowNull: false,
@@ -41,6 +53,20 @@ module.exports = {
   ),
 
   down: async (queryInterface) => queryInterface.sequelize.transaction(
-    async (transaction) => queryInterface.dropTable('SessionReportPilotFiles', { transaction }),
+    async (transaction) => {
+      const loggedUser = '0';
+      const sessionSig = __filename;
+      const auditDescriptor = 'RUN MIGRATIONS';
+      await queryInterface.sequelize.query(
+        `SELECT
+                set_config('audit.loggedUser', '${loggedUser}', TRUE) as "loggedUser",
+                set_config('audit.transactionId', NULL, TRUE) as "transactionId",
+                set_config('audit.sessionSig', '${sessionSig}', TRUE) as "sessionSig",
+                set_config('audit.auditDescriptor', '${auditDescriptor}', TRUE) as "auditDescriptor";`,
+        { transaction },
+      );
+
+      await queryInterface.dropTable('SessionReportPilotFiles', { transaction });
+    },
   ),
 };
