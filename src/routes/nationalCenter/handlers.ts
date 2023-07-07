78c802ecb2181312dc5932f32d6c929c5788f9ec
Matt Bevilacqua <thewatermethod@gmail.com>
2023-07-07 13:56:31 -0400

    add test for regular app route

--- src/routes/nationalCenter/handlers.ts
+++ src/routes/nationalCenter/handlers.ts
@@ -5,11 +5,11 @@ import handleErrors from '../../lib/apiErrorHandler';
 const logContext = 'HANDLERS:NationalCenter';
 
 /* eslint-disable import/prefer-default-export */
-export function getHandler(req: Request, res: Response) {
+export async function getHandler(req: Request, res: Response) {
   try {
     // permissions are already checked by the authMiddleware
     // all we look for is site access
-    const centers = findAll();
+    const centers = await findAll();
     return res.status(200).json(centers);
   } catch (error) {
     return handleErrors(req, res, error, logContext);
