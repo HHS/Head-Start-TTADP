b37ca3db0c894c57a3d31bec4db8845bd15e43ed
Adam Levin <adam.levin@adhocteam.us>
2023-07-10 14:37:05 -0400

    specialist name filter with tests

--- frontend/src/permissions.js
+++ frontend/src/permissions.js
@@ -84,6 +84,18 @@ const hasReadWrite = (user) => {
 };
 
 /**
+ * Search the user's permissions for a approve report permission regardless of region
+ * @param {*} user - user object
+ * @returns {boolean} - True if the user has approve activity report, false otherwise
+ */
+const hasApproveActivityReport = (user) => {
+  const { permissions } = user;
+  return permissions && permissions.find(
+    (p) => p.scopeId === SCOPE_IDS.APPROVE_ACTIVITY_REPORTS,
+  ) !== undefined;
+};
+
+/**
  * Search the user's permissions for a read/write permisions for a region
  * @param {*} user - user object
  * @param {number} region - region id
@@ -137,4 +149,5 @@ export {
   canEditOrCreateGoals,
   canChangeGoalStatus,
   canEditOrCreateSessionReports,
+  hasApproveActivityReport,
 };
