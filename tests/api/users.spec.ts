18f566f08fed443de298180f5e57d9ef309d8afd
Adam Levin <adam.levin@adhocteam.us>
2023-07-14 09:41:19 -0400

    see if this fixes api test

--- tests/api/users.spec.ts
+++ tests/api/users.spec.ts
@@ -88,7 +88,7 @@ test.describe('get /users/training-report-users', () => {
   });
   test('200', async ({ request }) => {
     const response = await request.get(
-      `${root}/users/training-report-users?regionId=1`,
+      `${root}/users/training-report-users?regionId=1&eventId=1`,
       { headers: { 'playwright-user-id': '1' } }, // has no home region
     );
     expect(response.status()).toBe(200);
