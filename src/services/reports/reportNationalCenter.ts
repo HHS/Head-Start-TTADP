029e74f5aba1a1ae83a55c90df702a10b9dcae0d
GarrettEHill <garretthill@gmail.com>
2023-08-09 16:37:19 -0700

    Update reportNationalCenter.ts

--- src/services/reports/reportNationalCenter.ts
+++ src/services/reports/reportNationalCenter.ts
@@ -1,50 +1,47 @@
-export {};
+import db from '../../models';
+import {
+  syncGenericEnums,
+  getGenericEnums,
+  getGenericEnum,
+} from './reportGenericEnum';
+import { auditLoger } from '../../logger';
+
 const {
   NationalCenter,
   ReportNationalCenter,
-} = require('../../models');
-const { auditLoger } = require('../../logger');
+} = db;
 
-// TODO: this needs alot of work
 const syncNationalCenters = async (
   report: { id: number, type: string, regionId: number },
-) => {
-  try {
-  // in parallel:
-  //    validate that the type is valid for the report type
-  //    get current collaborators for this report having this type
-  // filter to the positive, nuteral, and negative lists
-  // in parallel:
-  //    perform in insert/update/delete based on the sub lists
-  //        if a sublist is empty, do not call the db at all for that sublist
-  } catch (err) {
-    auditLoger.error(err);
-    throw err;
-  }
-};
+  nationalCenterIds: number[] | null = null,
+) => syncGenericEnums(
+  ReportNationalCenter,
+  { model: NationalCenter, as: 'nationalCenter' },
+  report,
+  nationalCenterIds,
+);
 
 const getNationalCenters = async (
   report: { id: number, type: string, regionId: number },
   nationalCenterIds: number[] | null = null,
-):Promise<object[]> => ReportNationalCenter.findAll({
-  attributes: [
-    // filter this down to whats needed.
-  ],
-  where: {
-    reportId: report.id,
-    ...(nationalCenterIds && { nationalCenterIds }),
-  },
-  include: [
-    // fill out anything thats needed
-  ],
-});
+):Promise<object[]> => getGenericEnums(
+  ReportNationalCenter,
+  { model: NationalCenter, as: 'nationalCenter' },
+  report,
+  nationalCenterIds,
+);
 
 const getNationalCenter = async (
   report: { id: number, type: string, regionId: number },
   nationalCenterId: number,
-):Promise<object[]> => getNationalCenters(report, [nationalCenterId]);
+):Promise<object[]> => getGenericEnum(
+  ReportNationalCenter,
+  { model: NationalCenter, as: 'nationalCenter' },
+  report,
+  nationalCenterId,
+);
 
-module.exports = {
+export {
   syncNationalCenters,
   getNationalCenters,
   getNationalCenter,
