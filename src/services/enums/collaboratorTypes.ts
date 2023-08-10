afa3b4649869f4e4c56e6ba47debe768e444d79e
GarrettEHill <garretthill@gmail.com>
2023-08-10 00:57:31 -0700

    Update collaboratorTypes.ts

--- src/services/enums/collaboratorTypes.ts
+++ src/services/enums/collaboratorTypes.ts
@@ -1,21 +1,32 @@
 import * as genericEnum from './generic';
 import db from '../../models';
+import { ENTITY_TYPE } from '../../constants';
 
 const { CollaboratorType } = db;
 
-const findAll = async () => genericEnum.findAll(CollaboratorType);
+const findAll = async (
+  data: { validFor: typeof ENTITY_TYPE[keyof typeof ENTITY_TYPE] },
+) => genericEnum.findAll(CollaboratorType, data);
 
 const findById = async (
   id: number,
 ) => genericEnum.findById(CollaboratorType, id);
 
 const create = async (
-  data: { name: string, mapsTo?: number },
+  data: {
+    name: string,
+    validFor: typeof ENTITY_TYPE[keyof typeof ENTITY_TYPE],
+    mapsTo?: number,
+  },
 ) => genericEnum.create(CollaboratorType, data);
 
 const updateById = async (
   id: number,
-  data: { name?: string, mapsTo?: number },
+  data: {
+    name?: string,
+    validFor?: typeof ENTITY_TYPE[keyof typeof ENTITY_TYPE],
+    mapsTo?: number,
+  },
 ) => genericEnum.updateById(CollaboratorType, id, data);
 
 const deleteById = async (
