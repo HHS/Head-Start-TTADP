0e2abbb83bb3fcda90508b843ebbb8455f20e563
nvms <jon@pye.rs>
2023-08-16 17:54:30 -0400

    does this test pass now?

--- tests/e2e/activity-report-text-search-filter.spec.ts
+++ tests/e2e/activity-report-text-search-filter.spec.ts
@@ -102,7 +102,7 @@ test.describe('Activity Report Text Search Filter', () => {
     await page.getByLabel('TTA objective *').click();
     await page.getByLabel('TTA objective *').fill('Prepare your first meal.');
     await page.locator('.css-125guah-control > .css-g1d714-ValueContainer').click();
-    await page.locator('#react-select-19-option-0').click();
+    await page.keyboard.press('Enter');
     await blur(page);
     // Links.
     await page.getByTestId('textInput').click();
