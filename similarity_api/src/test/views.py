9935e04cb22ad67e6c1d73bf1fa5cab81802b82b
nvms <jon@pye.rs>
2023-08-16 10:12:56 -0400

    rm test view

--- similarity_api/src/test/views.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-# from application import db
-
-test_app = Blueprint("test_app", __name__)
-
-@test_app.route("/test", methods=["GET"])
-def hello():
-    return "<h1>Hello, World!</h1>"
