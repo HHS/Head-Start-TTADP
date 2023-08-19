43706a5376165deac4c030ee9d774e188267a1d3
GarrettEHill <garretthill@gmail.com>
2023-08-19 01:04:43 -0700

    Update goalTemplate.js

--- src/models/goalTemplate.js
+++ src/models/goalTemplate.js
@@ -71,7 +71,11 @@ export default (sequelize, DataTypes) => {
     },
     isFoiaable: {
       type: DataTypes.BOOLEAN,
-      default: false,
+      defaultValue: false,
+    },
+    isReferenced: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
   }, {
     sequelize,
