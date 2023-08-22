589cf969a4b65e46758eff461f253a5355d4f51a
GarrettEHill <garretthill@gmail.com>
2023-08-22 00:28:27 -0700

    Update reportNationalCenter.js

--- src/models/reportNationalCenter.js
+++ src/models/reportNationalCenter.js
@@ -2,6 +2,7 @@ const {
   Model,
 } = require('sequelize');
 const { REPORT_TYPE, NATIONAL_CENTER_ACTING_AS } = require('../constants');
+const { generateJunctionTableAssociations } = require('./helpers/associations');
 
 /**
  * Status table. Stores topics used in activity reports and tta plans.
@@ -12,33 +13,20 @@ const { REPORT_TYPE, NATIONAL_CENTER_ACTING_AS } = require('../constants');
 export default (sequelize, DataTypes) => {
   class ReportNationalCenter extends Model {
     static associate(models) {
-      ReportNationalCenter.belongsTo(models.Report.scope({ method: ['reportType', REPORT_TYPE.REPORT_SESSION] }), {
-        foreignKey: 'reportId',
-        as: 'report',
-      });
-      ReportNationalCenter.belongsTo(models.NationalCenter, {
-        foreignKey: 'nationalCenterId',
-        as: 'nationalCenter',
-      });
       ReportNationalCenter.addScope(NATIONAL_CENTER_ACTING_AS.TRAINER, {
         where: {
           actingAs: NATIONAL_CENTER_ACTING_AS.TRAINER,
         },
       });
 
-      // Relocated from report.js as the scopes needed to be defined before the associations.
-      models.Report.scope({ method: ['reportType', REPORT_TYPE.REPORT_SESSION] })
-        .hasMany(models.ReportNationalCenter.scope(NATIONAL_CENTER_ACTING_AS.TRAINER), {
-          foreignKey: 'reportId',
-          as: 'reportTrainers',
-        });
-      models.Report.scope({ method: ['reportType', REPORT_TYPE.REPORT_SESSION] })
-        .belongsToMany(models.NationalCenter, {
-          through: models.ReportNationalCenter.scope(NATIONAL_CENTER_ACTING_AS.TRAINER),
-          foreignKey: 'reportId',
-          otherKey: 'nationalCenterId',
-          as: 'trainers',
-        });
+      // TODO: needs to support matrix
+      generateJunctionTableAssociations(
+        models.ReportNationalCenter, // TODO: needs to support .scope(NATIONAL_CENTER_ACTING_AS.TRAINER)
+        [
+          models.Report, // TODO: to support with & without .scope({ method: ['reportType', REPORT_TYPE.REPORT_SESSION] })
+          models.NationalCenter,
+        ],
+      );
     }
   }
   ReportNationalCenter.init({
