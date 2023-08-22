96e229b8159adebbcd718e524f9be5ce8cc27e57
GarrettEHill <garretthill@gmail.com>
2023-08-22 00:24:22 -0700

    Update reportFile.js

--- src/models/reportFile.js
+++ src/models/reportFile.js
@@ -1,37 +1,16 @@
 const { Model } = require('sequelize');
+const { generateJunctionTableAssociations } = require('./helpers/associations');
 
 export default (sequelize, DataTypes) => {
   class ReportFile extends Model {
     static associate(models) {
-      ReportFile.belongsTo(models.Report, {
-        foreignKey: 'reportId',
-        as: 'report',
-      });
-      ReportFile.belongsTo(models.File, {
-        foreignKey: 'fileId',
-        as: 'file',
-      });
-
-      models.Report.hasMany(models.ReportFile, {
-        foreignKey: 'reportId',
-        as: 'reportFiles',
-      });
-      models.Report.belongsToMany(models.File, {
-        through: models.ReportFile,
-        foreignKey: 'reportId',
-        as: 'files',
-      });
-
-      models.File.hasMany(models.ReportFile, {
-        foreignKey: 'fileId',
-        as: 'reportFiles',
-      });
-      models.File.belongsToMany(models.Report, {
-        through: models.ReportFile,
-        foreignKey: 'fileId',
-        otherKey: 'reportId',
-        as: 'reports',
-      });
+      generateJunctionTableAssociations(
+        models.ReportFile,
+        [
+          models.Report,
+          models.File,
+        ],
+      );
     }
   }
   ReportFile.init({
@@ -44,10 +23,22 @@ export default (sequelize, DataTypes) => {
     reportId: {
       type: DataTypes.BIGINT,
       allowNull: false,
+      references: {
+        model: {
+          tableName: 'Reports',
+        },
+        key: 'id',
+      },
     },
     fileId: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: {
+          tableName: 'Files',
+        },
+        key: 'id',
+      },
     },
   }, {
     sequelize,
