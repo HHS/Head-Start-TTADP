ef0d4aeb62a86ae23b0c6e996102bb268f6f4e4a
Adam Levin <adam.levin@adhocteam.us>
2023-08-23 10:21:25 -0400

    fix css for support type drawer

--- frontend/src/components/ObjectiveSupportType.js
+++ frontend/src/components/ObjectiveSupportType.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import PropTypes from 'prop-types';
 import { SUPPORT_TYPES } from '@ttahub/common';
 import { FormGroup, Label, Dropdown } from '@trussworks/react-uswds';
+import Drawer from './Drawer';
+import ContentFromFeedByTag from './ContentFromFeedByTag';
 import Req from './Req';
+import './ObjectiveSupportType.scss';
 
 export default function ObjectiveSupportType({
   supportType,
@@ -11,11 +14,32 @@ export default function ObjectiveSupportType({
   inputName,
   error,
 }) {
+  const drawerTriggerRef = useRef(null);
   return (
-    <FormGroup error={error.props.children}>
-      <Label htmlFor={inputName}>
-        Support type
-        <Req />
+    <>
+      <Drawer
+        triggerRef={drawerTriggerRef}
+        stickyHeader
+        stickyFooter
+        title="Support type guidance"
+      >
+        <ContentFromFeedByTag className="ttahub-drawer--objective-support-type-guidance" tagName="ttahub-tta-support-type" contentSelector="table" />
+      </Drawer>
+      <FormGroup error={error.props.children}>
+        <Label htmlFor={inputName}>
+          <>
+            Support type
+            {' '}
+            <Req />
+            <button
+              type="button"
+              className="usa-button usa-button--unstyled margin-left-1"
+              ref={drawerTriggerRef}
+            >
+              Get help choosing a support type
+            </button>
+          </>
+        </Label>
         <Dropdown
           onChange={(e) => onChangeSupportType(e.target.value)}
           id={inputName}
@@ -26,8 +50,8 @@ export default function ObjectiveSupportType({
           <option disabled hidden value="">Select one</option>
           {SUPPORT_TYPES.map((option) => (<option key={option}>{option}</option>))}
         </Dropdown>
-      </Label>
-    </FormGroup>
+      </FormGroup>
+    </>
   );
 }
 
