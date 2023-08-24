691bf3cf2ee474f917b83911a25a5077566c2503
Adam Levin <adam.levin@adhocteam.us>
2023-08-24 15:40:55 -0400

    FE test

--- frontend/src/pages/SessionForm/pages/__tests__/sessionSummary.js
+++ frontend/src/pages/SessionForm/pages/__tests__/sessionSummary.js
@@ -261,7 +261,7 @@ describe('sessionSummary', () => {
         userEvent.click(noIsaidNoIsaidNoFilesSir);
       });
 
-      const supportType = await screen.findByLabelText(/support type/i);
+      const supportType = await screen.findByRole('combobox', { name: /support type \* get help choosing a support type/i });
       act(() => {
         userEvent.selectOptions(supportType, 'Planning');
       });
