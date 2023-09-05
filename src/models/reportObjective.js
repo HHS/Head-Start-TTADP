d03a0bcaacb813784e00c059c328985baed8ab00
GarrettEHill <garretthill@gmail.com>
2023-09-05 09:34:54 -0700

    Update reportObjective.js

--- src/models/reportObjective.js
+++ src/models/reportObjective.js
@@ -3,7 +3,7 @@ const {
   Op,
 } = require('sequelize');
 const { REPORT_TYPE, ENTITY_TYPE } = require('../constants');
-const { generateJunctionTableAssociations } = require('./helpers/associations');
+const { generateJunctionTableAssociations } = require('./helpers/associationsAndScopes');
 
 export default (sequelize, DataTypes) => {
   class ReportObjective extends Model {
