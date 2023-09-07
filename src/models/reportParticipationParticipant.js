580351dcd23e118989fbcdad49a4c602d92c0868
GarrettEHill <garretthill@gmail.com>
2023-09-06 22:27:29 -0700

    Create reportParticipationParticipant.js

---
+++ src/models/reportParticipationParticipant.js
@@ -0,0 +1,54 @@
+const {
+  Model,
+} = require('sequelize');
+const { ENTITY_TYPE } = require('../constants');
+const {
+  automaticallyGenerateJunctionTableAssociations,
+} = require('./helpers/associationsAndScopes');
+
+/**
+ * Status table. Stores topics used in activity reports and tta plans.
+ *
+ * @param {} sequelize
+ * @param {*} DataTypes
+ */
+export default (sequelize, DataTypes) => {
+  class ReportParticipationParticipant extends Model {
+    static associate(models) {
+      automaticallyGenerateJunctionTableAssociations(this, models);
+    }
+  }
+  ReportParticipationParticipant.init({
+    id: {
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    reportParticipationId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: {
+          tableName: 'ReportParticipation',
+        },
+        key: 'id',
+      },
+    },
+    participantId: {
+      type: DataTypes.BIGINT,
+      allowNull: true,
+      references: {
+        model: {
+          tableName: 'Participants',
+        },
+        key: 'id',
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'ReportParticipationParticipant',
+    paranoid: true,
+  });
+  return ReportParticipationParticipant;
+};
