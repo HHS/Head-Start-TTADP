3332579311adbc31b55eb6af0d1d34457eaceb89
GarrettEHill <garretthill@gmail.com>
2023-09-06 22:28:33 -0700

    Update targetPopulation.js

--- src/models/targetPopulation.js
+++ src/models/targetPopulation.js
@@ -2,6 +2,9 @@ const {
   Model,
 } = require('sequelize');
 const { ENTITY_TYPE } = require('../constants');
+const {
+  automaticallyGenerateJunctionTableAssociations,
+} = require('./helpers/associationsAndScopes');
 
 /**
  * Status table. Stores topics used in activity reports and tta plans.
@@ -12,14 +15,7 @@ const { ENTITY_TYPE } = require('../constants');
 export default (sequelize, DataTypes) => {
   class TargetPopulation extends Model {
     static associate(models) {
-      TargetPopulation.belongsTo(models.TargetPopulation, {
-        foreignKey: 'mapsTo',
-        as: 'mapsToTargetPopulation',
-      });
-      TargetPopulation.hasMany(models.TargetPopulation, {
-        foreignKey: 'mapsTo',
-        as: 'mapsFromTargetPopulations',
-      });
+      automaticallyGenerateJunctionTableAssociations(this, models);
 
       models.TargetPopulation.addScope('defaultScope', {
         include: [{
@@ -44,10 +40,22 @@ export default (sequelize, DataTypes) => {
     validForId: {
       type: DataTypes.INTEGER,
       allowNull: false,
+      references: {
+        model: {
+          tableName: 'ValidFor',
+        },
+        key: 'id',
+      },
     },
     mapsTo: {
       type: DataTypes.INTEGER,
       allowNull: true,
+      references: {
+        model: {
+          tableName: 'TargetPopulations',
+        },
+        key: 'id',
+      },
     },
     latestName: {
       type: DataTypes.VIRTUAL(DataTypes.STRING),
