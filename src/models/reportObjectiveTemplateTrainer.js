998d47d81ddc78e17cbc3dd979bc7e33c71f76f9
GarrettEHill <garretthill@gmail.com>
2023-09-06 22:27:02 -0700

    Create reportObjectiveTemplateTrainer.js

---
+++ src/models/reportObjectiveTemplateTrainer.js
@@ -0,0 +1,42 @@
+const { Model } = require('sequelize');
+const { automaticallyGenerateJunctionTableAssociations } = require('./helpers/associationsAndScopes');
+
+export default (sequelize, DataTypes) => {
+  class ReportObjectiveTemplateTrainer extends Model {
+    static associate(models) {
+      automaticallyGenerateJunctionTableAssociations(this, models);
+    }
+  }
+  ReportObjectiveTemplateTrainer.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.BIGINT,
+    },
+    reportObjectiveTemplateId: {
+      type: DataTypes.BIGINT,
+      allowNull: false,
+      references: {
+        model: {
+          tableName: 'ReportObjectiveTemplates',
+        },
+        key: 'id',
+      },
+    },
+    nationalCenterId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: {
+          tableName: 'NationalCenters',
+        },
+        key: 'id',
+      },
+    },
+  }, {
+    sequelize,
+    modelName: 'ReportObjectiveTemplateTrainer',
+  });
+  return ReportObjectiveTemplateTrainer;
+};
