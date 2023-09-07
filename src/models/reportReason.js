197b728a8122bab445756e838aeb96471910249a
GarrettEHill <garretthill@gmail.com>
2023-09-06 22:27:39 -0700

    Update reportReason.js

--- src/models/reportReason.js
+++ src/models/reportReason.js
@@ -4,37 +4,16 @@ const {
 const {
   REPORT_TYPE,
 } = require('../constants');
+const { automaticallyGenerateJunctionTableAssociations } = require('./helpers/associationsAndScopes');
 
 /**
- * Status table. Stores topics used in activity reports and tta plans.
- *
  * @param {} sequelize
  * @param {*} DataTypes
  */
 export default (sequelize, DataTypes) => {
   class ReportReason extends Model {
     static associate(models) {
-      ReportReason.belongsTo(models.Report, {
-        foreignKey: 'reportId',
-        as: 'report',
-      });
-      ReportReason.belongsTo(models.Reason, {
-        foreignKey: 'reasonId',
-        as: 'reason',
-      });
-
-      models.Report.scope({ method: ['reportType', REPORT_TYPE.REPORT_TRAINING_EVENT] })
-        .hasMany(models.ReportReason, {
-          foreignKey: 'reportId',
-          as: 'reportReasons',
-        });
-      models.Report.scope({ method: ['reportType', REPORT_TYPE.REPORT_TRAINING_EVENT] })
-        .belongsToMany(models.Reason, {
-          through: models.ReportReason,
-          foreignKey: 'reportId',
-          otherKey: 'reasonId',
-          as: 'reasons',
-        });
+      automaticallyGenerateJunctionTableAssociations(this, models);
     }
   }
   ReportReason.init({
@@ -42,6 +21,7 @@ export default (sequelize, DataTypes) => {
       type: DataTypes.BIGINT,
       primaryKey: true,
       autoIncrement: true,
+      allowNull: false,
     },
     reportId: {
       type: DataTypes.BIGINT,
