1add0afa6fb8deaefb2a896eabb2bbe5a430a63e
Matt Bevilacqua <thewatermethod@gmail.com>
2023-09-08 14:32:05 -0400

    add column to ARO table

--- src/migrations/20230908131425-add-suspend-objective-reasons.js
+++ src/migrations/20230908131425-add-suspend-objective-reasons.js
@@ -10,8 +10,12 @@ module.exports = {
     await queryInterface.sequelize.transaction(async (transaction) => {
       const sessionSig = __filename;
       await prepMigration(queryInterface, transaction, sessionSig);
-      await queryInterface.addColumn('Objectives', 'suspendReason', { type: Sequelize.DataTypes.ENUM(SUSPEND_REASONS) }, { transaction });
-      return queryInterface.addColumn('Objectives', 'suspendContext', { type: Sequelize.TEXT }, { transaction });
+      return Promise.all([
+        queryInterface.addColumn('Objectives', 'suspendReason', { type: Sequelize.DataTypes.ENUM(SUSPEND_REASONS) }, { transaction }),
+        queryInterface.addColumn('Objectives', 'suspendContext', { type: Sequelize.TEXT }, { transaction }),
+        queryInterface.addColumn('ActivityReportObjectives', 'suspendReason', { type: Sequelize.DataTypes.ENUM(SUSPEND_REASONS) }, { transaction }),
+        queryInterface.addColumn('ActivityReportObjectives', 'suspendContext', { type: Sequelize.TEXT }, { transaction }),
+      ]);
     });
   },
 
@@ -19,8 +23,12 @@ module.exports = {
     await queryInterface.sequelize.transaction(async (transaction) => {
       const sessionSig = __filename;
       await prepMigration(queryInterface, transaction, sessionSig);
-      await queryInterface.removeColumn('Objectives', 'suspendReason', { transaction });
-      return queryInterface.removeColumn('Objectives', 'suspendContext', { transaction });
+      return Promise.all([
+        queryInterface.removeColumn('Objectives', 'suspendReason', { transaction }),
+        queryInterface.removeColumn('Objectives', 'suspendContext', { transaction }),
+        queryInterface.removeColumn('ActivityReportObjectives', 'suspendReason', { transaction }),
+        queryInterface.removeColumn('ActivityReportObjectives', 'suspendContext', { transaction }),
+      ]);
     });
   },
 };
