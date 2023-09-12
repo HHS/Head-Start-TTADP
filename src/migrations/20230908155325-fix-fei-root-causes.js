bfd9f0326d3f4d9f026792c225c4f75355a8b155
Adam Levin <adam.levin@adhocteam.us>
2023-09-12 09:58:52 -0400

    fix additional table per Matt

--- src/migrations/20230908155325-fix-fei-root-causes.js
+++ src/migrations/20230908155325-fix-fei-root-causes.js
@@ -26,6 +26,19 @@ module.exports = {
                 SET "response" = '{Workforce}'
             WHERE "id" IN (SELECT "id" FROM "ids");
 
+            with ids as (
+                SELECT
+                gfr.id
+                FROM "Grants" gr
+                JOIN "Goals" goal
+                    ON gr.id = goal."grantId"
+                JOIN "GoalFieldResponses" gfr
+                    ON goal.id = gfr."goalId"
+                WHERE "number" IN ('06CH011554', '06CH011558', '06CH010965', '06CH010720', '06HP000120', '06HP000296', '06CH011143'))
+                UPDATE "GoalFieldResponses"
+                    SET "response" = '{Workforce}'
+                WHERE "id" IN (SELECT "id" FROM "ids");
+
             -- Update workforce, transportation root causes for fei.
             with ids as (
                 SELECT
@@ -41,6 +54,19 @@ module.exports = {
                 UPDATE "ActivityReportGoalFieldResponses"
                     SET "response" = '{Workforce, Transportation}'
                 WHERE "id" IN (SELECT "id" FROM "ids");
+
+                with ids as (
+                    SELECT
+                    gfr.id
+                    FROM "Grants" gr
+                    JOIN "Goals" goal
+                        ON gr.id = goal."grantId"
+                    JOIN "GoalFieldResponses" gfr
+                        ON goal.id = gfr."goalId"
+                         WHERE "number" IN ('06CH011469', '06CH011503', '06CH011919'))
+                    UPDATE "GoalFieldResponses"
+                        SET "response" = '{Workforce, Transportation}'
+                    WHERE "id" IN (SELECT "id" FROM "ids");
 
                 -- Update workforce, facilities root causes for fei.
                 with ids as (
@@ -58,6 +84,19 @@ module.exports = {
                         SET "response" = '{Workforce, Facilities}'
                     WHERE "id" IN (SELECT "id" FROM "ids");
 
+                    with ids as (
+                        SELECT
+                        gfr.id
+                        FROM "Grants" gr
+                        JOIN "Goals" goal
+                            ON gr.id = goal."grantId"
+                        JOIN "GoalFieldResponses" gfr
+                            ON goal.id = gfr."goalId"
+                            WHERE "number" IN ('06CH010885', '06CH010663', '06HP000249', '06CH011990', '06HP000184', '06CH011272'))
+                        UPDATE "GoalFieldResponses"
+                                SET "response" = '{Workforce, Facilities}'
+                        WHERE "id" IN (SELECT "id" FROM "ids");
+
                     -- Update Other ECE Options root causes for fei.
                     with ids as (
                         SELECT
@@ -74,6 +113,19 @@ module.exports = {
                             SET "response" = '{Other ECE Options}'
                         WHERE "id" IN (SELECT "id" FROM "ids");
 
+                        with ids as (
+                            SELECT
+                            gfr.id
+                            FROM "Grants" gr
+                            JOIN "Goals" goal
+                                ON gr.id = goal."grantId"
+                            JOIN "GoalFieldResponses" gfr
+                                ON goal.id = gfr."goalId"
+                                WHERE "number" IN ('06CH010745', '06HP000265', '06HP000473'))
+                            UPDATE "GoalFieldResponses"
+                                    SET "response" = '{Other ECE Options}'
+                            WHERE "id" IN (SELECT "id" FROM "ids");
+
                         -- Update Workforce, Other ECE Options root causes for fei.
                         with ids as (
                             SELECT
@@ -89,6 +141,19 @@ module.exports = {
                             UPDATE "ActivityReportGoalFieldResponses"
                                 SET "response" = '{Workforce, Other ECE Options}'
                             WHERE "id" IN (SELECT "id" FROM "ids");
+
+                            with ids as (
+                                SELECT
+                                gfr.id
+                                FROM "Grants" gr
+                                JOIN "Goals" goal
+                                    ON gr.id = goal."grantId"
+                                JOIN "GoalFieldResponses" gfr
+                                    ON goal.id = gfr."goalId"
+                                    WHERE "number" IN ('06CH011414'))
+                                UPDATE "GoalFieldResponses"
+                                        SET "response" = '{Workforce, Other ECE Options}'
+                                WHERE "id" IN (SELECT "id" FROM "ids");
             `, { transaction });
     });
   },
