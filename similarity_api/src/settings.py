fc47c2f6f9f5bbd4fe6be513c22f853cf572606b
nvms <jon@pye.rs>
2023-09-15 09:40:24 -0400

    better

--- similarity_api/src/settings.py
+++ similarity_api/src/settings.py
@@ -4,26 +4,24 @@ import json
 def get_postgres_credentials_from_vcap():
     vcap_services = os.environ.get('VCAP_SERVICES')
 
-    # Ensure VCAP_SERVICES exists and parse its JSON content
     if vcap_services:
         services = json.loads(vcap_services)
 
-        # Assuming the PostgreSQL service key is "postgresql"
         if 'postgresql' in services:
             postgres_service = services['postgresql'][0]
             return postgres_service['credentials']
 
     return None
 
-# Fetch credentials from VCAP_SERVICES or use the direct environment variables
 vcap_credentials = get_postgres_credentials_from_vcap()
 
 DB_USERNAME = os.environ.get("POSTGRES_USERNAME") or (vcap_credentials and vcap_credentials.get('username'))
 DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD") or (vcap_credentials and vcap_credentials.get('password'))
 DB_HOST = os.environ.get("POSTGRES_HOST") or (vcap_credentials and vcap_credentials.get('host'))
-DB_NAME = os.environ.get("POSTGRES_DB") or (vcap_credentials and vcap_credentials.get('database'))
+DB_NAME = os.environ.get("POSTGRES_DB") or (vcap_credentials and vcap_credentials.get('db_name'))
+PORT = os.environ.get("PORT") or (vcap_credentials and vcap_credentials.get('port')) or 5432
 
 SQLALCHEMY_TRACK_MODIFICATIONS = False
-SQLALCHEMY_DATABASE_URI = f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+SQLALCHEMY_DATABASE_URI = (vcap_credentials and vcap_credentials.get('uri')) or f"postgresql://{DB_USERNAME}:{DB_PASSWORD}@{DB_HOST}:{PORT}/{DB_NAME}"
 
 API_KEY = os.environ["SIMILARITY_API_KEY"]
