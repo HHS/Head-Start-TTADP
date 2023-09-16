5d52645043b4ab7709afc7360a40363dcc7f6f1e
GarrettEHill <garretthill@gmail.com>
2023-09-16 09:24:35 -0700

    Create reportParticipationParticipant.ts

---
+++ src/services/reports/reportParticipationParticipant.ts
@@ -0,0 +1,55 @@
+import db from '../../models';
+import {
+  type EnumInfo,
+  type EntityGenericEnum,
+  type EnumSyncResponse,
+  getEntityGenericEnum,
+  syncEntityGenericEnum,
+  includeEntityGenericEnums,
+} from '../enums/generic';
+import { REPORT_TYPE } from '../../constants';
+
+const {
+  Participant,
+  ReportParticipationParticipant,
+} = db;
+
+const participantsEnumInfo:EnumInfo = {
+  model: Participant,
+  as: 'participants',
+  keyName: 'Participants',
+};
+
+const syncReportParticipationParticipants = async (
+  entity: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
+  participantEnums: { id?: number, name?: string }[] | null = null,
+): Promise<EnumSyncResponse> => syncEntityGenericEnum(
+  ReportParticipationParticipant,
+  participantsEnumInfo,
+  { name: 'reportParticipationId', ...entity },
+  participantEnums,
+);
+
+const getReportAudiences = async (
+  entity: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
+  participantIds: number[] | null = null,
+):Promise<EntityGenericEnum[]> => getEntityGenericEnum(
+  ReportParticipationParticipant,
+  participantsEnumInfo,
+  { name: 'reportParticipationId', ...entity },
+  participantIds,
+);
+
+const includeReportAudience = (
+  type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE],
+) => includeEntityGenericEnums(
+  ReportParticipationParticipant,
+  participantsEnumInfo,
+  { name: 'reportParticipationId', type },
+);
+
+export {
+  syncReportParticipationParticipants,
+  getReportAudiences,
+  includeReportAudience,
+};
