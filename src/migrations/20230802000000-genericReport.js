9c42a692ad9a6162c9cd174257a08a1cce7fd9d6
GarrettEHill <garretthill@gmail.com>
2023-09-18 01:33:50 -0700

    Update 20230802000000-genericReport.js

--- src/migrations/20230802000000-genericReport.js
+++ src/migrations/20230802000000-genericReport.js
@@ -37,6 +37,7 @@ module.exports = {
        *  - ReportTrainingEvents-
        *  - ReportTrainingSessions-
        *  - ReportApprovals-
+       *  - ReportAudiences
        *  - ReportRecipients-
        *  - ReportCollaborators-
        *    - ReportCollaboratorTypes-
