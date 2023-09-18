0ccd6daa4eab0abbc981895084062e4c9c00e650
GarrettEHill <garretthill@gmail.com>
2023-09-18 01:35:27 -0700

    Create reportObjectiveTopic.ts

---
+++ src/services/reports/reportObjectiveTopic.ts
@@ -0,0 +1,13 @@
+const syncReportObjectiveTopics = async (
+  entity: {},
+) => {};
+
+const includeReportObjectiveTopics = () => ({});
+
+const getReportObjectiveTopics = async () => {};
+
+export {
+  syncReportObjectiveTopics,
+  getReportObjectiveTopics,
+  includeReportObjectiveTopics,
+};
