399e321305f35006dc5e84a2e224586f8d517f4b
GarrettEHill <garretthill@gmail.com>
2023-09-18 01:34:30 -0700

    Create reportCollaboratorRole.ts

---
+++ src/services/reports/reportCollaboratorRole.ts
@@ -0,0 +1,13 @@
+const syncReportCollaboratorRoles = async (
+  entity: {},
+) => {};
+
+const includeReportCollaboratorRoles = () => ({});
+
+const getReportCollaboratorRoles = async () => {};
+
+export {
+  syncReportCollaboratorRoles,
+  getReportCollaboratorRoles,
+  includeReportCollaboratorRoles,
+};
