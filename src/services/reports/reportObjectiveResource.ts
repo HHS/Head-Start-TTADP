2757450d5813c99df34aa03f47738e06922afb61
GarrettEHill <garretthill@gmail.com>
2023-09-18 01:35:01 -0700

    Create reportObjectiveResource.ts

---
+++ src/services/reports/reportObjectiveResource.ts
@@ -0,0 +1,13 @@
+const syncReportObjectiveResources = async (
+  entity: {},
+) => {};
+
+const includeReportObjectiveResources = () => ({});
+
+const getReportObjectiveResources = async () => {};
+
+export {
+  syncReportObjectiveResources,
+  getReportObjectiveResources,
+  includeReportObjectiveResources,
+};
