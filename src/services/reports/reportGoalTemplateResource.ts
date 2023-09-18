26381f5783a802c4413e4ee9514b58897523b01c
GarrettEHill <garretthill@gmail.com>
2023-09-18 01:34:46 -0700

    Create reportGoalTemplateResource.ts

---
+++ src/services/reports/reportGoalTemplateResource.ts
@@ -0,0 +1,13 @@
+const syncReportGoalTemplateResources = async (
+  entity: {},
+) => {};
+
+const includeReportGoalTemplateResources = () => ({});
+
+const getReportGoalTemplateResources = async () => {};
+
+export {
+  syncReportGoalTemplateResources,
+  getReportGoalTemplateResources,
+  includeReportGoalTemplateResources,
+};
