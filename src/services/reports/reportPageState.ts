751aa0d389b2019469ed44d4262c5c33d0d2c7d7
GarrettEHill <garretthill@gmail.com>
2023-09-18 01:35:36 -0700

    Create reportPageState.ts

---
+++ src/services/reports/reportPageState.ts
@@ -0,0 +1,13 @@
+const syncReportPageStates = async (
+  entity: {},
+) => {};
+
+const includeReportPageStates = () => ({});
+
+const getReportPageStates = async () => {};
+
+export {
+  syncReportPageStates,
+  getReportPageStates,
+  includeReportPageStates,
+};
