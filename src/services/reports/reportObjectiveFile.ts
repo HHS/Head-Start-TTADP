dff16536660a208229b4faf3f770cad32890c7b7
GarrettEHill <garretthill@gmail.com>
2023-09-18 01:34:58 -0700

    Create reportObjectiveFile.ts

---
+++ src/services/reports/reportObjectiveFile.ts
@@ -0,0 +1,13 @@
+const syncReportObjectiveFiles = async (
+  entity: {},
+) => {};
+
+const includeReportObjectiveFiles = () => ({});
+
+const getReportObjectiveFiles = async () => {};
+
+export {
+  syncReportObjectiveFiles,
+  getReportObjectiveFiles,
+  includeReportObjectiveFiles,
+};
