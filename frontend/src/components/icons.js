f8227b263743fbef44797633dcfa4855bdd4b57a
Matt Bevilacqua <thewatermethod@gmail.com>
2023-09-19 13:59:26 -0400

    move to a fontkit

--- frontend/src/components/icons.js
+++ frontend/src/components/icons.js
@@ -9,14 +9,14 @@ import {
   faPencil,
   faTrash,
 } from '@fortawesome/free-solid-svg-icons';
-import { faPenCircle } from '@fortawesome/pro-solid-svg-icons';
+// import { faPenCircle } from '@fortawesome/pro-solid-svg-icons';
 import colors from '../colors';
 
 const InProgress = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.ttahubMediumBlue} icon={faClock} />;
 const Closed = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.successDarker} icon={faCheckCircle} />;
 const NotStarted = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.ttahubOrange} icon={faMinusCircle} />;
 const NoStatus = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.baseLighter} icon={faExclamationCircle} />;
-const Draft = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.baseDarkest} icon={faPenCircle} />;
+const Draft = () => <i className="fa-regular fa-pen-circle margin-right-1 flex-align-self-center" style={{ fill: colors.baseDarkest }} />;
 const Ceased = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.errorDark} icon={faTimesCircle} />;
 const Pencil = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.ttahubMediumBlue} icon={faPencil} />;
 const Trash = () => <FontAwesomeIcon className="margin-right-1 flex-align-self-center" size="1x" color={colors.ttahubMediumBlue} icon={faTrash} />;
