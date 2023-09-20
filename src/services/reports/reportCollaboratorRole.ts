e6f9503cc9f2392f17c279b54d599573f5c6003d
GarrettEHill <garretthill@gmail.com>
2023-09-20 09:35:13 -0700

    Update reportCollaboratorRole.ts

--- src/services/reports/reportCollaboratorRole.ts
+++ src/services/reports/reportCollaboratorRole.ts
@@ -1,10 +1,43 @@
+import db from '../../models';
+import {
+  type EnumInfo,
+  type EntityGenericEnum,
+  type EnumSyncResponse,
+  getEntityGenericEnum,
+  syncEntityGenericEnum,
+  includeEntityGenericEnums,
+} from '../enums/generic';
+
+const {
+  ReportCollaboratorRoles,
+  Role,
+} = db;
+
+const rolesEnumInfo:EnumInfo = {
+  model: Role,
+  as: 'roles',
+  keyName: 'roles',
+};
+
 const syncReportCollaboratorRoles = async (
   entity: {},
 ) => {};
 
-const includeReportCollaboratorRoles = () => ({});
+const includeReportCollaboratorRoles = () => includeEntityGenericEnums(
+  ReportCollaboratorRoles,
+  rolesEnumInfo,
+  { name: 'reportParticipationId', type },
+);
 
-const getReportCollaboratorRoles = async () => {};
+const getReportCollaboratorRoles = async (
+  entity: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
+  participantIds: number[] | null = null,
+):Promise<EntityGenericEnum[]> => getEntityGenericEnum(
+  ReportCollaboratorRoles,
+  rolesEnumInfo,
+  { name: 'reportCollaboratorRoleId', ...entity },
+  participantIds,
+);
 
 export {
   syncReportCollaboratorRoles,
