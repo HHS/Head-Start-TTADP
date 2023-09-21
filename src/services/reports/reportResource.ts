1cf0e087f9fefde86d35052c629d198b63963336
GarrettEHill <garretthill@gmail.com>
2023-09-20 17:18:32 -0700

    Update reportResource.ts

--- src/services/reports/reportResource.ts
+++ src/services/reports/reportResource.ts
@@ -1,1 +1,9 @@
 // TODO: how to handle this, check how to pipe everything through the other resource system.
+
+const syncReportResources = async () => {};
+const includeReportResources = () => {};
+
+export {
+  syncReportResources,
+  includeReportResources,
+};
