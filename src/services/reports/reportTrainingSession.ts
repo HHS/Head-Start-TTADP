e327a6a5efaa88c80d799f0b12043df11e4e552e
GarrettEHill <garretthill@gmail.com>
2023-09-20 17:18:47 -0700

    Update reportTrainingSession.ts

---
+++ src/services/reports/reportTrainingSession.ts
@@ -0,0 +1,8 @@
+const syncReportTrainingSession = async () => {};
+
+const includeReportTrainingSession = async () => {};
+
+export {
+  syncReportTrainingSession,
+  includeReportTrainingSession,
+};
