31fc4cd4db38d8bf5271a5ea03b8156864732ce6
GarrettEHill <garretthill@gmail.com>
2023-09-20 17:18:40 -0700

    Update reportTrainingEvent.ts

--- src/services/reports/reportTrainingEvent.ts
+++ src/services/reports/reportTrainingEvent.ts
@@ -7,12 +7,6 @@ import {
   TRAINING_TYPE,
 } from '../../constants';
 import { ReportDataType, ReportDescriptor, syncReport } from './report';
-import { syncReportGoalTemplates } from './reportGoalTemplate';
-import { syncCollaboratorsForType } from './reportCollaborator';
-import { syncReportNationalCenters } from './reportNationalCenter';
-import { syncReportReasons } from './reportReason';
-import { syncReportTargetPopulations } from './reportTargetPopulation';
-import organizer from '../../models/organizer';
 
 const {
   ReportTrainingEvent,
@@ -24,34 +18,13 @@ interface ReportTrainingEventDataType {
   regionId?: number,
   name?: string,
   organizerId?: number,
-  audiance?: typeof AUDIENCE[keyof typeof AUDIENCE],
   trainingType?: typeof TRAINING_TYPE[keyof typeof TRAINING_TYPE],
   vision?: string,
 }
 
-interface FullReportTrainingEventDataType
-  extends ReportTrainingEventDataType, ReportDataType {}
-
 const reportTrainingEventRemapping: Record<string, string> = {
   reportTrainingEventId: 'id',
 };
-
-/**
- * This function filters the given data object and separates it into two parts:
- * - matched: an object that contains only the properties that match the structure of
- *            ReportTrainingEventDataType
- * - unmatched: an object that contains the remaining properties that do not match the
- *              structure of ReportTrainingEventDataType
- *
- * @param data - The data object to be filtered
- * @returns A promise that resolves to an object with two properties: matched and unmatched
- */
-const filterData = async (
-  data: Record<string, any>,
-): Promise<{
-  matched: ReportTrainingEventDataType,
-  unmatched: Record<string, any>,
-}> => filterDataToModel(data, ReportTrainingEvent);
 
 const createOrUpdateReportTrainingEvent = async (
   data: ReportTrainingEventDataType,
@@ -61,81 +34,40 @@ const createOrUpdateReportTrainingEvent = async (
     reportTrainingEvent = await ReportTrainingEvent.findById(data.id);
     const changedData = collectChangedValues(data, reportTrainingEvent);
     if (changedData.length) {
-      await ReportTrainingEvent.update(
+      return ReportTrainingEvent.update(
         changedData,
         { individualHooks: true },
       );
     }
   } else { // new report Path
-    reportTrainingEvent = await ReportTrainingEvent
+    return ReportTrainingEvent
       .create(data); // TODO: have create return the object
   }
-  return reportTrainingEvent;
+  return Promise.resolve();
 };
 
 const syncReportTrainingEvent = async (
-  data: FullReportTrainingEventDataType,
-  metaDataProcessors: Array<(...args: any[]) => Promise<any>>,
+  report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
+  data: ReportTrainingEventDataType,
 ) => {
-  const { matched: filteredData, unmatched } = await filterData(data);
+  const {
+    matched,
+    unmatched,
+  } = await filterDataToModel(
+    {
+      ...data,
+      reportId: report.id,
+      reportType: report.type,
+    },
+    ReportTrainingEvent,
+  );
 
-  const reportDescriptor: ReportDescriptor = {
-    reportId: data.reportId,
-    reportType: REPORT_TYPE.REPORT_TRAINING_EVENT,
-    regionId: data.regionId,
+  return {
+    promises: Promise.all([
+      createOrUpdateReportTrainingEvent(matched),
+    ]),
+    unmatched,
   };
-  // TODO: Refector to use metaDataProcessors array, and collect and return all unmatched data sets
-  await Promise.all([
-    createOrUpdateReportTrainingEvent(filteredData),
-    (unmatched.goal)
-      ? syncReportGoalTemplates(
-        reportDescriptor,
-        [{
-          ...(unmatched.goalTemplateId && { goalTemplateId: unmatched.goalTemplateId }),
-          name: unmatched.goal,
-        }],
-      )
-      : Promise.resolve(),
-    (unmatched.owner) // Owner
-      ? syncCollaboratorsForType(
-        reportDescriptor,
-        COLLABORATOR_TYPES.OWNER,
-        [unmatched.owner],
-      )
-      : Promise.resolve(),
-    (unmatched.creator) // instatiator
-      ? syncCollaboratorsForType(
-        reportDescriptor,
-        COLLABORATOR_TYPES.INSTANTIATOR,
-        [unmatched.creator],
-      )
-      : Promise.resolve(),
-    (unmatched.colaboratorIds) // editor/collaborator
-      ? syncCollaboratorsForType(
-        reportDescriptor,
-        COLLABORATOR_TYPES.EDITOR,
-        colaboratorIds,
-      )
-      : Promise.resolve(),
-    (unmatched.reasons) // Reason
-      ? syncReportReasons(
-        reportDescriptor,
-        unmatched.reasons,
-      )
-      : Promise.resolve(), // Reason
-    (unmatched["National Center(s) Requested"]) // TODO: can we get this to a good value, not a string with spaces as a key
-      ? syncReportNationalCenters(
-        reportDescriptor,
-        unmatched["National Center(s) Requested"],
-      )
-      : Promise.resolve(), // National Center
-    (unmatched.targetPopulations) // TODO: can we get this to a good value, not a string with spaces as a key
-      ? syncReportTargetPopulations(
-        reportDescriptor,
-        unmatched.targetPopulations,
-      )
-      : Promise.resolve(), // target populations
-  ]);
 };
 
 const includeReportTrainingEvent = () => ({
