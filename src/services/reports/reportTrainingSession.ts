70aa01de57e0776374862c6ed1b096346576621f
GarrettEHill <garretthill@gmail.com>
2023-09-25 12:41:16 -0700

    Update reportTrainingSession.ts

--- src/services/reports/reportTrainingSession.ts
+++ src/services/reports/reportTrainingSession.ts
@@ -1,6 +1,71 @@
-const syncReportTrainingSession = async () => {};
+import { filterDataToModel, collectChangedValues } from '../../lib/modelUtils';
+import db from '../../models';
+import {
+  REPORT_TYPE,
+} from '../../constants';
 
-const includeReportTrainingSession = async () => {};
+const {
+  ReportTrainingSession,
+  Organizer,
+} = db;
+
+const createOrUpdateReportTrainingSession = async (
+  data,
+) => {
+  let reportTrainingSession;
+  if (data.id) { // sync/update report path
+    reportTrainingSession = await ReportTrainingSession.findById(data.id);
+    const changedData = collectChangedValues(data, reportTrainingSession);
+    if (changedData.length) {
+      return ReportTrainingSession.update(
+        changedData,
+        { individualHooks: true },
+      );
+    }
+  } else { // new report Path
+    return ReportTrainingSession
+      .create(data); // TODO: have create return the object
+  }
+  return Promise.resolve();
+};
+
+const syncReportTrainingSession = async (
+  report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
+  data,
+) => {
+  const {
+    matched,
+    unmatched,
+  } = await filterDataToModel(
+    {
+      ...data,
+      reportId: report.id,
+      reportType: report.type,
+    },
+    ReportTrainingSession,
+  );
+
+  return {
+    promises: Promise.all([
+      createOrUpdateReportTrainingSession(matched),
+    ]),
+    unmatched,
+  };
+};
+
+const includeReportTrainingSession = async () => ({
+  model: ReportTrainingSession,
+  as: 'reportTrainingSession',
+  required: true,
+  attributes: [
+    'id',
+    'regionId',
+    'name',
+    'reportTrainingEventId',
+    'createdAt',
+    'updatedAt',
+  ],
+});
 
 export {
   syncReportTrainingSession,
