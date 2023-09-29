e45513d59e644b5a690d3c4314f9bcf19063bb8b
GarrettEHill <garretthill@gmail.com>
2023-09-29 07:54:13 -0700

    Create reportResources.js

---
+++ src/models/helpers/reportResources.js
@@ -0,0 +1,42 @@
+const processForResources = async (
+  sequelize,
+  instance,
+  options,
+) => {
+  // eslint-disable-next-line global-require
+  const { checkForSyncableReportResources } = require('../../services/reports/reportResource');
+  checkForSyncableReportResources(
+    {
+      id: (!instance.reportId && options.model.getTableName() === 'Reports')
+        ? instance.id
+        : instance.reportId,
+    },
+    options.model,
+    instance.id,
+    (instance.changed() || [])
+      .reduce((acc, field) => {
+        acc[field] = instance.get(field);
+        return acc;
+      }, {}),
+  );
+};
+
+const cleanupResources = async (
+  sequelize,
+  instance,
+  options,
+) => sequelize.models.ReportResource.destroy({
+  where: {
+    reportId: (!instance.reportId && options.model.getTableName() === 'Reports')
+      ? instance.id
+      : instance.reportId,
+    tableName: options.model.getTableName(),
+    tableId: instance.id,
+  },
+  individualHooks: true,
+});
+
+export {
+  processForResources,
+  cleanupResources,
+};
