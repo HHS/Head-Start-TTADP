abe177d43f6a6d50d76beeb6c44d25adad3020b4
nvms <jon@pye.rs>
2023-09-29 13:17:20 -0400

    log to stderr

--- similarity_api/src/db/db.py
+++ similarity_api/src/db/db.py
@@ -1,6 +1,8 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import text
 
+import logging
+
 db = SQLAlchemy()
 
 def query_many(query_str, data):
@@ -10,5 +12,6 @@ def query_many(query_str, data):
         out = [{column: value for column, value in row._mapping.items()} for row in result]
         return out
     except Exception as e:
+        logging.exception(e)
         return None
 
