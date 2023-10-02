aecb7bb7e258d0a4015dfa2b6e3fc2f95b4fb9db
GarrettEHill <garretthill@gmail.com>
2023-10-02 14:53:04 -0700

    Update reportObjective.ts

--- src/services/reports/reportObjective.ts
+++ src/services/reports/reportObjective.ts
@@ -1,1 +1,11 @@
 // TODO: everything
+
+const syncReportObjectives = async () => {};
+const includeReportObjectives = () => {};
+const getReportObjectives = async () => {};
+
+export {
+  syncReportObjectives,
+  includeReportObjectives,
+  getReportObjectives,
+};
