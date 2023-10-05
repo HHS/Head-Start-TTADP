c47c4a4f3982ce40136d8fee5bfa50c9a041200b
GarrettEHill <garretthill@gmail.com>
2023-10-05 14:09:24 -0700

    Create goalTemplateResource.ts

---
+++ src/services/goalsAndObjectives/goalTemplateResource.ts
@@ -0,0 +1,50 @@
+import db from '../../models';
+import { RemappingDefinition, includeToFindAll, remap } from '../../lib/modelUtils';
+
+const {
+  GoalTemplateResource,
+  Resource,
+} = db;
+
+// TODO: finish
+const syncGoalTemplateResources = async () => {};
+
+const includeGoalTemplateResources = () => ({
+  model: GoalTemplateResource,
+  as: 'goalTemplateResources',
+  required: false,
+  attributes: [
+    'id',
+    'resourceId',
+    'sourceFields',
+    'isFoiaable',
+    'isReferenced',
+  ],
+  includes: [{
+    model: Resource,
+    as: 'resource',
+    required: true,
+    attributes: [
+      'id',
+      'domain',
+      'url',
+      'title',
+      'metadata',
+    ],
+  }],
+});
+
+const getGoalTemplateResources = async (
+  goalTemplateId: number,
+) => includeToFindAll(
+  includeGoalTemplateResources,
+  {
+    goalTemplateId,
+  },
+);
+
+export {
+  syncGoalTemplateResources,
+  includeGoalTemplateResources,
+  getGoalTemplateResources,
+};
