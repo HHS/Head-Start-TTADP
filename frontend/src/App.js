847bd926d11a7505a7e697bd2a269bb5125172d2
Matt Bevilacqua <thewatermethod@gmail.com>
2023-10-06 14:30:24 -0400

    attempt to push userdata

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -76,6 +76,35 @@ function App() {
 
   useEffect(() => {
     try {
+      if (!user) {
+        return;
+      }
+      if (window.dataLayer && Array.isArray(window.dataLayer)) {
+        const eventNames = window.dataLayer.map((item) => item.event);
+        if (eventNames.includes('userData')) {
+          return;
+        }
+
+        if (!user.id || !user.roles) {
+          return;
+        }
+
+        const event = {
+          event: 'userData',
+          user_id: user.id,
+          user_roles: user.roles,
+        };
+
+        window.dataLayer.push(event);
+      }
+    } catch (err) {
+    // eslint-disable-next-line no-console
+      console.log('Error sending user data to Google Analytics', err);
+    }
+  }, [user]);
+
+  useEffect(() => {
+    try {
       const readNotifications = window.localStorage.getItem(WHATSNEW_NOTIFICATIONS_KEY) || '[]';
 
       if (readNotifications) {
