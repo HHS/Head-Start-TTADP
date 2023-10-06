a5aff1d6cd77f8910d7e42db9454db7cc7a82216
GarrettEHill <garretthill@gmail.com>
2023-10-06 12:41:39 -0700

    Update isFlagged.ts

--- src/models/helpers/isFlagged.ts
+++ src/models/helpers/isFlagged.ts
@@ -1,5 +1,14 @@
 import { Op, Sequelize } from 'sequelize';
+import { skipIf } from './flowControl';
 
+/**
+ * This function automatically populates a flag field in an instance if it is undefined or null.
+ * It also ensures that the flag field is included in the fields array of options.
+ * @param flagName - The name of the flag field.
+ * @param sequelize - The Sequelize instance.
+ * @param instance - The instance to populate the flag field for.
+ * @param options - The options object containing the fields array.
+ */
 const autoPopulateIsFlagged = (
   flagName: string,
   sequelize,
@@ -7,8 +16,7 @@ const autoPopulateIsFlagged = (
   options,
 ): void => {
   // Check if flagName is undefined or null
-  if (instance[flagName] === undefined
-    || instance[flagName] === null) {
+  if (instance[flagName] === undefined || instance[flagName] === null) {
     // Set flagName to false
     instance.set(flagName, false);
 
@@ -20,30 +28,65 @@ const autoPopulateIsFlagged = (
   }
 };
 
+/**
+ * This function auto-populates the 'isFoiaable' property in the given instance using the
+ * provided sequelize ORM.
+ * @param {Sequelize} sequelize - The sequelize instance.
+ * @param {Object} instance - The instance to be populated.
+ * @param {Object} options - Additional options for auto-population.
+ * @returns {void}
+ */
 const autoPopulateIsFoiaable = (
   sequelize,
   instance,
   options,
-): void => autoPopulateIsFlagged(
-  'isFoiaable',
-  sequelize,
-  instance,
-  options,
-);
+): void => {
+  // Call the autoPopulateIsFlagged function with the necessary arguments
+  autoPopulateIsFlagged(
+    'isFoiaable', // Flag name
+    sequelize, // Sequelize instance
+    instance, // Instance to be populated
+    options, // Additional options
+  );
+};
 
+/**
+ * This function is used to auto-populate the 'isReferenced' flag in a Sequelize model instance.
+ * It calls the 'autoPopulateIsFlagged' function with the provided parameters.
+ *
+ * @param sequelize - The Sequelize instance.
+ * @param instance - The Sequelize model instance.
+ * @param options - Additional options for auto-population.
+ */
 const autoPopulateIsReferenced = (
-  sequelize,
-  instance,
-  options,
-): void => autoPopulateIsFlagged(
-  'isReferenced',
-  sequelize,
-  instance,
-  options,
-);
+  sequelize, // The Sequelize instance.
+  instance, // The Sequelize model instance.
+  options, // Additional options for auto-population.
+): void => {
+  autoPopulateIsFlagged( // Call the 'autoPopulateIsFlagged' function.
+    'isReferenced', // The flag name.
+    sequelize, // The Sequelize instance.
+    instance, // The Sequelize model instance.
+    options, // Additional options for auto-population.
+  );
+};
 
 //----------------------------------------------------
 
+/**
+ * Recalculates the value of a flag for a given set of model instances.
+ * @param flagName - The name of the flag to recalculate.
+ * @param sequelize - The Sequelize instance.
+ * @param options - Additional options for the query.
+ * @param ids - The IDs of the model instances to recalculate the flag for.
+ * @param model - The Sequelize model to perform the query on.
+ * @param reportIntermediateModel - The intermediate model for the report association (optional).
+ * @param reportAs - The alias for the report association (optional).
+ * @param activityReportIntermediateModel - The intermediate model for the activity report
+ * association (optional).
+ * @param activityReportIntermediateAs - The alias for the activity report association (optional).
+ * @returns A promise that resolves with an array containing the results of the update operations.
+ */
 const recalculateIsFlagged = async (
   flagName: string,
   sequelize,
@@ -57,6 +100,7 @@ const recalculateIsFlagged = async (
     activityReportIntermediateAs,
   } = null,
 ) => {
+  // Define the where clause for the report association
   const reportWhere = {
     model: sequelize.models.Report,
     as: 'reports',
@@ -78,6 +122,7 @@ const recalculateIsFlagged = async (
     ],
   };
 
+  // Define the where clause for the activity report association
   const activityReportWhere = {
     model: sequelize.models.ActivityReport,
     as: 'activityReports',
@@ -91,6 +136,7 @@ const recalculateIsFlagged = async (
     },
   };
 
+  // Perform the query to calculate the flag value for each model instance
   const deltas = await model.findAll({
     attributes: [
       'id',
@@ -132,6 +178,7 @@ const recalculateIsFlagged = async (
     transaction: options.transaction,
   });
 
+  // Separate the model instances into two arrays based on the flag value
   const { nowFlagged, clearFlagged } = deltas.reduce((acc, delta) => {
     if (delta[flagName]) {
       acc.nowFlagged.push(delta.id);
@@ -141,6 +188,7 @@ const recalculateIsFlagged = async (
     return acc;
   }, { nowFlagged: [], clearFlagged: [] });
 
+  // Update the flag value for the model instances that need to be flagged or unflagged
   return Promise.all([
     (nowFlagged && nowFlagged.length)
       ? model.update(
@@ -171,16 +219,30 @@ const recalculateIsFlagged = async (
   ]);
 };
 
+/**
+ * Recalculates the 'isFoiaable' flag for a given model and its associated intermediate models.
+ *
+ * @param sequelize - The Sequelize instance.
+ * @param options - The Sequelize options.
+ * @param ids - The IDs of the records to recalculate.
+ * @param model - The model to recalculate.
+ * @param reportIntermediateModel - The intermediate model used for reporting.
+ * @param reportAs - The alias for the intermediate model used for reporting.
+ * @param activityReportIntermediateModel - The intermediate model used for activity reporting.
+ * @param activityReportIntermediateAs - The alias for the intermediate model used for activity
+ * reporting.
+ * @returns A Promise that resolves when the recalculation is complete.
+ */
 const recalculateIsFoiaable = async (
   sequelize,
   options,
   ids,
   model,
   {
-    reportIntermediateModel,
-    reportAs,
-    activityReportIntermediateModel,
-    activityReportIntermediateAs,
+    reportIntermediateModel, // Intermediate model used for reporting
+    reportAs, // Alias for the intermediate model used for reporting
+    activityReportIntermediateModel, // Intermediate model used for activity reporting
+    activityReportIntermediateAs, // Alias for the intermediate model used for activity reporting
   } = null,
 ) => recalculateIsFlagged(
   'isFoiaable',
@@ -196,6 +258,19 @@ const recalculateIsFoiaable = async (
   },
 );
 
+/**
+ * Recalculates the "isReferenced" flag for a given model.
+ * @param sequelize - The Sequelize instance.
+ * @param options - Additional options for the operation.
+ * @param ids - An array of IDs to recalculate.
+ * @param model - The model to perform the recalculation on.
+ * @param reportIntermediateModel - The intermediate model used for reporting.
+ * @param reportAs - The alias for the intermediate model used for reporting.
+ * @param activityReportIntermediateModel - The intermediate model used for activity reporting.
+ * @param activityReportIntermediateAs - The alias for the intermediate model used for activity
+ * reporting.
+ * @returns A promise that resolves when the recalculation is complete.
+ */
 const recalculateIsReferenced = async (
   sequelize,
   options,
@@ -333,6 +408,15 @@ const findFlaggedModelsFromModel = (
 
 //----------------------------------------------------
 
+/**
+ * Sets the model attributes as flagged.
+ * @param {string} flagName - The name of the flag.
+ * @param {Model} model - The model to update.
+ * @param {string} prefix - The prefix for attribute names.
+ * @param {object} options - Additional options for the update.
+ * @param {number} modelId - The ID of the model instance to update.
+ * @returns {Promise<void[]>} A promise that resolves when all updates are complete.
+ */
 const setModelAttributesAsFlagged = async (
   flagName,
   model,
@@ -340,20 +424,22 @@ const setModelAttributesAsFlagged = async (
   options,
   modelId,
 ) => {
+  // Find flagged models from the given model
   const {
-    includes,
-    aggregates,
-    aggregateObjects,
+    includes, // Related models to include in the query
+    aggregates, // Aggregated attributes to select in the query
+    aggregateObjects, // Objects containing information about aggregated models
   } = findFlaggedModelsFromModel(
     flagName,
     model,
     prefix,
   );
 
+  // Fetch the foiaable data sets and selected aggregates for the given modelId
   const [foiaableDataSets] = await model.findAll({
     attributes: [
-      ['id', `${prefix}Id`],
-      ...aggregates,
+      ['id', `${prefix}Id`], // Include the ID with the specified prefix
+      ...aggregates, // Include the selected aggregates
     ],
     where: { id: modelId },
     includes,
@@ -362,6 +448,7 @@ const setModelAttributesAsFlagged = async (
     transaction: options.transaction,
   });
 
+  // Update each aggregate model with isFoiaable flag based on the corresponding dataset ID
   return Promise.all(aggregateObjects.map(({
     aggregateModel,
     aggregateName,
@@ -375,11 +462,20 @@ const setModelAttributesAsFlagged = async (
   )));
 };
 
+/**
+ * Sets the model attributes as "foiaable" by calling the setModelAttributesAsFlagged function.
+ * @param {object} model - The model object.
+ * @param {string} prefix - The prefix for the attributes.
+ * @param {object} options - The options for setting the attributes.
+ * @param {string} modelId - The ID of the model.
+ * @returns {Promise<void>} - A promise that resolves when the attributes are set.
+ */
 const setModelAttributesAsFoiaable = async (
   model,
   prefix,
   options,
   modelId,
+  // Call the setModelAttributesAsFlagged function to set the attributes as "foiaable"
 ) => setModelAttributesAsFlagged(
   'isFoiaable',
   model,
@@ -388,44 +484,124 @@ const setModelAttributesAsFoiaable = async (
   modelId,
 );
 
+/**
+ * Sets the model attributes as referenced.
+ * @param {object} model - The model object.
+ * @param {string} prefix - The prefix for the attributes.
+ * @param {object} options - The options object.
+ * @param {string} modelId - The ID of the model.
+ * @returns {Promise<void>} - A promise that resolves when the attributes are set.
+ */
 const setModelAttributesAsReferenced = async (
   model,
   prefix,
   options,
   modelId,
 ) => setModelAttributesAsFlagged(
-  'isReferenced',
-  model,
-  prefix,
-  options,
-  modelId,
+  'isReferenced', // Flag to set the attributes as referenced
+  model, // The model object
+  prefix, // The prefix for the attributes
+  options, // The options object
+  modelId, // The ID of the model
 );
 
 //----------------------------------------------------
 
+/**
+ * Retrieves the foiaable columns for a given model.
+ * @param {object} models - The object containing all the Sequelize models.
+ * @param {object} model - The Sequelize model for which to retrieve the foiaable columns.
+ * @returns {Promise<Array<object>>} - A promise that resolves to an array of objects representing
+ * the foiaable columns.
+ */
 const getFoiaableColumnsForModel = async (
   models,
   model,
 ) => models.Foiaable.findAll({
-  attributes: ['column'],
-  where: { table: model.getTableName() },
-  raw: true,
+  attributes: ['column'], // Selects only the 'column' attribute from the result set
+  where: { table: model.getTableName() }, // Filters the result set based on the 'table' attribute matching the model's table name
+  raw: true, // Returns raw data instead of Sequelize instances
 });
 
+/**
+ * Checks if there is an attempt to change foiaable columns on a model instance.
+ * @param sequelize - The Sequelize instance.
+ * @param instance - The model instance being checked.
+ * @param options - Additional options for the check.
+ */
 const checkForAttemptToChangeFoiaableValue = async (
   sequelize,
   instance,
   options,
 ) => {
-  const foiaableColumns = await getFoiaableColumnsForModel(sequelize.models, instance.constructor);
+  // Check if the instance has the 'isFoiaable' attribute set to true
+  if (instance.get('isFoiaable')) {
+    // Get the list of foiaable columns for the model
+    const foiaableColumns = await getFoiaableColumnsForModel(
+      sequelize.models,
+      instance.constructor,
+    );
 
-  const updatedColumns = instance.changed();
+    // Get the list of updated columns on the instance
+    const updatedColumns = instance.changed();
 
-  // Check if any updated column matches the foiaable columns
-  const updatedFoiaableColumns = updatedColumns.find((column) => foiaableColumns.includes(column));
+    // Find any updated column that matches a foiaable column
+    const updatedFoiaableColumns = updatedColumns
+      .find((column) => foiaableColumns.includes(column));
 
-  if (updatedFoiaableColumns && updatedFoiaableColumns.length > 0) {
-    throw new Error(`Cannot update foiaable columns (${updatedFoiaableColumns.join(',')}) on table ${instance.constructor.getTableName()}`);
+    // If there are updated foiaable columns, throw an error
+    if (updatedFoiaableColumns && updatedFoiaableColumns.length > 0) {
+      throw new Error(`Cannot update foiaable columns (${updatedFoiaableColumns.join(',')}) on table ${instance.constructor.getTableName()}`);
+    }
+  }
+};
+
+/**
+ * Checks if a record is foiaable and throws an error if it is.
+ * @param sequelize - The Sequelize instance.
+ * @param instance - The instance of the model.
+ * @param options - The options for the operation.
+ */
+const checkForAttemptToRemoveFoiaableValue = async (
+  sequelize,
+  instance,
+  options,
+) => {
+  // Check if the record is foiaable
+  if (instance.get('isFoiaable')) {
+    // Throw an error if the record is foiaable
+    throw new Error(`Cannot remove foiaable record in table ${instance.constructor.getTableName()}`);
+  }
+};
+
+//----------------------------------------------------
+
+/**
+ * This function is responsible for auto-populating a flag in an instance of a Sequelize model.
+ * It checks if the 'autoPopulateFlag' option is set to true, and if not, it sets the flag to
+ * false and adds it to the list of fields to be saved.
+ *
+ * @param sequelize - The Sequelize instance.
+ * @param instance - The instance of the Sequelize model.
+ * @param options - The options object.
+ * @param flagName - The name of the flag to be auto-populated.
+ */
+const autoPopulateFlag = (
+  sequelize,
+  instance,
+  options,
+  flagName,
+) => {
+  // Check if the 'autoPopulateFlag' option is set to true, and skip if so
+  if (skipIf(options, 'autoPopulateFlag')) return;
+
+  // Check if the flag is undefined or null
+  // If so, set the flag to false and add it to the list of fields to be saved
+  if (instance[flagName] === undefined || instance[flagName] === null) {
+    instance.set(flagName, false);
+    if (!options.fields.includes(flagName)) {
+      options.fields.push(flagName);
+    }
   }
 };
 
@@ -444,4 +620,6 @@ export {
   setModelAttributesAsReferenced,
   getFoiaableColumnsForModel,
   checkForAttemptToChangeFoiaableValue,
+  checkForAttemptToRemoveFoiaableValue,
+  autoPopulateFlag,
 };
