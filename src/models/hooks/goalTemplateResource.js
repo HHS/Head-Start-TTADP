e7a030d1cd16abac6e199a55e9ab125a07902166
GarrettEHill <garretthill@gmail.com>
2023-10-06 15:43:12 -0700

    Update goalTemplateResource.js

--- src/models/hooks/goalTemplateResource.js
+++ src/models/hooks/goalTemplateResource.js
@@ -1,10 +1,36 @@
 /* eslint-disable import/prefer-default-export */
-const { cleanupOrphanResources } = require('../helpers/orphanCleanupHelper');
+import { cleanupOrphanResources } from '../helpers/orphanCleanupHelper';
+import { skipIf } from '../helpers/flowControl';
+import {
+  checkForAttemptToChangeFoiaableValue,
+  checkForAttemptToRemoveFoiaableValue,
+  autoPopulateFlag,
+} from '../helpers/isFlagged';
 
 const afterDestroy = async (sequelize, instance, options) => {
   await cleanupOrphanResources(sequelize, instance.resourceId);
 };
 
+const beforeValidate = async (sequelize, instance, options) => {
+  if (skipIf(options, 'beforeValidate')) return;
+  if (!Array.isArray(options.fields)) {
+    options.fields = []; //eslint-disable-line
+  }
+  autoPopulateFlag(sequelize, instance, options, 'isFoiaable');
+  autoPopulateFlag(sequelize, instance, options, 'isReferenced');
+};
+
+const beforeUpdate = async (sequelize, instance, options) => {
+  await checkForAttemptToChangeFoiaableValue(sequelize, instance, options);
+};
+
+const beforeDestroy = async (sequelize, instance, options) => {
+  await checkForAttemptToRemoveFoiaableValue(sequelize, instance, options);
+};
+
 export {
+  beforeValidate,
+  beforeUpdate,
+  beforeDestroy,
   afterDestroy,
 };
