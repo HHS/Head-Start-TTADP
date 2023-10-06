7fabf110deeab86b2687e1c3205667d0a339a17d
GarrettEHill <garretthill@gmail.com>
2023-10-06 15:41:43 -0700

    Update objectiveTemplateTopic.js

--- src/models/hooks/objectiveTemplateTopic.js
+++ src/models/hooks/objectiveTemplateTopic.js
@@ -1,9 +1,31 @@
-import { checkForAttemptToChangeFoiaableValue } from '../helpers/isFlagged';
+import { skipIf } from '../helpers/flowControl';
+import {
+  checkForAttemptToChangeFoiaableValue,
+  checkForAttemptToRemoveFoiaableValue,
+  autoPopulateFlag,
+} from '../helpers/isFlagged';
+
+const beforeValidate = async (sequelize, instance, options) => {
+  if (skipIf(options, 'beforeValidate')) return;
+  if (!Array.isArray(options.fields)) {
+    options.fields = []; //eslint-disable-line
+  }
+  autoPopulateFlag(sequelize, instance, options, 'onAR');
+  autoPopulateFlag(sequelize, instance, options, 'onApprovedAR');
+  autoPopulateFlag(sequelize, instance, options, 'isFoiaable');
+  autoPopulateFlag(sequelize, instance, options, 'isReferenced');
+};
 
 const beforeUpdate = async (sequelize, instance, options) => {
   await checkForAttemptToChangeFoiaableValue(sequelize, instance, options);
 };
 
+const beforeDestroy = async (sequelize, instance, options) => {
+  await checkForAttemptToChangeFoiaableValue(sequelize, instance, options);
+};
+
 export {
+  beforeValidate,
   beforeUpdate,
+  beforeDestroy,
 };
