bc8116b9c59cfe0a522fe464df69d383c6a28e9e
GarrettEHill <garretthill@gmail.com>
2023-10-11 10:15:26 -0700

    Update reportCollaborator.js

--- src/models/hooks/reportCollaborator.js
+++ src/models/hooks/reportCollaborator.js
@@ -3,20 +3,18 @@ import {
   cleanupResources,
 } from '../helpers/reportResources';
 
-const collectRoles = async (sequelize, instance, options) => {
-  const roleIds = await sequelize.models.UserRoles.findAll({
-    attributes: [
-      'roleId',
-    ],
-    where: { userId: instance.userId },
-    raw: true,
-  });
-  return sequelize.models.ReportCollaborativeRole.bulkCreate(
-    roleIds.map(({ roleId }) => ({
-      reportCollaboratorId: instance.id,
-      roleId,
-    })),
-  ); // TODO - allow hooks and on collision update.
+const syncRoles = async (sequelize, instance, options) => {
+  // eslint-disable-next-line global-require
+  const { syncReportCollaboratorRoles } = require('../../services/reports/reportCollaboratorRole');
+  return syncReportCollaboratorRoles(
+    { id: instance.reportId },
+    {
+      reportCollaborator: [{
+        reportCollaboratorId: instance.id,
+        userId: instance.userId,
+      }],
+    },
+  );
 };
 
 const beforeValidate = async (sequelize, instance, options) => {
@@ -27,11 +25,12 @@ const beforeUpdate = async (sequelize, instance, options) => {
 
 const afterUpdate = async (sequelize, instance, options) => {
   await processForResources(sequelize, instance, options);
+  await syncRoles(sequelize, instance, options);
 };
 
 const afterCreate = async (sequelize, instance, options) => {
   await processForResources(sequelize, instance, options);
-  await collectRoles(sequelize, instance, options);
+  await syncRoles(sequelize, instance, options);
 };
 
 const beforeDestroy = async (sequelize, instance, options) => {
