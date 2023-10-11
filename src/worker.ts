6a15ffb764c00f289c85e87e3b1554d7e3d3a090
Adam Levin <adam.levin@adhocteam.us>
2023-10-11 15:34:37 -0400

    testing

--- src/worker.ts
+++ src/worker.ts
@@ -1,12 +1,5 @@
-/* eslint-disable global-require */
-if (process.env.NODE_ENV === 'production') {
-  /* eslint-disable import/first */
-  require('newrelic');
-}
-
 import {} from 'dotenv/config';
 import throng from 'throng';
-import { logger } from './logger';
 import {
   processScanQueue,
 } from './services/scanQueue';
@@ -26,34 +19,29 @@ import {
   processMaintenanceQueue,
 } from './lib/maintenance';
 
-const processors = [
-  // File Scanning Queue
-  processScanQueue,
-
-  // AWS Elasticsearch Queue
-  processAWSElasticsearchQueue,
-
-  // S3 Queue.
-  processS3Queue,
-
-  // Resource Queue.
-  processResourceQueue,
-
-  // Notifications Queue
-  processNotificationQueue,
-
-  // Maintenance Queue
-  processMaintenanceQueue,
-];
-
 // Number of workers to spawn
 const workers = process.env.WORKER_CONCURRENCY || 2;
 
 // Pull jobs off the redis queue and process them.
-async function workerStart() {
-  logger.info('Worker started');
-  processors.forEach((processor) => processor());
+async function start() {
+  // File Scanning Queue
+  processScanQueue();
+
+  // AWS Elasticsearch Queue
+  processAWSElasticsearchQueue();
+
+  // S3 Queue.
+  processS3Queue();
+
+  // Resource Queue.
+  processResourceQueue();
+
+  // Notifications Queue
+  processNotificationQueue();
+
+  // Maintenance Queue
+  processMaintenanceQueue();
 }
 
 // spawn workers and start them
-throng({ workers, start: workerStart });
+throng({ workers, start });
