da77e35c4271d6ae31ba3dc449b456aeebf2410f
GarrettEHill <garretthill@gmail.com>
2023-10-11 00:35:28 -0700

    Update reportObjectiveTopic.ts

--- src/services/reports/reportObjectiveTopic.ts
+++ src/services/reports/reportObjectiveTopic.ts
@@ -1,9 +1,37 @@
 // TODO: everything
+import db from '../../models';
+
+import {
+  type EnumInfo,
+  findAll,
+  type EntityEnumModel,
+  type EntityGenericEnum,
+  type EnumSyncResponse,
+  getEntityGenericEnum,
+  syncEntityGenericEnum,
+  includeEntityGenericEnums,
+} from '../enums/generic';
+
+const {
+  ReportObjectiveTopic,
+  Topic,
+} = db;
+
+const TopicEnumInfo:EnumInfo = {
+  model: Topic,
+  as: 'topic',
+  keyName: 'Topic', // TODO: make sure this is right
+};
+
 const syncReportObjectiveTopics = async (
   entity: {},
 ) => {};
 
-const includeReportObjectiveTopics = () => ({});
+const includeReportObjectiveTopics = () => includeEntityGenericEnums(
+  ReportObjectiveTopic,
+  TopicEnumInfo,
+  { name: 'reportObjectiveId' },
+);
 
 const getReportObjectiveTopics = async () => {};
 
