2578d7c05391c66a6200ff728482a287c60d2417
GarrettEHill <garretthill@gmail.com>
2023-10-11 08:52:18 -0700

    Update reportCollaborator.js

--- src/models/hooks/reportCollaborator.js
+++ src/models/hooks/reportCollaborator.js
@@ -3,6 +3,22 @@ import {
   cleanupResources,
 } from '../helpers/reportResources';
 
+const collectRoles = async (sequelize, instance, options) => {
+  const roleIds = await sequelize.models.UserRoles.findAll({
+    attributes: [
+      'roleId',
+    ],
+    where: { userId: instance.userId },
+    raw: true,
+  });
+  return sequelize.models.ReportCollaborativeRole.bulkCreate(
+    roleIds.map(({ roleId }) => ({
+      reportCollaboratorId: instance.id,
+      roleId,
+    })),
+  ); // TODO - allow hooks and on collision update.
+};
+
 const beforeValidate = async (sequelize, instance, options) => {
 };
 
@@ -15,6 +31,7 @@ const afterUpdate = async (sequelize, instance, options) => {
 
 const afterCreate = async (sequelize, instance, options) => {
   await processForResources(sequelize, instance, options);
+  await collectRoles(sequelize, instance, options);
 };
 
 const beforeDestroy = async (sequelize, instance, options) => {
