268c6fa3de47b9dc7332415a2a504158884b8816
GarrettEHill <garretthill@gmail.com>
2023-10-13 10:38:31 -0700

    Update enum.ts

--- src/scopes/helpers/enum.ts
+++ src/scopes/helpers/enum.ts
@@ -1,31 +1,54 @@
-import { Op } from 'sequelize';
-import { sequelize } from '../../models';
+import { numericFilter } from './numeric';
+import { singleValueTextFilter } from './text';
 
-const enumFilter = (
+const tableEnumFilter = (
+  isIncluded: boolean,
+  tableAlias: string,
+  values: string[] | number[],
+) => {
+  const isAllStrings = values.every((item) => typeof item === 'string');
+  const isAllNumbers = values.every((item) => typeof item === 'number');
+
+  if (isAllStrings) {
+    return singleValueTextFilter(isIncluded, tableAlias, 'name', values as string[]);
+  }
+  if (isAllNumbers) {
+    return numericFilter(isIncluded, tableAlias, 'id', values as number[]);
+  }
+
+  throw new Error('Mixed values are not supported');
+};
+
+const valueEnumFilter = (
   isIncluded: boolean,
   tableAlias: string,
   column: string,
   values: string[],
-) => sequelize.where(
-  sequelize.col(`"${tableAlias}"."${column}"`),
-  {
-    ...(
-      isIncluded
-        ? { [Op.iRegexp]: values.join('|') }
-        : { [Op.notIRegexp]: values.join('|') }
-    ),
-  },
+) => singleValueTextFilter(
+  isIncluded,
+  tableAlias,
+  column,
+  values,
 );
 
-const filterEnums = (
+const filterTableEnums = (
+  tableAlias: string,
+) => ({
+  in: (query) => tableEnumFilter(true, tableAlias, query),
+  nin: (query) => tableEnumFilter(false, tableAlias, query),
+});
+
+const filterValueEnums = (
   tableAlias: string,
   column: string,
 ) => ({
-  in: (query) => enumFilter(true, tableAlias, column, query),
-  nin: (query) => enumFilter(false, tableAlias, column, query),
+  in: (query) => valueEnumFilter(true, tableAlias, column, query),
+  nin: (query) => valueEnumFilter(false, tableAlias, column, query),
 });
 
 export {
-  enumFilter,
-  filterEnums,
+  tableEnumFilter as enumFilter,
+  valueEnumFilter,
+  filterTableEnums,
+  filterValueEnums,
 };
