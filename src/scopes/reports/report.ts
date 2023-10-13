a6b8b2010dd7c14f38078c0b0b339451a522cf27
GarrettEHill <garretthill@gmail.com>
2023-10-13 10:38:50 -0700

    Update report.ts

--- src/scopes/reports/report.ts
+++ src/scopes/reports/report.ts
@@ -1,11 +1,13 @@
 import { Op } from 'sequelize';
 import { filterFieldDates } from '../helpers/dates';
+import { filterEnums } from '../helpers/enum';
 
 const topicToQuery = {
   startDate: filterFieldDates('startDate'),
   endDate: filterFieldDates('endDate'),
   createdAt: filterFieldDates('createdAt'),
   updatedAt: filterFieldDates('updatedAt'),
+  status: filterEnums('status'),
 };
 
 export {
