26b6eb1fe784927cc6341cd9256b924158eb2921
GarrettEHill <garretthill@gmail.com>
2023-10-16 00:12:37 -0700

    Update enum.ts

--- src/scopes/helpers/enum.ts
+++ src/scopes/helpers/enum.ts
@@ -4,19 +4,25 @@ import { singleValueTextFilter } from './text';
 const tableEnumFilter = (
   isIncluded: boolean,
   tableAlias: string,
-  values: string[] | number[],
-) => {
-  const isAllStrings = values.every((item) => typeof item === 'string');
-  const isAllNumbers = values.every((item) => typeof item === 'number');
+  values: (string | number)[],
+): object => {
+  const typeCounts = values.reduce((acc, value) => {
+    // eslint-disable-next-line no-plusplus
+    acc[typeof value]++;
+    return acc;
+  }, { number: 0, string: 0 });
 
-  if (isAllStrings) {
+  if (typeCounts.string === values.length) {
+    // TODO: this wont return valid results, the function needs to be a sub query matching to the
+    // ids from the "enum" table
     return singleValueTextFilter(isIncluded, tableAlias, 'name', values as string[]);
   }
-  if (isAllNumbers) {
+
+  if (typeCounts.number === values.length) {
     return numericFilter(isIncluded, tableAlias, 'id', values as number[]);
   }
 
-  throw new Error('Mixed values are not supported');
+  throw new Error('Mixed values are not supported. Values must be all strings or all numbers.');
 };
 
 const valueEnumFilter = (
@@ -31,18 +37,34 @@ const valueEnumFilter = (
   values,
 );
 
+// This function creates a filter object for table enums.
+// The filter object has two properties: "in" and "nin".
+// The "in" property is a function that calls the "tableEnumFilter" function
+// with the "true" argument.
+// The "nin" property is a function that calls the "tableEnumFilter" function
+// with the "false" argument.
 const filterTableEnums = (
   tableAlias: string,
 ) => ({
+  // Function that filters values that are included in the query
   in: (query) => tableEnumFilter(true, tableAlias, query),
+  // Function that filters values that are not included in the query
   nin: (query) => tableEnumFilter(false, tableAlias, query),
 });
 
+// This function creates a filter object for value enums.
+// The filter object has two properties: "in" and "nin".
+// The "in" property is a function that calls the "valueEnumFilter" function
+// with the "true" argument.
+// The "nin" property is a function that calls the "valueEnumFilter" function
+// with the "false" argument.
 const filterValueEnums = (
   tableAlias: string,
   column: string,
 ) => ({
+  // Function that filters values that are included in the query
   in: (query) => valueEnumFilter(true, tableAlias, column, query),
+  // Function that filters values that are not included in the query
   nin: (query) => valueEnumFilter(false, tableAlias, column, query),
 });
 
