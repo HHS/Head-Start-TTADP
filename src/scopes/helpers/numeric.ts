93c5a358c154b7c247a8c877c18c26044bced104
GarrettEHill <garretthill@gmail.com>
2023-10-16 00:12:46 -0700

    Update numeric.ts

--- src/scopes/helpers/numeric.ts
+++ src/scopes/helpers/numeric.ts
@@ -1,6 +1,7 @@
 import { Op } from 'sequelize';
 import { sequelize } from '../../models';
 
+// TODO: for some reason the query generated is making each of the numbers a string
 const numericFilter = (
   isIncluded: boolean,
   tableAlias: string,
@@ -21,8 +22,18 @@ const filterNumerics = (
   tableAlias: string,
   column: string,
 ) => ({
-  in: (query) => numericFilter(true, tableAlias, column, query),
-  nin: (query) => numericFilter(false, tableAlias, column, query),
+  in: (query) => numericFilter(
+    true,
+    tableAlias,
+    column,
+    query,
+  ),
+  nin: (query) => numericFilter(
+    false,
+    tableAlias,
+    column,
+    query,
+  ),
 });
 
 export {
