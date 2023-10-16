b3fa2e52401b6b572a98f549bd6819dc9cc47dbd
GarrettEHill <garretthill@gmail.com>
2023-10-16 00:14:01 -0700

    Update reportTrainingEvent.ts

--- src/scopes/reports/reportTrainingEvent.ts
+++ src/scopes/reports/reportTrainingEvent.ts
@@ -1,8 +1,40 @@
+import { Op } from 'sequelize';
+import { filterAssociation } from '../helpers/utils';
 import { filterTableEnums, filterValueEnums } from '../helpers/enum';
 import { filterNumerics } from '../helpers/numeric';
 
+// SQL query to select the report ID from the "Reports" table based on the report type
+const regionsSql = `SELECT
+    rte."reportId"
+  FROM "ReportTrainingEvents" rte
+  WHERE rte."regionId"`;
+
+// Function to create a filter for report types
+const regionsFilter = (
+  includes: boolean, // Whether to include or exclude the report types
+  regions: number[],
+) => ({
+  [Op.and]: [
+    filterAssociation(
+      regionsSql,
+      regions,
+      {
+        // Negate the value of 'includes' to exclude report types if 'includes' is false
+        exclude: !includes,
+        comparator: 'in',
+      },
+    ),
+  ],
+});
+
+// Function to create a filter object for report types
+const filterRegions = () => ({
+  in: (query) => regionsFilter(true, query), // Include report types in the query
+  nin: (query) => regionsFilter(false, query), // Exclude report types from the query
+});
+
 const topicToQuery = {
-  region: filterNumerics('reportTraningEvent', 'reportId'),
+  region: filterRegions(),
   eventId: undefined, // TODO
   name: undefined, // TODO
   organizer: filterTableEnums('organizer'),
@@ -11,5 +43,8 @@ const topicToQuery = {
 };
 
 export {
+  regionsSql,
+  regionsFilter,
+  filterRegions,
   topicToQuery,
 };
