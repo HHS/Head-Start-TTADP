81bf155027b5b67c85599a2425b0f8010b7dd58a
GarrettEHill <garretthill@gmail.com>
2023-10-17 00:45:13 -0700

    Update reportRecipient.js

--- src/models/reportRecipient.js
+++ src/models/reportRecipient.js
@@ -12,8 +12,8 @@ const { automaticallyGenerateJunctionTableAssociations } = require('./helpers/as
  */
 export default (sequelize, DataTypes) => {
   class ReportRecipient extends Model {
-    static associate(models) {
-      automaticallyGenerateJunctionTableAssociations(this, models);
+    static async associate(models) {
+      await automaticallyGenerateJunctionTableAssociations(this, models);
 
       ReportRecipient.addScope('defaultScope', {
         include: [
