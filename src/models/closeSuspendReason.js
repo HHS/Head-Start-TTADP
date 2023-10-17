6170948d58e3127d362f279f1fbf0b57d8d08f1b
GarrettEHill <garretthill@gmail.com>
2023-10-17 00:55:07 -0700

    Update closeSuspendReason.js

--- src/models/closeSuspendReason.js
+++ src/models/closeSuspendReason.js
@@ -12,8 +12,8 @@ const { automaticallyGenerateJunctionTableAssociations } = require('./helpers/as
  */
 export default (sequelize, DataTypes) => {
   class CloseSuspendReason extends Model {
-    static associate(models) {
-      automaticallyGenerateJunctionTableAssociations(this, models);
+    static async associate(models) {
+      await automaticallyGenerateJunctionTableAssociations(this, models);
 
       CloseSuspendReason.addScope('validFor', (name) => ({
         include: [{
