95099ecb57bfea4437935275631caafc7ecbab27
GarrettEHill <garretthill@gmail.com>
2023-10-17 00:47:56 -0700

    Update reportGoalTemplate.js

--- src/models/reportGoalTemplate.js
+++ src/models/reportGoalTemplate.js
@@ -14,8 +14,8 @@ const {
 
 export default (sequelize, DataTypes) => {
   class ReportGoalTemplate extends Model {
-    static associate(models) {
-      automaticallyGenerateJunctionTableAssociations(this, models);
+    static async associate(models) {
+      await automaticallyGenerateJunctionTableAssociations(this, models);
     }
   }
   ReportGoalTemplate.init({
