775975de55e2c163750a65f42e66785f004839ed
GarrettEHill <garretthill@gmail.com>
2023-10-17 00:45:55 -0700

    Update reportObjectiveTemplateTopic.js

--- src/models/reportObjectiveTemplateTopic.js
+++ src/models/reportObjectiveTemplateTopic.js
@@ -3,8 +3,8 @@ const { automaticallyGenerateJunctionTableAssociations } = require('./helpers/as
 
 export default (sequelize, DataTypes) => {
   class ReportObjectiveTemplateTopic extends Model {
-    static associate(models) {
-      automaticallyGenerateJunctionTableAssociations(this, models);
+    static async associate(models) {
+      await automaticallyGenerateJunctionTableAssociations(this, models);
     }
   }
   ReportObjectiveTemplateTopic.init({
