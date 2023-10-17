3cb1e98c71e819966f01f3b2691ff6f8cd5ebf7e
Matt Bevilacqua <thewatermethod@gmail.com>
2023-10-17 09:29:16 -0400

    fix error and include LDM files

--- src/migrations/20231017130822-account-for-merge-goals.js
+++ src/migrations/20231017130822-account-for-merge-goals.js
@@ -4,8 +4,8 @@ const {
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up(queryInterface) {
-    await queryInterface.sequelize.transaction(async (transaction, Sequelize) => {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
       const sessionSig = __filename;
       await prepMigration(queryInterface, transaction, sessionSig);
 
