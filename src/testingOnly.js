a6201a0724a8b3202d039eb408664a7be0e5a806
GarrettEHill <garretthill@gmail.com>
2023-10-16 21:38:55 -0700

    Update testingOnly.js

--- src/testingOnly.js
+++ src/testingOnly.js
@@ -2,14 +2,19 @@ import express from 'express';
 
 import {} from 'dotenv/config';
 import { auditLogger } from './logger';
+import db from './models';
 import testingRouter from './routes/testingOnly';
 
-const app = express();
+let app;
 
-app.use('/testingOnly', testingRouter);
+db.isReady.then(() => {
+  app = express();
 
-app.listen(9999, '0.0.0.0', () => {
-  auditLogger.info('TestingOnly listening on port 9999');
+  app.use('/testingOnly', testingRouter);
+
+  app.listen(9999, '0.0.0.0', () => {
+    auditLogger.info('TestingOnly listening on port 9999');
+  });
 });
 
 export default app;
