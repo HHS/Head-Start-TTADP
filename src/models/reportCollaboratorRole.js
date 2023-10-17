ec92453b18b336a6d6d05b4d875bdd61925c25ed
GarrettEHill <garretthill@gmail.com>
2023-10-17 00:48:24 -0700

    Update reportCollaboratorRole.js

--- src/models/reportCollaboratorRole.js
+++ src/models/reportCollaboratorRole.js
@@ -11,8 +11,8 @@ const { automaticallyGenerateJunctionTableAssociations } = require('./helpers/as
  */
 export default (sequelize, DataTypes) => {
   class ReportCollaboratorRole extends Model {
-    static associate(models) {
-      automaticallyGenerateJunctionTableAssociations(this, models);
+    static async associate(models) {
+      await automaticallyGenerateJunctionTableAssociations(this, models);
     }
   }
   ReportCollaboratorRole.init({
