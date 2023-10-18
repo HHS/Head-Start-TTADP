d0424e778043e9c460be9e8d26fce91ecda69525
GarrettEHill <garretthill@gmail.com>
2023-10-18 01:17:44 -0700

    Update reportTargetPopulation.ts

--- src/services/reports/reportTargetPopulation.ts
+++ src/services/reports/reportTargetPopulation.ts
@@ -14,12 +14,19 @@ const {
   ReportTargetPopulation,
 } = db;
 
-const targetPopulationEnumInfo:EnumInfo = {
+// Define an EnumInfo object for the target population enum
+const targetPopulationEnumInfo: EnumInfo = {
   model: TargetPopulation,
   as: 'targetPopulation',
   keyName: 'TargetPopulations',
 };
 
+/**
+ * Synchronize report target populations with the given target population enums.
+ * @param report - The report object containing id and type properties.
+ * @param targetPopulationEnums - An array of target population enums or null.
+ * @returns A promise that resolves to an EnumSyncResponse object.
+ */
 const syncReportTargetPopulations = async (
   report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
   targetPopulationEnums: { id?: number, name?: string }[] | null = null,
@@ -30,16 +37,27 @@ const syncReportTargetPopulations = async (
   targetPopulationEnums,
 );
 
+/**
+ * Get report target populations based on the given report and target population IDs.
+ * @param report - The report object containing id and type properties.
+ * @param targetPopulationIds - An array of target population IDs or null.
+ * @returns A promise that resolves to an array of ReportGenericEnumType objects.
+ */
 const getReportTargetPopulations = async (
   report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
   targetPopulationIds: number[] | null = null,
-):Promise<ReportGenericEnumType[]> => getReportGenericEnums(
+): Promise<ReportGenericEnumType[]> => getReportGenericEnums(
   ReportTargetPopulation,
   targetPopulationEnumInfo,
   report,
   targetPopulationIds,
 );
 
+/**
+ * Include report target populations for the specified report type.
+ * @param reportType - The report type.
+ * @returns A function that can be used to include target populations in a report.
+ */
 const includeReportTargetPopulations = (
   reportType: typeof REPORT_TYPE[keyof typeof REPORT_TYPE],
 ) => includeGenericEnums(
