8706faf42ff625e6fe1619833992a3cb58ff024a
GarrettEHill <garretthill@gmail.com>
2023-10-18 01:18:25 -0700

    Update reportParticipation.ts

--- src/services/reports/reportParticipation.ts
+++ src/services/reports/reportParticipation.ts
@@ -8,6 +8,13 @@ const {
   ReportParticipation,
 } = db;
 
+/**
+ * Synchronizes report participation data with the database.
+ *
+ * @param report - The report object containing the report ID and type.
+ * @param data - The data object containing participant counts and details.
+ * @returns An object containing promises for any updates made and unmatched participants.
+ */
 const syncReportParticipation = async (
   report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
   data: {
@@ -22,7 +29,9 @@ const syncReportParticipation = async (
     attributes: [], // TODO fix
     where: { reportId: report.id },
   });
+
   let reportParticipationUpdate: Promise<any>;
+
   // create id not exists
   if (!reportParticipation) {
     reportParticipation = await ReportParticipation.create({
@@ -42,6 +51,7 @@ const syncReportParticipation = async (
       },
     );
   }
+
   const {
     promises: participantPromises,
     unmatched: participantUnmatched,
@@ -62,22 +72,32 @@ const syncReportParticipation = async (
   };
 };
 
+/**
+ * includeReportParticipation function.
+ * @param type - The type of report.
+ * @returns An object with the necessary configuration for including report participations.
+ */
 const includeReportParticipation = (
   type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE],
 ) => ({
-  model: ReportParticipation,
-  as: 'reportParticipations',
-  required: false,
-  attributes: [
+  model: ReportParticipation, // The model to include in the query.
+  as: 'reportParticipations', // The alias for the included model.
+  required: false, // Whether the inclusion is optional or not.
+  attributes: [ // The attributes to include from the included model.
     'participantCount',
     'inpersonParticipantCount',
     'virtualParticipantCount',
   ],
-  include: [
+  include: [ // Additional includes for the included model.
     includeReportParticipationParticipants(type),
   ],
 });
 
+/**
+ * Retrieves the report participation for a given report ID.
+ * @param reportId - The ID of the report.
+ * @returns A promise that resolves to the report participation.
+ */
 const getReportParticipation = async (
   reportId: number,
 ) => includeToFindAll(
