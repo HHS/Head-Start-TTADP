399260336c0fe3d439a29722f2339658e6b89be2
Adam Levin <adam.levin@adhocteam.us>
2023-10-20 09:34:27 -0400

    add clean up script

--- src/migrations/20231019233102-cleanup-ongoing-monitoring-topic.js
+++ src/migrations/20231019233102-cleanup-ongoing-monitoring-topic.js
@@ -12,33 +12,7 @@ module.exports = {
       const oldTopicId = 127;
       const newTopicId = 130;
 
-      await queryInterface.sequelize.query(`
-      -- Update activity report topics.
-      UPDATE "ActivityReports"
-      SET
-          topics = array_replace(topics, 'Ongoing Monitoring Management System', 'Ongoing Monitoring and Continuous Improvement')
-        WHERE topics @> ARRAY['Ongoing Monitoring Management System']::varchar[];
-
-        -- Update "ActivityReportObjectiveTopics" table.
-        UPDATE "ActivityReportObjectiveTopics"
-        SET "topicId" = ${newTopicId}
-        WHERE "topicId" = ${oldTopicId};
-
-        -- Update "ObjectiveTemplateTopics" table.
-        UPDATE "ObjectiveTemplateTopics"
-        SET "topicId" = ${newTopicId}
-        WHERE "topicId" = ${oldTopicId};
-
-        -- Update "ObjectiveTopics" table.
-        UPDATE "ObjectiveTopics"
-        SET "topicId" = ${newTopicId}
-        WHERE "topicId" = ${oldTopicId};
-
-        -- Update "RoleTopics" table.
-        UPDATE "RoleTopics"
-        SET "topicId" = ${newTopicId}
-        WHERE "topicId" = ${oldTopicId};
-   `, { transaction });
+      await queryInterface.sequelize.query(``, { transaction });
     });
   },
 
