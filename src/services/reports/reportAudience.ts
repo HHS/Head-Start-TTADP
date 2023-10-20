50b8ce781d918b1c9eb8244b07972e20cb609a9d
GarrettEHill <garretthill@gmail.com>
2023-10-19 23:49:34 -0700

    Update reportAudience.ts

--- src/services/reports/reportAudience.ts
+++ src/services/reports/reportAudience.ts
@@ -2,53 +2,58 @@ import db from '../../models';
 import {
   EnumInfo,
   EnumSyncResponse,
-  ReportGenericEnumType,
-  syncGenericEnums,
-  getReportGenericEnums,
-  includeGenericEnums,
+  getGenericReportEnums,
+  includeGenericReportEnums,
+  syncGenericReportEnums,
 } from './reportGenericEnum';
 import { REPORT_TYPE } from '../../constants';
 
 const {
-  Audience,
   ReportAudience,
 } = db;
 
 const audienceEnumInfo:EnumInfo = {
-  model: Audience,
-  as: 'audience',
-  keyName: 'Audiences',
+  model: ReportAudience,
+  alias: 'audience',
 };
 
+// This function syncs report audiences with the provided report and audience enums.
 const syncReportAudiences = async (
+  // The report object containing id and type properties
   report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
+  // Optional array of audience enums or null if not provided
   audienceEnums: { id?: number, name?: string }[] | null = null,
-): Promise<EnumSyncResponse> => syncGenericEnums(
-  ReportAudience,
-  audienceEnumInfo,
-  report,
-  audienceEnums,
+): Promise<EnumSyncResponse> => syncGenericReportEnums(
+  report, // The report object to sync audiences with
+  audienceEnumInfo, // The enum info object for report audiences
+  audienceEnums, // The array of audience enums or null
 );
 
+// This function retrieves report audiences based on the provided report and audience ids.
 const getReportAudiences = async (
+  // The report object containing id and type properties
   report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
-  audienceIds: number[] | null = null,
-):Promise<ReportGenericEnumType[]> => getReportGenericEnums(
-  ReportAudience,
-  audienceEnumInfo,
-  report,
-  audienceIds,
+  // Optional array of audience ids or names or null if not provided
+  audiences: (number | string)[] | null = null,
+) => getGenericReportEnums(
+  report, // The report object to sync audiences with
+  audienceEnumInfo, // The enum info object for report audiences
+  audiences, // The array of audience enums or null
 );
 
+// This function includes report audience enums based on the provided report type.
 const includeReportAudience = (
-  reportType: typeof REPORT_TYPE[keyof typeof REPORT_TYPE],
-) => includeGenericEnums(
-  ReportAudience,
+  // The report type to include audience enums for
+  reportType?: typeof REPORT_TYPE[keyof typeof REPORT_TYPE],
+) => includeGenericReportEnums(
+  {
+    ...(reportType && { type: reportType }),
+  },
   audienceEnumInfo,
-  reportType,
 );
 
 export {
+  audienceEnumInfo,
   syncReportAudiences,
   getReportAudiences,
   includeReportAudience,
