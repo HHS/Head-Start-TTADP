1afa51069737ccccaab2fe2eebd3aabbf23f98e5
GarrettEHill <garretthill@gmail.com>
2023-10-20 11:39:16 -0700

    Update reportObjective.ts

--- src/services/reports/reportObjective.ts
+++ src/services/reports/reportObjective.ts
@@ -1,7 +1,26 @@
-// TODO: everything
+import db from '../../models';
+import { includeReportObjectiveFiles } from './reportObjectiveFile';
+import { includeReportObjectiveResources } from './reportObjectiveResource';
+import { includeReportObjectiveTopics } from './reportObjectiveTopic';
+import { includeReportObjectiveTrainers } from './reportObjectiveTrainer';
 
-const syncReportObjectives = async () => {};
-const includeReportObjectives = () => {};
+const {
+  ReportObjective,
+} = db;
+
+const syncReportObjectives = async () => {};// TODO: everything
+const includeReportObjectives = () => ({
+  model: ReportObjective,
+  as: 'reportObjective',
+  required: false,
+  attributes: [],
+  include: [
+    includeReportObjectiveFiles(),
+    includeReportObjectiveResources(),
+    includeReportObjectiveTopics(),
+    includeReportObjectiveTrainers(),
+  ],
+});
 const getReportObjectives = async () => {};
 
 export {
