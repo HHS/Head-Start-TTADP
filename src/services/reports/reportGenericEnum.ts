ffe14754f19804d28c7588227060d25edb9035e3
GarrettEHill <garretthill@gmail.com>
2023-10-19 23:49:11 -0700

    Update reportGenericEnum.ts

--- src/services/reports/reportGenericEnum.ts
+++ src/services/reports/reportGenericEnum.ts
@@ -1,62 +1,62 @@
-import { Model, Op } from 'sequelize';
-import { auditLogger } from '../../logger';
 import {
   type EnumInfo,
-  findAll,
-  type EntityEnumModel,
-  type EntityGenericEnum,
   type EnumSyncResponse,
-  getEntityGenericEnum,
-  syncEntityGenericEnum,
-  includeEntityGenericEnums,
+  includeGenericEnums,
+  getGenericEnums,
+  syncGenericEnums,
 } from '../enums/generic';
 import { REPORT_TYPE } from '../../constants';
-import validFor from '../../models/validFor';
 
-interface ReportGenericEnumType extends EntityGenericEnum {
-  reportId?: number,
-}
-
-const getReportGenericEnums = async (
-  reportEnumModel: EntityEnumModel,
-  enumInfo: EnumInfo,
+/**
+ * Retrieves generic enums for a specific report.
+ * @param report - The report object containing the report ID and type.
+ * @param reportEnum - The report enum object containing the model, alias, and optional
+ * entityTypeFiltered flag.
+ * @param genericEnums - An optional array of generic enums.
+ * @returns A promise that resolves to the generic enums for the report.
+ */
+const getGenericReportEnums = async (
   report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
-  genericEnumIds: number[] | null = null,
-):Promise<ReportGenericEnumType[]> => getEntityGenericEnum(
-  reportEnumModel,
-  enumInfo,
-  { name: 'reportId', ...report },
-  genericEnumIds,
-);
-
-const syncGenericEnums = async (
-  model: EntityEnumModel,
-  enumInfo: EnumInfo,
-  report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
-  genericEnums: { id?: number, name?: string }[] | null = null,
-): Promise<EnumSyncResponse> => syncEntityGenericEnum(
-  model,
-  enumInfo,
-  { name: 'reportId', ...report },
+  reportEnum: EnumInfo,
+  genericEnums: (number | string)[] | null = null,
+) => getGenericEnums(
+  { name: 'reportId', ...report }, // The filter object for retrieving report-specific generic enums.
+  reportEnum,
   genericEnums,
 );
 
-const includeGenericEnums = (
-  model: EntityEnumModel,
-  enumInfo: EnumInfo,
-  reportType: typeof REPORT_TYPE[keyof typeof REPORT_TYPE],
-) => includeEntityGenericEnums(
-  model,
-  enumInfo,
-  { name: 'reportId', type: reportType },
+// This function synchronizes generic enums for a specific report.
+const syncGenericReportEnums = async (
+  // The report object containing id and type.
+  report: { id: number, type: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
+  reportEnum: EnumInfo,
+  genericEnums: { id?: number, name?: string }[] | null = null,
+): Promise<EnumSyncResponse> => syncGenericEnums(
+  // The filter object for syncing generic enums for a specific report.
+  { name: 'reportId', ...report },
+  reportEnum,
+  genericEnums,
+);
+
+/**
+ * Includes generic report enums based on the given report and reportEnum.
+ * @param report - The report object containing id and type properties.
+ * @param reportEnum - The reportEnum object containing model, alias, and entityTypeFiltered
+ * properties.
+ * @returns The result of including generic enums based on the filter object and reportEnum.
+ */
+const includeGenericReportEnums = (
+  report: { id?: number, type?: typeof REPORT_TYPE[keyof typeof REPORT_TYPE] },
+  reportEnum: EnumInfo,
+) => includeGenericEnums(
+  { name: 'reportId', ...report }, // The filter object for retrieving report-specific generic enums.
+  reportEnum,
 );
 
 export {
   type EnumInfo,
-  type ReportGenericEnumType,
-  type EntityEnumModel,
   type EnumSyncResponse,
-  getReportGenericEnums,
-  syncGenericEnums,
-  includeGenericEnums,
+  getGenericReportEnums,
+  includeGenericReportEnums,
+  syncGenericReportEnums,
 };
