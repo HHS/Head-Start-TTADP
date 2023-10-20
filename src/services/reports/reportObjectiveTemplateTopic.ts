eb11b2512c9b057ebbaf54cc8d789dc9e24670e0
GarrettEHill <garretthill@gmail.com>
2023-10-19 23:49:05 -0700

    Update reportObjectiveTemplateTopic.ts

--- src/services/reports/reportObjectiveTemplateTopic.ts
+++ src/services/reports/reportObjectiveTemplateTopic.ts
@@ -1,11 +1,55 @@
-// TODO: everything
+import db from '../../models';
+import {
+  type EnumInfo,
+  type EnumSyncResponse,
+  includeGenericEnums,
+  getGenericEnums,
+  syncGenericEnums,
+} from '../enums/generic';
+
+const {
+  ReportObjectiveTemplateTopic,
+} = db;
+
+const ReportObjectiveTemplateTopicEnumInfo:EnumInfo = {
+  model: ReportObjectiveTemplateTopic,
+  alias: 'topic',
+  entityTypeFiltered: false,
+};
+
 const syncReportObjectiveTemplateTopics = async (
-  entity: {},
-) => {};
+  entity: { id: number },
+  topics: { id?: number, name?: string }[] | null = null,
+): Promise<EnumSyncResponse> => syncGenericEnums(
+  {
+    name: 'reportObjectiveTemplateId',
+    ...entity,
+  },
+  ReportObjectiveTemplateTopic,
+  topics,
+);
 
-const includeReportObjectiveTemplateTopics = () => ({});
+const includeReportObjectiveTemplateTopics = (
+  entity: { id: number },
+) => includeGenericEnums(
+  {
+    name: 'reportObjectiveTemplateId',
+    ...entity,
+  },
+  ReportObjectiveTemplateTopic,
+);
 
-const getReportObjectiveTemplateTopics = async () => {};
+const getReportObjectiveTemplateTopics = async (
+  entity: { id: number },
+  topics: (number | string)[] | null = null,
+) => getGenericEnums(
+  {
+    name: 'reportObjectiveTemplateId',
+    ...entity,
+  },
+  ReportObjectiveTemplateTopicEnumInfo,
+  topics,
+);
 
 export {
   syncReportObjectiveTemplateTopics,
