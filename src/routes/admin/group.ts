91956c47d5a3c7735ab2e45cec32bc0363a1e4bc
Matt Bevilacqua <thewatermethod@gmail.com>
2023-10-24 13:48:04 -0400

    admin handler for get groups by region

---
+++ src/routes/admin/group.ts
@@ -0,0 +1,39 @@
+/* eslint-disable import/prefer-default-export */
+import express, { Response, Request } from 'express';
+import httpCodes from 'http-codes';
+import transactionWrapper from '../transactionWrapper';
+import { handleError } from '../../lib/apiErrorHandler';
+import { groupsByRegion } from '../../services/groups';
+
+const namespace = 'ADMIN:GROUPS';
+const logContext = { namespace };
+
+/**
+   * Gets the redis info from the client, as if you'd run "info"
+   * redis cli
+   *
+   * https://redis.io/commands/info/
+   *
+   * @param {Request} _req - request
+   * @param {Response} res - response
+   */
+export async function getGroupsByRegion(req: Request, res: Response) {
+  // admin access is already checked in the middleware
+  try {
+    const { regionId } = req.params;
+    if (!regionId) {
+      res.status(httpCodes.BAD_REQUEST).json({ error: 'regionId is required' });
+    }
+
+    const groups = await groupsByRegion(Number(regionId));
+    res.status(httpCodes.OK).json(groups);
+  } catch (err) {
+    await handleError(req, res, err, logContext);
+  }
+}
+
+const router = express.Router();
+
+router.get('/:regionId', transactionWrapper(getGroupsByRegion));
+
+export default router;
