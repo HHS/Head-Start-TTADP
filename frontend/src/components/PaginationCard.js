cd439f6114959926a2ff0e35c7f3b5a516838e62
Matt Bevilacqua <thewatermethod@gmail.com>
2023-10-25 15:54:02 -0400

    update for unlabelled image/accessibility

--- frontend/src/components/PaginationCard.js
+++ frontend/src/components/PaginationCard.js
@@ -1,6 +1,5 @@
 import React, { useRef, useLayoutEffect } from 'react';
 import { useMediaQuery } from 'react-responsive';
-import { uniqueId } from 'lodash';
 import PropTypes from 'prop-types';
 import { Dropdown, Pagination } from '@trussworks/react-uswds';
 
@@ -33,22 +32,14 @@ function PaginationCard({
     const paginationLinks = el.current.querySelectorAll('.usa-pagination__link:not(.usa-pagination__link--assigned)');
 
     Array.from(paginationLinks).forEach((link) => {
-      const linkText = link.querySelector('.usa-pagination__link-text');
       const svg = link.querySelector('svg');
 
       // we need both of these for going any further to make sense
-      if (!linkText || !svg) {
+      if (!svg) {
         return;
       }
 
-      // generate a unique id
-      const id = uniqueId('pagination-link-');
-      // add the id to the link text
-      linkText.setAttribute('id', id);
-      // associate the svg with the link text
-      svg.setAttribute('aria-labelledby', id);
-      // mark the parent link as already having been processed
-      link.classList.add('usa-pagination__link--assigned');
+      svg.setAttribute('aria-hidden', 'true');
     });
   });
 
