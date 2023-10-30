8b891a3733667bfb52731c24701da8ffcd3e6db9
nvms <jon@pye.rs>
2023-10-30 11:15:14 -0400

    table migration

---
+++ src/migrations/20231030150724-sim_score_cache.js
@@ -0,0 +1,36 @@
+const {
+  prepMigration,
+} = require('../lib/migration');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+      await queryInterface.createTable(
+        'SimScoreCache',
+        {
+          id: {
+            type: Sequelize.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+          goal1: { allowNull: false, type: Sequelize.INTEGER },
+          goal2: { allowNull: false, type: Sequelize.INTEGER },
+          score: { allowNull: false, type: Sequelize.FLOAT },
+          createdAt: { allowNull: false, type: Sequelize.DATE },
+          updatedAt: { allowNull: false, type: Sequelize.DATE },
+        },
+      );
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+      await queryInterface.dropTable('SimScoreCache', { transaction });
+    });
+  },
+};
