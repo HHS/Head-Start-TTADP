5f9eea5492583ead330eedd4ec463395213c7c23
GarrettEHill <garretthill@gmail.com>
2023-11-02 00:16:40 -0700

    Update zip.ts

--- src/lib/stream/zip.ts
+++ src/lib/stream/zip.ts
@@ -8,6 +8,10 @@ class ZipStream {
     this.zipStream = zipStream;
   }
 
+  /**
+   * Retrieves a list of files contained in the zip stream.
+   * @returns A promise that resolves to an array of file paths.
+   */
   async listFiles(): Promise<string[]> {
     const files: string[] = [];
 
@@ -24,6 +28,11 @@ class ZipStream {
     return files;
   }
 
+  /**
+   * Retrieves details about a specific file in the zip stream.
+   * @param filePath - The path of the file to retrieve details for.
+   * @returns A promise that resolves to a FileInfo object if the file exists, or null otherwise.
+   */
   async getFileDetails(filePath: string): Promise<FileInfo | null> {
     const entry = await this.zipStream
       .pipe(unzipper.ParseOne(filePath))
@@ -42,6 +51,23 @@ class ZipStream {
     };
   }
 
+  /**
+   * Retrieves details for all files.
+   * @returns A promise that resolves to an array of FileInfo objects or null.
+   */
+  async getAllFileDetails(): Promise<(FileInfo | null)[]> {
+    // Retrieve the list of files
+    const files = await this.listFiles();
+
+    // Retrieve the details for each file using Promise.all and map
+    return Promise.all(files.map(this.getFileDetails));
+  }
+
+  /**
+   * Retrieves a readable stream for a specific file in the zip stream.
+   * @param filePath - The path of the file to retrieve the stream for.
+   * @returns A promise that resolves to a Readable stream if the file exists, or null otherwise.
+   */
   async getFileStream(filePath: string): Promise<Readable | null> {
     const fileStream = await this.zipStream
       .pipe(unzipper.ParseOne(filePath))
@@ -52,6 +78,9 @@ class ZipStream {
   }
 }
 
+/**
+ * Represents information about a file.
+ */
 interface FileInfo {
   name: string;
   type: string;
