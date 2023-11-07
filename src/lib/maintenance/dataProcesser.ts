9ca79103232f335831d54bb753976cdda8306b05
GarrettEHill <garretthill@gmail.com>
2023-11-06 16:18:55 -0800

    Update dataProcesser.ts

--- src/lib/maintenance/dataProcesser.ts
+++ src/lib/maintenance/dataProcesser.ts
@@ -1,24 +1,35 @@
-import * as fs from 'fs';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Op } from 'sequelize';
 import { Readable } from 'stream';
 import FtpClient, { FileInfo as ftpFileInfo, FTPSettings } from '../stream/ftp';
 import S3Client from '../stream/s3';
 import ZipStream, { FileInfo as zipFileInfo } from '../stream/zip';
 import EncodingConverter from '../stream/encoding';
 import XMLStream from '../stream/xml';
+import { remap, collectChangedValues } from '../dataObjectUtils';
+import { filterDataToModel } from '../modelUtils';
 
 const processRecords = async (
   processDefinition,
   xmlClient,
-  recordActions = {
+  recordActions: {
+    inserts: Promise<any>[],
+    updates: Promise<any>[],
+    deletes: Promise<any>[],
+  } = {
     inserts: [],
-    updated: [],
+    updates: [],
     deletes: [],
   },
-) => {
+): Promise<{
+  inserts: Promise<any>[],
+  updates: Promise<any>[],
+  deletes: Promise<any>[],
+}> => {
   const record = await xmlClient.getNextRecord();
   if (record) {
-    /** TODO
-     * 1: use the mapsTo method to format data to structure needed
+    /**
+     * 1: use the remap method to format data to structure needed
      * 2. use the filterDataToModel to match what is expected
      * 3. check for existing record
      * 4a. if new
@@ -29,15 +40,62 @@ const processRecords = async (
      *  2. update
      *  2. recordActions.update.push(uuid)
      */
+    const data = remap(record, processDefinition.remapDefs);
+    const filteredData = await filterDataToModel(data, processDefinition.model);
+    const currentData = await processDefinition.model.FindOne({
+      where: {
+        data: {
+          [Op.contains]: {
+            [processDefinition.key]: filteredData[processDefinition.key],
+          },
+        },
+      },
+    });
+    if (currentData) {
+      const insert = processDefinition.model.create(
+        filteredData,
+        {
+          independentHooks: true,
+          returning: true,
+          plain: true,
+        },
+      );
+      recordActions.inserts.push(insert);
+    } else {
+      const delta = collectChangedValues(filteredData, currentData);
+      const update = processDefinition.model.update(
+        delta,
+        {
+          where: { id: delta.id },
+          independentHooks: true,
+          returning: true,
+          plain: true,
+        },
+      );
+      recordActions.updates.push(update);
+    }
   } else {
-    /** TODO
+    /**
      * 1. Find all records not in recordActions.inserts and recordActions.update
      * 2. delete
      * 3. recordActions.delete.push(uuid)
      * 4. save data - recordActions
      */
-    return Promise.resolve();
+    const affectedData = await Promise.all([
+      ...recordActions.inserts,
+      ...recordActions.updates,
+    ]);
+
+    const destroys = processDefinition.model.destroy({
+      where: { id: { [Op.not]: affectedData.map(({ id }) => id) } },
+      independentHooks: true,
+    });
+
+    recordActions.deletes.push(destroys);
+
+    return Promise.resolve(recordActions);
   }
+
   return processRecords(
     processDefinition,
     xmlClient,
@@ -49,15 +107,24 @@ const processFile = async (
   processDefinition,
   fileInfo: zipFileInfo,
   fileStream: Readable,
-) => {
+): Promise<{
+  inserts: Promise<any>[],
+  updates: Promise<any>[],
+  deletes: Promise<any>[],
+}> => {
+  if (!processDefinition?.remapDefs) throw new Error(`Remapping definitions not found for '${fileInfo.name}'`);
+  if (!processDefinition?.model) throw new Error(`Model not found for '${fileInfo.name}'`);
+  if (!processDefinition?.key) throw new Error(`Key not found for '${fileInfo.name}'`);
+
   const usableStream = await EncodingConverter.forceStreamEncoding(fileStream, 'utf8');
   const xmlClient = new XMLStream(usableStream);
+
   return processRecords(processDefinition, xmlClient);
 };
 
 const processFiles = async (
   zipClient: ZipStream,
-  filesToProcess: (zipFileInfo | null)[],
+  filesToProcess: (zipFileInfo)[],
   processDefinitions: string[],
 ) => {
   if (processDefinitions.length === 0) return;
@@ -68,7 +135,12 @@ const processFiles = async (
       .find(({ name }) => name === nextToProcess);
     if (fileInfoToProcess) {
       const fileStream = await zipClient.getFileStream(fileInfoToProcess.name);
-      await processFile(nextToProcess, fileInfoToProcess, fileStream);
+      const processingData = await processFile(
+        nextToProcess,
+        fileInfoToProcess,
+        fileStream,
+      );
+      // TODO - save/log file processing data
     } else {
       // TODO - save/log that file to process was not found
     }
@@ -83,19 +155,19 @@ const processFTP = async (processDefinitions) => {
   const ftpClient = new FtpClient(ftpSettings);
   const latestFtpFile = await ftpClient.getLatest('/');
 
-  // TODO - save data
+  // TODO - save/log data
 
   const s3Client = new S3Client();
   const s3LoadedFile = await s3Client.uploadFileAsStream(key, latestFtpFile.stream);
 
-  // TODO - save data
+  // TODO - save/log data
 
   const s3FileStream = await s3Client.downloadFileAsStream(key);
 
   const zipClient = new ZipStream(s3FileStream);
   const fileDetails = await zipClient.getAllFileDetails();
 
-  // TODO - save data
+  // TODO - save/log data
 
   await processFiles(zipClient, fileDetails, processDefinitions);
 };
