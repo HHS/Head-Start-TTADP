7f4f610bc52b29994f6307eba77cd2a9db51dfb6
Adam Levin <adam.levin@adhocteam.us>
2023-11-08 14:18:56 -0500

    WIP

---
+++ src/migrations/20231108141134-restructure-national-centers.js
@@ -0,0 +1,63 @@
+const {
+  prepMigration,
+} = require('../lib/migration');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+
+      await queryInterface.sequelize.query(
+      /* sql */`
+      -- Delete all data from table "NationalCenters".
+        DELETE FROM "NationalCenters";
+
+     -- Remove the column 'mapsTo' from table "NationalCenters".
+        ALTER TABLE "NationalCenters"
+        DROP COLUMN "mapsTo";
+      `, { transaction });
+
+      // Create table 'UserNationalCenters'.
+      await queryInterface.createTable('UserNationalCenters', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER,
+        },
+        userId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: {
+              tableName: 'Users',
+            },
+            key: 'id',
+          },
+        },
+        nationalCenterId: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+          references: {
+            model: {
+              tableName: 'NationalCenters',
+            },
+            key: 'id',
+          },
+        },
+        createdAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+        updatedAt: {
+          allowNull: false,
+          type: Sequelize.DATE,
+        },
+      }, { transaction });
+    });
+  },
+
+  down: async () => {},
+};
