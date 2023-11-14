8f25f186ee134b342c73ef7611cc85e81b93a084
GarrettEHill <garretthill@gmail.com>
2023-11-14 11:15:27 -0800

    Update encoding.test.js

--- src/lib/stream/tests/encoding.test.js
+++ src/lib/stream/tests/encoding.test.js
@@ -1,170 +1,83 @@
-import { Readable, Transform } from 'stream';
+import { Readable } from 'stream';
 import EncodingConverter from '../encoding';
 
 describe('EncodingConverter', () => {
-  describe('detectEncodingType', () => {
-    test('should return utf16le if data starts with BOM', async () => {
-      const stream = Readable.from('\uFEFFHello, World!');
-      const encodingType = await EncodingConverter.detectEncodingType(stream);
-      expect(encodingType).toBe('utf16le');
-    });
+  it('should convert encoding of the input stream', () => {
+    const sourceEncoding = 'utf16le';
+    const targetEncoding = 'utf-8';
+    const converter = new EncodingConverter(targetEncoding, sourceEncoding);
 
-    test('should return utf8 if data does not start with BOM', async () => {
-      const stream = Readable.from('Hello, World!');
-      const encodingType = await EncodingConverter.detectEncodingType(stream);
-      expect(encodingType).toBe('utf8');
-    });
+    // Create a readable stream with known encoding
+    const readable = new Readable();
+    readable.push(Buffer.from('Hello, world!', sourceEncoding));
+    readable.push(null); // Signal end of stream
 
-    test('should reject with error if stream emits an error', async () => {
-      const stream = new Readable();
-      const error = new Error('Stream error');
-      const detectPromise = EncodingConverter.detectEncodingType(stream);
+    // Collect data from the output stream
+    const chunks = [];
 
-      process.nextTick(() => {
-        stream.emit('error', error);
+    return new Promise((resolve) => {
+      converter.on('data', (chunk) => chunks.push(chunk));
+      converter.on('end', () => {
+        const result = Buffer.concat(chunks).toString(targetEncoding);
+        expect(result).toBe('Hello, world!');
+        resolve();
       });
 
-      await expect(detectPromise).rejects.toThrow(error);
-    });
-  });
-
-  describe('createEncodingConversionStream', () => {
-    test('should transform source encoding to target encoding', async () => {
-      const sourceEncoding = 'utf16le';
-      const targetEncoding = 'utf8';
-      const conversionStream = EncodingConverter.createEncodingConversionStream(
-        sourceEncoding,
-        targetEncoding,
-      );
-
-      const inputString = 'Hello, World!';
-      const expectedOutputBuffer = Buffer.from(inputString, targetEncoding);
-
-      const outputChunks = [];
-
-      conversionStream.on('data', (chunk) => {
-        outputChunks.push(chunk);
-      });
-
-      conversionStream.write(Buffer.from(inputString, sourceEncoding));
-      conversionStream.end();
-
-      await new Promise((resolve) => {
-        conversionStream.on('end', () => {
-          const outputBuffer = Buffer.concat(outputChunks);
-          expect(outputBuffer).toEqual(expectedOutputBuffer);
-          resolve();
-        });
-      });
+      // Pipe the readable stream through the converter
+      readable.pipe(converter);
     });
   });
 
-  describe('createUtf16ToUtf8Stream', () => {
-    test('should create a stream that converts utf16le to utf8', async () => {
-      const conversionStream = EncodingConverter.createUtf16ToUtf8Stream();
+  it('should default to utf-8 when unable to detect source encoding', () => {
+    const targetEncoding = 'utf-8';
+    const converter = new EncodingConverter(targetEncoding);
 
-      const inputString = 'Hello, World!';
-      const expectedOutputBuffer = Buffer.from(inputString, 'utf8');
+    // Create a readable stream without specifying encoding
+    const readable = new Readable();
+    readable.push(Buffer.from('Some text'));
+    readable.push(null); // Signal end of stream
 
-      const outputChunks = [];
+    const chunks = [];
 
-      conversionStream.on('data', (chunk) => {
-        outputChunks.push(chunk);
+    return new Promise((resolve) => {
+      converter.on('data', (chunk) => chunks.push(chunk));
+      converter.on('end', () => {
+        const result = Buffer.concat(chunks).toString(targetEncoding);
+        expect(result).toBe('Some text');
+        resolve();
       });
 
-      conversionStream.write(Buffer.from(inputString, 'utf16le'));
-      conversionStream.end();
-
-      await new Promise((resolve) => {
-        conversionStream.on('end', () => {
-          const outputBuffer = Buffer.concat(outputChunks);
-          expect(outputBuffer).toEqual(expectedOutputBuffer);
-          resolve();
-        });
-      });
+      readable.pipe(converter);
     });
   });
 
-  describe('createUtf8ToUtf16Stream', () => {
-    test('should create a stream that converts utf8 to utf16le', async () => {
-      const conversionStream = EncodingConverter.createUtf8ToUtf16Stream();
-
-      const inputString = 'Hello, World!';
-      const expectedOutputBuffer = Buffer.from(inputString, 'utf16le');
-
-      const outputChunks = [];
-
-      conversionStream.on('data', (chunk) => {
-        outputChunks.push(chunk);
-      });
-
-      conversionStream.write(Buffer.from(inputString, 'utf8'));
-      conversionStream.end();
-
-      await new Promise((resolve) => {
-        conversionStream.on('end', () => {
-          const outputBuffer = Buffer.concat(outputChunks);
-          expect(outputBuffer).toEqual(expectedOutputBuffer);
-          resolve();
-        });
-      });
-    });
+  it('should handle errors during conversion', () => {
+    const invalidEncoding = 'invalid-encoding';
+    // Wrap the instantiation in a function so that toThrow can catch the error.
+    expect(() => new EncodingConverter(invalidEncoding))
+      .toThrow(`Unsupported encoding detected: ${invalidEncoding}`);
   });
 
-  describe('forceStreamEncoding', () => {
-    test('should return a passthrough stream if source and target encodings are the same', async () => {
-      const stream = new Readable({
-        read() {},
-      });
-      const targetEncoding = 'utf8';
-      const transformStream = await EncodingConverter.forceStreamEncoding(stream, targetEncoding);
+  it('should process remaining buffer on flush', () => {
+    const sourceEncoding = 'ascii';
+    const targetEncoding = 'utf-8';
+    const converter = new EncodingConverter(targetEncoding);
 
-      const inputString = 'Hello, World!';
-      const expectedOutputBuffer = Buffer.from(inputString, targetEncoding);
+    const readable = new Readable();
+    readable.push(Buffer.from('Incomplete buffer less than 1024 bytes', sourceEncoding));
+    readable.push(null); // Signal end of stream
 
-      const onData = jest.fn();
-      transformStream.on('data', onData);
+    const chunks = [];
 
-      process.nextTick(() => {
-        stream.push(Buffer.from(inputString, targetEncoding));
-        stream.push(null);
-      });
-
-      await new Promise((resolve) => {
-        transformStream.on('end', () => {
-          expect(onData).toHaveBeenCalledTimes(1);
-          expect(onData.mock.calls[0][0]).toEqual(expectedOutputBuffer);
-          resolve();
-        });
-      });
-    });
-
-    test('should return a conversion stream if source and target encodings are different', async () => {
-      const stream = new Readable({
-        read() {},
-      });
-      const sourceEncoding = 'utf16le';
-      const targetEncoding = 'utf8';
-      const transformStream = await EncodingConverter.forceStreamEncoding(stream, targetEncoding);
-
-      const inputString = 'Hello, World!';
-      const expectedOutputBuffer = Buffer.from(inputString, targetEncoding);
-
-      const onData = jest.fn();
-      transformStream.on('data', onData);
-
-      process.nextTick(() => {
-        stream.push(Buffer.from(inputString, sourceEncoding));
-        stream.push(null);
+    return new Promise((resolve) => {
+      converter.on('data', (chunk) => chunks.push(chunk));
+      converter.on('end', () => {
+        const result = Buffer.concat(chunks).toString(targetEncoding);
+        expect(result).toBe('Incomplete buffer less than 1024 bytes');
+        resolve();
       });
 
-      await new Promise((resolve) => {
-        transformStream.on('end', () => {
-          expect(onData).toHaveBeenCalledTimes(1);
-          expect(onData.mock.calls[0][0]).toEqual(expectedOutputBuffer);
-          resolve();
-        });
-      });
+      readable.pipe(converter);
     });
   });
 });
