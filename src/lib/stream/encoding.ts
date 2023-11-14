fc5d8447de80eaec641517f8a1b7d7fdd1a92ee2
GarrettEHill <garretthill@gmail.com>
2023-11-14 11:15:17 -0800

    Update encoding.ts

--- src/lib/stream/encoding.ts
+++ src/lib/stream/encoding.ts
@@ -1,92 +1,139 @@
 import { Transform } from 'stream';
+import * as chardet from 'chardet';
 
-class EncodingConverter {
-  /**
-   * Detects the encoding type of a readable stream.
-   * @param stream - The readable stream to detect the encoding type from.
-   * @returns A promise that resolves with the detected encoding type.
-   */
-  static detectEncodingType(stream: NodeJS.ReadableStream): Promise<string> {
-    return new Promise((resolve, reject) => {
-      let data = '';
-      // Accumulate data chunks from the stream
-      stream.on('data', (chunk) => {
-        data += chunk.toString();
-      });
+class EncodingConverter extends Transform {
+  private detectEncoding: boolean;
 
-      // When all data has been read, check if it starts with BOM character '\uFEFF'
-      stream.on('end', () => {
-        if (data.startsWith('\uFEFF')) {
-          resolve('utf16le'); // If it does, resolve with 'utf16le'
-        } else {
-          resolve('utf8'); // Otherwise, resolve with 'utf8'
-        }
-      });
+  private buffer: Buffer;
 
-      // If there is an error reading the stream, reject the promise with the error
-      stream.on('error', (err) => {
-        reject(err);
-      });
-    });
-  }
+  // Define a set of supported encodings for Buffer methods
+  private static readonly supportedEncodings: Set<BufferEncoding> = new Set([
+    'ascii',
+    'utf8',
+    'utf-8',
+    'utf16le',
+    'ucs2',
+    'ucs-2',
+    'base64',
+    'base64url',
+    'latin1',
+    'binary',
+    'hex',
+  ]);
 
-  /**
-   * Creates a transform stream that converts the encoding of the input data.
-   * @param sourceEncoding - The source encoding of the input data.
-   * @param targetEncoding - The target encoding for the output data.
-   * @returns A transform stream that performs the encoding conversion.
-   */
-  static createEncodingConversionStream(sourceEncoding: string, targetEncoding: string): Transform {
-    return new Transform({
-      transform(chunk, encoding, callback) {
-        const sourceString = chunk.toString(sourceEncoding as BufferEncoding);
-        const targetBuffer = Buffer.from(sourceString, targetEncoding as BufferEncoding);
-        this.push(targetBuffer); // Push the converted buffer to the output
-        callback(); // Signal that the transformation is complete
-      },
-    });
-  }
-
-  /**
-   * Creates a transform stream that converts UTF-16 encoded data to UTF-8.
-   * @returns A transform stream that performs the encoding conversion from UTF-16 to UTF-8.
-   */
-  static createUtf16ToUtf8Stream(): Transform {
-    return this.createEncodingConversionStream('utf16le', 'utf8');
-  }
-
-  /**
-   * Creates a transform stream that converts UTF-8 encoded data to UTF-16.
-   * @returns A transform stream that performs the encoding conversion from UTF-8 to UTF-16.
-   */
-  static createUtf8ToUtf16Stream(): Transform {
-    return this.createEncodingConversionStream('utf8', 'utf16le');
-  }
-
-  /**
-   * Forces the encoding of a readable stream to the specified target encoding.
-   * If the source encoding matches the target encoding, a pass-through transform stream is returned.
-   * Otherwise, a transform stream that performs the encoding conversion is returned.
-   * @param stream - The readable stream to force the encoding on.
-   * @param targetEncoding - The target encoding to force on the stream.
-   * @returns A promise that resolves with the transform stream for the forced encoding.
-   */
-  static async forceStreamEncoding(
-    stream: NodeJS.ReadableStream,
-    targetEncoding: string,
-  ): Promise<Transform> {
-    const sourceEncoding = await EncodingConverter.detectEncodingType(stream);
-    if (sourceEncoding === targetEncoding) {
-      // If the source and target encodings are the same, return a pass-through transform stream
-      return new Transform({
-        transform(chunk, encoding, callback) {
-          this.push(chunk); // Push the input chunk as-is to the output
-          callback(); // Signal that the transformation is complete
-        },
-      });
+  constructor(
+    private targetEncoding: string,
+    private sourceEncoding?: string,
+  ) {
+    super();
+    if (targetEncoding
+      && EncodingConverter.supportedEncodings
+        .has(targetEncoding.toLowerCase() as BufferEncoding)) {
+      this.targetEncoding = targetEncoding.toLowerCase();
+    } else {
+      throw new Error(`Unsupported encoding detected: ${targetEncoding}`);
     }
-    // Otherwise, return a transform stream that performs the encoding conversion
-    return EncodingConverter.createEncodingConversionStream(sourceEncoding, targetEncoding);
+    if (sourceEncoding
+      && EncodingConverter.supportedEncodings
+        .has(sourceEncoding.toLowerCase() as BufferEncoding)) {
+      this.sourceEncoding = sourceEncoding.toLowerCase();
+    } else if (sourceEncoding) {
+      throw new Error(`Unsupported encoding detected: ${sourceEncoding}`);
+    }
+    this.detectEncoding = !this.sourceEncoding; // Flag to indicate if we need to detect encoding
+    this.buffer = Buffer.alloc(0); // Initialize an empty buffer
+  }
+
+  // eslint-disable-next-line no-underscore-dangle
+  _transform(
+    chunk: Buffer,
+    encoding: string,
+    callback: (error?: Error | null, data?: Buffer) => void,
+  ): void {
+    if (this.detectEncoding) {
+      // Continue collecting chunks until we have enough to detect the encoding
+      this.buffer = Buffer.concat([this.buffer, chunk]);
+      if (this.buffer.length >= 1024) {
+        // We have enough data to detect the encoding
+        this.detectEncoding = false; // Set flag to false as we've detected the encoding
+        // Default to utf-8 if no encoding is detected
+        // this.sourceEncoding = chardet.detect(this.buffer) || 'utf-8';
+        const detectedEncoding = chardet.detect(this.buffer);
+
+        // Check if the detected encoding is supported
+        if (detectedEncoding
+          && EncodingConverter.supportedEncodings
+            .has(detectedEncoding.toLowerCase() as BufferEncoding)) {
+          this.sourceEncoding = detectedEncoding.toLowerCase();
+        } else {
+          throw new Error(`Unsupported encoding detected: ${detectedEncoding}`);
+        }
+
+        const x = chardet.analyse(this.buffer);
+        console.log(x);
+        this.sourceEncoding = 'utf-8';
+
+        // If the source encoding matches the target encoding, pass through the entire buffer
+        if (this.sourceEncoding === this.targetEncoding) {
+          this.push(this.buffer);
+          this.buffer = Buffer.alloc(0);
+          callback();
+        } else {
+          // Convert the buffered data with the detected encoding
+          this.convertBuffer(callback);
+        }
+      } else {
+        // Wait for more data to detect encoding
+        callback();
+      }
+    } else {
+      // Encoding has been detected, convert the chunk directly
+      this.convertChunk(chunk, callback);
+    }
+  }
+
+  convertBuffer(callback: (error?: Error | null, data?: Buffer) => void) {
+    try {
+      const data = this.buffer.toString(this.sourceEncoding as BufferEncoding);
+      const convertedBuffer = Buffer.from(data, this.targetEncoding as BufferEncoding);
+      this.push(convertedBuffer);
+      this.buffer = Buffer.alloc(0); // Clear the buffer
+      callback();
+    } catch (error) {
+      callback(error);
+    }
+  }
+
+  convertChunk(chunk: Buffer, callback: (error?: Error | null, data?: Buffer) => void) {
+    try {
+      const data = chunk.toString(this.sourceEncoding as BufferEncoding);
+      const convertedChunk = Buffer.from(data, this.targetEncoding as BufferEncoding);
+      this.push(convertedChunk);
+      callback();
+    } catch (error) {
+      callback(error);
+    }
+  }
+
+  // eslint-disable-next-line no-underscore-dangle
+  _flush(callback: (error?: Error | null, data?: Buffer) => void): void {
+    if (this.detectEncoding && this.buffer.length > 0) {
+      // If flush is called and we're still detecting the encoding,
+      // perform the conversion on the remaining buffer.
+      this.sourceEncoding = chardet.detect(this.buffer) || 'utf-8';
+
+      // If the source encoding matches the target encoding, pass through the remaining buffer
+      if (this.sourceEncoding === this.targetEncoding) {
+        this.push(this.buffer);
+        this.buffer = Buffer.alloc(0);
+        callback();
+      } else {
+        this.convertBuffer(callback);
+      }
+    } else {
+      // No more data, just call the callback
+      callback();
+    }
   }
 }
 
