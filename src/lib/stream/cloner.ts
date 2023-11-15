378abfe428c0b02c7445985740bac53ece5d4ea2
GarrettEHill <garretthill@gmail.com>
2023-11-14 16:29:09 -0800

    Update cloner.ts

--- src/lib/stream/cloner.ts
+++ src/lib/stream/cloner.ts
@@ -13,37 +13,7 @@ class Cloner {
 
     // Initialize clones as PassThrough streams.
     this.clones = Array.from({ length: n }, () => new PassThrough());
-
-    this.cloneStreams();
-  }
-
-  /**
-   * Clone the source stream by piping its data to each clone.
-   */
-  private cloneStreams() {
-    // When data is received from the source stream
-    this.sourceStream.on('data', (chunk) => {
-      // Write the chunk to each clone stream
-      this.clones.forEach((clone) => {
-        clone.write(chunk);
-      });
-    });
-
-    // When the source stream ends
-    this.sourceStream.on('end', () => {
-      // End each clone stream
-      this.clones.forEach((clone) => {
-        clone.end();
-      });
-    });
-
-    // When an error occurs in the source stream
-    this.sourceStream.on('error', (err) => {
-      // Emit the error to each clone stream
-      this.clones.forEach((clone) => {
-        clone.emit('error', err);
-      });
-    });
+    this.clones.forEach((clone) => sourceStream.pipe(clone));
   }
 
   /**
