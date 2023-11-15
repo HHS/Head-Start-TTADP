3708cd30031caaac247b064fd501c9482df97a6c
nvms <jon@pye.rs>
2023-11-15 16:33:48 -0500

    more tests

--- frontend/src/pages/RecipientRecord/pages/components/__tests__/ClassReview.js
+++ frontend/src/pages/RecipientRecord/pages/components/__tests__/ClassReview.js
@@ -21,8 +21,8 @@ describe('ClassReview', () => {
       fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
         received: '05/01/2023',
         ES: 6,
-        CO: 3,
-        IS: 7,
+        CO: 0,
+        IS: 0,
       });
 
       renderClassReview();
@@ -33,8 +33,8 @@ describe('ClassReview', () => {
       fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
         received: '05/01/2023',
         ES: 5.1,
-        CO: 3,
-        IS: 7,
+        CO: 0,
+        IS: 0,
       });
 
       renderClassReview();
@@ -45,12 +45,120 @@ describe('ClassReview', () => {
       fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
         received: '05/01/2023',
         ES: 5.1,
-        CO: 3,
-        IS: 7,
+        CO: 0,
+        IS: 0,
       });
 
       renderClassReview();
       expect((await screen.findByTestId('class-es'))).toHaveTextContent('Below quality');
+    });
+  });
+
+  describe('classroom organization', () => {
+    afterEach(() => {
+      fetchMock.restore();
+    });
+
+    it('above all thresholds', async () => {
+      fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
+        received: '05/01/2023',
+        ES: 0,
+        CO: 6,
+        IS: 0,
+      });
+
+      renderClassReview();
+      expect(await screen.findByTestId('class-co')).toHaveTextContent('Above all thresholds');
+    });
+
+    it('below quality', async () => {
+      fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
+        received: '05/01/2023',
+        ES: 0,
+        CO: 5.1,
+        IS: 0,
+      });
+
+      renderClassReview();
+      expect((await screen.findByTestId('class-co'))).toHaveTextContent('Below quality');
+    });
+
+    it('below competetive', async () => {
+      fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
+        received: '05/01/2023',
+        ES: 0,
+        CO: 4.9,
+        IS: 0,
+      });
+
+      renderClassReview();
+      expect((await screen.findByTestId('class-co'))).toHaveTextContent('Below competetive');
+    });
+  });
+
+  describe('instructional support', () => {
+    afterEach(() => {
+      fetchMock.restore();
+    });
+
+    it('above all thresholds', async () => {
+      fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
+        received: '05/01/2023',
+        ES: 0,
+        CO: 0,
+        IS: 3.1,
+      });
+
+      renderClassReview();
+      expect(await screen.findByTestId('class-is')).toHaveTextContent('Above all thresholds');
+    });
+
+    it('below quality - after 2025-08-01', async () => {
+      fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
+        received: '08/02/2025',
+        ES: 0,
+        CO: 0,
+        IS: 2.5,
+      });
+
+      renderClassReview();
+      expect((await screen.findByTestId('class-is'))).toHaveTextContent('Below quality');
+    });
+
+    it('below quality - between 2020-11-09 and 2025-07-31', async () => {
+      fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
+        received: '01/01/2025',
+        ES: 0,
+        CO: 0,
+        IS: 2.4,
+      });
+
+      renderClassReview();
+      expect((await screen.findByTestId('class-is'))).toHaveTextContent('Below quality');
+    });
+
+    it('below competetive - after 2025-08-01', async () => {
+      fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
+        received: '08/02/2025',
+        ES: 0,
+        CO: 0,
+        IS: 2.4,
+      });
+
+      renderClassReview();
+      expect((await screen.findByTestId('class-is'))).toHaveTextContent('Below competetive');
+    });
+
+    it('below competetive - between 2020-11-09 and 2025-07-31', async () => {
+      fetchMock.getOnce(`/api/monitoring/class/${grantId}`, {
+        received: '01/01/2025',
+        ES: 0,
+        CO: 0,
+        IS: 2.2,
+      });
+
+      renderClassReview();
+      expect((await screen.findByTestId('class-is'))).toHaveTextContent('Below competetive');
     });
   });
 });
