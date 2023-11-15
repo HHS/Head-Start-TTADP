dc04f5b8ca0fd029fadf3b628ad8d82e39de88a7
nvms <jon@pye.rs>
2023-11-15 14:09:31 -0500

    test data

--- frontend/src/fetchers/monitoring.js
+++ frontend/src/fetchers/monitoring.js
@@ -4,8 +4,14 @@ import { get } from '.';
 const classUrl = join('/', 'api', 'monitoring');
 
 const getClassScores = async (grantId) => {
-  const scores = await get(join(classUrl, 'class', grantId));
-  return scores.json();
+  // const scores = await get(join(classUrl, 'class', grantId));
+  // return scores.json();
+  return {
+    received: '05/01/2023',
+    ES: 5,
+    CO: 3,
+    IS: 7,
+  };
 };
 
 export default getClassScores;
