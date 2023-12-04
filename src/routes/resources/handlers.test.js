dde9ab8da1a984d094ec217f77a3277d3ce7c9d3
Matt Bevilacqua <thewatermethod@gmail.com>
2023-12-04 14:26:35 -0500

    Fix resource handler test

--- src/routes/resources/handlers.test.js
+++ src/routes/resources/handlers.test.js
@@ -1,6 +1,11 @@
 import { getResourcesDashboardData } from './handlers';
 import { resourceDashboardPhase1 } from '../../services/dashboards/resource';
+import { activityReports } from '../../services/activityReports';
 import { getUserReadRegions } from '../../services/accessValidation';
+
+jest.mock('../../services/activityReports', () => ({
+  activityReports: jest.fn(),
+}));
 
 jest.mock('../../services/dashboards/resource', () => ({
   resourceDashboardPhase1: jest.fn(),
@@ -11,7 +16,12 @@ describe('Resources handler', () => {
   describe('getResourcesDashboardData', () => {
     it('should return all dashboard data', async () => {
       const responseData = { overview: {}, use: {} };
-      const resourcesData = { resourcesDashboardOverview: {}, resourcesUse: {} };
+      activityReports.mockResolvedValue({});
+      const resourcesData = {
+        resourcesDashboardOverview: {},
+        resourcesUse: {},
+        activityReports: {},
+      };
       resourceDashboardPhase1.mockResolvedValue(responseData);
       getUserReadRegions.mockResolvedValue([1]);
       const req = { session: { userId: 1 }, query: {} };
