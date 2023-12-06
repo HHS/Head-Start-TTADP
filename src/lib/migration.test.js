404b46b4197e5eb7a14405ccf9d1e5a4f0ee6f7d
Matt Bevilacqua <thewatermethod@gmail.com>
2023-12-06 16:18:22 -0500

    Update given improved logic

--- src/lib/migration.test.js
+++ src/lib/migration.test.js
@@ -232,7 +232,7 @@ describe('updateUsersFlagsEnum', () => {
     const valuesToRemove = ['value1', 'value2'];
     await updateUsersFlagsEnum(queryInterface, transaction, valuesToRemove);
 
-    expect(queryInterface.sequelize.query).toHaveBeenCalledTimes(11);
+    expect(queryInterface.sequelize.query).toHaveBeenCalledTimes(12);
   });
 });
 
