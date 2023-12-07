7d13aaf768a120007134156c6ee3354df5cc7edc
nvms <jon@pye.rs>
2023-12-07 11:28:56 -0500

    simplify this test

--- frontend/src/pages/RecipientRecord/pages/components/__tests__/ClassReview.js
+++ frontend/src/pages/RecipientRecord/pages/components/__tests__/ClassReview.js
@@ -1,8 +1,7 @@
+/* eslint-disable jest/expect-expect */
 import '@testing-library/jest-dom';
 import React from 'react';
-import {
-  render, screen,
-} from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import fetchMock from 'fetch-mock';
 
 import ClassReview from '../ClassReview';
@@ -11,170 +10,50 @@ const grantNumber = '1';
 const regionId = 1;
 const recipientId = 1;
 
-describe('ClassReview', () => {
-  const renderClassReview = () => render(
-    <ClassReview
-      grantNumber={grantNumber}
-      regionId={regionId}
-      recipientId={recipientId}
-    />,
-  );
+const apiUrl = `/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`;
 
-  afterAll(() => {
+const renderClassReview = () => render(
+  <ClassReview
+    grantNumber={grantNumber}
+    regionId={regionId}
+    recipientId={recipientId}
+  />,
+);
+
+const testThreshold = async (area, score, expectedText, received = '05/01/2023') => {
+  fetchMock.getOnce(apiUrl, {
+    received,
+    ES: area === 'ES' ? score : 0,
+    CO: area === 'CO' ? score : 0,
+    IS: area === 'IS' ? score : 0,
+  });
+
+  renderClassReview();
+  expect(await screen.findByTestId(`class-${area.toLowerCase()}`)).toHaveTextContent(expectedText);
+};
+
+describe('ClassReview', () => {
+  afterEach(() => {
     fetchMock.restore();
   });
 
   describe('emotional support', () => {
-    afterEach(() => {
-      fetchMock.restore();
-    });
-
-    it('above all thresholds', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-        received: '05/01/2023',
-        ES: 6,
-        CO: 0,
-        IS: 0,
-      });
-
-      renderClassReview();
-      expect(await screen.findByTestId('class-es')).toHaveTextContent('Above all thresholds');
-    });
-
-    it('below quality', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-        received: '05/01/2023',
-        ES: 5.1,
-        CO: 0,
-        IS: 0,
-      });
-
-      renderClassReview();
-      expect((await screen.findByTestId('class-es'))).toHaveTextContent('Below quality');
-    });
-
-    it('below competetive', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-        received: '05/01/2023',
-        ES: 5.1,
-        CO: 0,
-        IS: 0,
-      });
-
-      renderClassReview();
-      expect((await screen.findByTestId('class-es'))).toHaveTextContent('Below quality');
-    });
+    it('above all thresholds', () => testThreshold('ES', 6, 'Above all thresholds'));
+    it('below quality', () => testThreshold('ES', 5.1, 'Below quality'));
+    it('below competetive', () => testThreshold('ES', 5.1, 'Below quality'));
   });
 
   describe('classroom organization', () => {
-    afterEach(() => {
-      fetchMock.restore();
-    });
-
-    it('above all thresholds', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-        received: '05/01/2023',
-        ES: 0,
-        CO: 6,
-        IS: 0,
-      });
-
-      renderClassReview();
-      expect(await screen.findByTestId('class-co')).toHaveTextContent('Above all thresholds');
-    });
-
-    it('below quality', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-        received: '05/01/2023',
-        ES: 0,
-        CO: 5.1,
-        IS: 0,
-      });
-
-      renderClassReview();
-      expect((await screen.findByTestId('class-co'))).toHaveTextContent('Below quality');
-    });
-
-    it('below competetive', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-        received: '05/01/2023',
-        ES: 0,
-        CO: 4.9,
-        IS: 0,
-      });
-
-      renderClassReview();
-      expect((await screen.findByTestId('class-co'))).toHaveTextContent('Below competetive');
-    });
+    it('above all thresholds', () => testThreshold('CO', 6, 'Above all thresholds'));
+    it('below quality', () => testThreshold('CO', 5.1, 'Below quality'));
+    it('below competetive', () => testThreshold('CO', 4.9, 'Below competetive'));
   });
 
   describe('instructional support', () => {
-    afterEach(() => {
-      fetchMock.restore();
-    });
-
-    it('above all thresholds', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-        received: '05/01/2023',
-        ES: 0,
-        CO: 0,
-        IS: 3.1,
-      });
-
-      renderClassReview();
-      expect(await screen.findByTestId('class-is')).toHaveTextContent('Above all thresholds');
-    });
-
-    it('below quality - after 2025-08-01', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-
-        received: '08/02/2025',
-        ES: 0,
-        CO: 0,
-        IS: 2.5,
-      });
-
-      renderClassReview();
-      expect((await screen.findByTestId('class-is'))).toHaveTextContent('Below quality');
-    });
-
-    it('below quality - between 2020-11-09 and 2025-07-31', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-
-        received: '01/01/2025',
-        ES: 0,
-        CO: 0,
-        IS: 2.4,
-      });
-
-      renderClassReview();
-      expect((await screen.findByTestId('class-is'))).toHaveTextContent('Below quality');
-    });
-
-    it('below competetive - after 2025-08-01', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-
-        received: '08/02/2025',
-        ES: 0,
-        CO: 0,
-        IS: 2.4,
-      });
-
-      renderClassReview();
-      expect((await screen.findByTestId('class-is'))).toHaveTextContent('Below competetive');
-    });
-
-    it('below competetive - between 2020-11-09 and 2025-07-31', async () => {
-      fetchMock.getOnce(`/api/monitoring/class/${recipientId}/region/${regionId}/grant/${grantNumber}`, {
-
-        received: '01/01/2025',
-        ES: 0,
-        CO: 0,
-        IS: 2.2,
-      });
-
-      renderClassReview();
-      expect((await screen.findByTestId('class-is'))).toHaveTextContent('Below competetive');
-    });
+    it('above all thresholds', () => testThreshold('IS', 3.1, 'Above all thresholds'));
+    it('below quality - after 2025-08-01', () => testThreshold('IS', 2.5, 'Below quality'));
+    it('below quality - between 2020-11-09 and 2025-07-31', () => testThreshold('IS', 2.4, 'Below quality'));
+    it('below competetive - after 2025-08-01', () => testThreshold('IS', 2.4, 'Below competetive', '08/02/2025'));
+    it('below competetive - between 2020-11-09 and 2025-07-31', () => testThreshold('IS', 2.2, 'Below competetive'));
   });
 });
