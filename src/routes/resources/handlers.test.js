c9e5bdd0d4b9e63263c85d24bab46b13d499c6a2
Matt Bevilacqua <thewatermethod@gmail.com>
2023-12-08 12:09:39 -0500

    Add test for resource handler

--- src/routes/resources/handlers.test.js
+++ src/routes/resources/handlers.test.js
@@ -15,18 +15,38 @@ jest.mock('../../services/accessValidation');
 describe('Resources handler', () => {
   describe('getResourcesDashboardData', () => {
     it('should return all dashboard data', async () => {
-      const responseData = { overview: {}, use: {} };
+      const responseData = { overview: {}, use: {}, reportIds: [1] };
       activityReports.mockResolvedValue({});
       const resourcesData = {
         resourcesDashboardOverview: {},
         resourcesUse: {},
         activityReports: {},
+        reportIds: [1],
       };
       resourceDashboardPhase1.mockResolvedValue(responseData);
       getUserReadRegions.mockResolvedValue([1]);
-      const req = { session: { userId: 1 }, query: {} };
+      const req = {
+        session: { userId: 1 },
+        query: {
+          sortBy: 'id',
+          direction: 'asc',
+          limit: 10,
+          offset: 0,
+        },
+      };
       const res = { json: jest.fn() };
       await getResourcesDashboardData(req, res);
+      expect(activityReports).toHaveBeenCalledWith(
+        {
+          sortBy: 'id',
+          sortDir: 'asc',
+          offset: 0,
+          limit: 10,
+        },
+        true,
+        1,
+        [1],
+      );
       expect(res.json).toHaveBeenCalledWith(resourcesData);
     });
   });
