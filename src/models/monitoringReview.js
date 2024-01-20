62a220136326422a2090e8f395c31975ced41033
GarrettEHill <garretthill@gmail.com>
2024-01-19 17:33:28 -0800

    Create monitoringReview.js

---
+++ src/models/monitoringReview.js
@@ -0,0 +1,87 @@
+const { Model } = require('sequelize');
+
+export default (sequelize, DataTypes) => {
+  class MonitoringReview extends Model {
+    static associate(models) {
+      models.MonitoringReviewStatus.hasMany(
+        models.MonitoringReview,
+        {
+          foreignKey: 'statusId',
+          targetKey: 'statusId',
+          as: 'monitoringReviews',
+        },
+      );
+
+      models.MonitoringReview.belongsTo(
+        models.MonitoringReviewStatus,
+        {
+          foreignKey: 'statusId',
+          targetKey: 'statusId',
+          as: 'status',
+        },
+      );
+    }
+  }
+  MonitoringReview.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    reviewId: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    contentId: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    statusId: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+    startDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+    endDate: {
+      type: DataTypes.DATEONLY,
+      allowNull: true,
+    },
+    reviewType: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    reportDeliveryDate: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    outcome: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    hash: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    sourceCreatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    sourceUpdatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    sourceDeletedAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+  }, {
+    sequelize,
+    modelName: 'MonitoringReview',
+    tableName: 'MonitoringReviews',
+    paranoid: true,
+  });
+  return MonitoringReview;
+};
