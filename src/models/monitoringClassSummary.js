4fe7f1348b7b6b2a9b1a54dabc037c99f430d806
GarrettEHill <garretthill@gmail.com>
2024-01-19 17:33:14 -0800

    Create monitoringClassSummary.js

---
+++ src/models/monitoringClassSummary.js
@@ -0,0 +1,97 @@
+const { Model } = require('sequelize');
+
+export default (sequelize, DataTypes) => {
+  class MonitoringClassSummary extends Model {
+    static associate(models) {
+      models.MonitoringReview.hasMany(
+        models.MonitoringClassSummary,
+        {
+          foreignKey: 'reviewId',
+          targetKey: 'reviewId',
+          as: 'monitoringClassSummaries',
+        },
+      );
+
+      models.MonitoringClassSummary.belongsTo(
+        models.MonitoringReview,
+        {
+          foreignKey: 'reviewId',
+          targetKey: 'reviewId',
+          as: 'monitoringReview',
+        },
+      );
+
+      models.Grant.hasMany(
+        models.MonitoringClassSummary,
+        {
+          foreignKey: 'number',
+          targetKey: 'grantNumber',
+          as: 'monitoringClassSummaries',
+        },
+      );
+
+      models.MonitoringClassSummary.belongsTo(
+        models.Grant,
+        {
+          foreignKey: 'grantNumber',
+          targetKey: 'number',
+          as: 'grant',
+        },
+      );
+    }
+  }
+  MonitoringClassSummary.init({
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    reviewId: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    grantNumber: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    emotionalSupport: {
+      type: DataTypes.DECIMAL(5, 4),
+      allowNull: true,
+    },
+    classroomOrganization: {
+      type: DataTypes.DECIMAL(5, 4),
+      allowNull: true,
+    },
+    instructionalSupport: {
+      type: DataTypes.DECIMAL(5, 4),
+      allowNull: true,
+    },
+    reportDeliveryDate: {
+      type: DataTypes.DATE,
+      allowNull: true,
+    },
+    hash: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    sourceCreatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    sourceUpdatedAt: {
+      allowNull: false,
+      type: DataTypes.DATE,
+    },
+    sourceDeletedAt: {
+      allowNull: true,
+      type: DataTypes.DATE,
+    },
+  }, {
+    sequelize,
+    modelName: 'MonitoringClassSummary',
+    tableName: 'MonitoringClassSummaries',
+    paranoid: true,
+  });
+  return MonitoringClassSummary;
+};
