c4a67c917161f46bc864bf0423097e763b5f49dc
Nathan Powell <nathan.powell@adhocteam.us>
2024-01-23 03:59:17 -0800

    Switch to adding deletedAt to indexes

--- src/migrations/20240119000000-add_deletedAt_to_indexes.js
+++ src/migrations/20240119000000-add_deletedAt_to_indexes.js
@@ -10,38 +10,33 @@ module.exports = {
       await prepMigration(queryInterface, transaction, sessionSig);
       await queryInterface.sequelize.query(
         `
-        -- In tables that use Sequelize's built-in "deletedAt" column, exclude deleted records
-        -- from unique indexes that don't include the pkey
-        
+        -- Original plan: In tables that use Sequelize's built-in "deletedAt" column, exclude deleted records
+        -- from unique indexes that don't include the pkey. However, this does not support using ON CONFLICT
+        -- in upserts. So, we need to include "deletedAt" in the index instead.
+
         ALTER TABLE "ActivityReportApprovers" DROP CONSTRAINT "unique_activityReportId_userId";
         CREATE UNIQUE INDEX "unique_activityReportId_userId"
-        ON "ActivityReportApprovers" ("activityReportId", "userId")
-        WHERE "deletedAt" IS NULL;
+        ON "ActivityReportApprovers" ("activityReportId", "userId", "deletedAt");
 
         ALTER TABLE "CollaboratorTypes" DROP CONSTRAINT "CollaboratorTypes_name_validForId_unique";
         CREATE UNIQUE INDEX "CollaboratorTypes_name_validForId_unique"
-        ON "CollaboratorTypes" (name, "validForId")
-        WHERE "deletedAt" IS NULL;
+        ON "CollaboratorTypes" (name, "validForId", "deletedAt");
 
         ALTER TABLE "Courses" DROP CONSTRAINT "Courses_name_unique";
         CREATE UNIQUE INDEX "Courses_name_unique"
-        ON "Courses" (name)
-        WHERE "deletedAt" IS NULL;
+        ON "Courses" (name, "deletedAt");
 
         ALTER TABLE "GoalCollaborators" DROP CONSTRAINT "GoalCollaborators_goalId_userId_collaboratorTypeId_unique";
         CREATE UNIQUE INDEX "GoalCollaborators_goalId_userId_collaboratorTypeId_unique"
-        ON "GoalCollaborators" ("goalId", "userId", "collaboratorTypeId")
-        WHERE "deletedAt" IS NULL;
+        ON "GoalCollaborators" ("goalId", "userId", "collaboratorTypeId", "deletedAt");
 
         ALTER TABLE "ObjectiveCollaborators" DROP CONSTRAINT "ObjectiveCollaborators_objectiveId_userId_collaboratorTypeId_un";
         CREATE UNIQUE INDEX "ObjectiveCollaborators_objectiveId_userId_collaboratorTypeId_un"
-        ON "ObjectiveCollaborators" ("objectiveId", "userId", "collaboratorTypeId")
-        WHERE "deletedAt" IS NULL;
+        ON "ObjectiveCollaborators" ("objectiveId", "userId", "collaboratorTypeId", "deletedAt");
 
         ALTER TABLE "ValidFor" DROP CONSTRAINT "ValidFor_option_unique";
         CREATE UNIQUE INDEX "ValidFor_option_unique"
-        ON "ValidFor" (name)
-        WHERE "deletedAt" IS NULL;
+        ON "ValidFor" (name, "deletedAt");
         `,
         { transaction },
       );
