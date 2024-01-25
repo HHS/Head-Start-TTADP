b7ff44ead391eef6e9379c03e642c67d1502a7ff
GarrettEHill <garretthill@gmail.com>
2024-01-25 05:15:04 -0800

    Update genericFile.js

--- src/models/hooks/genericFile.js
+++ src/models/hooks/genericFile.js
@@ -46,7 +46,7 @@ const propagateDestroyToFile = async (sequelize, instance, options) => {
     && file.objectiveTemplateFiles.length === 0
     && file.sessionFiles.length === 0
     && file.communicationLogFiles.length === 0
-    && file.importFile.length === 0) {
+    && file.importFile?.id === null) {
     await sequelize.models.File.destroy({
       where: { id: file.id },
       individualHooks: true,
