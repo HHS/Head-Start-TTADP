1336c303bcf44aee6425e7da06252deb64f859d3
nvms <jon@pye.rs>
2024-01-29 11:06:10 -0500

    create goals when updating text if goals don't yet exist

--- src/models/hooks/sessionReportPilot.js
+++ src/models/hooks/sessionReportPilot.js
@@ -131,9 +131,20 @@ const participantsAndNextStepsComplete = async (sequelize, instance, options) =>
   }
 };
 
-export const createGoalsForSessionRecipientsIfNecessary = async (sequelize, instance, options) => {
-  try {
-    const { event, recipients } = JSON.parse(instance.data.val);
+export const createGoalsForSessionRecipientsIfNecessary = async (sequelize, sessionReportOrInstance, options) => {
+  const processSessionReport = async (sessionReport) => {
+    let event;
+    let recipients;
+
+    if (sessionReport && sessionReport.data) {
+      const data = (typeof sessionReport.data.val === 'string')
+        ? JSON.parse(sessionReport.data.val)
+        : sessionReport.data;
+
+      event = data.event;
+      recipients = data.recipients;
+    }
+
     if (!event?.data?.goal || !event.id) return;
 
     const eventId = Number(event.id);
@@ -143,48 +154,56 @@ export const createGoalsForSessionRecipientsIfNecessary = async (sequelize, inst
     // eslint-disable-next-line no-restricted-syntax
     for await (const { value: grantValue } of recipients) {
       const grantId = Number(grantValue);
+
+      // Check if a Goal already exists for the grantId.
+      const existingGoal = await sequelize.models.Goal.findOne({
+        where: { grantId, name: event.data.goal },
+      });
+
+      // If a Goal already exists, do not create a new one and continue with the next recipient.
+      // eslint-disable-next-line no-continue
+      if (existingGoal) continue;
+
       const grant = await sequelize.models.Grant.findByPk(grantId, { transaction: options.transaction });
       if (!grant) throw new Error('Grant not found');
 
-      const sessionId = instance.id;
-
-      const existing = await sequelize.models.EventReportPilotGoal.findOne({
-        where: {
-          eventId,
-          grantId,
-        },
-        transaction: options.transaction,
-      });
+      const sessionId = sessionReport.id;
 
       const hasCompleteSession = await sequelize.models.SessionReportPilot.findOne({
-        where: {
-          eventId,
-          'data.status': TRAINING_REPORT_STATUSES.COMPLETE,
-        },
+        where: { eventId, 'data.status': TRAINING_REPORT_STATUSES.COMPLETE },
         transaction: options.transaction,
       });
 
       const status = hasCompleteSession ? 'In Progress' : 'Draft';
 
-      if (!existing) {
-        const newGoal = await sequelize.models.Goal.create({
-          name: event.data.goal,
-          grantId,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-          status,
-          createdVia: 'tr',
-        }, { transaction: options.transaction });
+      // Since there is no existing Goal, create it and the EventReportPilotGoal record.
+      const newGoal = await sequelize.models.Goal.create({
+        name: event.data.goal,
+        grantId,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+        status,
+        createdVia: 'tr',
+      }, { transaction: options.transaction });
 
-        await sequelize.models.EventReportPilotGoal.create({
-          goalId: newGoal.id,
-          eventId,
-          sessionId,
-          grantId,
-          createdAt: new Date(),
-          updatedAt: new Date(),
-        }, { transaction: options.transaction });
-      }
+      await sequelize.models.EventReportPilotGoal.create({
+        goalId: newGoal.id,
+        eventId,
+        sessionId,
+        grantId,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }, { transaction: options.transaction });
+    }
+  };
+
+  try {
+    if (sessionReportOrInstance instanceof sequelize.Sequelize.Model) {
+      await processSessionReport(sessionReportOrInstance);
+    } else {
+      const instance = await sequelize.models.SessionReportPilot.findByPk(sessionReportOrInstance.id, { transaction: options.transaction });
+      if (!instance) throw new Error('SessionReportPilot instance not found');
+      await processSessionReport(instance);
     }
   } catch (error) {
     auditLogger.error(JSON.stringify({ error }));
