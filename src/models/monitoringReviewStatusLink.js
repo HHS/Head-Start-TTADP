7171c2a38a850841f0e102ed02540daefd1e2247
GarrettEHill <garretthill@gmail.com>
2024-02-02 00:13:15 -0800

    Update monitoringReviewStatusLink.js

--- src/models/monitoringReviewStatusLink.js
+++ src/models/monitoringReviewStatusLink.js
@@ -1,12 +1,10 @@
 import { Model } from 'sequelize';
 
 /**
- * This table exists only as linking bridge between the tables needing
- * to match on reviewId. This is due to a limitation in sequelize that requires that
- * associations between tables must be made using a primary key. based on the data structure
- * of the incoming data what was not possible while maintaining the structure of the
- * incoming data. Maintaining the structure of the incoming data simplifies the effort to
- * keep the data in sync.
+ * The incoming data does not have traditional primary keys that can be used in
+ * Sequelize, and changing the data to fit Sequelize's expectations would complicate
+ * synchronization efforts and make it harder to identify and diagnose any errors
+ * in the incoming data.
  */
 
 export default (sequelize, DataTypes) => {
