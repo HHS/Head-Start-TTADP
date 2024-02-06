71ffda9921c7897471fc3ea910c8f2c729b6d4fc
GarrettEHill <garretthill@gmail.com>
2024-02-06 15:17:11 -0800

    plumb timebox for cli

--- src/tools/importSystem.ts
+++ src/tools/importSystem.ts
@@ -1,12 +1,19 @@
 import { download, process } from '../lib/importSystem';
 
-export default async function manuallyTriggerImportSystem(action: string, importIdStr: string) {
+export default async function manuallyTriggerImportSystem(
+  action: string,
+  importIdStr: string,
+  timeBoxStr: string,
+) {
   if (!importIdStr || importIdStr.length === 0) throw new Error(`Bad or missing importId: '${importIdStr}'`);
   const importId = Number(importIdStr);
+  const timeBox = timeBoxStr && timeBoxStr.length
+    ? Number(timeBoxStr)
+    : undefined;
   if (!Number.isNaN(importId)) {
     switch (action) {
       case 'download':
-        await download(importId);
+        await download(importId, timeBox);
         break;
       case 'process':
         await process(importId);
--- src/tools/importSystemCLI.ts
+++ src/tools/importSystemCLI.ts
@@ -4,11 +4,12 @@ import { auditLogger } from '../logger';
 // Get the first and second arguments
 const action = process.argv[2];
 const importId = process.argv[3];
+const timeBox = process.argv[4];
 
 // Check if both arguments are provided
 if (action && importId) {
   // Call the function with the argument
-  importSystem(action, importId).then(() => process.exit()).catch((e) => {
+  importSystem(action, importId, timeBox).then(() => process.exit()).catch((e) => {
     auditLogger.error(e);
     process.exit(1);
   });
