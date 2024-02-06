d2cb80e6afec412f26c5a0a179591349c9ba3813
Matt Bevilacqua <thewatermethod@gmail.com>
2024-02-06 17:05:33 -0500

    remove line deletion

--- src/routes/goals/handlers.js
+++ src/routes/goals/handlers.js
@@ -283,6 +283,7 @@ export async function getSimilarGoalsByText(req, res) {
     const user = await userById(userId);
 
     const canCreate = new Goal(user, null, parseInt(regionId, DECIMAL_BASE)).canCreate();
+
     if (!canCreate) {
       res.sendStatus(httpCodes.FORBIDDEN);
       return;
