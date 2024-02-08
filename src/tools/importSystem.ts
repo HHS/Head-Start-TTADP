afad7ff869e3a109ff157df537e6bd1dd1e9ce86
GarrettEHill <garretthill@gmail.com>
2024-02-08 07:43:14 -0800

    better error handling

--- src/tools/importSystem.ts
+++ src/tools/importSystem.ts
@@ -1,27 +1,33 @@
 import { download, process } from '../lib/importSystem';
+import { auditLogger } from '../logger';
 
 export default async function manuallyTriggerImportSystem(
   action: string,
   importIdStr: string,
   timeBoxStr: string,
 ) {
-  if (!importIdStr || importIdStr.length === 0) throw new Error(`Bad or missing importId: '${importIdStr}'`);
-  const importId = Number(importIdStr);
-  const timeBox = timeBoxStr && timeBoxStr.length
-    ? Number(timeBoxStr)
-    : undefined;
-  if (!Number.isNaN(importId)) {
-    switch (action) {
-      case 'download':
-        await download(importId, timeBox);
-        break;
-      case 'process':
-        await process(importId);
-        break;
-      default:
-        throw new Error(`Unknown action: '${action}'`);
+  try {
+    if (!importIdStr || importIdStr.length === 0) throw new Error(`Bad or missing importId: '${importIdStr}'`);
+    const importId = Number(importIdStr);
+    const timeBox = timeBoxStr && timeBoxStr.length
+      ? Number(timeBoxStr)
+      : undefined;
+    if (!Number.isNaN(importId)) {
+      switch (action) {
+        case 'download':
+          await download(importId, timeBox);
+          break;
+        case 'process':
+          await process(importId);
+          break;
+        default:
+          throw new Error(`Unknown action: '${action}'`);
+      }
+    } else {
+      throw new Error(`Bad or missing importId: '${importIdStr}'`);
     }
-  } else {
-    throw new Error(`Bad or missing importId: '${importIdStr}'`);
+  } catch (e) {
+    auditLogger.error(e.message);
+    throw new Error(`Unknown error: '${e.message}'`);
   }
 }
