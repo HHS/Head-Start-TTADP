2fe0e96a443eff44a89b7b68f09ef55f02eb316d
Matt Bevilacqua <thewatermethod@gmail.com>
2024-02-16 12:03:56 -0500

    Alter goal card and add test

--- frontend/src/components/GoalCards/__tests__/GoalCard.js
+++ frontend/src/components/GoalCards/__tests__/GoalCard.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import join from 'url-join';
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { SCOPE_IDS } from '@ttahub/common';
+import fetchMock from 'fetch-mock';
+import { Router } from 'react-router';
+import { createMemoryHistory } from 'history';
 import GoalCard from '../GoalCard';
 import UserContext from '../../../UserContext';
+import AppLoadingContext from '../../../AppLoadingContext';
 
 describe('GoalCard', () => {
+  afterEach(() => fetchMock.restore());
+  const goalApi = join('/', 'api', 'goals');
   const goal = {
     id: 1,
     ids: [1],
@@ -17,6 +25,7 @@ describe('GoalCard', () => {
     goalNumbers: ['G-1'],
     source: 'The inferno',
     createdVia: 'rtr',
+    onAR: true,
     objectives: [
       {
         id: 1,
@@ -51,23 +60,29 @@ describe('GoalCard', () => {
     hideGoalOptions: false,
   };
 
-  const renderGoalCard = (props = DEFAULT_PROPS, defaultGoal = goal) => {
+  const history = createMemoryHistory();
+
+  const renderGoalCard = (props = DEFAULT_PROPS, defaultGoal = goal, user = DEFAULT_USER) => {
     render((
-      <UserContext.Provider value={{ user: DEFAULT_USER }}>
-        <GoalCard
-          goal={defaultGoal}
-          recipientId="1"
-          regionId="1"
-          showCloseSuspendGoalModal={() => {}}
-          performGoalStatusUpdate={() => {}}
-          handleGoalCheckboxSelect={() => {}}
-          isChecked={false}
-          marginX={3}
-          marginY={2}
-        // eslint-disable-next-line react/jsx-props-no-spreading
-          {...props}
-        />
-      </UserContext.Provider>));
+      <Router history={history}>
+        <AppLoadingContext.Provider value={{ setIsAppLoading: () => {} }}>
+          <UserContext.Provider value={{ user }}>
+            <GoalCard
+              goal={defaultGoal}
+              recipientId="1"
+              regionId="1"
+              showCloseSuspendGoalModal={() => {}}
+              performGoalStatusUpdate={() => {}}
+              handleGoalCheckboxSelect={() => {}}
+              isChecked={false}
+              marginX={3}
+              marginY={2}
+          // eslint-disable-next-line react/jsx-props-no-spreading
+              {...props}
+            />
+          </UserContext.Provider>
+        </AppLoadingContext.Provider>
+      </Router>));
   };
 
   it('shows the checkbox by default', () => {
@@ -88,7 +103,7 @@ describe('GoalCard', () => {
   });
 
   it('shows the goal status as a button by default', () => {
-    renderGoalCard();
+    renderGoalCard({ });
     const status = screen.getByText(/In Progress/i);
     expect(status.tagName).toEqual('BUTTON');
   });
@@ -102,6 +117,209 @@ describe('GoalCard', () => {
   it('shows the goal options by default', () => {
     renderGoalCard();
     expect(screen.getByTestId('ellipsis-button')).toBeInTheDocument();
+  });
+
+  it('shows only one options by default', async () => {
+    renderGoalCard();
+    userEvent.click(screen.getByTestId('ellipsis-button'));
+    const button = await screen.findByText(/Edit/i);
+    expect(button).toBeInTheDocument();
+  });
+
+  it('shows only one options by if the goal is not "draft" or "not started"', async () => {
+    const user = {
+      name: 'test@test.com',
+      homeRegionId: 1,
+      permissions: [
+        {
+          scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+        {
+          scopeId: SCOPE_IDS.APPROVE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+      ],
+    };
+    renderGoalCard(DEFAULT_PROPS, { ...goal, onAR: false }, user);
+    userEvent.click(screen.getByTestId('ellipsis-button'));
+    const button = await screen.findByText(/Edit/i);
+    expect(button).toBeInTheDocument();
+    const deleteButton = screen.queryByText(/Delete/i);
+    expect(deleteButton).not.toBeInTheDocument();
+  });
+
+  it('shows only one options by if the goal is on AR', async () => {
+    const user = {
+      name: 'test@test.com',
+      homeRegionId: 1,
+      permissions: [
+        {
+          scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+        {
+          scopeId: SCOPE_IDS.APPROVE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+      ],
+    };
+    renderGoalCard(DEFAULT_PROPS, { ...goal, goalStatus: 'Draft' }, user);
+    userEvent.click(screen.getByTestId('ellipsis-button'));
+    const button = await screen.findByText(/Edit/i);
+    expect(button).toBeInTheDocument();
+    const deleteButton = screen.queryByText(/Delete/i);
+    expect(deleteButton).not.toBeInTheDocument();
+  });
+
+  it('can delete if user is approver and goal is Draft and not on AR', async () => {
+    const user = {
+      name: 'test@test.com',
+      homeRegionId: 1,
+      permissions: [
+        {
+          scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+        {
+          scopeId: SCOPE_IDS.APPROVE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+      ],
+    };
+    renderGoalCard(DEFAULT_PROPS, { ...goal, goalStatus: 'Draft', onAR: false }, user);
+    userEvent.click(screen.getByTestId('ellipsis-button'));
+    const button = await screen.findByText(/Edit/i);
+    expect(button).toBeInTheDocument();
+    const deleteButton = screen.queryByText(/Delete/i);
+    expect(deleteButton).toBeInTheDocument();
+  });
+
+  it('can delete if user is approver and goal is Not Started and not on AR', async () => {
+    const user = {
+      name: 'test@test.com',
+      homeRegionId: 1,
+      permissions: [
+        {
+          scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+        {
+          scopeId: SCOPE_IDS.APPROVE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+      ],
+    };
+    renderGoalCard(DEFAULT_PROPS, { ...goal, goalStatus: 'Not Started', onAR: false }, user);
+    userEvent.click(screen.getByTestId('ellipsis-button'));
+    const button = await screen.findByText(/Edit/i);
+    expect(button).toBeInTheDocument();
+    const deleteButton = screen.queryByText(/Delete/i);
+    expect(deleteButton).toBeInTheDocument();
+  });
+
+  it('can delete if user is admin and goal is Draft and not on AR', async () => {
+    const user = {
+      name: 'test@test.com',
+      homeRegionId: 1,
+      permissions: [
+        {
+          scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+        {
+          scopeId: SCOPE_IDS.ADMIN,
+          regionId: 14,
+        },
+      ],
+    };
+    renderGoalCard(DEFAULT_PROPS, { ...goal, goalStatus: 'Draft', onAR: false }, user);
+    userEvent.click(screen.getByTestId('ellipsis-button'));
+    const button = await screen.findByText(/Edit/i);
+    expect(button).toBeInTheDocument();
+    const deleteButton = screen.queryByText(/Delete/i);
+    expect(deleteButton).toBeInTheDocument();
+  });
+
+  it('can delete if user is admin and goal is Not Started and not on AR', async () => {
+    const user = {
+      name: 'test@test.com',
+      homeRegionId: 1,
+      permissions: [
+        {
+          scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+        {
+          scopeId: SCOPE_IDS.ADMIN,
+          regionId: 14,
+        },
+      ],
+    };
+    renderGoalCard(DEFAULT_PROPS, { ...goal, goalStatus: 'Not Started', onAR: false }, user);
+    userEvent.click(screen.getByTestId('ellipsis-button'));
+    const button = await screen.findByText(/Edit/i);
+    expect(button).toBeInTheDocument();
+    const deleteButton = screen.queryByText(/Delete/i);
+    expect(deleteButton).toBeInTheDocument();
+  });
+
+  it('calls delete function on click', async () => {
+    const user = {
+      name: 'test@test.com',
+      homeRegionId: 1,
+      permissions: [
+        {
+          scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+        {
+          scopeId: SCOPE_IDS.ADMIN,
+          regionId: 14,
+        },
+      ],
+    };
+    renderGoalCard(DEFAULT_PROPS, { ...goal, goalStatus: 'Not Started', onAR: false }, user);
+    userEvent.click(screen.getByTestId('ellipsis-button'));
+    const button = await screen.findByText(/Edit/i);
+    expect(button).toBeInTheDocument();
+    const deleteButton = screen.queryByText(/Delete/i);
+    const url = `${goalApi}?goalIds=1`;
+    fetchMock.delete(url, {});
+    history.push = jest.fn();
+    userEvent.click(deleteButton);
+    await waitFor(() => expect(fetchMock.called(url)).toBe(true));
+    expect(history.push).toHaveBeenCalledWith('/recipient-tta-records/1/region/1/rttapa', { message: 'Goal deleted successfully' });
+    expect(document.querySelector('.smart-hub-border-base-error')).toBeNull();
+  });
+
+  it('handles an error on delete', async () => {
+    const user = {
+      name: 'test@test.com',
+      homeRegionId: 1,
+      permissions: [
+        {
+          scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+          regionId: 1,
+        },
+        {
+          scopeId: SCOPE_IDS.ADMIN,
+          regionId: 14,
+        },
+      ],
+    };
+    renderGoalCard(DEFAULT_PROPS, { ...goal, goalStatus: 'Not Started', onAR: false }, user);
+    userEvent.click(screen.getByTestId('ellipsis-button'));
+    const button = await screen.findByText(/Edit/i);
+    expect(button).toBeInTheDocument();
+    const deleteButton = screen.queryByText(/Delete/i);
+    const url = `${goalApi}?goalIds=1`;
+    fetchMock.delete(url, 500);
+    userEvent.click(deleteButton);
+    history.push = jest.fn();
+    await waitFor(() => expect(fetchMock.called(url)).toBe(true));
+    expect(history.push).not.toHaveBeenCalled();
+    expect(document.querySelector('.smart-hub-border-base-error')).not.toBeNull();
   });
 
   it('can hide the goal options', () => {
