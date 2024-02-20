59eae1063f85a457423af01258570be768702440
Krys Wisnaskas <krystyna@adhocteam.us>
2024-02-20 14:51:57 -0500

    Fix monitoring services tests

--- src/services/monitoring.test.js
+++ src/services/monitoring.test.js
@@ -1,12 +1,35 @@
 import sequelize from 'sequelize';
 import { classScore, monitoringData } from './monitoring';
+import db from '../models';
+
+const {
+  MonitoringClassSummary,
+} = db;
 
 describe('monitoring services', () => {
   const RECIPIENT_ID = 1;
   const REGION_ID = 14;
-  const GRANT_NUMBER = '09HP044444';
+  const GRANT_NUMBER = '09CH033333';
+
+  beforeAll(async () => {
+    await MonitoringClassSummary.findOrCreate({
+      where: { grantNumber: GRANT_NUMBER },
+      defaults: {
+        reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808',
+        grantNumber: GRANT_NUMBER,
+        emotionalSupport: 6.2303,
+        classroomOrganization: 5.2303,
+        instructionalSupport: 3.2303,
+        reportDeliveryDate: '2023-05-22 21:00:00-07',
+        hash: 'seedhashclasssum1',
+        sourceCreatedAt: '2023-05-22 21:00:00-07',
+        sourceUpdatedAt: '2023-05-22 21:00:00-07',
+      },
+    });
+  });
 
   afterAll(async () => {
+    await MonitoringClassSummary.destroy({ where: { grantNumber: GRANT_NUMBER } });
     await sequelize.close();
   });
 
