42805d0fbad2d382a3a21f9dfef2f4b7f1169df4
Nathan Powell <nathan.powell@adhocteam.us>
2024-02-20 07:39:41 -0800

    add protective query to prevent invalid migration use

--- src/migrations/20240116000001-add-effectivedate_for_migrated_personnel.js
+++ src/migrations/20240116000001-add-effectivedate_for_migrated_personnel.js
@@ -13,19 +13,49 @@ module.exports = {
         -------------
         -- PROCESS --
         -------------
-        -- Get all the roles * programs * grants
-        -- Get the history of all roles, so:
+        -- 0: Make sure the migration is valid
+        -- 1: Get all the roles * programs * grants
+        -- 2: Get the history of all roles, so:
         --   roles * programs * grants * emails
-        -- Get the instances where people were set active
+        -- 3: Get the instances where people were set active
         --   This is separate because they may be set active
         --   in a different transaction than when the rest
         --   of their data was set
-        -- Count how many times each role * program * grant was assigned
-        -- Get the latest activation timestamp for the currently assigned personnel
+        -- 4: Count how many times each role * program * grant was assigned
+        -- 5: Get the latest activation timestamp for the currently assigned personnel
         --   No one appears to have been assigned to the same role * program * grant
         --   more than once, so this is more of a "due diligence" item
-        -- Update any blank effectiveDates with the latest activation timestamp
+        -- 6: Update any blank effectiveDates with the latest activation timestamp
 
+        -- 0: Make sure the migration is valid
+        -- (The logic is not guaranteed to work if the values that associate personnel
+        -- with a role in a ProgramPersonnel record have been updated more than once.)
+        CREATE TEMP TABLE invalid_test
+        AS
+        WITH program_personnel_association_updates AS (
+        SELECT
+          data_id,
+          COUNT(DISTINCT new_row_data->>'email') email_cnt,
+          COUNT(DISTINCT new_row_data->>'programId') program_cnt,
+          COUNT(DISTINCT new_row_data->>'grantId') grant_cnt,
+          COUNT(DISTINCT new_row_data->>'role') role_cnt
+        FROM "ZALProgramPersonnel"
+        GROUP BY 1
+        )
+        SELECT *
+        FROM program_personnel_association_updates
+        WHERE email_cnt > 1
+          OR program_cnt > 1
+          OR grant_cnt > 1
+          OR role_cnt > 1
+        ;
+        -- This will cause a divide by zero error that fails the transaction if invalid_test is not empty
+        SELECT
+          1/(LEAST(COUNT(*),1) - 1)
+        FROM invalid_test
+        ;
+        
+        -- 1: Get all the roles * programs * grants
         WITH all_assigned_roles AS (
         SELECT DISTINCT
           "programId" pid,
@@ -33,6 +63,7 @@ module.exports = {
           role
         FROM "ProgramPersonnel"
         ),
+        -- 2: Get the history of all roles
         all_personnel_roles AS (
         SELECT DISTINCT
           (new_row_data->>'programId')::int zpid,
@@ -60,6 +91,7 @@ module.exports = {
           data_id ppid,
           email,
           zpp.dml_timestamp effective_date,
+          -- 4: Count how many times each role * program * grant was assigned
           ROW_NUMBER() OVER (
             PARTITION BY data_id, email
             ORDER BY zpp.dml_timestamp DESC
@@ -70,14 +102,17 @@ module.exports = {
           AND pp."grantId" = tapr.grid
           AND pp.role = tapr.role
           AND pers_ass_over_time_cnt > 1
+        -- 3: Get the instances where people were set active
         JOIN "ZALProgramPersonnel" zpp
           ON pp.id = zpp.data_id
           AND (zpp.new_row_data->>'active')::bool = TRUE
         WHERE pp.active = TRUE
         )
         UPDATE "ProgramPersonnel" pp
+        -- 6: Update any blank effectiveDates
         SET "effectiveDate" = effective_date
         FROM ordered_effective_dates
+        -- 5: Get the latest activation timestamp
         WHERE date_order = 1
           AND ppid = id
           AND pp."effectiveDate" IS NULL;
