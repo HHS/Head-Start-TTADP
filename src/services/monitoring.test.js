18751051322bbb54bd8997dea2c6af825a3c87bd
Krys Wisnaskas <krystyna@adhocteam.us>
2024-02-23 15:18:53 -0500

    Fix indent

--- src/services/monitoring.test.js
+++ src/services/monitoring.test.js
@@ -19,77 +19,76 @@ const GRANT_NUMBER = '01HP044446';
 const GRANT_ID = 665;
 
 async function createMonitoringData() {
-    await MonitoringClassSummary.findOrCreate({
-      where: { grantNumber: GRANT_NUMBER },
-      defaults: {
-        reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808',
-        grantNumber: GRANT_NUMBER,
-        emotionalSupport: 6.2303,
-        classroomOrganization: 5.2303,
-        instructionalSupport: 3.2303,
-        reportDeliveryDate: '2023-05-22 21:00:00-07',
-        hash: 'seedhashclasssum1',
-        sourceCreatedAt: '2023-05-22 21:00:00-07',
-        sourceUpdatedAt: '2023-05-22 21:00:00-07',
-      },
-    });
+  await MonitoringClassSummary.findOrCreate({
+    where: { grantNumber: GRANT_NUMBER },
+    defaults: {
+      reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808',
+      grantNumber: GRANT_NUMBER,
+      emotionalSupport: 6.2303,
+      classroomOrganization: 5.2303,
+      instructionalSupport: 3.2303,
+      reportDeliveryDate: '2023-05-22 21:00:00-07',
+      hash: 'seedhashclasssum1',
+      sourceCreatedAt: '2023-05-22 21:00:00-07',
+      sourceUpdatedAt: '2023-05-22 21:00:00-07',
+    },
+  });
 
-    await MonitoringReviewGrantee.findOrCreate({
-      where: { grantNumber: GRANT_NUMBER },
-      defaults: {
-        reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808',
-        granteeId: '14FC5A81-8E27-4B06-A107-9C28762BC2F6',
-        grantNumber: GRANT_NUMBER,
-        sourceCreatedAt: '2024-02-12 14:31:55.74-08',
-        sourceUpdatedAt: '2024-02-12 14:31:55.74-08',
-        createTime: '2023-11-14 21:00:00-08',
-        updateTime: '2024-02-12 14:31:55.74-08',
-        updateBy: 'Support Team',
-      },
-    });
+  await MonitoringReviewGrantee.findOrCreate({
+    where: { grantNumber: GRANT_NUMBER },
+    defaults: {
+      reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808',
+      granteeId: '14FC5A81-8E27-4B06-A107-9C28762BC2F6',
+      grantNumber: GRANT_NUMBER,
+      sourceCreatedAt: '2024-02-12 14:31:55.74-08',
+      sourceUpdatedAt: '2024-02-12 14:31:55.74-08',
+      createTime: '2023-11-14 21:00:00-08',
+      updateTime: '2024-02-12 14:31:55.74-08',
+      updateBy: 'Support Team',
+    },
+  });
 
-    await MonitoringReview.findOrCreate({
-      where: { reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808' },
-      defaults: {
-        reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808',
-        contentId: '653DABA6-DE64-4081-B5B3-9A126487E8F',
-        statusId: 6006,
-        startDate: '2024-02-12',
-        endDate: '2024-02-12',
-        reviewType: 'FA-1',
-        reportDeliveryDate: '2023-02-21 21:00:00-08',
-        outcome: 'Complete',
-        hash: 'seedhashrev2',
-        sourceCreatedAt: '2023-02-22 21:00:00-08',
-        sourceUpdatedAt: '2023-02-22 21:00:00-08',
-      },
-    });
+  await MonitoringReview.findOrCreate({
+    where: { reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808' },
+    defaults: {
+      reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808',
+      contentId: '653DABA6-DE64-4081-B5B3-9A126487E8F',
+      statusId: 6006,
+      startDate: '2024-02-12',
+      endDate: '2024-02-12',
+      reviewType: 'FA-1',
+      reportDeliveryDate: '2023-02-21 21:00:00-08',
+      outcome: 'Complete',
+      hash: 'seedhashrev2',
+      sourceCreatedAt: '2023-02-22 21:00:00-08',
+      sourceUpdatedAt: '2023-02-22 21:00:00-08',
+    },
+  });
 
-    await MonitoringReviewLink.findOrCreate({
-      where: { reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808' },
-      defaults: {
-        reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808',
-      },
-    });
+  await MonitoringReviewLink.findOrCreate({
+    where: { reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808' },
+    defaults: {
+      reviewId: 'C48EAA67-90B9-4125-9DB5-0011D6D7C808',
+    },
+  });
 
-    await MonitoringReviewStatusLink.findOrCreate({
-      where: { statusId: 6006 },
-      defaults: {
-        statusId: 6006,
-      },
-    });
+  await MonitoringReviewStatusLink.findOrCreate({
+    where: { statusId: 6006 },
+    defaults: {
+      statusId: 6006,
+    },
+  });
 
-    await MonitoringReviewStatus.findOrCreate({
-      where: { statusId: 6006 },
-      defaults: {
-        statusId: 6006,
-        name: 'Complete',
-        sourceCreatedAt: '2024-02-12 14:31:55.74-08',
-        sourceUpdatedAt: '2024-02-12 14:31:55.74-08',
-      },
-    });
+  await MonitoringReviewStatus.findOrCreate({
+    where: { statusId: 6006 },
+    defaults: {
+      statusId: 6006,
+      name: 'Complete',
+      sourceCreatedAt: '2024-02-12 14:31:55.74-08',
+      sourceUpdatedAt: '2024-02-12 14:31:55.74-08',
+    },
+  });
 }
-
 
 describe('monitoring services', () => {
   beforeAll(async () => {
@@ -176,7 +175,6 @@ describe('monitoring services', () => {
       });
 
       expect(grantNumberLink).not.toBeNull();
-
 
       const data = await monitoringData({
         recipientId,
