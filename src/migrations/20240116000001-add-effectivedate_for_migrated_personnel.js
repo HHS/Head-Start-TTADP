0b207ca851f8b58a9832ddad11c303945b0e0972
Nathan Powell <nathan.powell@adhocteam.us>
2024-02-26 06:59:51 -0800

    change assumptions to set effectiveDate for multiple people assigned to a role

--- src/migrations/20240116000001-add-effectivedate_for_migrated_personnel.js
+++ src/migrations/20240116000001-add-effectivedate_for_migrated_personnel.js
@@ -13,21 +13,19 @@ module.exports = {
         -------------
         -- PROCESS --
         -------------
-        -- 0: Make sure the migration is valid
-        -- 1: Get all the roles * programs * grants
-        -- 2: Get the history of all roles, so:
+        -- 1: Make sure the migration is valid
+        -- 2: Get unique role assignments, so:
         --   roles * programs * grants * emails
         -- 3: Get the instances where people were set active
         --   This is separate because they may be set active
         --   in a different transaction than when the rest
         --   of their data was set
-        -- 4: Count how many times each role * program * grant was assigned
-        -- 5: Get the latest activation timestamp for the currently assigned personnel
+        -- 4: Get the latest activation timestamp for the currently assigned personnel
         --   No one appears to have been assigned to the same role * program * grant
         --   more than once, so this is more of a "due diligence" item
-        -- 6: Update any blank effectiveDates with the latest activation timestamp
+        -- 5: Update any blank effectiveDates with the latest activation timestamp
 
-        -- 0: Make sure the migration is valid
+        -- 1: Make sure the migration is valid
         -- (The logic is not guaranteed to work if the values that associate personnel
         -- with a role in a ProgramPersonnel record have been updated more than once.)
         CREATE TEMP TABLE invalid_test
@@ -55,53 +53,29 @@ module.exports = {
         FROM invalid_test
         ;
         
-        -- 1: Get all the roles * programs * grants
-        WITH all_assigned_roles AS (
+        -- 2: Get unique role assignments
+        WITH all_personnel_roles AS (
         SELECT DISTINCT
-          "programId" pid,
-          "grantId" grid,
-          role
-        FROM "ProgramPersonnel"
-        ),
-        -- 2: Get the history of all roles
-        all_personnel_roles AS (
-        SELECT DISTINCT
-          (new_row_data->>'programId')::int zpid,
-          (new_row_data->>'grantId')::int zgrid,
-          new_row_data->>'role' zrole,
-          new_row_data->>'email' zemail,
+          (new_row_data->>'programId')::int pid,
+          (new_row_data->>'grantId')::int grid,
+          new_row_data->>'role' role,
+          new_row_data->>'email' email,
           data_id
         FROM "ZALProgramPersonnel"
         ),
-        total_assignments_per_role AS (
-        SELECT
-          COUNT(*) pers_ass_over_time_cnt,
-          pid,
-          grid,
-          role
-        FROM all_assigned_roles aar
-        JOIN all_personnel_roles apr
-          ON pid = zpid
-          AND grid = zgrid
-          AND role = zrole
-        GROUP BY 2,3,4
-        ),
         ordered_effective_dates AS (
         SELECT
-          data_id ppid,
-          email,
+          pp.id ppid,
           zpp.dml_timestamp effective_date,
-          -- 4: Count how many times each role * program * grant was assigned
+          -- 4: Count how many times each email * role * program * grant was assigned
           ROW_NUMBER() OVER (
-            PARTITION BY data_id, email
+            PARTITION BY apr.data_id
             ORDER BY zpp.dml_timestamp DESC
           ) date_order
         FROM "ProgramPersonnel" pp
-        JOIN total_assignments_per_role tapr
-          ON pp."programId" = tapr.pid
-          AND pp."grantId" = tapr.grid
-          AND pp.role = tapr.role
-          AND pers_ass_over_time_cnt > 1
+        JOIN all_personnel_roles apr
+          ON pp.id = apr.data_id
+          AND pp.email = apr.email
         -- 3: Get the instances where people were set active
         JOIN "ZALProgramPersonnel" zpp
           ON pp.id = zpp.data_id
@@ -109,10 +83,10 @@ module.exports = {
         WHERE pp.active = TRUE
         )
         UPDATE "ProgramPersonnel" pp
-        -- 6: Update any blank effectiveDates
+        -- 5: Update any blank effectiveDates
         SET "effectiveDate" = effective_date
         FROM ordered_effective_dates
-        -- 5: Get the latest activation timestamp
+        -- 4: Get the latest activation timestamp
         WHERE date_order = 1
           AND ppid = id
           AND pp."effectiveDate" IS NULL;
