1b6d279153ec8209c254f98765c56690051f9029
nvms <jon@pye.rs>
2024-02-28 10:37:20 -0500

    rm comment

--- frontend/src/components/GoalForm/index.js
+++ frontend/src/components/GoalForm/index.js
@@ -935,7 +935,6 @@ export default function GoalForm({
           </>
         ) : null }
 
-        {/* <!-- here --> */}
         <form onSubmit={onSubmit}>
           { showForm && (
             <Form
