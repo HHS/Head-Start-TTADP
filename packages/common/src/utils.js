c7a37ab91c84281bd08d306b0355cd3084464641
Matt Bevilacqua <thewatermethod@gmail.com>
2024-04-03 13:01:42 -0400

    Remove function to use lodash version instead

--- packages/common/src/utils.js
+++ packages/common/src/utils.js
@@ -30,24 +30,6 @@ function determineMergeGoalStatus(statuses) {
     return GOAL_STATUS.DRAFT;
 }
 
-const entityMap = {
-  '<': '&lt;',
-  '>': '&gt;',
-  '&': '&amp;',
-  '"': '&quot;',
-  "'": '&#39;',
-  '/': '&#47;',
-  '[': '&#91;',
-  ']': '&#93;',
-};
-
-function escapeHtml(str) {
-  return str.replace(/[<>&"'\/]/g, function (s) {
-    return entityMap[s];
-  });
-}
-
 module.exports = {
   determineMergeGoalStatus,
-  escapeHtml,
 }
