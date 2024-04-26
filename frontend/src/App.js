67d0852233d8a7e3a064b1eac4d8c3d515d40dd9
Matt Bevilacqua <thewatermethod@gmail.com>
2024-04-26 09:27:15 -0400

    remove feature flag

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -258,9 +258,7 @@ function App() {
           path="/dashboards/resources-dashboard"
           render={() => (
             <AppWrapper authenticated logout={logout}>
-              <FeatureFlag flag="resources_dashboard" renderNotFound>
-                <ResourcesDashboard user={user} />
-              </FeatureFlag>
+              <ResourcesDashboard user={user} />
             </AppWrapper>
           )}
         />
