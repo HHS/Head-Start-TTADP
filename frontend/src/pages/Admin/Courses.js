13e0290d0d9cffee613c47ae2a4555906ecd2730
Matt Bevilacqua <thewatermethod@gmail.com>
2024-05-01 13:33:47 -0400

    Add export courses button

--- frontend/src/pages/Admin/Courses.js
+++ frontend/src/pages/Admin/Courses.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Button } from '@trussworks/react-uswds';
 import CsvImport from './components/CsvImport';
+import { getCourses } from '../../fetchers/courses';
 
 function Courses() {
+  const [courses, setCourses] = useState();
   const primaryIdColumn = 'course name';
   const typeName = 'courses';
   const apiPathName = 'courses';
+
+  useEffect(() => {
+    async function get() {
+      const response = await getCourses();
+      setCourses(response);
+    }
+
+    if (!courses) {
+      get();
+    }
+  }, [courses]);
+
+  const exportCourses = () => {
+    // export courses as CSV
+    let csv = 'course name\n';
+    courses.forEach((course) => {
+      csv += `"${course.name}"\n`;
+    });
+
+    const hiddenElement = document.createElement('a');
+    hiddenElement.href = `data:text/csv;charset=utf-8,${encodeURI(csv)}`;
+    hiddenElement.target = '_blank';
+    hiddenElement.download = 'courses.csv';
+    hiddenElement.click();
+  };
 
   const validCsvHeaders = [
     'course name',
@@ -23,6 +51,17 @@ function Courses() {
         apiPathName={apiPathName}
         primaryIdColumn={primaryIdColumn}
       />
+
+      {courses && (
+      <Button
+        outline
+        type="button"
+        onClick={exportCourses}
+      >
+        Export courses as CSV
+      </Button>
+      )}
+
     </>
   );
 }
