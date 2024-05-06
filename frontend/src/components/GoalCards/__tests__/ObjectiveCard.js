a438cb5dbb81f54da094a53b956cab4b3991b2a6
Matt Bevilacqua <thewatermethod@gmail.com>
2024-05-06 16:27:11 -0400

    Test objective status change

--- frontend/src/components/GoalCards/__tests__/ObjectiveCard.js
+++ frontend/src/components/GoalCards/__tests__/ObjectiveCard.js
@@ -1,6 +1,11 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { SCOPE_IDS } from '@ttahub/common';
+import {
+  render, screen, act, waitFor,
+} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import fetchMock from 'fetch-mock';
 import { Router } from 'react-router';
 import { createMemoryHistory } from 'history';
 import ObjectiveCard from '../ObjectiveCard';
@@ -8,10 +13,34 @@ import UserContext from '../../../UserContext';
 
 describe('ObjectiveCard', () => {
   const history = createMemoryHistory();
+  const renderObjectiveCard = (objective, dispatchStatusChange = jest.fn()) => {
+    render(
+      <UserContext.Provider value={{
+        user: {
+          permissions: [{
+            scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+            regionId: 1,
+          }],
+        },
+      }}
+      >
+        <Router history={history}>
+          <ObjectiveCard
+            objective={objective}
+            regionId={1}
+            goalStatus="In Progress"
+            objectivesExpanded
+            dispatchStatusChange={dispatchStatusChange}
+          />
+        </Router>
+      </UserContext.Provider>,
+    );
+  };
 
   it('renders legacy reports', async () => {
     const objective = {
       id: 123,
+      ids: [123],
       title: 'This is an objective',
       endDate: '2020-01-01',
       reasons: ['reason1', 'reason2'],
@@ -29,18 +58,49 @@ describe('ObjectiveCard', () => {
         },
       ],
     };
-    render(
-      <UserContext.Provider value={{ user: {} }}>
-        <Router history={history}>
-          <ObjectiveCard objective={objective} objectivesExpanded />
-        </Router>
-      </UserContext.Provider>,
-    );
+    renderObjectiveCard(objective);
     expect(screen.getByText('This is an objective')).toBeInTheDocument();
     expect(screen.getByText('2020-01-01')).toBeInTheDocument();
     expect(screen.getByText('reason1')).toBeInTheDocument();
     expect(screen.getByText('reason2')).toBeInTheDocument();
     const link = screen.getByText('r-123');
     expect(link).toHaveAttribute('href', '/activity-reports/legacy/123');
+  });
+
+  it('updates objective status', async () => {
+    const objective = {
+      id: 123,
+      ids: [123],
+      title: 'This is an objective',
+      endDate: '2020-01-01',
+      reasons: ['reason1', 'reason2'],
+      status: 'In Progress',
+      grantNumbers: ['grant1', 'grant2'],
+      topics: [],
+      supportTypes: ['Planning'],
+      activityReports: [],
+    };
+    const dispatchStatusChange = jest.fn();
+    renderObjectiveCard(objective, dispatchStatusChange);
+
+    expect(screen.getByText('This is an objective')).toBeInTheDocument();
+
+    const changeButton = await screen.findByRole('button', { name: /change status/i });
+    act(() => {
+      userEvent.click(changeButton);
+    });
+
+    expect(dispatchStatusChange).toHaveBeenCalledWith([123], 'In Progress');
+    fetchMock.put('/api/objectives/status', { ids: [123], status: 'Complete' });
+
+    const completeButton = await screen.findByRole('button', { name: /complete/i });
+    act(() => {
+      userEvent.click(completeButton);
+    });
+
+    expect(fetchMock.called('/api/objectives/status')).toBe(true);
+    await waitFor(() => {
+      expect(dispatchStatusChange).toHaveBeenCalledWith([123], 'Complete');
+    });
   });
 });
