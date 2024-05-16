8b65d265f655709d4c59e0eef99acc4bca5658b3
GarrettEHill <garretthill@gmail.com>
2024-05-15 23:05:41 -0700

    Update resource.test.js

--- src/lib/resource.test.js
+++ src/lib/resource.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-useless-escape */
 import axios from 'axios';
-import { expect } from '@playwright/test';
+// import { expect } from '@playwright/test';
 import { auditLogger } from '../logger';
 import { getResourceMetaDataJob } from './resource';
 import db, { Resource } from '../models';
