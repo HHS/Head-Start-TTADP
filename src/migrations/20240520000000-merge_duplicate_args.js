0f42b961813c6724fa7b2873259362726fb5f7d2
Nathan Powell <nathan.powell@adhocteam.us>
2024-05-21 07:12:03 -0700

    dedupe ARG and create timeseries functions, restore missing ARGFRs

---
+++ src/migrations/20240520000000-merge_duplicate_args.js
@@ -0,0 +1,229 @@
+const {
+  prepMigration,
+} = require('../lib/migration');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+      await queryInterface.sequelize.query(`
+
+      -- Creating it as a function because we'll need to rerun this in the future
+      -- up until and unless all issues producing duplicate ARGs are addressed
+
+      CREATE OR REPLACE FUNCTION dedupe_args()
+      RETURNS VOID LANGUAGE sql AS
+      $$
+      -- There are some duplicate ARGs, meaning link records that connect the same
+      -- AR-Goal pairs. This migration merges them down to the link record that was
+      -- most recently updated and thus presumably has the latest status & etc.
+      -- Merging rather than simply deleting is necessary to account for
+      -- ActivityReportGoalFieldResponses and ActivityReportGoalResources, both of
+      -- which link to ARGs and so may need to be moved and deconflicted.
+      -- Neither ActivityReportGoalResources nor ActivityReportGoalFieldResponses
+      -- have applicable records at time of writing, but this may not remain true in
+      -- the future when this runs.
+
+      DROP TABLE IF EXISTS arg_merges;
+      CREATE TEMP TABLE arg_merges
+      AS
+      WITH link_counts AS (
+      SELECT
+        "activityReportId" arid,
+        "goalId" gid,
+        COUNT(*) link_cnt
+      FROM "ActivityReportGoals"
+      GROUP BY 1,2
+      ),
+      latest_updated AS (
+      SELECT
+        arid,
+        gid,
+        arg.id argid,
+        ROW_NUMBER() OVER (
+          PARTITION BY arid,gid
+          ORDER BY "updatedAt" DESC, arg.id
+        ) updated_rank
+      FROM "ActivityReportGoals" arg
+      JOIN link_counts
+        ON "activityReportId" = arid
+        AND "goalId" = gid
+      WHERE link_cnt > 1
+      )
+      SELECT
+        id donor_arg,
+        argid target_arg
+      FROM "ActivityReportGoals" arg
+      JOIN latest_updated
+        ON "activityReportId" = arid
+        AND "goalId" = gid
+        AND updated_rank = 1
+      ;
+
+      -- Relink any ActivityReportGoalFieldResponses connected to the
+      -- duplicate (and therefore donor) ARG
+      -- Because there could theoretically be multiple prompts on
+      -- multiple duplicates, we need to rank the ARGFRs referring to
+      -- a particular prompt-goal pair and select just one of each.
+      -- There's one target_arg per goal so we use that as a proxy.
+      -- Just for simplicity, ARGFRs that are already on the target
+      -- ARG are left alone and the corresponding responses on donors
+      -- will be deleted.
+      --
+      -- At time of writing this is all theoretical as there aren't
+      -- any reponses at all for FEI goals with duplicate ARGs, but this
+      -- could change by the time it runs
+      DROP TABLE IF EXISTS relinked_argfrs;
+      CREATE TEMP TABLE relinked_argfrs
+      AS
+      WITH updater AS (
+        WITH argfr_on_donor_args AS (
+          SELECT
+            donor_arg,
+            target_arg,
+            argfr."activityReportGoalId" argid,
+            argfr."goalTemplateFieldPromptId" promptid,
+            argfr.id argfrid,
+            ROW_NUMBER() OVER (
+              PARTITION BY target_arg, argfr."goalTemplateFieldPromptId"
+              ORDER BY argfr."activityReportGoalId" = target_arg, argfr."updatedAt" DESC, argfr.id
+            ) choice_rank
+          FROM arg_merges am
+          JOIN "ActivityReportGoalFieldResponses" argfr
+            ON am.donor_arg = argfr."activityReportGoalId"
+        ), unmatched AS (
+          SELECT
+            donor_arg,
+            argid,
+            argfrid
+          FROM argfr_on_donor_args aoda
+          WHERE choice_rank = 1
+            AND argid != target_arg
+        )
+        UPDATE "ActivityReportGoalFieldResponses" AS argfr
+        SET "activityReportGoalId" = target_arg
+        FROM arg_merges am
+        JOIN unmatched u
+          ON u.donor_arg = am.donor_arg
+        WHERE argfr.id = u.argfrid
+        RETURNING
+          id argfrid,
+          am.donor_arg original_arg
+      ) SELECT * FROM updater
+      ;
+
+      -- Delete duplicate objective ARGFRs
+      DROP TABLE IF EXISTS deleted_argfrs;
+      CREATE TEMP TABLE deleted_argfrs
+      AS
+      WITH updater AS (
+        DELETE FROM "ActivityReportGoalFieldResponses"
+        USING arg_merges
+        WHERE "activityReportGoalId" = donor_arg
+        RETURNING
+          id argfrid,
+          donor_arg
+      ) SELECT * FROM updater
+      ;
+
+      -- Relink any ActivityReportGoalResources connected to the
+      -- duplicate (and therefore donor) ARG
+      -- Because there could theoretically be multiple resources on
+      -- multiple duplicates, we need to rank the ARGRs referring to
+      -- a particular resource-goal pair and select just one of each.
+      -- There's one target_arg per goal so we use that as a proxy.
+      -- Just for simplicity, ARGRs that are already on the target
+      -- ARG are left alone and the corresponding responses on donors
+      -- will be deleted.
+      --
+      -- At time of writing this is all theoretical as there aren't
+      -- any ARGRs at all but this could change by the time it runs
+      CREATE TEMP TABLE relinked_argrs
+      AS
+      WITH updater AS (
+        WITH argr_on_donor_args AS (
+          SELECT
+            donor_arg,
+            target_arg,
+            argr."activityReportGoalId" argid,
+            argr."resourceId" resourceid,
+            argr.id argrid,
+            ROW_NUMBER() OVER (
+              PARTITION BY target_arg, argr."resourceId"
+              ORDER BY argr."activityReportGoalId" = target_arg, argr."updatedAt" DESC, argr.id
+            ) choice_rank
+          FROM arg_merges am
+          JOIN "ActivityReportGoalResources" argr
+            ON am.donor_arg = argr."activityReportGoalId"
+        ), unmatched AS (
+          SELECT
+            donor_arg,
+            argid,
+            argrid
+          FROM argr_on_donor_args aoda
+          WHERE choice_rank = 1
+            AND argid != target_arg
+        )
+        UPDATE "ActivityReportGoalResources" AS argr
+        SET "activityReportGoalId" = target_arg
+        FROM arg_merges am
+        JOIN unmatched u
+          ON u.donor_arg = am.donor_arg
+        WHERE argr.id = u.argrid
+        RETURNING
+          id argrid,
+          am.donor_arg original_arg
+      ) SELECT * FROM updater
+      ;
+      -- Delete duplicate objective ARGRs
+      DROP TABLE IF EXISTS deleted_argrs;
+      CREATE TEMP TABLE deleted_argrs
+      AS
+      WITH updater AS (
+        DELETE FROM "ActivityReportGoalResources"
+        USING arg_merges
+        WHERE "activityReportGoalId" = donor_arg
+        RETURNING
+          id argrid,
+          donor_arg
+      ) SELECT * FROM updater
+      ;
+
+      -- Delete duplicate ARGs
+      DROP TABLE IF EXISTS deleted_args;
+      CREATE TEMP TABLE deleted_args
+      AS
+      WITH updater AS (
+        DELETE FROM "ActivityReportGoals"
+        USING arg_merges
+        WHERE id = donor_arg
+          AND target_arg != donor_arg
+        RETURNING
+          donor_arg
+      ) SELECT * FROM updater
+      ;
+      SELECT
+        1 op_order,
+        'relinked_argfrs' op_name,
+        COUNT(*) record_cnt
+      FROM relinked_argfrs
+      UNION SELECT 2, 'deleted_argfrs', COUNT(*) FROM deleted_argfrs
+      UNION SELECT 3, 'relinked_argrs', COUNT(*) FROM relinked_argrs
+      UNION SELECT 4, 'deleted_argrs', COUNT(*) FROM deleted_argrs
+      UNION SELECT 5, 'deleted_args', COUNT(*) FROM deleted_args
+      ORDER BY 1
+      ;
+
+      $$
+      ;
+      -- Actually call the function
+      SELECT dedupe_args();
+      `, { transaction });
+    });
+  },
+  async down() {
+    // rolling back merges and deletes would be a mess
+  },
+};
---
+++ src/migrations/20240520000001-create-timeseries-and-root-causes.js
@@ -0,0 +1,366 @@
+const {
+  prepMigration,
+} = require('../lib/migration');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+
+      // This starts by creating a stored procedure that can be
+      // used now and in the future to create a time series table with a
+      // name of the form <source tablename>_timeseries, e.g. Goals_timeseries
+      
+      // usage: SELECT create_timeseries_from_audit_log('<tablename>')
+      // example: SELECT create_timeseries_from_audit_log('Goals')
+      // creates Goals_timeseries
+      
+      // Resulting tables are like their source tables except:
+      // * id becomes data_id and is only unique when combined with the timeband
+      // * timeband_start identifies the beginning of the timeband of a particular
+      //   record state
+      // * timeband_end identifies the ending of the timeband were the record has
+      //   that state
+      // * enums are converted to text values because enums change over time and
+      //   thus historical data may not fit current enums
+
+      // Root causes were not saving to ActivityReportGoalFieldResponses
+      // on multi-recipient ARs where a goal already had a root cause
+      // for one recipient. Because ActivityReportGoalFieldResponses
+      // is supposed to have the root cause as it was at report approval
+      // time, which can differ from the current root causes for goals
+      // as recorded in GoalFieldResponses. Calculating what the state of
+      // root causes at the time of AR approval is aided by having a
+      // time series of GoalFieldResponses to join into with the addition of
+      // of a check like:
+      // LEFT JOIN GoalFieldResponses_timeseries gfrt
+      //   ON arg."goalId" = gfrt."goalId"
+      //   AND ar."approvedAt" BETWEEN gfrt.timeband_start AND gfrt.timeband_end
+
+      await queryInterface.sequelize.query(`
+
+      CREATE OR REPLACE FUNCTION create_timeseries_from_audit_log(tablename text)
+      RETURNS VOID LANGUAGE plpgsql AS
+      $$
+      DECLARE
+        qry text := '';
+        wtext text := '';
+        rec record;
+      BEGIN
+
+      -- Get the column list for the main table
+      qry := format('
+      DROP TABLE IF EXISTS clist;
+      CREATE TEMP TABLE clist
+      AS
+      SELECT
+        column_name cname
+        ,ordinal_position cnum
+        ,data_type  ctype
+      FROM information_schema.columns ic
+      WHERE table_schema = %L
+        AND table_name = %L'
+      ,'public'
+      ,tablename);
+
+      EXECUTE qry;
+      qry := '';
+
+      -- Get the pg_typeof column datatypes for the main table
+      -- these are more precise than the information schema types
+      qry := 'DROP TABLE IF EXISTS ctypes;
+      CREATE TEMP TABLE ctypes
+      AS';
+
+      FOR rec IN
+        SELECT * FROM clist ORDER BY cnum
+      LOOP
+        wtext := wtext || format('
+          SELECT cname, cnum, ctype, pg_typeof( %I ) pgtype FROM %I JOIN clist ON %L = cname UNION'
+        ,rec.cname
+        ,tablename
+        ,rec.cname);
+      END LOOP;
+
+      qry := qry || LEFT(wtext,-6) || '
+      ORDER BY cnum';
+      wtext := '';
+
+      EXECUTE qry;
+      qry := '';
+
+      -- set up the beginning and end of time
+      qry := format('DROP TABLE IF EXISTS timeband;
+      CREATE TEMP TABLE timeband
+      AS
+      SELECT
+        %L::timestamp timebegin,
+        NOW() timeend'
+      ,'2020-01-01');
+
+      EXECUTE qry;
+      qry := '';
+      wtext := '';
+
+      -- assemble flat_z, containing the typed columns with changed data
+      -- there will be one record per audit log entry, plus one for the
+      -- current value
+      -- This assumes every table as an id column, which the audit log
+      -- also assumes
+      qry := format('DROP TABLE IF EXISTS flat_z;
+      CREATE TEMP TABLE flat_z
+      AS
+      SELECT
+        id zid
+        ,data_id
+        ,dml_timestamp
+        ,dml_type = %L is_insert
+        ,FALSE is_current_record'
+      ,'INSERT');
+
+      FOR rec IN
+        SELECT * FROM ctypes WHERE cname != 'id' ORDER BY cnum
+      LOOP
+        CASE
+          WHEN rec.ctype = 'USER-DEFINED' THEN -- for enums
+            -- because the enums have changed over time we
+            -- are only casting to text. The other option
+            -- of building a new enum containing all historical
+            -- enums is both more complex and won't make
+            -- using the resulting time series any easier
+            wtext := wtext || format('
+            ,(old_row_data->>%L)::%s AS %I'
+            ,rec.cname
+            ,'text'
+            ,rec.cname);
+          WHEN rec.ctype = 'ARRAY' THEN -- for string arrays
+            wtext := wtext || format('
+            ,(
+              string_to_array(
+                TRIM(
+                  TRIM(
+                    regexp_replace((old_row_data->>%L), %L , %L, %L
+                    ), %L
+                  ),%L
+                ), %L
+              )
+            )::%s AS %I'
+            ,rec.cname
+            ,'", "'
+            ,'X-X-X'
+            ,'g'
+            ,'["'
+            ,'"]'
+            ,'X-X-X'
+            ,rec.pgtype
+            ,rec.cname);
+          ELSE -- for everything else
+            wtext := wtext || format('
+            ,(old_row_data->>%L)::%s AS %I'
+            ,rec.cname
+            ,rec.ctype
+            ,rec.cname);
+        END CASE;
+        -- this detects whether the column was updated to be null
+        wtext := wtext || format('
+        ,(old_row_data->%L) = %L %I'
+        ,rec.cname
+        ,'null'
+        ,rec.cname || '_isnull');
+      END LOOP;
+
+      qry := qry || wtext || format('
+      FROM %I
+      UNION ALL
+      SELECT
+        9223372036854775807 --max bigint so these always sort last
+        ,id
+        ,timeend
+        ,FALSE
+        ,TRUE'
+      , 'ZAL' || tablename);
+      wtext := '';
+
+      FOR rec IN
+        SELECT * FROM ctypes WHERE cname != 'id' ORDER BY cnum
+      LOOP
+        CASE
+          WHEN rec.ctype = 'USER-DEFINED' THEN -- for enums
+            -- this is to match pushing enums to text in
+            -- records pulled from the audit log
+            wtext := wtext || format('
+            ,%I::%s'
+            ,rec.cname
+            ,'text');
+          ELSE
+            wtext := wtext || format('
+            ,%I'
+            ,rec.cname);
+        END CASE;
+        wtext := wtext || format('
+        ,%I IS NULL %I'
+        ,rec.cname
+        ,rec.cname || '_isnull');
+      END LOOP;
+
+      qry := qry || wtext || format('
+      FROM %I
+      CROSS JOIN timeband
+      ORDER BY 2,1'
+      ,tablename);
+      wtext := '';
+
+      EXECUTE qry;
+      qry := '';
+
+      -- create group ids for each column to identify which iteration
+      -- of column value each record should have
+      qry := 'DROP TABLE IF EXISTS group_z;
+      CREATE TEMP TABLE group_z
+      AS
+      SELECT
+        zid
+        ,data_id';
+
+      FOR rec IN
+        SELECT * FROM ctypes WHERE cname != 'id' ORDER BY cnum
+      LOOP
+        wtext := wtext || format('
+        ,SUM(CASE WHEN %I OR %I IS NOT NULL THEN 1 ELSE 0 END) OVER (PARTITION BY data_id ORDER BY zid DESC ROWS UNBOUNDED PRECEDING) AS %I'
+        ,rec.cname || '_isnull'
+        ,rec.cname
+        ,rec.cname || '_group');
+      END LOOP;
+
+      qry := qry || wtext || E'\n' || 'FROM flat_z';
+      wtext := '';
+
+      EXECUTE qry;
+      qry := '';
+
+      -- spread the value from the records with update values throughout their respective groups
+      -- also create the start and end timestamps using adjacent timestamps. Add one millisecond
+      -- to the previous record's timestamp so it's not possible to match both with a BETWEEN.
+      -- This is not implausible if a large number of records are updated at the same time in a
+      -- shared transaction
+      qry := format('DROP TABLE IF EXISTS banded_z;
+      CREATE TEMP TABLE banded_z
+      AS
+      SELECT
+        fz.zid
+        ,fz.data_id
+        ,fz.is_insert
+        ,fz.is_current_record
+        ,(LAG(fz.dml_timestamp) OVER (PARTITION BY fz.data_id ORDER BY fz.zid)) + (1 * interval %L) timeband_start
+        ,fz.dml_timestamp timeband_end'
+        ,'1  ms');
+
+      FOR rec IN
+        SELECT * FROM ctypes WHERE cname != 'id' ORDER BY cnum
+      LOOP
+        wtext := wtext || format('
+        ,FIRST_VALUE(fz.%I) OVER (PARTITION BY fz.data_id, %I ORDER BY fz.zid DESC) AS %I'
+        ,rec.cname
+        ,rec.cname || '_group'
+        ,rec.cname);
+      END LOOP;
+
+      qry := qry || wtext || '
+      FROM flat_z fz
+      JOIN group_z gz
+        ON fz.zid = gz.zid
+        AND fz.data_id = gz.data_id';
+      wtext := '';
+
+      EXECUTE qry;
+      qry := '';
+
+      -- create the actual time series table
+      qry := format('DROP TABLE IF EXISTS %I;
+      CREATE TEMP TABLE %I
+      AS
+      SELECT
+        data_id
+        ,CASE
+          WHEN is_current_record AND timeband_start IS NULL THEN timebegin 
+          ELSE COALESCE(timeband_start, timebegin)
+        END timeband_start
+        ,timeband_end'
+        ,tablename || '_timeseries'
+        ,tablename || '_timeseries');
+
+      FOR rec IN
+        SELECT * FROM ctypes WHERE cname != 'id' ORDER BY cnum
+      LOOP
+        wtext := wtext || format('
+        ,%I'
+        ,rec.cname);
+      END LOOP;
+
+      qry := qry || wtext || '
+      FROM banded_z
+      CROSS JOIN timeband
+      WHERE NOT is_insert';
+      wtext := '';
+
+      EXECUTE qry;
+
+      END
+      $$
+      ;
+      `, { transaction });
+      
+      // Putting this in a separate transaction because we want
+      // create_timeseries_from_audit_log() created regardless
+      await queryInterface.sequelize.query(/* sql */`
+
+      -- Create GoalFieldResponses_timeseries
+      SELECT create_timeseries_from_audit_log('GoalFieldResponses');
+
+      -- Pull the data necessary to create an ARGFR from the historical
+      -- state of the associated GFR
+      DROP TABLE IF EXISTS argfrs_to_insert;
+      CREATE TEMP TABLE argfrs_to_insert
+      AS
+      SELECT
+        arg.id argid,
+        gfrt."goalId" gid,
+        gfrt.data_id gfrid,
+        gfrt."goalTemplateFieldPromptId",
+        gfrt.response
+      FROM "ActivityReports" ar
+      JOIN "ActivityReportGoals" arg
+        ON ar.id = arg."activityReportId"
+      JOIN "GoalFieldResponses_timeseries" gfrt
+        ON arg."goalId" = gfrt."goalId"
+        AND ar."approvedAt" BETWEEN timeband_start AND timeband_end
+      LEFT JOIN "ActivityReportGoalFieldResponses" argfr
+        ON arg.id = argfr."activityReportGoalId"
+      WHERE argfr.id IS NULL
+      ;
+      
+      -- Insert the records
+      INSERT INTO "ActivityReportGoalFieldResponses" (
+        "activityReportGoalId",
+        "goalTemplateFieldPromptId",
+        response,
+        "createdAt",
+        "updatedAt"
+      )
+      SELECT
+        argid,
+        "goalTemplateFieldPromptId",
+        response,
+        NOW(),
+        NOW()
+      FROM argfrs_to_insert
+      ;
+      `, { transaction });
+    });
+  },
+  async down() {
+    // rolling back merges and deletes would be a mess
+  },
+};
