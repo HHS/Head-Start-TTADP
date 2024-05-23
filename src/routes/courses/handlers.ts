3629770fa5a6c6b4fbec704006dbe304949dc06c
Adam Levin <adam.levin@adhocteam.us>
2024-05-23 15:06:14 -0400

    put some stuff back

--- src/routes/courses/handlers.ts
+++ src/routes/courses/handlers.ts
@@ -1,18 +1,16 @@
 /* eslint-disable import/prefer-default-export */
 import { Request, Response } from 'express';
-// import filtersToScopes from '../../scopes';
+import filtersToScopes from '../../scopes';
 import handleErrors from '../../lib/apiErrorHandler';
-// import { setReadRegions } from '../../services/accessValidation';
+import { setReadRegions } from '../../services/accessValidation';
 import {
   getAllCourses,
 } from '../../services/course';
-/*
 import { currentUserId } from '../../services/currentUser';
 import getCachedResponse from '../../lib/cache';
 import { getCourseUrlWidgetData } from '../../services/dashboards/course';
-*/
 
-// const RESOURCE_DATA_CACHE_VERSION = 1.5;
+const COURSE_DATA_CACHE_VERSION = 1.5;
 
 const namespace = 'HANDLERS:COURSES';
 const logContext = {
@@ -29,11 +27,11 @@ export async function allCourses(req: Request, res: Response) {
     await handleErrors(err, req, res, logContext);
   }
 }
-/*
+
 export async function getCourseUrlWidgetDataWithCache(req, res) {
   const userId = await currentUserId(req, res);
   const query = await setReadRegions(req.query, userId);
-  const key = `getCourseWidgetUrlData?v=${RESOURCE_DATA_CACHE_VERSION}&${JSON.stringify(query)}`;
+  const key = `getCourseWidgetUrlData?v=${COURSE_DATA_CACHE_VERSION}&${JSON.stringify(query)}`;
 
   const response = await getCachedResponse(
     key,
@@ -47,4 +45,3 @@ export async function getCourseUrlWidgetDataWithCache(req, res) {
 
   res.json(response);
 }
-*/
