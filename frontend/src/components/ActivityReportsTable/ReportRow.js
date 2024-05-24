dea101982fa9085476fa63ad73d26dd7940b54fc
Matt Bevilacqua <thewatermethod@gmail.com>
2024-05-24 16:23:23 -0400

    Add eslint ignore to two files (pending further investigation)

--- frontend/src/components/ActivityReportsTable/ReportRow.js
+++ frontend/src/components/ActivityReportsTable/ReportRow.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/control-has-associated-label */
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import { Checkbox } from '@trussworks/react-uswds';
