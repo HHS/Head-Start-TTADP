2bf169f4600a1c8127aa163a032e8db47caa598e
GarrettEHill <garretthill@gmail.com>
2024-05-29 12:37:25 -0700

    Create 20240529000000-change-import-schedule.js

---
+++ src/migrations/20240529000000-change-import-schedule.js
@@ -0,0 +1,30 @@
+const { prepMigration } = require('../lib/migration');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+      await queryInterface.sequelize.query(/* sql */`
+      UPDATE "Imports"
+      SET schedule = '30 8 * * *'
+      WHERE name = 'ITAMS Monitoring Data'
+      AND schedule = '0 7 * * *';
+      `, transaction );
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+      await queryInterface.sequelize.query(/* sql */`
+      UPDATE "Imports"
+      SET schedule = '0 7 * * *'
+      WHERE name = 'ITAMS Monitoring Data'
+      AND schedule = '30 8 * * *';
+      `, transaction );
+    });
+  },
+};
