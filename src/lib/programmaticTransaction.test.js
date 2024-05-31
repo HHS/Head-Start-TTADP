32baed0e9be7fb1f062fb01978efbbe5523e4476
GarrettEHill <garretthill@gmail.com>
2024-05-31 11:26:51 -0700

    tests

---
+++ src/lib/programmaticTransaction.test.js
@@ -0,0 +1,51 @@
+import {
+  captureSnapshot,
+  rollbackToSnapshot,
+} from 'programmaticTransaction';
+
+import {
+  Grant,
+  Topic,
+  sequelize,
+} from '../models';
+
+describe('Programmatic Transaction', () => {
+  it('Insert', async () => {
+    const snapshot = await captureSnapshot();
+    await Topic.create({
+      name: 'Test Topic',
+    });
+    let topic = await Topic.findOne({ where: { name: 'Test Topic' } });
+    expect(topic).not.toBeNull();
+    await rollbackToSnapshot(snapshot);
+    topic = await Topic.findOne({ where: { name: 'Test Topic' } });
+    expect(topic).toBeNull();
+  });
+  it('Update', async () => {
+    const snapshot = await captureSnapshot();
+    let grant = await Grant.findOne({
+      where: { status: 'Active' },
+      order: [['id', 'ASC']],
+      limit: 1
+    });
+    await grant.update({ status: 'Inactive' });
+    await grant.reload();
+    expect(grant.status).toBe('Inactive');
+    await rollbackToSnapshot(snapshot);
+    await grant.reload();
+    expect(grant.status).toBe('Active');
+  });
+  it('Delete', async () => {
+    await Topic.create({
+      name: 'Test Topic',
+    });
+    const snapshot = await captureSnapshot();
+    await Topic.destroy({ where: { name: 'Test Topic' }, force: true });
+    let topic = Topic.findOne({ where: { name: 'Test Topic' } });
+    expect(topic).toBeNull();
+    await rollbackToSnapshot(snapshot);
+    topic = Topic.findOne({ where: { name: 'Test Topic' } });
+    expect(topic).not.toBeNull();
+    await Topic.destroy({ where: { name: 'Test Topic' }, force: true });
+  });
+});
