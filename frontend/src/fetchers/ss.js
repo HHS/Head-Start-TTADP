444c6306954fdff8274cb6cec6479f5208f23b85
Krys Wisnaskas <krystyna@adhocteam.us>
2024-06-04 10:14:30 -0400

    More errors

--- frontend/src/fetchers/ss.js
+++ frontend/src/fetchers/ss.js
@@ -6,9 +6,8 @@ const sheetsUrl = join('/', 'api', 'admin', 'ss');
 const activeSheetUrl = join('/', 'api', 'admin', 'ss', 'sheet');
 
 export const getSheets = async () => {
-  console.log('getting sheets');
   const response = await get(sheetsUrl);
-  console.log(response);
+
   if (!response.ok) {
     throw new Error('Error fetching sheets');
   }
@@ -17,7 +16,6 @@ export const getSheets = async () => {
 
 export const getSheetById = async (sheetId) => {
   const response = await get(join(activeSheetUrl, sheetId));
-  console.log(response);
 
   if (!response.ok) {
     throw new Error(`Error fetching sheet with ID ${sheetId}: ${response.status}`);
