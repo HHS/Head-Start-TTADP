dfe0efada2be07854b124d8119968b0280073256
Adam Levin <adam.levin@adhocteam.us>
2024-06-10 12:21:49 -0400

    clean topics and resources others were empty

--- src/migrations/20240610110823-remove-unwanted-objectives.js
+++ src/migrations/20240610110823-remove-unwanted-objectives.js
@@ -7,6 +7,37 @@ module.exports = {
     async (transaction) => {
       await prepMigration(queryInterface, transaction, __filename);
       await queryInterface.sequelize.query(/* sql */`
+
+      -- Delete from ARO Topics.
+      DELETE FROM "ActivityReportObjectiveTopics" where "activityReportObjectiveId" IN (
+        SELECT id FROM "ActivityReportObjectives" WHERE "objectiveId" IN (
+            179385,
+            179427,
+            179494,
+            179500,
+            179550,
+            179606,
+            179635,
+            179556,
+            180440,
+            177557) AND "status" = 'Not Started'
+        );
+
+     -- Delete from ARO Resources.
+        DELETE FROM "ActivityReportObjectiveResources" where "activityReportObjectiveId" IN (
+            SELECT id FROM "ActivityReportObjectives" WHERE "objectiveId" IN (
+                179385,
+                179427,
+                179494,
+                179500,
+                179550,
+                179606,
+                179635,
+                179556,
+                180440,
+                177557) AND "status" = 'Not Started'
+            );
+
       -- Delete ARO's.
       DELETE FROM "ActivityReportObjectives" WHERE "objectiveId" IN (
         SELECT id FROM "Objectives" WHERE id IN (
