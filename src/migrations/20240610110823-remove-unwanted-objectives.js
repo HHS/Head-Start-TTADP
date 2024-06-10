82305473c06cc99e665a7205d454a547934f516f
Adam Levin <adam.levin@adhocteam.us>
2024-06-10 15:35:27 -0400

    updates per Garrett

--- src/migrations/20240610110823-remove-unwanted-objectives.js
+++ src/migrations/20240610110823-remove-unwanted-objectives.js
@@ -3,68 +3,120 @@ const {
 } = require('../lib/migration');
 
 module.exports = {
-  up: async (queryInterface, Sequelize) => queryInterface.sequelize.transaction(
+  up: async (queryInterface) => queryInterface.sequelize.transaction(
     async (transaction) => {
       await prepMigration(queryInterface, transaction, __filename);
       await queryInterface.sequelize.query(/* sql */`
 
+      -- Get all unwanted objectives for this Goal.
+      DROP TABLE IF EXISTS ObjectivesToRemove;
+      CREATE TEMP TABLE ObjectivesToRemove
+            AS
+      SELECT
+      o."id"
+      FROM "Objectives" o
+      LEFT JOIN "ActivityReportObjectives" aro
+        ON o.id = aro."objectiveId"
+      LEFT JOIN "ActivityReports" ar
+        ON aro."activityReportId" = ar.id
+      WHERE o."goalId" = 66089
+        AND o."status" = 'Not Started'
+        AND COALESCE(ar."calculatedStatus",'draft') = 'draft';
+
       -- Delete from ARO Topics.
-      DELETE FROM "ActivityReportObjectiveTopics" where "activityReportObjectiveId" IN (
+      DROP TABLE IF EXISTS DeleteTopics;
+      CREATE TEMP TABLE DeleteTopics
+            AS
+      WITH updatetopics AS (
+      DELETE FROM "ActivityReportObjectiveTopics" WHERE "activityReportObjectiveId" IN (
         SELECT id FROM "ActivityReportObjectives" WHERE "objectiveId" IN (
-            179385,
-            179427,
-            179494,
-            179500,
-            179550,
-            179606,
-            179635,
-            179556,
-            180440,
-            177557) AND "status" = 'Not Started'
-        );
+          SELECT id FROM ObjectivesToRemove
+        )
+      )
+      RETURNING
+      id
+      ) SELECT * FROM updatetopics;
+
+
 
      -- Delete from ARO Resources.
-        DELETE FROM "ActivityReportObjectiveResources" where "activityReportObjectiveId" IN (
-            SELECT id FROM "ActivityReportObjectives" WHERE "objectiveId" IN (
-                179385,
-                179427,
-                179494,
-                179500,
-                179550,
-                179606,
-                179635,
-                179556,
-                180440,
-                177557) AND "status" = 'Not Started'
-            );
+     DROP TABLE IF EXISTS DeleteResources;
+      CREATE TEMP TABLE DeleteResources
+            AS
+      WITH updateresources AS (
+      DELETE FROM "ActivityReportObjectiveResources" WHERE "activityReportObjectiveId" IN (
+        SELECT id FROM "ActivityReportObjectives" WHERE "objectiveId" IN (
+          SELECT id FROM ObjectivesToRemove
+        )
+      )
+      RETURNING
+      id
+      ) SELECT * FROM updateresources;
+
+      -- Delete from ARO Files.
+      DROP TABLE IF EXISTS DeleteFiles;
+      CREATE TEMP TABLE DeleteFiles
+            AS
+      WITH updatedfiles AS (
+      DELETE FROM "ActivityReportObjectiveFiles" WHERE "activityReportObjectiveId" IN (
+        SELECT id FROM "ActivityReportObjectives" WHERE "objectiveId" IN (
+          SELECT id FROM ObjectivesToRemove
+        )
+      )
+      RETURNING
+      id
+      ) SELECT * FROM updatedfiles;
+
+      -- Delete from AR Courses.
+      DROP TABLE IF EXISTS DeleteCourses;
+      CREATE TEMP TABLE DeleteCourses
+            AS
+      WITH updatedcourses AS (
+      DELETE FROM "ActivityReportObjectiveCourses" WHERE "activityReportObjectiveId" IN (
+        SELECT id FROM "ActivityReportObjectives" WHERE "objectiveId" IN (
+          SELECT id FROM ObjectivesToRemove
+        )
+      )
+      RETURNING
+      id
+      ) SELECT * FROM updatedcourses;
 
       -- Delete ARO's.
+      DROP TABLE IF EXISTS DeleteAROs;
+      CREATE TEMP TABLE DeleteAROs
+            AS
+      WITH updatearos AS (
       DELETE FROM "ActivityReportObjectives" WHERE "objectiveId" IN (
-        SELECT id FROM "Objectives" WHERE id IN (
-            179385,
-            179427,
-            179494,
-            179500,
-            179550,
-            179606,
-            179635,
-            179556,
-            180440,
-            177557) AND "status" = 'Not Started'
-      );
+        SELECT id FROM ObjectivesToRemove
+      )
+      RETURNING
+      id
+      ) SELECT * FROM updatearos;
 
-        -- Delete objectives.
-        DELETE FROM "Objectives" WHERE id IN (
-          179385,
-          179427,
-          179494,
-          179500,
-          179550,
-          179606,
-          179635,
-          179556,
-          180440,
-          177557) AND "status" = 'Not Started';
+      -- Delete objectives.
+      DROP TABLE IF EXISTS DeleteObjectives;
+      CREATE TEMP TABLE DeleteObjectives
+            AS
+      WITH updateobjectives AS (
+      DELETE FROM "Objectives" WHERE id IN (
+         SELECT id FROM ObjectivesToRemove
+      )
+      RETURNING
+      id
+      ) SELECT * FROM updateobjectives;
+
+      -- Get Delete counts using union.
+      SELECT COUNT(*), 'ARO Topics' FROM DeleteTopics
+      UNION ALL
+      SELECT COUNT(*), 'ARO Resources' FROM DeleteResources
+      UNION ALL
+      SELECT COUNT(*), 'ARO Files' FROM DeleteFiles
+      UNION ALL
+      SELECT COUNT(*), 'ARO Courses' FROM DeleteCourses
+      UNION ALL
+      SELECT COUNT(*), 'AROs' FROM DeleteAROs
+      UNION ALL
+      SELECT COUNT(*), 'Objectives' FROM DeleteObjectives;
         `, { transaction });
     },
   ),
