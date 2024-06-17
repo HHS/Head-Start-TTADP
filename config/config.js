b863e05d9abe05b98e50f1b83d9b345e3a468b2a
GarrettEHill <garretthill@gmail.com>
2024-06-17 14:46:09 -0700

    Update config.js

--- config/config.js
+++ config/config.js
@@ -4,6 +4,29 @@ const singleLineLogger = (
   queryString,
 ) => console.log(queryString.replace(/\n/g, '\\n')); // eslint-disable-line no-console
 
+const connectionValidation = async (connection) => {
+  try {
+    await connection.authenticate();
+    // eslint-disable-next-line no-console
+    console.info('Connection validated successfully');
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error('Connection validation failed:', {
+      message: error.message,
+      stack: error.stack,
+      original: error.original,
+      name: error.name,
+      code: error.code,
+      detail: error.detail,
+      hint: error.hint,
+      position: error.position,
+      internalQuery: error.internalQuery,
+      severity: error.severity,
+    });
+    throw error;
+  }
+};
+
 module.exports = {
   development: {
     username: process.env.POSTGRES_USERNAME,
@@ -15,6 +38,9 @@ module.exports = {
     logging: singleLineLogger,
     logQueryParameters: true,
     minifyAliases: true,
+    pool: {
+      validate: connectionValidation,
+    },
   },
   test: {
     username: process.env.POSTGRES_USERNAME,
@@ -25,6 +51,9 @@ module.exports = {
     dialect: 'postgres',
     logging: false,
     minifyAliases: true,
+    pool: {
+      validate: connectionValidation,
+    },
   },
   dss: {
     use_env_variable: 'DATABASE_URL',
@@ -36,6 +65,9 @@ module.exports = {
     dialect: 'postgres',
     logging: singleLineLogger,
     minifyAliases: true,
+    pool: {
+      validate: connectionValidation,
+    },
   },
   production: {
     use_env_variable: 'DATABASE_URL',
@@ -49,6 +81,9 @@ module.exports = {
     minifyAliases: true,
     dialectOptions: {
       ssl: true,
+    },
+    pool: {
+      validate: connectionValidation,
     },
   },
 };
