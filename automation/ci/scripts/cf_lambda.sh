115b722f360e539d6dde4c06e60034335c43dc36
GarrettEHill <garretthill@gmail.com>
2024-07-29 14:56:37 -0700

    more time before timeout

--- automation/ci/scripts/cf_lambda.sh
+++ automation/ci/scripts/cf_lambda.sh
@@ -426,7 +426,7 @@ function run_task {
 function monitor_task {
     local app_name=$1
     local task_name=$2
-    local timeout=${3:-200}  # Default timeout in seconds
+    local timeout=${3:-300}  # Default timeout in seconds
     validate_parameters "$app_name"
     validate_parameters "$task_name"
     local start_time
