21b30bfcccfaf89c826b817c21c8ba21e6c3e585
GarrettEHill <garrett.hill@adhocteam.us>
2024-08-16 13:09:32 -0700

    Update src/migrations/20240815000000-remove-dup-aros-via-uploadHandler.js

--- src/migrations/20240815000000-remove-dup-aros-via-uploadHandler.js
+++ src/migrations/20240815000000-remove-dup-aros-via-uploadHandler.js
@@ -397,7 +397,6 @@ module.exports = {
 
         -- Insert non-matching records into ActivityReportObjectives
         -- There should never be an INSERT performed here, but the code is present to complete the structure UID structure
-        -- There should never be an INSERT performed here, but the code is present to complete the structure UID structure
         CREATE TEMP TABLE temp_inserted_objectives AS
         WITH inserted_rows AS (
             INSERT INTO "ActivityReportObjectives" (
