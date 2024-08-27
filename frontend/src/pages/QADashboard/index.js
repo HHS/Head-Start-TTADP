867a45d75c3978da067252b66563f5c28e63ad0f
Adam Levin <adam.levin@adhocteam.us>
2024-08-27 14:08:20 -0400

    adding overview to qa dashbaord

--- frontend/src/pages/QADashboard/index.js
+++ frontend/src/pages/QADashboard/index.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
+import { Grid, Alert } from '@trussworks/react-uswds';
+import QAOverview from '../../widgets/QualityAssuranceDashboardOverview';
 
 export default function QADashboard() {
+  const [error] = useState(null);
+
   return (
     <>
       <Helmet>
@@ -11,6 +15,26 @@ export default function QADashboard() {
         <h1 className="landing margin-top-0 margin-bottom-3">
           Quality assurance dashboard
         </h1>
+        <Grid row>
+          {error && (
+          <Alert className="margin-bottom-2" type="error" role="alert">
+            {error}
+          </Alert>
+          )}
+        </Grid>
+        <QAOverview
+          data={{
+            recipientsWithNoTTA: { pct: '2.52%', filterApplicable: true },
+            recipientsWithOhsStandardFeiGoals: { pct: '73.25%', filterApplicable: false },
+            recipientsWithOhsStandardClass: { pct: '14.26%', filterApplicable: false },
+          }}
+          loading={false}
+          fields={[
+            'Recipients with no TTA',
+            'Recipients with OHS standard FEI goal',
+            'Recipients with OHS standard CLASS goal',
+          ]}
+        />
       </div>
     </>
   );
---
+++ frontend/src/widgets/__tests__/QualityAssuranceDashbaordOverview.js
@@ -0,0 +1,54 @@
+/* eslint-disable jest/no-disabled-tests */
+import '@testing-library/jest-dom';
+import React from 'react';
+import { Router } from 'react-router-dom';
+import { createMemoryHistory } from 'history';
+import { render, screen } from '@testing-library/react';
+import { QualityAssuranceDashboardOverview } from '../QualityAssuranceDashboardOverview';
+
+const renderQualityAssuranceDashboardOverview = (props) => {
+  const history = createMemoryHistory();
+
+  render(
+    <Router history={history}>
+      <QualityAssuranceDashboardOverview loading={props.loading} data={props.data} />
+    </Router>,
+  );
+};
+
+describe('Quality Assurance Dashboard Overview Widget', () => {
+  it('handles undefined data', async () => {
+    renderQualityAssuranceDashboardOverview({ data: undefined });
+    expect(screen.getByText(/Recipients with no TTA/i)).toBeInTheDocument();
+    expect(screen.getByText(/Recipients with OHS standard FEI goal/i)).toBeInTheDocument();
+    expect(screen.getByText(/Recipients with OHS standard CLASS goal/i)).toBeInTheDocument();
+  });
+
+  it('shows the correct data', async () => {
+    const data = {
+      recipientsWithNoTTA: {
+        pct: '11%',
+        filterApplicable: true,
+      },
+      recipientsWithOhsStandardFeiGoals: {
+        pct: '22%',
+        filterApplicable: true,
+      },
+      recipientsWithOhsStandardClass: {
+        pct: '33.5%',
+        filterApplicable: false,
+      },
+    };
+
+    renderQualityAssuranceDashboardOverview({ data });
+
+    expect(screen.getByText(/Recipients with no TTA/i)).toBeInTheDocument();
+    expect(screen.getByText(/Recipients with OHS standard FEI goal/i)).toBeInTheDocument();
+    expect(screen.getByText(/Recipients with OHS standard CLASS goal/i)).toBeInTheDocument();
+
+    expect(await screen.findByText(/11%/)).toBeVisible();
+    expect(await screen.findByText(/22%/)).toBeVisible();
+    expect(await screen.findByText(/33.5%/)).toBeVisible();
+    expect(await screen.findByText(/One or more of the selected filters cannot be applied to this data./)).toBeVisible();
+  });
+});
