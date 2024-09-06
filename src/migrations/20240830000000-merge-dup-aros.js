ff251b66fad12e39fe682ab566fe383c52ef8ead
Nathan Powell <nathan.powell@adhocteam.us>
2024-09-06 12:05:00 -0700

    add unique constraint on ActivityReportObjectives

--- src/migrations/20240830000000-merge-dup-aros.js
+++ src/migrations/20240830000000-merge-dup-aros.js
@@ -571,12 +571,21 @@ module.exports = {
         DROP TABLE IF EXISTS temp_inserted_objectives;
         DROP TABLE IF EXISTS temp_deleted_objectives;
       `, { transaction });
+
+      await queryInterface.sequelize.query(`
+        DROP INDEX IF EXISTS "activity_report_objectives_activity_report_id_objective_id";
+        CREATE UNIQUE INDEX  "activity_report_objectives_activity_report_id_objective_id_unique" ON "ActivityReportObjectives" ("activityReportId","objectiveId");
+    `, { transaction });
     },
   ),
 
   down: async (queryInterface) => queryInterface.sequelize.transaction(
     async (transaction) => {
       await prepMigration(queryInterface, transaction, __filename);
+      await queryInterface.sequelize.query(`
+        DROP INDEX IF EXISTS "activity_report_objectives_activity_report_id_objective_id_unique";
+        CREATE INDEX "activity_report_objectives_activity_report_id_objective_id" ON "ActivityReportObjectives" ("activityReportId","objectiveId");
+    `, { transaction });
     },
   ),
 };
