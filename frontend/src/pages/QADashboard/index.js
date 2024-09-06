0d59c65c21abf3867faec767e32e8518d449711b
Adam Levin <adam.levin@adhocteam.us>
2024-09-06 14:20:23 -0400

    remove error for coverage

--- frontend/src/pages/QADashboard/index.js
+++ frontend/src/pages/QADashboard/index.js
@@ -1,10 +1,8 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
-import { Grid, Alert } from '@trussworks/react-uswds';
 import QAOverview from '../../widgets/QualityAssuranceDashboardOverview';
 
 export default function QADashboard() {
-  const [error] = useState(null);
   const [isLoading] = useState(false);
 
   return (
@@ -16,13 +14,6 @@ export default function QADashboard() {
         <h1 className="landing margin-top-0 margin-bottom-3">
           Quality assurance dashboard
         </h1>
-        <Grid row>
-          {error && (
-          <Alert className="margin-bottom-2" type="error" role="alert">
-            {error}
-          </Alert>
-          )}
-        </Grid>
         <QAOverview
           data={{
             recipientsWithNoTTA: { pct: '2.52%', filterApplicable: true },
