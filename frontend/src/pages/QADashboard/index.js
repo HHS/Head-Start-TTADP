1c8ba989bc7243ac6764ef72e2ddd726060064df
Matt Bevilacqua <thewatermethod@gmail.com>
2024-09-16 09:13:47 -0400

    Initial commit

--- frontend/src/pages/QADashboard/index.js
+++ frontend/src/pages/QADashboard/index.js
@@ -1,6 +1,25 @@
 import React, { useState } from 'react';
 import { Helmet } from 'react-helmet';
+import {
+  Grid,
+} from '@trussworks/react-uswds';
 import QAOverview from '../../widgets/QualityAssuranceDashboardOverview';
+import DeliveryMethod from '../../widgets/DeliveryMethodGraph';
+
+const DATA = [
+  {
+    name: 'Recipient Rec TTA', x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'], y: [1, 2, 3, 4, 5, 6], month: [false, false, false, false, false, false],
+  },
+  {
+    name: 'Hours of Training', x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'], y: [7, 8, 9, 0, 0, 0], month: [false, false, false, false, false, false],
+  },
+  {
+    name: 'Hours of Technical Assistance', x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'], y: [0, 0, 0, 10, 11.2348732847, 12], month: [false, false, false, false, false, false],
+  },
+  {
+    name: 'Hours of Both', x: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'], y: [0, 13, 0, 14, 0, 0], month: [false, false, false, false, false, false],
+  },
+];
 
 export default function QADashboard() {
   const [isLoading] = useState(false);
@@ -22,6 +41,15 @@ export default function QADashboard() {
           }}
           loading={isLoading}
         />
+
+        <div>
+          <Grid row>
+            <DeliveryMethod
+              data={DATA}
+              loading={false}
+            />
+          </Grid>
+        </div>
       </div>
     </>
   );
