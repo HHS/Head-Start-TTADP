66ad14c3d8fa018699cac49d722ec46a0217fc42
GarrettEHill <garretthill@gmail.com>
2024-10-04 14:37:29 -0700

    more cleanup

--- src/migrations/20241002000000-clean-training-report-data.js
+++ src/migrations/20241002000000-clean-training-report-data.js
@@ -7,8 +7,30 @@ module.exports = {
       const sessionSig = __filename;
       await prepMigration(queryInterface, transaction, sessionSig);
       return queryInterface.sequelize.query(`
-         UPDATE "SessionReportPilots" SET data = data - 'event';
-         UPDATE "EventReportPilots" SET data = data - 'sessions';
+        UPDATE "SessionReportPilots" SET data = data - 'event';
+        UPDATE "EventReportPilots" SET data = data - 'sessions';
+        UPDATE "SessionReportPilots"
+        SET data = COALESCE(
+          jsonb_set(
+                data,
+                '{recipients}',
+                (
+                    SELECT jsonb_agg(
+                        (
+                            elem
+                            || jsonb_build_object(
+                                'label', COALESCE(elem->>'label', elem->>'name'),
+                                'value', COALESCE(elem->>'value', elem->>'activityRecipientId')::INT
+                            )
+                        ) - 'name' - 'activityRecipientId'
+                    )
+                    FROM jsonb_array_elements(data->'recipients') AS t(elem)
+                ),
+                false
+            ),
+          data
+        )
+        WHERE data ? 'recipients';
       `);
     });
   },
