5b46c0f85c3aaa4615d9ad2309ff771aa6155406
Matt Bevilacqua <thewatermethod@gmail.com>
2024-10-23 15:11:25 -0400

    Add test for newgoal form component

--- frontend/src/components/NewGoal/__tests__/index.js
+++ frontend/src/components/NewGoal/__tests__/index.js
@@ -1,29 +1,25 @@
 import '@testing-library/jest-dom';
 import React from 'react';
-// import moment from 'moment';
 import {
   render,
-  //   screen,
-  //   within,
-  //   waitFor,
-  act,
+  screen,
+  act,  
 } from '@testing-library/react';
 import { SCOPE_IDS } from '@ttahub/common';
-// import selectEvent from 'react-select-event';
-// import fetchMock from 'fetch-mock';
+import selectEvent from 'react-select-event';
 import { Router } from 'react-router';
 import { createMemoryHistory } from 'history';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import UserContext from '../../../UserContext';
 import NewGoal from '..';
 import AppLoadingContext from '../../../AppLoadingContext';
-// import useNewGoalAction from '../../../hooks/useNewGoalAction';
-
-jest.mock('../../../hooks/useNewGoalAction');
 
 describe('NewGoalForm', () => {
   const defaultUser = {
-    permissions: [{ regionId: 1, scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS }],
+    permissions: [{
+      regionId: 1,
+      scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+    }],
   };
 
   const defaultRecipient = {
@@ -38,32 +34,72 @@ describe('NewGoalForm', () => {
   const renderNewGoalForm = (
     user = defaultUser,
     recipient = defaultRecipient,
-  ) => {
-    render(
-      <Router history={history}>
-        <AppLoadingContext.Provider value={{
-          setIsAppLoading: jest.fn(),
+  ) => render(
+    <Router history={history}>
+      <AppLoadingContext.Provider value={{
+        setIsAppLoading: jest.fn(),
+      }}
+      >
+        <UserContext.Provider value={{
+          user,
         }}
         >
-          <UserContext.Provider value={{
-            user,
-          }}
-          >
-            <NewGoal
-              recipient={recipient}
-              regionId="1"
-            />
-          </UserContext.Provider>
-        </AppLoadingContext.Provider>
-      </Router>,
-    );
-  };
+          <NewGoal
+            recipient={recipient}
+            regionId="1"
+          />
+        </UserContext.Provider>
+      </AppLoadingContext.Provider>
+    </Router>,
+  );
 
   it('renders without error', async () => {
     act(() => {
       renderNewGoalForm();
     });
 
-    expect(true).toBe(true);
+    const grant = screen.queryByTestId('read-only-value');
+
+    expect(grant.textContent).toBe('123 EHS');
+  });
+
+  it('allows grant selection and submission', async () => {
+    const recipient = {
+      id: 1,
+      grants: [
+        { id: 1, status: 'Active', numberWithProgramTypes: '123 EHS' },
+        { id: 2, status: 'Active', numberWithProgramTypes: '456 EHS' },
+      ],
+    };
+
+    act(() => {
+      renderNewGoalForm(defaultUser, recipient);
+    });
+
+    await act(async () => {
+      await selectEvent.select(screen.getByLabelText(/Recipient grant numbers/), '456 EHS');
+    });
+
+    const submitButton = screen.getByRole('button', { name: 'Save and continue' });
+
+    expect(submitButton).toBeInTheDocument();
+
+    act(() => {
+      userEvent.click(submitButton);
+    });
+  });
+
+  it('redirects if a user does not have the proper permissions', async () => {
+    const regionTwoUser = {
+      permissions: [{
+        regionId: 2,
+        scopeId: SCOPE_IDS.READ_WRITE_ACTIVITY_REPORTS,
+      }],
+    };
+    act(() => {
+      renderNewGoalForm(regionTwoUser);
+    });
+
+    expect(history.location.pathname).toBe('/something-went-wrong/401');
   });
 });
