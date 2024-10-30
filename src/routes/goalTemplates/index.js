420e9b606c8a0431d913e1520aeab55215c963ed
Matt Bevilacqua <thewatermethod@gmail.com>
2024-10-30 11:27:17 -0400

    Add route, handler, test

--- src/routes/goalTemplates/index.js
+++ src/routes/goalTemplates/index.js
@@ -1,11 +1,17 @@
 import express from 'express';
 import transactionWrapper from '../transactionWrapper';
 import authMiddleware from '../../middleware/authMiddleware';
-import { getGoalTemplates, getPrompts, getOptionsByPromptName } from './handlers';
+import {
+  getGoalTemplates,
+  getPrompts,
+  getOptionsByPromptName,
+  getSource,
+} from './handlers';
 import { checkGoalTemplateIdParam } from '../../middleware/checkIdParamMiddleware';
 
 const router = express.Router();
 router.get('/', authMiddleware, transactionWrapper(getGoalTemplates));
 router.get('/:goalTemplateId/prompts/', authMiddleware, checkGoalTemplateIdParam, transactionWrapper(getPrompts));
+router.get('/:goalTemplateId/source/', authMiddleware, checkGoalTemplateIdParam, transactionWrapper(getSource));
 router.get('/options', transactionWrapper(getOptionsByPromptName));
 export default router;
