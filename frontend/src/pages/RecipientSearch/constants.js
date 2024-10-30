43755628c9d81509064d855670faa1dcbaef0593
nvms <jon@pye.rs>
2024-10-30 09:36:54 -0400

    add grants goalResponse alongside goals goalResponse

--- frontend/src/pages/RecipientSearch/constants.js
+++ frontend/src/pages/RecipientSearch/constants.js
@@ -1,6 +1,6 @@
 /* eslint-disable import/prefer-default-export */
 import { regionFilter } from '../../components/filter/activityReportFilters';
-import { goalNameFilter } from '../../components/filter/goalFilters';
+import { feiRootCauseFilter, goalNameFilter } from '../../components/filter/goalFilters';
 import { groupsFilter, recipientsWithoutTTA, grantStatusFilter } from '../../components/filter/grantFilters';
 
 const RECIPIENT_SEARCH_FILTER_CONFIG = [
@@ -9,6 +9,7 @@ const RECIPIENT_SEARCH_FILTER_CONFIG = [
   regionFilter,
   recipientsWithoutTTA,
   grantStatusFilter,
+  feiRootCauseFilter,
 ];
 
 // sort by display prop
