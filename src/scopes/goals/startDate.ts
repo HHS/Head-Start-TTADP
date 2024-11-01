4d8592e69ecc59d4c9ae0853f3b5d1f87776cf0e
nvms <jon@pye.rs>
2024-11-01 12:18:58 -0400

    update filter

--- src/scopes/goals/startDate.ts
+++ src/scopes/goals/startDate.ts
@@ -1,25 +1,43 @@
-import { filterAssociation } from './utils';
+import { Op, WhereOptions } from 'sequelize';
+import { sequelize } from '../../models';
 
-const goalIds = `
-  SELECT DISTINCT
-    "ActivityReportGoals"."goalId"
-  FROM "ActivityReportGoals"
-  INNER JOIN "ActivityReports"
-  ON "ActivityReportGoals"."activityReportId" = "ActivityReports"."id"
-  WHERE "ActivityReports"."startDate"`;
+function getDateSql(dates: string[], operator: string) {
+  const dateClause = (operator === 'BETWEEN')
+    ? `${dates[0]} AND ${dates[1]}`
+    : dates[0];
 
-export function beforeStartDate(date) {
-  return filterAssociation(goalIds, date, false, '<=');
+  return sequelize.literal(`(
+    SELECT DISTINCT "goalId"
+    FROM "ActivityReportGoals"
+    INNER JOIN "ActivityReports"
+    ON "ActivityReportGoals"."activityReportId" = "ActivityReports"."id"
+    WHERE "ActivityReports"."startDate" ${operator} ${dateClause}
+  )`);
 }
 
-export function afterStartDate(date) {
-  return filterAssociation(goalIds, date, false, '>=');
+export function beforeStartDate(date: string): WhereOptions {
+  return {
+    id: {
+      [Op.in]: getDateSql([`'${date}'`], '<='),
+    },
+  };
 }
 
-export function withinStartDates(dates: string[]) {
+export function afterStartDate(date: string): WhereOptions {
+  return {
+    id: {
+      [Op.in]: getDateSql([`'${date}'`], '>='),
+    },
+  };
+}
+
+export function withinStartDates(dates: string[]): WhereOptions {
   // got: [ '2022/11/23-2023/02/22' ]
-  // need: [ '2022/11/23', '2023/02/22']
-  const escapedDateStrings = dates[0].split('-').map((d) => `'${d}'`);
-  // now: "'2022/11/23'", "'2023/02/22'"
-  return filterAssociation(goalIds, escapedDateStrings, false, 'BETWEEN');
+  // need: [ '2022-11-23', '2023-02-22']
+  const escapedDates = dates[0].split('-').map((d) => `'${d.replace(/\//g, '-')}'`);
+  return {
+    id: {
+      [Op.in]: getDateSql(escapedDates, 'BETWEEN'),
+    },
+  };
 }
