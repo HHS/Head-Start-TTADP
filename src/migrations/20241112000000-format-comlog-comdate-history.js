c995e280af345f042dd881078d4ab70a4b75396a
Nathan Powell <nathan.powell@adhocteam.us>
2024-11-12 05:49:36 -0800

    push all communicationDate strings to dd/mm/yyyy format

---
+++ src/migrations/20241112000000-format-comlog-comdate-history.js
@@ -0,0 +1,79 @@
+const { prepMigration } = require('../lib/migration');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+      return queryInterface.sequelize.query(`
+        -- This reformats all historical communicationDate values to
+        -- mm/dd/yyyy
+        -- Assumptions of preexisting data:
+        -- -always day-month-year
+        -- -always separated by a slash, period, or space [/. ]
+        -- -if there is an extra separator it impacts the year
+        -- -if the third position is at least two characters, it's the year, else it's the fourth position
+        -- -the first four characters of a long year string holds the year
+        --
+        -- These assumptions are based on the data we have, so aren't guaranteed to be correct if this is rerun
+        -- However the logic is slightly overengineered for extra robustness if new errors show up
+
+        DROP TABLE IF EXISTS comdate_corrections;
+        CREATE TEMP TABLE comdate_corrections
+        AS
+        WITH reseparated AS (
+        SELECT
+          id clid,
+          data->>'communicationDate' orig,
+          regexp_replace(data->>'communicationDate','[-. ]','/','g') reseparated
+        FROM "CommunicationLogs"
+        WHERE data->>'communicationDate' !~ '^\d\d/\d\d/\d\d\d\d$'
+          AND COALESCE(data->>'communicationDate','') != ''
+        ),
+        date_particles AS (
+        SELECT
+          clid,
+          orig,
+          SPLIT_PART(reseparated,'/',1) day_part,
+          SPLIT_PART(reseparated,'/',2) month_part,
+          CASE
+            WHEN LENGTH(SPLIT_PART(reseparated,'/',3)) > 1 THEN SPLIT_PART(reseparated,'/',3)
+            ELSE SPLIT_PART(reseparated,'/',4)
+          END AS year_part
+        FROM reseparated
+        ),
+        padded_particles AS (
+        SELECT
+          clid,
+          orig,
+          LPAD(day_part,2,'0') padded_day,
+          LPAD(month_part,2,'0') padded_month,
+          LPAD(
+            LEFT(year_part,4),
+            4,
+            '20'
+          ) padded_year
+        FROM date_particles
+        )
+        SELECT
+          clid,
+          orig,
+          padded_day || '/' || padded_month || '/' || padded_year reformat
+        FROM padded_particles
+        ;
+
+        UPDATE "CommunicationLogs"
+        SET data = jsonb_set(data, '{communicationDate}', to_jsonb(reformat))
+        FROM comdate_corrections
+        WHERE id = clid
+        ;
+
+      `);
+    });
+  },
+
+  async down() {
+    // no rollbacks
+  },
+};
