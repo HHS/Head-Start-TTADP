09d96c2e3d67f2636d45f83d7b2e85f5b91a6d29
Matt Bevilacqua <thewatermethod@gmail.com>
2024-11-13 11:04:00 -0500

    Revert some e2e changes

--- tests/e2e/activity-report.spec.ts
+++ tests/e2e/activity-report.spec.ts
@@ -175,7 +175,7 @@ test.describe('Activity Report', () => {
     await page.getByRole('button', { name: 'Save and continue' }).click();
 
     await page.getByRole('button', { name: 'Supporting attachments not started' }).click();
-    await page.getByRole('button', { name: 'Goals and objectives In Progress' }).click();
+    await page.getByRole('button', { name: 'Goals and objectives not started' }).click();
 
     // create the first goal
 
@@ -762,7 +762,7 @@ test.describe('Activity Report', () => {
     await page.getByRole('button', { name: 'Save and continue' }).click();
 
     await page.getByRole('button', { name: 'Supporting attachments not started' }).click();
-    await page.getByRole('button', { name: /Goals and objectives in progress/i }).click();
+    await page.getByRole('button', { name: 'Goals and objectives not started' }).click();
 
     // create the goal
     await page.getByLabel(/Select recipient's goal/i).click();
