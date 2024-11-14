fb2240c24787b20ffad7ad6000d63f93facf53c0
GarrettEHill <garretthill@gmail.com>
2024-11-14 12:05:19 -0800

    lint

--- frontend/src/components/BuildInfo.js
+++ frontend/src/components/BuildInfo.js
@@ -6,20 +6,34 @@ function BuildInfo() {
 
   useEffect(() => {
     fetch('/api/admin/buildInfo')
-      .then(response => response.json())
-      .then(data => setBuildInfo(data))
-      .catch(error => console.error("Error fetching build info:", error));
+      .then((response) => response.json())
+      .then((data) => setBuildInfo(data));
   }, []);
 
   if (!buildInfo) return null;
 
   return (
-    <footer style={{ padding: '1em', textAlign: 'center', fontSize: '0.8em', color: '#666' }}>
+    <footer style={
+        {
+            padding: '1em',
+            textAlign: 'center',
+            fontSize: '0.8em',
+            color: '#666'
+        }
+    }>
       <p>
-        Branch: {buildInfo.branch}<br />
-        Commit: {buildInfo.commit}<br />
-        Build Number: {buildInfo.buildNumber}<br />
-        Deployed on: {buildInfo.timestamp}<br />
+        Branch:
+        {buildInfo.branch}
+        <br />
+        Commit:
+        {buildInfo.commit}
+        <br />
+        Build Number:
+        {buildInfo.buildNumber}
+        <br />
+        Deployed on:
+        {buildInfo.timestamp}
+        <br />
       </p>
     </footer>
   );
