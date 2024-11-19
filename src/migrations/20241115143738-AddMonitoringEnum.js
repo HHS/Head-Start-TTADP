abbaa718c467723292d634cd30781e472ffaa852
Adam Levin <84350609+AdamAdHocTeam@users.noreply.github.com>
2024-11-19 17:02:04 -0500

    Update src/migrations/20241115143738-AddMonitoringEnum.js
    Co-authored-by: Matt Bevilacqua <thewatermethod@gmail.com>

--- src/migrations/20241115143738-AddMonitoringEnum.js
+++ src/migrations/20241115143738-AddMonitoringEnum.js
@@ -7,9 +7,9 @@ module.exports = {
     await queryInterface.sequelize.transaction(async (transaction) => {
       const sessionSig = __filename;
       await prepMigration(queryInterface, transaction, sessionSig);
-      return Promise.all(Object.values(GOAL_CREATED_VIA).map((action) => queryInterface.sequelize.query(`
-         ALTER TYPE "enum_Goals_createdVia" ADD VALUE IF NOT EXISTS '${action}';
-      `)));
+      return queryInterface.sequelize.query(`
+         ALTER TYPE "enum_Goals_createdVia" ADD VALUE IF NOT EXISTS 'monitoring';
+      `);
     });
   },
 
