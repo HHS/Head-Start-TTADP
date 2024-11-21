f528e95dd9b961e238e57a19d223cebfb3e841f6
Adam Levin <adam.levin@adhocteam.us>
2024-11-21 16:50:51 -0500

    add function with test to get the grant citations

--- src/services/citations.js
+++ src/services/citations.js
@@ -1,15 +1,117 @@
+/* eslint-disable no-plusplus */
 /* eslint-disable import/prefer-default-export */
 import { sequelize } from '../models';
 
 /*
   The purpose of this function is to get citations by grant id.
-  We then need to format the response for how it needs to be displayed on the FE.
+  We then need to format the response for how it needs to be
+  displayed on the FE for selection on objectives.
 */
 export async function getCitationsByGrantIds(grantIds) {
-  /* Utilize Garrett's suggestion here for getting citations for all grants in the grantIds array */
-  const citationsFromDB = await sequelize.query(
+  /*
+   Questions:
+   - Do we need to take into account the grant replacements table? (what if a grant was replaced?)
+   - Is it enough to join on grant number? Or do we need to use links table?
+   */
+
+  const cutOffStartDate = '2024-10-01'; // TODO: Set this before we deploy to prod.
+
+  // Query to get the citations by grant id.
+  const grantsByCitations = await sequelize.query(
+    /* sql */
     `
+      SELECT
+        ms."standardId",
+        ms.citation,
+        JSONB_AGG( DISTINCT
+          JSONB_BUILD_OBJECT(
+            'findingId', mf."findingId",
+            'grantId', gr.id,
+            'grantNumber', gr.number,
+            'reviewName', mfh."name",
+            'reportDeliveryDate', mfh."reportDeliveryDate",
+            'findingType', mf."findingType",
+            'findingSource', mf."source",
+            'monitoringFindingStatusName', mfs."name"
+          )
+        ) grants
+      FROM "Grants" gr
+      JOIN "MonitoringReviewGrantees" mrg
+        ON gr.number = mrg."grantNumber"
+      JOIN (
+        -- The below 'DISTINCT ON' determines the single record to return values from by the 'ORDER BY' clause.
+        SELECT DISTINCT ON (mfh."findingId", gr.id)
+          mfh."findingId",
+          gr.id AS "grantId",
+          mr."reviewId",
+          mr."name",
+          mr."reportDeliveryDate"
+        FROM "MonitoringFindingHistories" mfh
+        JOIN "MonitoringReviews" mr
+          ON mfh."reviewId" = mr."reviewId"
+        JOIN "MonitoringReviewGrantees" mrg
+          ON mrg."reviewId" = mr."reviewId"
+        JOIN "Grants" gr
+          ON gr.number = mrg."grantNumber"
+        ORDER BY mfh."findingId", gr.id, mr."reportDeliveryDate" DESC
+      ) mfh -- Subquery ensures only the most recent history for each finding-grant combination
+      ON mfh."grantId" = gr.id
+      JOIN "MonitoringFindings" mf
+        ON mfh."findingId" = mf."findingId"
+      JOIN "MonitoringFindingStatuses" mfs
+        ON mf."statusId" = mfs."statusId"
+      JOIN "MonitoringFindingStandards" mfs2
+        ON mf."findingId" = mfs2."findingId"
+      JOIN "MonitoringStandards" ms
+        ON mfs2."standardId" = ms."standardId"
+      JOIN "MonitoringFindingGrants" mfg
+        ON mf."findingId" = mfg."findingId"
+        AND mrg."granteeId" = mfg."granteeId"
+      WHERE 1 = 1
+        AND gr.id IN (${grantIds.join(',')}) -- :grantIds
+        AND mfh."reportDeliveryDate" BETWEEN '${cutOffStartDate}' AND NOW() -- Between is inclusive.
+        AND gr.status = 'Active'
+        AND mfs.name = 'Active'
+      GROUP BY 1,2
+      ORDER BY 2,1;
     `,
   );
-  return ['bad'];
+
+  // From the response we need to get a list of citations for each grant.
+  const citationsByGrant = Object.values(grantsByCitations[0].reduce(
+    (acc, citation) => {
+      const { grants } = citation;
+      // Create a for loop to iterate over every object in the grants array.
+      for (let i = 0; i < grants.length; i++) {
+        const grant = grants[i];
+        // Check if the grant number is already in the accumulator.
+        if (!acc[grant.grantId]) {
+          acc[grant.grantId] = {
+            grantId: grant.grantId,
+            citations: [],
+          };
+        }
+
+        // Build a citation object to push into the array.
+        const citationObject = {
+          findingType: grant.findingType,
+          findingSource: grant.findingSource,
+          grantId: grant.grantId,
+          standardId: citation.standardId,
+          citation: citation.citation,
+          findingId: grant.findingId,
+          reviewName: grant.reviewName,
+          grantNumber: grant.grantNumber,
+          reportDeliveryDate: grant.reportDeliveryDate,
+          monitoringFindingStatusName: grant.monitoringFindingStatusName,
+        };
+        // Push the citation into the array for the grant number.
+        acc[grant.grantId].citations.push(citationObject);
+      }
+      return acc;
+    },
+    {},
+  ));
+
+  return citationsByGrant;
 }
---
+++ src/services/citations.test.js
@@ -0,0 +1,355 @@
+/* eslint-disable max-len */
+/* eslint-disable prefer-destructuring */
+import { v4 as uuidv4 } from 'uuid';
+import faker from '@faker-js/faker';
+import { getCitationsByGrantIds } from './citations';
+import db, {
+  Recipient,
+  Grant,
+  MonitoringReviewGrantee,
+  MonitoringReview,
+  MonitoringFindingHistory,
+  MonitoringFinding,
+  MonitoringFindingStatus,
+  MonitoringReviewStatus,
+  MonitoringFindingStandard,
+  MonitoringStandard,
+  MonitoringFindingGrant,
+} from '../models';
+import { captureSnapshot, rollbackToSnapshot } from '../lib/programmaticTransaction';
+
+// create a function to create a citation for a grant.
+const createMonitoringData = async (
+  grantNUmber, // Grant Number.
+  reviewStatusId, // Status ID.
+  reportDeliveryDate, // Report Delivery Date must be between start cutoff and todays date.
+  reviewType, // Review Type must be in ('AIAN-DEF', 'RAN', 'Follow-up', 'FA-1', 'FA1-FR', 'FA-2', 'FA2-CR', 'Special')
+  monitoringReviewStatusName, // Monitoring Review Status Name must be 'Complete'.
+  citationsArray, // Array of citations to create.
+) => {
+  const reviewId = uuidv4();
+  const granteeId = uuidv4();
+
+  // MonitoringReviewGrantee.
+  await MonitoringReviewGrantee.create({
+    id: faker.datatype.number({ min: 9999 }),
+    grantNumber: grantNUmber,
+    reviewId,
+    granteeId,
+    createTime: new Date(),
+    updateTime: new Date(),
+    updateBy: 'Support Team',
+    sourceCreatedAt: new Date(),
+    sourceUpdatedAt: new Date(),
+  }, { individualHooks: true });
+
+  // MonitoringReview.
+  await MonitoringReview.create({
+    reviewId,
+    contentId: faker.datatype.uuid(),
+    statusId: reviewStatusId,
+    name: faker.random.words(3),
+    startDate: new Date(),
+    endDate: new Date(),
+    reviewType,
+    reportDeliveryDate,
+    reportAttachmentId: faker.datatype.uuid(),
+    outcome: faker.random.words(5),
+    hash: faker.datatype.uuid(),
+    sourceCreatedAt: new Date(),
+    sourceUpdatedAt: new Date(),
+  }, { individualHooks: true });
+
+  // MonitoringReviewStatus.
+  await MonitoringReviewStatus.create({
+    statusId: reviewStatusId,
+    name: monitoringReviewStatusName,
+    sourceCreatedAt: new Date(),
+    sourceUpdatedAt: new Date(),
+  }, { individualHooks: true });
+
+  // MonitoringFindingHistory (this is the primary finding table and the relationship to citation is 1<>1).
+  // If we wanted one grant to have multiple citations, we would need to create multiple findings here and below.
+  await Promise.all(citationsArray.map(async (citation) => {
+    const findingId = uuidv4();
+    const findingStatusId = faker.datatype.number({ min: 9999 });
+    await MonitoringFindingHistory.create({
+      reviewId,
+      findingHistoryId: uuidv4(),
+      findingId,
+      statusId: findingStatusId,
+      narrative: faker.random.words(10),
+      ordinal: faker.datatype.number({ min: 1, max: 10 }),
+      determination: faker.random.words(5),
+      hash: faker.datatype.uuid(),
+      sourceCreatedAt: new Date(),
+      sourceUpdatedAt: new Date(),
+      sourceDeletedAt: null,
+    }, { individualHooks: true });
+
+    // MonitoringFinding.
+    await MonitoringFinding.create({
+      findingId,
+      statusId: findingStatusId,
+      findingType: citation.monitoringFindingType,
+      hash: faker.datatype.uuid(),
+      source: 'Internal Controls',
+      sourceCreatedAt: new Date(),
+      sourceUpdatedAt: new Date(),
+    }, { individualHooks: true });
+
+    // MonitoringFindingStatus.
+    await MonitoringFindingStatus.create({
+      statusId: findingStatusId,
+      name: citation.monitoringFindingStatusName,
+      sourceCreatedAt: new Date(),
+      sourceUpdatedAt: new Date(),
+    }, { individualHooks: true });
+
+    // MonitoringFindingGrant.
+    await MonitoringFindingGrant.create({
+      findingId,
+      granteeId,
+      statusId: findingStatusId,
+      findingType: citation.monitoringFindingGrantFindingType,
+      source: 'Discipline',
+      correctionDeadLine: new Date(),
+      reportedDate: new Date(),
+      closedDate: null,
+      hash: faker.datatype.uuid(),
+      sourceCreatedAt: new Date(),
+      sourceUpdatedAt: new Date(),
+      sourceDeletedAt: null,
+    }, { individualHooks: true });
+
+    // MonitoringFindingStandard (this table joins a finding to a standard (citation)).
+    const standardId = faker.datatype.number({ min: 9999 });
+    const citable = faker.datatype.number({ min: 1, max: 10 });
+    await MonitoringFindingStandard.create({
+      findingId,
+      standardId, // Integer
+      sourceCreatedAt: new Date(),
+      sourceUpdatedAt: new Date(),
+    }, { individualHooks: true });
+
+    // MonitoringStandard.
+    await MonitoringStandard.create({
+      standardId,
+      citation: citation.citationText,
+      sourceCreatedAt: new Date(),
+      sourceUpdatedAt: new Date(),
+      contentId: uuidv4(),
+      hash: uuidv4(),
+      citable,
+    }, { individualHooks: true });
+  }));
+};
+
+describe('citations service', () => {
+  afterAll(async () => {
+    await db.sequelize.close();
+  });
+  describe('getCitationsByGrantIds()', () => {
+    let snapShot;
+
+    let recipient1;
+    let recipient2;
+
+    let grant1; // Recipient 1
+    let grant1a; // Recipient 1
+    let grant2; // Recipient 2
+    let grant3; // Recipient 2 (Inactive)
+
+    beforeAll(async () => {
+      // Capture a snapshot of the database before running the test.
+      snapShot = await captureSnapshot();
+
+      // Grant Numbers.
+      const grantNumber1 = faker.datatype.string(8);
+      const grantNumber1a = faker.datatype.string(8);
+      const grantNumber2 = faker.datatype.string(8);
+      const grantNumber3 = faker.datatype.string(8);
+
+      // Recipients 1.
+      recipient1 = await Recipient.create({
+        id: faker.datatype.number({ min: 64000 }),
+        name: faker.random.alphaNumeric(6),
+      });
+
+      // Recipients 2.
+      recipient2 = await Recipient.create({
+        id: faker.datatype.number({ min: 64000 }),
+        name: faker.random.alphaNumeric(6),
+      });
+
+      // Grants.
+      const grants = await Grant.bulkCreate([
+        {
+          // Grant 1 for Recipient 1.
+          id: faker.datatype.number({ min: 9999 }),
+          number: grantNumber1,
+          recipientId: recipient1.id,
+          regionId: 1,
+          startDate: new Date(),
+          endDate: new Date(),
+          status: 'Active',
+        },
+        {
+          // Grant 1a for Recipient 1.
+          id: faker.datatype.number({ min: 9999 }),
+          number: grantNumber1a,
+          recipientId: recipient1.id,
+          regionId: 1,
+          startDate: new Date(),
+          endDate: new Date(),
+          status: 'Active',
+        },
+        {
+          // Grant 2 for Recipient 2.
+          id: faker.datatype.number({ min: 9999 }),
+          number: grantNumber2,
+          recipientId: recipient2.id,
+          regionId: 1,
+          startDate: new Date(),
+          endDate: new Date(),
+          status: 'Active',
+        },
+        {
+          // Grant 3 for Recipient 2 (Inactive).
+          id: faker.datatype.number({ min: 9999 }),
+          number: grantNumber3,
+          recipientId: recipient2.id,
+          regionId: 1,
+          startDate: new Date(),
+          endDate: new Date(),
+          status: 'Inactive',
+        },
+      ]);
+
+      // set the grants.
+      grant1 = grants[0];
+      grant1a = grants[1];
+      grant2 = grants[2];
+      grant3 = grants[3];
+
+      /*
+        Citation Object Properties:
+          citationText, // Citation Text
+          monitoringFindingType, // Monitoring Finding ('Deficiency', 'Significant Deficiency', 'Material Weakness', 'No Finding').
+          monitoringFindingStatusName, // Monitoring Finding Status name must be 'Active'.
+          monitoringFindingGrantFindingType, // Monitoring Finding Grant Finding Type must be in ('Corrective Action', 'Management Decision', 'No Finding').
+        */
+
+      // Create Monitoring Review Citations.
+      const grant1Citations1 = [
+        {
+          citationText: 'Grant 1 - Citation 1 - Good',
+          monitoringFindingType: 'Citation 1 Monitoring Finding Type',
+          monitoringFindingStatusName: 'Active',
+          monitoringFindingGrantFindingType: 'Corrective Action',
+        },
+        {
+          citationText: 'Grant 1 - Citation 2 - Bad MFS name',
+          monitoringFindingType: 'Citation 2 Monitoring Finding Type',
+          monitoringFindingStatusName: 'Abandoned',
+          monitoringFindingGrantFindingType: 'Corrective Action',
+        },
+        {
+          citationText: 'Grant 1 - Citation 3 - Good 2',
+          monitoringFindingType: 'Citation 3 Monitoring Finding Type',
+          monitoringFindingStatusName: 'Active',
+          monitoringFindingGrantFindingType: 'Corrective Action',
+        },
+      ];
+
+      // Grant 1.
+      await createMonitoringData(grant1.number, 1, new Date(), 'AIAN-DEF', 'Complete', grant1Citations1);
+
+      // Grant 1a (make sure other grant citations comeback).
+      const grant1Citations1a = [
+        {
+          citationText: 'Grant 1a - Citation 1 - Good',
+          monitoringFindingType: 'Citation 4 Monitoring Finding Type',
+          monitoringFindingStatusName: 'Active',
+          monitoringFindingGrantFindingType: 'Grant 1a Corrective Action',
+        },
+      ];
+      await createMonitoringData(grant1a.number, 2, new Date(), 'AIAN-DEF', 'Complete', grant1Citations1a);
+
+      // Grant 2.
+      const grant1Citations2 = [
+        {
+          citationText: 'Grant 2 - Citation 1 - Good',
+          monitoringFindingType: 'citation 5 Monitoring Finding Type',
+          monitoringFindingStatusName: 'Active',
+          monitoringFindingGrantFindingType: 'Corrective Action',
+        },
+      ];
+        // Before delivery date.
+      await createMonitoringData(grant2.number, 3, new Date('2024-09-01'), 'AIAN-DEF', 'Complete', grant1Citations2);
+      // After delivery date (tomorrow).
+      await createMonitoringData(grant2.number, 4, new Date(new Date().setDate(new Date().getDate() + 1)), 'AIAN-DEF', 'Complete', grant1Citations2);
+
+      // Grant 3 (inactive).
+      const grant1Citations3 = [
+        {
+          citationText: 'Grant 3 - Citation 1 - Good',
+          monitoringFindingType: 'Material Weakness',
+          monitoringFindingStatusName: 'Active',
+          monitoringFindingGrantFindingType: 'Corrective Action',
+        },
+      ];
+      await createMonitoringData(grant3.number, 5, new Date(), 'AIAN-DEF', 'Complete', grant1Citations3);
+    });
+
+    afterAll(async () => {
+      // Rollback any changes made to the database during the test.
+      await rollbackToSnapshot(snapShot);
+    });
+
+    it('correctly retrieves citations per grant', async () => {
+      // Call the service to get the citations by grant ids.
+      const citationsToAssert = await getCitationsByGrantIds([grant1.id, grant1a.id, grant2.id, grant3.id]);
+      // Get the first citation object for grant 1.
+      const grant1Citations = citationsToAssert.find((g) => g.grantId === grant1.id);
+      expect(grant1Citations).toBeDefined();
+      expect(grant1Citations.citations.length).toBe(2);
+
+      // Get the first citation ''Grant 1 - Citation 1 - Good'.
+      const citation1 = grant1Citations.citations.find((c) => c.citation === 'Grant 1 - Citation 1 - Good');
+      expect(citation1).toBeDefined();
+      expect(citation1.findingType).toBe('Citation 1 Monitoring Finding Type');
+      expect(citation1.findingSource).toBe('Internal Controls');
+      expect(citation1.grantNumber).toBe(grant1.number);
+
+      // Get the second citation ''Grant 1 - Citation 3 - Good 2'.
+      const citation2 = grant1Citations.citations.find((c) => c.citation === 'Grant 1 - Citation 3 - Good 2');
+      expect(citation2).toBeDefined();
+      expect(citation2.findingType).toBe('Citation 3 Monitoring Finding Type');
+      expect(citation2.findingSource).toBe('Internal Controls');
+      expect(citation2.grantNumber).toBe(grant1.number);
+
+      // Look for Grant1a.
+      const grant1aCitations = citationsToAssert.find((g) => g.grantId === grant1a.id);
+      expect(grant1aCitations).toBeDefined();
+      expect(grant1aCitations.citations.length).toBe(1);
+
+      // Get the first citation ''Grant 1a - Citation 1 - Good'.
+      const citation1a = grant1aCitations.citations.find((c) => c.citation === 'Grant 1a - Citation 1 - Good');
+      expect(citation1a).toBeDefined();
+      expect(citation1a.findingType).toBe('Citation 4 Monitoring Finding Type');
+      expect(citation1a.findingSource).toBe('Internal Controls');
+      expect(citation1a.grantNumber).toBe(grant1a.number);
+
+      // Looks for Grant2.
+      const grant2Citations = citationsToAssert.find((g) => g.grantId === grant2.id);
+      // Assert we don\'t have any citations for grant 2.
+      expect(grant2Citations).toBeUndefined();
+
+      // Looks for Grant3.
+      const grant3Citations = citationsToAssert.find((g) => g.grantId === grant3.id);
+      // Assert we don\'t have any citations for grant 3.
+      expect(grant3Citations).toBeUndefined();
+    });
+  });
+});
