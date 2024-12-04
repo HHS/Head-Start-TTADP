df6d0e5898508aeefead32650cb37c4112ed6098
nvms <jon@pye.rs>
2024-12-04 11:02:57 -0500

    more coverage

--- src/policies/group.test.js
+++ src/policies/group.test.js
@@ -150,4 +150,93 @@ describe('Group', () => {
       expect(g.grants).toEqual([]);
     });
   });
+
+  describe('canEditGroup', () => {
+    it('should return true if the user is a Creator of the group', () => {
+      const user = { id: 1 };
+      const group = { groupCollaborators: [{ user: { id: 1 }, collaboratorType: { name: 'Creator' } }] };
+
+      const g = new Group(user, [], group);
+
+      expect(g.canEditGroup()).toBe(true);
+    });
+
+    it('should return true if the user is a Co-Owner of the group', () => {
+      const user = { id: 1 };
+      const group = { groupCollaborators: [{ user: { id: 1 }, collaboratorType: { name: 'Co-Owner' } }] };
+
+      const g = new Group(user, [], group);
+
+      expect(g.canEditGroup()).toBe(true);
+    });
+
+    it('should return false if the user is not a Creator or Co-Owner of the group', () => {
+      const user = { id: 1 };
+      const group = { groupCollaborators: [{ user: { id: 2 }, collaboratorType: { name: 'Member' } }] };
+
+      const g = new Group(user, [], group);
+
+      expect(g.canEditGroup()).toBe(false);
+    });
+  });
+
+  describe('canUseGroup', () => {
+    it('should return true if the user is a collaborator of the group', () => {
+      const user = { id: 1 };
+      const group = { groupCollaborators: [{ user: { id: 1 }, collaboratorType: { name: 'Member' } }] };
+
+      const g = new Group(user, [], group);
+
+      expect(g.canUseGroup()).toBe(true);
+    });
+
+    it('should return true if the group is public and the user has access to the group\'s region', () => {
+      const user = {
+        id: 1,
+        permissions: [{
+          region: 1,
+          scopeId: SCOPES.READ_REPORTS,
+        }],
+      };
+
+      const grants = [{
+        region: 1,
+      }];
+
+      const group = { isPublic: true };
+
+      const g = new Group(user, grants, group);
+
+      expect(g.canUseGroup()).toBe(true);
+    });
+
+    it('should return false if the group is public but the user does not have access to the group\'s region', () => {
+      const user = {
+        id: 1,
+        permissions: [{
+          regionId: 1,
+          scopeId: SCOPES.READ_REPORTS,
+        }],
+      };
+
+      const grants = [{
+        regionId: 2,
+      }];
+
+      const group = { isPublic: true };
+
+      const g = new Group(user, grants, group);
+
+      expect(g.canUseGroup()).toBe(false);
+    });
+
+    it('should return false if the user is not a collaborator and the group is not public', () => {
+      const user = { id: 1 };
+      const group = { isPublic: false };
+
+      const g = new Group(user, [], group);
+
+      expect(g.canUseGroup()).toBe(false);
+    });
+  });
 });
