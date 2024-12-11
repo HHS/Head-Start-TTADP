1263f0ddaaef530d95337f6dba2a6836431f9546
Adam Levin <adam.levin@adhocteam.us>
2024-12-11 09:41:26 -0500

    update FE test for new error logic

--- frontend/src/pages/ActivityReport/Pages/__tests__/goalsObjectives.js
+++ frontend/src/pages/ActivityReport/Pages/__tests__/goalsObjectives.js
@@ -160,9 +160,11 @@ describe('goals objectives', () => {
     });
 
     it('the display goals shows a warning if no grants are selected', async () => {
-      renderGoals([], 'recipient');
+      renderGoals([], 'recipient', [], false, false, jest.fn(), '2021-01-01');
       expect(screen.queryByText('Goals and objectives')).toBeNull();
-      expect(await screen.findByText(/To create goals, first select a recipient/i)).toBeVisible();
+      expect(await screen.findByText(/to add goals and objectives, indicate in the/i)).toBeVisible();
+      expect(await screen.findByText(/who the activity was for/i)).toBeVisible();
+      expect(screen.queryByText(/start date of the activity/i)).toBeNull();
     });
 
     it('you can click the little add new button', async () => {
@@ -289,39 +291,39 @@ describe('goals objectives', () => {
   describe('when activity recipient type is not "recipient" or "other-entity"', () => {
     it('both warnings are displayed when the report type and start date are missing', async () => {
       renderGoals([1], null);
-      expect(await screen.findByText('To add goals and objectives, indicate in the')).toBeVisible();
-      expect(await screen.findByText('who the activity was for')).toBeVisible();
-      expect(await screen.findByText('the start date of the activity')).toBeVisible();
+      expect(await screen.findByText(/to add goals and objectives, indicate in the/i)).toBeVisible();
+      expect(await screen.findByText(/who the activity was for/i)).toBeVisible();
+      expect(await screen.findByText(/start date of the activity/i)).toBeVisible();
     });
 
     it('shows the report type warning when the report type is missing', async () => {
       renderGoals([1], null, [], false, false, jest.fn(), '2021-01-01');
-      expect(await screen.findByText('To add goals and objectives, indicate in the')).toBeVisible();
-      expect(await screen.findByText('who the activity was for')).toBeVisible();
-      expect(screen.queryByText('the start date of the activity')).toBeNull();
+      expect(await screen.findByText(/to add goals and objectives, indicate in the/i)).toBeVisible();
+      expect(await screen.findByText(/who the activity was for/i)).toBeVisible();
+      expect(screen.queryByText(/start date of the activity/i)).toBeNull();
     });
 
     it('shows the start date warning when the start date is missing', async () => {
-      renderGoals([1], 'recipient', [], false, false, jest.fn());
-      expect(await screen.findByText('To add goals and objectives, indicate in the')).toBeVisible();
-      expect(screen.queryByText('who the activity was for')).toBeNull();
-      expect(await screen.findByText('the start date of the activity')).toBeVisible();
+      renderGoals([1], 'recipient', [], false, false, jest.fn(), null);
+      expect(await screen.findByText(/to add goals and objectives, indicate in the/i)).toBeVisible();
+      expect(screen.queryByText(/who the activity was for/i)).toBeNull();
+      expect(await screen.findByText(/start date of the activity/i)).toBeVisible();
     });
 
     it('hides the warnings when the report type and start date are present', async () => {
       renderGoals([1], 'recipient', [], false, false, jest.fn(), '2021-01-01');
-      expect(screen.queryByText('To add goals and objectives, indicate in the')).toBeNull();
-      expect(screen.queryByText('who the activity was for')).toBeNull();
-      expect(screen.queryByText('the start date of the activity')).toBeNull();
+      expect(screen.queryByText(/to add goals and objectives, indicate in the/i)).toBeNull();
+      expect(screen.queryByText(/who the activity was for/i)).toBeNull();
+      expect(screen.queryByText(/start date of the activity/i)).toBeNull();
       // Expect to find an h3 with the text "Goal summary".
       expect(await screen.findByText('Goal summary', { selector: '.margin-bottom-0.margin-top-4' })).toBeVisible();
     });
 
     it('shows the start date warning if the start date has the value of "Invalid date"', async () => {
       renderGoals([1], 'recipient', [], false, false, jest.fn(), 'Invalid date');
-      expect(await screen.findByText('To add goals and objectives, indicate in the')).toBeVisible();
-      expect(screen.queryByText('who the activity was for')).toBeNull();
-      expect(await screen.findByText('the start date of the activity')).toBeVisible();
+      expect(await screen.findByText(/to add goals and objectives, indicate in the/i)).toBeVisible();
+      expect(screen.queryByText(/who the activity was for/i)).toBeNull();
+      expect(await screen.findByText(/start date of the activity/i)).toBeVisible();
     });
   });
 
