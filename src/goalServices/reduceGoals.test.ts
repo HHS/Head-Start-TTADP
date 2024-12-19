cf460fc2d07da274265119e73428d61b684d8db2
Adam Levin <adam.levin@adhocteam.us>
2024-12-19 14:56:10 -0500

    add test coverage for reduceGoals

--- src/goalServices/reduceGoals.test.ts
+++ src/goalServices/reduceGoals.test.ts
@@ -231,4 +231,128 @@ describe('reduceGoals', () => {
     expect(result.length).toEqual(1);
     expect(result[0].objectiveCreatedHere).toEqual(true);
   });
+
+  it('should properly reduce objectives for activity report with activityReportObjectiveCitations', () => {
+    const newObjectives = [
+      {
+        id: 1,
+        otherEntityId: 123,
+        title: 'Objective 1',
+        status: 'Not Started',
+        topics: [],
+        resources: [],
+        files: [],
+        courses: [],
+        goalId: 6,
+        onApprovedAR: false,
+        onAR: false,
+        rtrOrder: 1,
+        activityReportObjectives: [
+          {
+            status: 'Not Started',
+            objectiveCreatedHere: true,
+            activityReportObjectiveResources: [],
+            activityReportObjectiveTopics: [],
+            activityReportObjectiveCourses: [],
+            activityReportObjectiveFiles: [],
+            activityReportObjectiveCitations: [
+              {
+                citation: 'Citation 1',
+                monitoringReferences: [
+                  {
+                    standardId: 1,
+                    findingSource: 'Source 1',
+                    acro: 'DEF',
+                  },
+                ],
+              },
+              {
+                citation: 'Citation 2',
+                monitoringReferences: [
+                  {
+                    standardId: 2,
+                    findingSource: 'Source 2',
+                    acro: 'ANC',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+      {
+        id: 2,
+        otherEntityId: 123,
+        title: 'Objective 2',
+        status: 'Not Started',
+        topics: [],
+        resources: [],
+        files: [],
+        courses: [],
+        goalId: 6,
+        onApprovedAR: false,
+        onAR: false,
+        rtrOrder: 1,
+        activityReportObjectives: [
+          {
+            status: 'Not Started',
+            objectiveCreatedHere: true,
+            activityReportObjectiveResources: [],
+            activityReportObjectiveTopics: [],
+            activityReportObjectiveCourses: [],
+            activityReportObjectiveFiles: [],
+            activityReportObjectiveCitations: [
+              {
+                citation: 'Citation 3',
+                monitoringReferences: [
+                  {
+                    standardId: 3,
+                    findingSource: 'Source 3',
+                    acro: 'ANC',
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    ];
+
+    const currentObjectives = [
+      {
+        status: 'Not Started',
+        title: 'Objective 1',
+        objectiveCreatedHere: false,
+        ids: [],
+        recipientIds: [],
+        activityReports: [],
+        topics: [],
+        resources: [],
+        files: [],
+        courses: [],
+        citations: [],
+      },
+    ];
+
+    // @ts-ignore
+    const result = reduceObjectivesForActivityReport(newObjectives, currentObjectives);
+    expect(result.length).toEqual(2);
+    expect(result[0].citations).toEqual([
+      {
+        id: 1,
+        name: 'DEF - Citation 1 - Source 1',
+      },
+      {
+        id: 2,
+        name: 'ANC - Citation 2 - Source 2',
+      },
+    ]);
+
+    expect(result[1].citations).toEqual([
+      {
+        id: 3,
+        name: 'ANC - Citation 3 - Source 3',
+      },
+    ]);
+  });
 });
