cc72c9ac6eb95dbc30cc1a2a9499af75011a5537
GarrettEHill <garretthill@gmail.com>
2024-12-20 16:08:33 -0800

    Add support for grants with monitoring from a grant that was replaced

--- src/services/citations.ts
+++ src/services/citations.ts
@@ -50,45 +50,9 @@ export async function getCitationsByGrantIds(
   // Query to get the citations by grant id.
   const grantsByCitations = await sequelize.query(
     /* sql */
-    `
-      SELECT
-        ms."standardId",
-        ms.citation,
-        JSONB_AGG( DISTINCT
-          JSONB_BUILD_OBJECT(
-            'findingId', mf."findingId",
-            'grantId', gr.id,
-            'grantNumber', gr.number,
-            'reviewName', mfh."name",
-            'reportDeliveryDate', mfh."reportDeliveryDate",
-            'findingType', mf."findingType",
-            'findingSource', mf."source",
-            'monitoringFindingStatusName', mfs."name",
-            'reportDeliveryDate', mfh."reportDeliveryDate",
-            'citation', ms.citation,
-            'severity', CASE
-                    WHEN mf."findingType" = 'Deficiency' THEN 1
-                    WHEN mf."findingType" = 'Noncompliance' THEN 2
-                    ELSE 3
-                  END,
-            'acro', CASE
-                    WHEN mf."findingType" = 'Deficiency' THEN 'DEF'
-                    WHEN mf."findingType" = 'Noncompliance' THEN 'ANC'
-                    ELSE 'AOC'
-                  END
-          )
-        ) grants
-      FROM "Grants" gr
-      JOIN "Goals" g
-        ON gr."id" = g."grantId"
-        AND g."status" NOT IN ('Closed', 'Suspended')
-      JOIN "GoalTemplates" gt
-        ON g."goalTemplateId" = gt."id"
-        AND gt."standard" = 'Monitoring'
-      JOIN "MonitoringReviewGrantees" mrg
-        ON gr.number = mrg."grantNumber"
-      JOIN (
-        -- The below 'DISTINCT ON' determines the single record to return values from by the 'ORDER BY' clause.
+    `WITH 
+      -- Subquery ensures only the most recent history for each finding-grant combination
+      "RecentMonitoring" AS ( 
         SELECT DISTINCT ON (mfh."findingId", gr.id)
           mfh."findingId",
           gr.id AS "grantId",
@@ -102,27 +66,65 @@ export async function getCitationsByGrantIds(
           ON mrg."reviewId" = mr."reviewId"
         JOIN "Grants" gr
           ON gr.number = mrg."grantNumber"
+        WHERE mr."reportDeliveryDate"::date BETWEEN '${cutOffStartDate}' AND '${reportStartDate}'
         ORDER BY mfh."findingId", gr.id, mr."reportDeliveryDate" DESC
-      ) mfh -- Subquery ensures only the most recent history for each finding-grant combination
-      ON mfh."grantId" = gr.id
-      JOIN "MonitoringFindings" mf
-        ON mfh."findingId" = mf."findingId"
-      JOIN "MonitoringFindingStatuses" mfs
-        ON mf."statusId" = mfs."statusId"
-      JOIN "MonitoringFindingStandards" mfs2
-        ON mf."findingId" = mfs2."findingId"
-      JOIN "MonitoringStandards" ms
-        ON mfs2."standardId" = ms."standardId"
-      JOIN "MonitoringFindingGrants" mfg
-        ON mf."findingId" = mfg."findingId"
-        AND mrg."granteeId" = mfg."granteeId"
-      WHERE 1 = 1
-        AND gr.id IN (${grantIds.join(',')}) -- :grantIds
-          AND mfh."reportDeliveryDate"::date BETWEEN '${cutOffStartDate}' AND '${reportStartDate}'
-        AND gr.status = 'Active'
-        AND mfs.name = 'Active'
-      GROUP BY 1,2
-      ORDER BY 2,1;
+      )
+    SELECT
+      ms."standardId",
+      ms.citation,
+      JSONB_AGG( DISTINCT
+        JSONB_BUILD_OBJECT(
+          'findingId', mf."findingId",
+          'grantId', grta."activeGrantId",
+          'originalGrantId', grta."grantId",
+          'grantNumber', gr.number,
+          'reviewName', rm."name",
+          'reportDeliveryDate', rm."reportDeliveryDate",
+          'findingType', mf."findingType",
+          'findingSource', mf."source",
+          'monitoringFindingStatusName', mfs."name",
+          'citation', ms.citation,
+          'severity', CASE
+                  WHEN mf."findingType" = 'Deficiency' THEN 1
+                  WHEN mf."findingType" = 'Noncompliance' THEN 2
+                  ELSE 3 -- Area of Concern
+                END,
+          'acro', CASE
+                  WHEN mf."findingType" = 'Deficiency' THEN 'DEF'
+                  WHEN mf."findingType" = 'Noncompliance' THEN 'ANC'
+                  ELSE 'AOC' -- Area of Concern
+                END
+        )
+      ) grants
+    FROM "GrantRelationshipToActive" grta
+    JOIN "Grants" gr
+      ON  grta."grantId" = gr.id
+    JOIN "Goals" g
+      ON grta."activeGrantId" = g."grantId"
+      AND g."status" NOT IN ('Closed', 'Suspended')
+    JOIN "GoalTemplates" gt
+      ON g."goalTemplateId" = gt."id"
+      AND gt."standard" = 'Monitoring'
+    JOIN "MonitoringReviewGrantees" mrg
+      ON gr.number = mrg."grantNumber"
+    JOIN "RecentMonitoring" rm 
+    ON rm."grantId" = gr.id
+    JOIN "MonitoringFindings" mf
+      ON rm."findingId" = mf."findingId"
+    JOIN "MonitoringFindingStatuses" mfs
+      ON mf."statusId" = mfs."statusId"
+    JOIN "MonitoringFindingStandards" mfs2
+      ON mf."findingId" = mfs2."findingId"
+    JOIN "MonitoringStandards" ms
+      ON mfs2."standardId" = ms."standardId"
+    JOIN "MonitoringFindingGrants" mfg
+      ON mf."findingId" = mfg."findingId"
+      AND mrg."granteeId" = mfg."granteeId"
+    WHERE 1 = 1
+      AND grta."activeGrantId" IN (${grantIds.join(',')}) -- :grantIds
+      AND mfs.name = 'Active'
+    GROUP BY 1,2
+    ORDER BY 2,1;
     `,
   );
 
