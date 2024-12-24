015e879a293bd5c457b312256bf270397c1983e7
Matt Bevilacqua <thewatermethod@gmail.com>
2024-12-24 10:47:19 -0500

    Add more tests for processFilesFromZip

--- src/lib/importSystem/tests/processFilesFromZip.test.js
+++ src/lib/importSystem/tests/processFilesFromZip.test.js
@@ -54,6 +54,16 @@ describe('processFilesFromZip', () => {
     expect(auditLogger.log).not.toHaveBeenCalled();
   });
 
+  it('throws an error if there is a failure to get a file stream', async () => {
+    zipClient.getFileStream.mockImplementation(() => null);
+
+    await processFilesFromZip(importFileId, zipClient, filesToProcess, processDefinitions.map((def) => ({ ...def })));
+
+    expect(updateAvailableDataFileMetadata).toHaveBeenCalledTimes(2);
+    expect(updateAvailableDataFileMetadata).toHaveBeenCalledWith(importFileId, { name: 'file1.xml' }, IMPORT_DATA_STATUSES.PROCESSING_FAILED, { recordCounts: { errors: { 'Failed to get stream from file1.xml': 1 } } });
+    expect(updateAvailableDataFileMetadata).toHaveBeenCalledWith(importFileId, { name: 'file2.xml' }, IMPORT_DATA_STATUSES.PROCESSING_FAILED, { recordCounts: { errors: { 'Failed to get stream from file2.xml': 1 } } });
+  });
+
   it('should process all files successfully', async () => {
     processFile.mockImplementation(() => ({
       errors: [],
@@ -111,6 +121,122 @@ describe('processFilesFromZip', () => {
     );
     expect(setImportDataFileStatusByPath).toHaveBeenCalledWith(importFileId, { name: 'file1.xml', path: 'path/to/file1.xml' }, IMPORT_DATA_STATUSES.PROCESSING);
     expect(setImportDataFileStatusByPath).toHaveBeenCalledWith(importFileId, { name: 'file2.xml', path: 'path/to/file2.xml' }, IMPORT_DATA_STATUSES.PROCESSING);
+  });
+
+  it('handles default values returned from processFile', async () => {
+    processFile.mockImplementation(() => ({
+      inserts: [],
+      updates: [],
+      deletes: [],
+      errors: [],
+    }));
+
+    await processFilesFromZip(importFileId, zipClient, filesToProcess, processDefinitions.map((def) => ({ ...def })));
+
+    expect(setImportDataFileStatusByPath).toHaveBeenCalledTimes(2);
+    expect(setImportDataFileStatusByPath).toHaveBeenCalledWith(importFileId, { name: 'file1.xml', path: 'path/to/file1.xml' }, IMPORT_DATA_STATUSES.PROCESSING);
+    expect(setImportDataFileStatusByPath).toHaveBeenCalledWith(importFileId, { name: 'file2.xml', path: 'path/to/file2.xml' }, IMPORT_DATA_STATUSES.PROCESSING);
+    expect(processFile).toHaveBeenCalledTimes(2);
+    expect(processFile).toHaveBeenCalledWith(
+      {
+        encoding: 'utf8', fileName: 'file2.xml', keys: ['key2'], remapDef: {}, tableName: 'table2',
+      },
+      { name: 'file2.xml', path: 'path/to/file2.xml' },
+      {},
+    );
+    expect(processFile).toHaveBeenCalledWith(
+      {
+        encoding: 'utf8', fileName: 'file1.xml', keys: ['key1'], remapDef: {}, tableName: 'table1',
+      },
+      { name: 'file1.xml', path: 'path/to/file1.xml' },
+      {},
+    );
+    expect(updateAvailableDataFileMetadata).toHaveBeenCalledTimes(2);
+    expect(updateAvailableDataFileMetadata).toHaveBeenCalledWith(
+      importFileId,
+      { name: 'file2.xml', path: 'path/to/file2.xml' },
+      IMPORT_STATUSES.PROCESSED,
+      {
+        hash: null,
+        recordCounts: {
+          deletes: 0, errors: {}, inserts: 0, updates: 0,
+        },
+        schema: null,
+      },
+    );
+    expect(updateAvailableDataFileMetadata).toHaveBeenCalledWith(
+      importFileId,
+      { name: 'file1.xml', path: 'path/to/file1.xml' },
+      IMPORT_STATUSES.PROCESSED,
+      {
+        hash: null,
+        recordCounts: {
+          deletes: 0, errors: {}, inserts: 0, updates: 0,
+        },
+        schema: null,
+      },
+    );
+    expect(setImportDataFileStatusByPath).toHaveBeenCalledWith(importFileId, { name: 'file1.xml', path: 'path/to/file1.xml' }, IMPORT_DATA_STATUSES.PROCESSING);
+    expect(setImportDataFileStatusByPath).toHaveBeenCalledWith(importFileId, { name: 'file2.xml', path: 'path/to/file2.xml' }, IMPORT_DATA_STATUSES.PROCESSING);
+  });
+
+  it('runs fns returned from processFile', async () => {
+    const insert = jest.fn();
+    const update = jest.fn();
+    const del = jest.fn();
+
+    processFile.mockImplementation(() => ({
+      inserts: [insert],
+      updates: [update],
+      deletes: [del],
+      errors: ['Failure', 'Failure', 'Near-failure'],
+    }));
+
+    const resolve = jest.spyOn(Promise, 'resolve');
+
+    await processFilesFromZip(importFileId, zipClient, filesToProcess, processDefinitions.map((def) => ({ ...def })));
+
+    expect(resolve).toHaveBeenCalledWith(insert);
+    expect(resolve).toHaveBeenCalledWith(update);
+    expect(resolve).toHaveBeenCalledWith(del);
+
+    expect(updateAvailableDataFileMetadata).toHaveBeenCalledTimes(2);
+    expect(updateAvailableDataFileMetadata).toHaveBeenCalledWith(
+      importFileId,
+      { name: 'file2.xml', path: 'path/to/file2.xml' },
+      IMPORT_STATUSES.PROCESSED,
+      {
+        hash: null,
+        recordCounts: {
+          deletes: 1,
+          errors: {
+            Failure: 2,
+            'Near-failure': 1,
+          },
+          inserts: 1,
+          updates: 1,
+        },
+        schema: null,
+      },
+    );
+    expect(updateAvailableDataFileMetadata).toHaveBeenCalledWith(
+      importFileId,
+      { name: 'file1.xml', path: 'path/to/file1.xml' },
+      IMPORT_STATUSES.PROCESSED,
+      {
+        hash: null,
+        recordCounts: {
+          deletes: 1,
+          errors: {
+            Failure: 2,
+            'Near-failure': 1,
+          },
+          inserts: 1,
+          updates: 1,
+        },
+        schema: null,
+      },
+    );
   });
 
   it('should handle errors when processing a file', async () => {
