b7b876cdae050f766f74c677dc0249aa6a142e52
Matt Bevilacqua <thewatermethod@gmail.com>
2025-01-13 15:52:42 -0500

    Improve test file

--- frontend/src/pages/RecipientRecord/pages/__tests__/Profile.js
+++ frontend/src/pages/RecipientRecord/pages/__tests__/Profile.js
@@ -6,6 +6,7 @@ import { render, screen } from '@testing-library/react';
 import Profile from '../Profile';
 import UserContext from '../../../../UserContext';
 import { GrantDataProvider } from '../GrantDataContext';
+import { mockRSSData } from '../../../../testHelpers';
 
 describe('Recipient Record - Profile', () => {
   const user = {
@@ -15,10 +16,8 @@ describe('Recipient Record - Profile', () => {
 
   const renderRecipientProfile = (summary) => {
     fetchMock.get('/api/recipient/1/region/1/leadership', []);
-    fetchMock.get('/api/monitoring/1/region/1/grant/asdfsjkfd', {
-      recipientId: 1, regionId: 1, reviewStatus: 'Compliant', reviewDate: '02/02/2024', reviewType: 'Follow-up', grant: 'asdfsjkfd',
-    });
-    fetchMock.get('/api/monitoring/class/1/region/1/grant/asdfsjkfd', {});
+    fetchMock.get('/api/feeds/item?tag=ttahub-class-thresholds', mockRSSData());
+
     render(
       <UserContext.Provider value={{ user }}>
         <GrantDataProvider>
@@ -33,6 +32,10 @@ describe('Recipient Record - Profile', () => {
   });
 
   it('renders the recipient summary approriately', async () => {
+    fetchMock.get('/api/monitoring/1/region/1/grant/asdfsjkfd', {
+      recipientId: 1, regionId: 1, reviewStatus: 'Compliant', reviewDate: '02/02/2024', reviewType: 'Follow-up', grant: 'asdfsjkfd',
+    });
+    fetchMock.get('/api/monitoring/class/1/region/1/grant/asdfsjkfd', {});
     const summary = {
       recipientId: '44',
       grants: [
@@ -55,9 +58,14 @@ describe('Recipient Record - Profile', () => {
 
     expect(fetchMock.called('/api/monitoring/1/region/1/grant/asdfsjkfd')).toBe(true);
     expect(fetchMock.called('/api/monitoring/class/1/region/1/grant/asdfsjkfd')).toBe(true);
+
+    expect(await screen.findByRole('heading', { name: /grant number asdfsjkfd/i })).toBeInTheDocument();
+    expect(await screen.findByRole('heading', { name: /Monitoring review/i })).toBeInTheDocument();
   });
 
   it('doesnt show the class/monitoring review headings if there is no grant data', async () => {
+    fetchMock.get('/api/monitoring/1/region/1/grant/asdfsjkfd', { body: null });
+    fetchMock.get('/api/monitoring/class/1/region/1/grant/asdfsjkfd', {});
     const summary = {
       recipientId: '44',
       grants: [{
@@ -71,5 +79,26 @@ describe('Recipient Record - Profile', () => {
 
     // should not see the heading "Grant number asdf"
     expect(screen.queryByRole('heading', { name: /grant number asdfsjkfd/i })).not.toBeInTheDocument();
+  });
+
+  it('displays class data', async () => {
+    fetchMock.get('/api/monitoring/1/region/1/grant/asdfsjkfd', { body: null });
+    fetchMock.get('/api/monitoring/class/1/region/1/grant/asdfsjkfd', {
+      recipientId: 1, regionId: 1, grantNumber: 'asdfsjkfd', received: '03/30/2023', ES: '5.8611', CO: '5.6296', IS: '3.2037',
+    });
+    const summary = {
+      recipientId: '44',
+      grants: [{
+        id: 1,
+        number: 'asdfsjkfd',
+        status: 'Froglike',
+        endDate: '2021-09-28',
+      }],
+    };
+    renderRecipientProfile(summary);
+
+    // should not see the heading "Grant number asdf"
+    expect(await screen.findByRole('heading', { name: /grant number asdfsjkfd/i })).toBeInTheDocument();
+    expect(await screen.findByRole('heading', { name: /CLASS® review/i })).toBeInTheDocument();
   });
 });
