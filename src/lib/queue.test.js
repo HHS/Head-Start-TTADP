58382e61b5ff06030718e7b163b6cf8e1824e754
GarrettEHill <garretthill@gmail.com>
2025-01-28 17:05:31 -0800

    Update queue.test.js

--- src/lib/queue.test.js
+++ src/lib/queue.test.js
@@ -1,4 +1,4 @@
-/* eslint-disableimport/first */
+/* eslint-disable import/first */
 const mockQueueConstructor = jest.fn(() => ({
   on: jest.fn(), // Mock the `.on` method
   close: jest.fn().mockResolvedValue(undefined), // Mock the `.close` method
