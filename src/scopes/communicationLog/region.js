407ff6d6a38a34c32a0ba8019629d974ad6886fa
nvms <jon@pye.rs>
2025-02-03 11:40:44 -0500

    fix scope

--- src/scopes/communicationLog/region.js
+++ src/scopes/communicationLog/region.js
@@ -1,14 +1,38 @@
 import { Op } from 'sequelize';
 import { sequelize } from '../../models';
 
+const constructLiteral = (regions) => {
+  const sql = `
+    (SELECT DISTINCT "communicationLogId"
+     FROM "CommunicationLogRecipients"
+     WHERE "recipientId" IN (
+       SELECT "id"
+       FROM "Recipients"
+       WHERE "deleted" = false
+         AND "id" IN (
+           SELECT "recipientId"
+           FROM "Grants"
+           WHERE "regionId" IN (${regions.map((regionId) => sequelize.escape(regionId)).join(',')})
+         )
+     )
+    )
+  `;
+
+  return sequelize.literal(sql);
+};
+
 export function withRegion(regions) {
-  return sequelize.where(
-    sequelize.literal(`(SELECT "communicationLogId" FROM "CommunicationLogRecipients" WHERE "recipientId" IN (SELECT "id" FROM "Recipients" WHERE "deleted" = false AND "id" IN (SELECT "recipientId" FROM "Grants" WHERE "regionId" IN (${regions.map((regionId) => sequelize.escape(regionId)).join(',')}))))`),
-  );
+  return {
+    id: {
+      [Op.in]: constructLiteral(regions),
+    },
+  };
 }
 
 export function withoutRegion(regions) {
-  return sequelize.where(
-    sequelize.literal(`(SELECT "communicationLogId" FROM "CommunicationLogRecipients" WHERE "recipientId" IN (SELECT "id" FROM "Recipients" WHERE "deleted" = false AND "id" IN (SELECT "recipientId" FROM "Grants" WHERE "regionId" NOT IN (${regions.map((regionId) => sequelize.escape(regionId)).join(',')}))))`),
-  );
+  return {
+    id: {
+      [Op.notIn]: constructLiteral(regions),
+    },
+  };
 }
