c4badb149fe7668a4952cdad66372d15fff20de4
nvms <jon@pye.rs>
2025-02-03 10:09:05 -0500

    mv stuff so no merge conflicts

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -44,7 +44,7 @@ import SomethingWentWrong from './components/SomethingWentWrong';
 import RecipientsWithNoTta from './pages/QADashboard/RecipientsWithNoTta';
 import RecipientsWithClassScoresAndGoals from './pages/QADashboard/RecipientsWithClassScoresAndGoals';
 import RecipientsWithOhsStandardFeiGoal from './pages/QADashboard/RecipientsWithOhsStandardFeiGoal';
-import RegionalCommunicationLog from './pages/RegionalCommunicationLog';
+import RegionalCommunicationLog from './pages/RegionalCommunicationLogDashboard';
 
 export default function Routes({
   alert,
