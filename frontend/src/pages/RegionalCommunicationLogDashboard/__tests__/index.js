3a51e46f227255da7ea5b082ff17215b70f6541f
nvms <jon@pye.rs>
2025-02-03 21:23:14 -0500

    add test

--- frontend/src/pages/RegionalCommunicationLogDashboard/__tests__/index.js
+++ frontend/src/pages/RegionalCommunicationLogDashboard/__tests__/index.js
@@ -16,6 +16,17 @@ describe('RegionalCommunicationLog', () => {
     }],
   };
 
+  const userWithTwoRegions = {
+    homeRegionId: 14,
+    permissions: [{
+      regionId: 1,
+      scopeId: SCOPE_IDS.READ_ACTIVITY_REPORTS,
+    }, {
+      regionId: 2,
+      scopeId: SCOPE_IDS.READ_ACTIVITY_REPORTS,
+    }],
+  };
+
   const renderComm = (u) => {
     const user = u || userCentralOffice;
     render(
@@ -31,8 +42,13 @@ describe('RegionalCommunicationLog', () => {
     );
   };
 
-  it('renders the page', async () => {
+  it('renders the page - user with one region', async () => {
     act(renderComm);
     expect(await screen.findByRole('heading', { name: /Communication logs - your region/i })).toBeInTheDocument();
+  });
+
+  it('renders the page - user with two regions', async () => {
+    act(() => renderComm(userWithTwoRegions));
+    expect(await screen.findByRole('heading', { name: /Communication logs - your regions/i })).toBeInTheDocument();
   });
 });
