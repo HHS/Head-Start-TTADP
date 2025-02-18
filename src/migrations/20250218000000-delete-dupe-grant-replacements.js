196a69704a06f9b482fdf72f3a7feca8bc4355d9
Nathan Powell <nathan.powell@adhocteam.us>
2025-02-17 21:58:30 -0800

    delete dupe grant replacements with null typeIds

---
+++ src/migrations/20250218000000-delete-dupe-grant-replacements.js
@@ -0,0 +1,69 @@
+const { prepMigration } = require('../lib/migration');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+      return queryInterface.sequelize.query(`
+        -- There are some old GrantReplacements records with null
+        -- grantReplacementTypeId values that are followed by another
+        -- GrantReplacements record that is the same except the
+        -- grantReplacementTypeId values are set. This removes the
+        -- null grantReplacementTypeId value set.
+
+        -- There are also many other GrantReplacements entries where
+        -- they have the same replacedGrantId, replacingGrantId, and
+        -- replacementDate, but different non-null grantReplacementTypeId
+        -- values. This leaves those intact because they are of potential
+        -- future interest.
+
+        -- Find the dupe sets but only choose those with at least one record
+        -- where "grantReplacementTypeId" IS NULL
+        DROP TABLE IF EXISTS dupe_grant_replacement_sets;
+        CREATE TEMP TABLE dupe_grant_replacement_sets
+        AS
+        SELECT
+          "replacedGrantId" old_grid,
+          "replacingGrantId" new_grid,
+          "replacementDate" replacedate,
+          COUNT(id) FILTER (WHERE "grantReplacementTypeId" IS NULL) nullid_cnt
+        FROM "GrantReplacements" gr
+        GROUP BY 1,2,3
+        HAVING BOOL_OR("grantReplacementTypeId" IS NULL)
+          AND COUNT(id) > 1
+        ;
+
+        -- delete the dupes with null grantReplacementTypeId
+        DROP TABLE IF EXISTS deleted_gr_dupes;
+        CREATE TEMP TABLE deleted_gr_dupes
+        AS
+        WITH updater AS (
+        DELETE FROM "GrantReplacements"
+        USING dupe_grant_replacement_sets
+        WHERE "replacedGrantId" = old_grid
+          AND "replacingGrantId" = new_grid
+          AND "replacementDate" = replacedate
+          AND "grantReplacementTypeId" IS NULL
+        RETURNING id deleted_grid
+        )
+        SELECT * FROM updater
+        ;
+        
+        
+
+        -- These should match
+        SELECT 1 ord,'dupes to delete' item, SUM(nullid_cnt) cnt FROM dupe_grant_replacement_sets
+        UNION
+        SELECT 2, 'dupes deleted' , COUNT(*)  FROM deleted_gr_dupes
+        ORDER BY 1
+        ;
+      `);
+    });
+  },
+
+  async down() {
+    // no rollbacks
+  },
+};
