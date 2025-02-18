122ad2acf5a1e29007d6eacf4c854547285cf783
nvms <jon@pye.rs>
2025-02-18 11:13:32 -0500

    rm unneeded branch

--- frontend/src/pages/RegionalCommunicationLogDashboard/constants.js
+++ frontend/src/pages/RegionalCommunicationLogDashboard/constants.js
@@ -7,7 +7,4 @@ const DASHBOARD_FILTER_CONFIG = [
   communicationDateFilter,
 ];
 
-// sort by display prop
-DASHBOARD_FILTER_CONFIG.sort((a, b) => a.display.localeCompare(b.display));
-
 export { DASHBOARD_FILTER_CONFIG };
