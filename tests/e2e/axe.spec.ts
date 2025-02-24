e6f6687cbc37761f37aa40f66209d521e80cae6d
Matt Bevilacqua <thewatermethod@gmail.com>
2025-02-24 16:14:16 -0500

    Break some AXE tests and try to bust CI cache

--- tests/e2e/axe.spec.ts
+++ tests/e2e/axe.spec.ts
@@ -2,28 +2,29 @@ import { test, expect, Page } from '@playwright/test';
 import AxeBuilder from '@axe-core/playwright';
 
 const axeUrls = [
-  'http://localhost:3000/',
-  'http://localhost:3000/activity-reports/new/activity-summary',
-  'http://localhost:3000/activity-reports/new/supporting-attachments',
-  'http://localhost:3000/activity-reports/new/goals-objectives',
-  'http://localhost:3000/activity-reports/new/next-steps',
-  'http://localhost:3000/activity-reports/new/review',
-  'http://localhost:3000/activity-reports/view/9999',
-  'http://localhost:3000/dashboards/regional-dashboard/activity-reports',
-  'http://localhost:3000/training-reports/not-started',
-  'http://localhost:3000/recipient-tta-records',
-  'http://localhost:3000/recipient-tta-records/9/region/1/profile',
-  'http://localhost:3000/recipient-tta-records/9/region/1/tta-history',
-  'http://localhost:3000/recipient-tta-records/9/region/1/rttapa',
-  'http://localhost:3000/recipient-tta-records/9/region/1/goals/new',
-  'http://localhost:3000/recipient-tta-records/9/region/1/rttapa/print',
+  { url: 'http://localhost:3000/', rules: [] },
+  { url: 'http://localhost:3000/activity-reports/new/activity-summary', rules: [] },
+  { url: 'http://localhost:3000/activity-reports/new/supporting-attachments', rules: [] },
+  { url: 'http://localhost:3000/activity-reports/new/goals-objectives', rules: [] },
+  { url: 'http://localhost:3000/activity-reports/new/next-steps', rules: [] },
+  { url: 'http://localhost:3000/activity-reports/new/review', rules: [] },
+  { url: 'http://localhost:3000/activity-reports/view/9999', rules: [] },
+  { url: 'http://localhost:3000/dashboards/regional-dashboard/activity-reports', rules: [] },
+  { url: 'http://localhost:3000/training-reports/not-started', rules: [] },
+  { url: 'http://localhost:3000/recipient-tta-records', rules: [] },
+  { url: 'http://localhost:3000/recipient-tta-records/9/region/1/profile', rules: [] },
+  { url: 'http://localhost:3000/recipient-tta-records/9/region/1/tta-history', rules: [] },
+  { url: 'http://localhost:3000/recipient-tta-records/9/region/1/rttapa', rules: [] },
+  { url: 'http://localhost:3000/recipient-tta-records/9/region/1/goals/new', rules: [] },
+  { url: 'http://localhost:3000/recipient-tta-records/9/region/1/rttapa/print', rules: [] },
 ];
 
-const testForAxeViolations = async (page: Page, url: string) => {
-  await page.goto(url, { waitUntil: 'domcontentloaded' });
+const testForAxeViolations = async (page: Page, url: { url: string, rules: string[] }) => {
+  await page.goto(url.url, { waitUntil: 'domcontentloaded' });
   await page.waitForTimeout(2500); // mirror the "sleep" in the original circle ci config
   const builder = new AxeBuilder({ page });
-  builder.disableRules(['aria-allowed-attr', 'empty-table-header']);  
+  // builder.disableRules([...url.rules, 'aria-allowed-attr', 'empty-table-header']);  
+  builder.disableRules(url.rules);
   const results = await builder.analyze();
   expect(results.violations).toEqual([]);
 };  
