9d0c36258a76bcf1ee2f34d0ed27cb10a4ffe134
Tom Meagher <tom.meagher@adhocteam.us>
2025-04-03 10:27:26 -0700

    linting

--- tools/parse-env.js
+++ tools/parse-env.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 
+/* eslint-disable no-continue */
+/* eslint-disable no-console */
+
 const fs = require('fs');
+const events = require('events');
 const readline = require('readline');
 const yargs = require('yargs/yargs');
 const { hideBin } = require('yargs/helpers');
 
-/* read in a yaml file, parse it, and write it to a file in env format*/
+/* read in a yaml file, parse it, and write it to a file in env format */
 
 function parseArgs() {
   return yargs(hideBin(process.argv))
@@ -13,13 +17,13 @@ function parseArgs() {
       alias: 'i',
       describe: 'yaml file to parse',
       type: 'string',
-      demandOption: true
+      demandOption: true,
     })
     .option('out_file', {
       alias: 'o',
       describe: 'env file to write',
       type: 'string',
-      demandOption: true
+      demandOption: true,
     })
     .check((argv) => {
       if (!fs.existsSync(argv.in_file)) {
@@ -30,48 +34,38 @@ function parseArgs() {
     .argv;
 }
 
-async function processLineByLine(env_file) {
-  console.log(`Parsing env file: ${env_file}`);
-  const fileStream = fs.createReadStream(env_file);
+async function processLineByLine(in_file, out_file) {
+  console.log(`Parsing yaml file: ${in_file}`);
+  console.log(`Writing env file: ${out_file}`);
+  const inStream = fs.createReadStream(in_file);
   const rl = readline.createInterface({
-    input: fileStream,
-    crlfDelay: Infinity
+    input: inStream,
+    crlfDelay: Infinity,
   });
-
-  const items = new Map();
-  for await (line of rl) {
-    line = line.trim().split('#')[0]
+  const outStream = fs.createWriteStream(out_file);
+  let i = 0;
+  /* eslint-disable-next-line no-restricted-syntax */
+  for await (const rawLine of rl) {
+    const line = rawLine.trim().split('#')[0];
     if (line.startsWith('#')) {
       continue; // skip comments
     }
     if (line.length === 0) {
       continue; // skip empty lines
     }
-    console.log(` - ${line}`);
     const [key, value] = line.split(':');
     if (key && value) {
-      const trimmedKey = key.trim();
-      const trimmedValue = value.trim();
-      items.set(trimmedKey, trimmedValue);
+      i += 1;
+      outStream.write(`${key.trim()}=${value.trim()}\n`);
     }
   }
-  return items;
-}
-
-async function writeEnvFile(out_file, items) {
-  console.log(`Writing env file: ${out_file}`);
-  const fileStream = fs.createWriteStream(out_file);
-  for (const [key, value] of items) {
-    fileStream.write(`${key}=${value}\n`);
-  };
-  fileStream.end();
-  console.log('Done writing env file');
+  outStream.end();
+  console.log(`Wrote ${i} lines`);
 }
 
 async function main() {
   const argv = parseArgs();
-  items = await processLineByLine(argv.in_file);
-  writeEnvFile(argv.out_file, items);
+  processLineByLine(argv.in_file, argv.out_file);
 }
 
 if (require.main === module) {
