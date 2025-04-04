4b7faf0423de0d5a0825cad6835322ed9fecba99
Krys Wisnaskas <krystyna@adhocteam.us>
2025-04-04 15:21:05 -0400

    [ttahub-3994] Test oidc integration

--- src/app.js
+++ src/app.js
@@ -6,12 +6,14 @@ import path from 'path';
 import join from 'url-join';
 import { omit } from 'lodash';
 import { INTERNAL_SERVER_ERROR } from 'http-codes';
+import axios from 'axios';
+import { SignJWT, importJWK } from 'jose';
 
 import { v4 as uuidv4 } from 'uuid';
 import crypto from 'crypto';
 
 import { registerEventListener } from './processHandler';
-import { hsesAuth } from './middleware/authMiddleware';
+// import { hsesAuth } from './middleware/authMiddleware';
 import { retrieveUserDetails } from './services/currentUser';
 import cookieSession from './middleware/sessionMiddleware';
 
@@ -75,6 +77,7 @@ app.use((req, res, next) => {
 });
 
 if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'dss') {
+  app.use(express.static(path.join(__dirname, '..', 'public')));
   app.use('/index.html', serveIndex);
   app.use(express.static(path.join(__dirname, '../client'), { index: false }));
 }
@@ -85,14 +88,61 @@ app.use('/api', require('./routes/apiDirectory').default);
 // Disable "X-Powered-By" header
 app.disable('x-powered-by');
 
+// Utility to sign client_assertion
+async function signClientAssertion() {
+  const base64 = process.env.PRIVATE_JWK_BASE64;
+  const jwk = JSON.parse(Buffer.from(base64, 'base64').toString('utf-8'));
+  const key = await importJWK(jwk, 'RS256');
+  const now = Math.floor(Date.now() / 1000);
+  const tokenEndpoint = `${process.env.AUTH_BASE}/oidc/api/openid_connect/token`;
+
+  return new SignJWT({
+    iss: process.env.AUTH_CLIENT_ID,
+    sub: process.env.AUTH_CLIENT_ID,
+    aud: tokenEndpoint,
+    jti: crypto.randomUUID(),
+  })
+    .setProtectedHeader({ alg: 'RS256' })
+    .setIssuedAt(now)
+    .setExpirationTime(now + 300)
+    .sign(key);
+}
+
 // TODO: change `app.get...` with `router.get...` once our oauth callback has been updated
 app.get(oauth2CallbackPath, cookieSession, async (req, res) => {
+  // try {
+  //   const user = await hsesAuth.code.getToken(req.originalUrl);
+  //   // user will have accessToken and refreshToken
+  //   logger.debug(`HSES AccessToken: ${user.accessToken}`);
   try {
-    const user = await hsesAuth.code.getToken(req.originalUrl);
-    // user will have accessToken and refreshToken
-    logger.debug(`HSES AccessToken: ${user.accessToken}`);
+    const { code } = req.query;
+    const redirectUri = `${process.env.REDIRECT_URI_HOST}/oauth2-client/login/oauth2/code/`;
+    const tokenEndpoint = `${process.env.AUTH_BASE}/oidc/api/openid_connect/token`;
 
-    const dbUser = await retrieveUserDetails(user);
+    const clientAssertion = await signClientAssertion();
+
+    const params = new URLSearchParams({
+      grant_type: 'authorization_code',
+      code,
+      redirect_uri: redirectUri,
+      client_id: process.env.AUTH_CLIENT_ID,
+      client_assertion_type: 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+      client_assertion: clientAssertion,
+    });
+
+    const tokenResponse = await axios.post(tokenEndpoint, params.toString(), {
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    });
+
+    const { accessToken } = tokenResponse.data;
+    // Retrieve user info
+    const userInfo = await axios.get(`${process.env.AUTH_BASE}/oidc/api/openid_connect/userinfo`, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    // const dbUser = await retrieveUserDetails(user);
+    const dbUser = await retrieveUserDetails(userInfo.data);
     req.session.userId = dbUser.id;
     req.session.uuid = uuidv4();
     auditLogger.info(`User ${dbUser.id} logged in`);
