5981524dc97e117dabc4a039ccf69c2d18c99406
nvms <jon@pye.rs>
2025-04-05 11:27:10 -0400

    fix migration, more model cleanup

--- src/migrations/20250405143306-remove-similarity-tables.js
+++ src/migrations/20250405143306-remove-similarity-tables.js
@@ -9,8 +9,19 @@ module.exports = {
       await prepMigration(queryInterface, transaction, sessionSig);
 
       await queryInterface.dropTable('ZALGoalSimilarityGroupGoals', { transaction });
+      await queryInterface.sequelize.query('DROP FUNCTION IF EXISTS "public"."ZALNoUpdateFGoalSimilarityGroupGoals" ()', { transaction });
+      await queryInterface.sequelize.query('DROP FUNCTION IF EXISTS "public"."ZALNoTruncateFGoalSimilarityGroupGoals" ()', { transaction });
+      await queryInterface.sequelize.query('DROP FUNCTION IF EXISTS "public"."ZALNoDeleteFGoalSimilarityGroupGoals" ()', { transaction });
+
       await queryInterface.dropTable('ZALGoalSimilarityGroups', { transaction });
+      await queryInterface.sequelize.query('DROP FUNCTION IF EXISTS "public"."ZALNoUpdateFGoalSimilarityGroups" ()', { transaction });
+      await queryInterface.sequelize.query('DROP FUNCTION IF EXISTS "public"."ZALNoTruncateFGoalSimilarityGroups" ()', { transaction });
+      await queryInterface.sequelize.query('DROP FUNCTION IF EXISTS "public"."ZALNoDeleteFGoalSimilarityGroups" ()', { transaction });
+
       await queryInterface.dropTable('ZALSimScoreGoalCaches', { transaction });
+      await queryInterface.sequelize.query('DROP FUNCTION IF EXISTS "public"."ZALNoUpdateFSimScoreGoalCaches" ()', { transaction });
+      await queryInterface.sequelize.query('DROP FUNCTION IF EXISTS "public"."ZALNoTruncateFSimScoreGoalCaches" ()', { transaction });
+      await queryInterface.sequelize.query('DROP FUNCTION IF EXISTS "public"."ZALNoDeleteFSimScoreGoalCaches" ()', { transaction });
 
       await queryInterface.dropTable('GoalSimilarityGroupGoals', { transaction });
       await queryInterface.dropTable('GoalSimilarityGroups', { transaction });
