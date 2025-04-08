633c8b6278f2119065efe5c1fc76e1726a7d2e8f
Krys Wisnaskas <krystyna@adhocteam.us>
2025-04-08 15:48:19 -0400

    Modify app.test.js

--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,8 @@
 import request from 'supertest';
 import axios from 'axios';
+import * as currentUserService from './services/currentUser';
 import app from './app';
-import { retrieveUserDetails } from './services/currentUser';
+// import { retrieveUserDetails } from './services/currentUser';
 
 jest.mock('./services/currentUser');
 jest.mock('axios');
@@ -13,6 +14,14 @@ describe('TTA Hub server', () => {
   beforeEach(() => {
     jest.resetModules();
     process.env = { ...ORIGINAL_ENV };
+
+    // Ensure all env vars used in the route are set
+    process.env.NODE_ENV = 'test';
+    process.env.BYPASS_AUTH = 'false';
+    process.env.REDIRECT_URI_HOST = 'http://localhost:3000';
+    process.env.AUTH_BASE = 'https://mock-auth.com';
+    process.env.AUTH_CLIENT_ID = 'mock-client-id';
+    process.env.TTA_SMART_HUB_URI = 'http://localhost:3000';
 
     // Mock token response
     axios.post.mockResolvedValue({
@@ -29,22 +38,23 @@ describe('TTA Hub server', () => {
       },
     });
 
-    retrieveUserDetails.mockResolvedValue({
+    currentUserService.retrieveUserDetails.mockResolvedValue({
       id: 1,
     });
   });
 
-  afterAll(() => {
-    process.env = ORIGINAL_ENV;
-  });
+  // afterAll(() => {
+  //   process.env = ORIGINAL_ENV;
+  // });
 
   afterEach(() => {
     jest.resetAllMocks();
+    process.env = ORIGINAL_ENV;
   });
 
   test('retrieves user details to login', async () => {
-    process.env.NODE_ENV = 'test';
-    process.env.BYPASS_AUTH = 'false';
+    // process.env.NODE_ENV = 'test';
+    // process.env.BYPASS_AUTH = 'false';
 
     const resp = await request(app)
       .get('/oauth2-client/login/oauth2/code/?code=test-code')
@@ -52,7 +62,7 @@ describe('TTA Hub server', () => {
 
     expect(axios.post).toHaveBeenCalled();
     expect(axios.get).toHaveBeenCalled();
-    expect(retrieveUserDetails).toHaveBeenCalled();
+    expect(currentUserService.retrieveUserDetails).toHaveBeenCalled();
     expect(resp.status).toBe(302);
   });
 });
