5f05921a52ccca1dd034e0e9ef51a24123ed5273
Tom Meagher <tom.meagher@adhocteam.us>
2025-04-11 01:59:58 -0700

    lint

--- src/tools/queryMonitoringCLI.js
+++ src/tools/queryMonitoringCLI.js
@@ -3,15 +3,15 @@
 
 import { sequelize } from '../models';
 
-const result = await sequelize.query(
-  'SELECT LEFT(r.name,35) recipient, "regionId" region, \
-        COUNT(*) cnt FROM "Goals" g \
-        JOIN "Grants" gr ON g."grantId" = gr.id \
-        JOIN "Recipients" r ON gr."recipientId" = r.id \
-        WHERE "createdVia" = \'monitoring\' \
-        AND g."createdAt" > (NOW() - INTERVAL \'24 hours\') \
-        GROUP BY 1,2 \
-        ORDER BY 2,1;',
-);
+const result = await sequelize.literal(`(
+  SELECT LEFT(r.name,35) recipient, "regionId" region,
+    COUNT(*) cnt FROM "Goals" g
+    JOIN "Grants" gr ON g."grantId" = gr.id
+    JOIN "Recipients" r ON gr."recipientId" = r.id
+    WHERE "createdVia" = 'monitoring'
+    AND g."createdAt" > (NOW() - INTERVAL '24 hours')
+    GROUP BY 1,2
+    ORDER BY 2,1
+  )`);
 
 console.log(result);
