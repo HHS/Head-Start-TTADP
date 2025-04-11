3db120a8b64d3adb9d709a169d07ec12597297aa
Tom Meagher <tom.meagher@adhocteam.us>
2025-04-11 00:02:53 -0700

    lint

--- src/tools/queryMonitoringCLI.js
+++ src/tools/queryMonitoringCLI.js
@@ -1,19 +1,17 @@
-import sequelize from '../models';
-
 /* eslint-disable no-multi-str */
-/* eslint-disable quotes */
-/* eslint-disable no-restricted-syntax */
 /* eslint-disable no-console */
 
-const result = await sequelize.query('SELECT LEFT(r.name,35) recipient, "regionId" region, \
-    COUNT(*) cnt FROM "Goals" g \
-    JOIN "Grants" gr ON g."grantId" = gr.id \
-    JOIN "Recipients" r ON gr."recipientId" = r.id \
-    WHERE "createdVia" = \'monitoring\' \
-    AND g."createdAt" > (NOW() - INTERVAL \'24 hours\') \
-    GROUP BY 1,2 \
-    ORDER BY 2,1;');
+import { sequelize } from '../models';
+
+const result = sequelize.query(
+  'SELECT LEFT(r.name,35) recipient, "regionId" region, \
+        COUNT(*) cnt FROM "Goals" g \
+        JOIN "Grants" gr ON g."grantId" = gr.id \
+        JOIN "Recipients" r ON gr."recipientId" = r.id \
+        WHERE "createdVia" = \'monitoring\' \
+        AND g."createdAt" > (NOW() - INTERVAL \'24 hours\') \
+        GROUP BY 1,2 \
+        ORDER BY 2,1;',
+);
 
 console.log(result);
-
-// return sequelize.queryInterface.sequelize.query(
