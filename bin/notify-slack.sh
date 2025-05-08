b1f724f6544506e6b45978211d0e886ce2ab225d
Tom Meagher <tom.meagher@adhocteam.us>
2025-05-08 16:07:15 -0700

    quotes

--- bin/notify-slack.sh
+++ bin/notify-slack.sh
@@ -22,7 +22,7 @@ echo "Sending to #${SLACK_CHANNEL}: ${MESSAGE_TEXT}"
 response=$(curl -s -X POST \
     -H "Authorization: Bearer ${SLACK_BOT_TOKEN}" \
     -H 'Content-type: application/json;charset=utf-8' \
-    --data "{
+    --data "{ \
         \"channel\": \"${SLACK_CHANNEL}\",
         \"text\": \"${MESSAGE_TEXT}\"
      }" \
