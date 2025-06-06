87e064a2e0eccf9e25fbea9ca32ceefb110c58aa
Krys Wisnaskas <krystyna@adhocteam.us>
2025-06-06 15:57:05 -0400

    Wait for server

--- tests/e2e/init/globalSetup.ts
+++ tests/e2e/init/globalSetup.ts
@@ -1,6 +1,15 @@
+import waitOn from 'wait-on';
+
 async function globalSetup() {
-  // ...
-  process.env.SEND_NOTIFICATIONS = ''; 
+  process.env.SEND_NOTIFICATIONS = '';
+
+  // Wait for the reseed server to be up
+  await waitOn({
+    resources: ['http://localhost:9999/testingOnly/reseed'],
+    timeout: 15000, // 15 seconds max
+    interval: 250,
+    headers: { accept: 'application/json' },
+  });
 }
 
 export default globalSetup;
