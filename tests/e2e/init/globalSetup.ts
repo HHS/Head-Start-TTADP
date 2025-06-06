07915cb9e15d058baffd2bc5bbb3a5cb6c43e458
Krys Wisnaskas <krystyna@adhocteam.us>
2025-06-06 17:26:59 -0400

    Update globalSetup

--- tests/e2e/init/globalSetup.ts
+++ tests/e2e/init/globalSetup.ts
@@ -1,14 +1,34 @@
+import { spawn } from 'child_process';
 import waitOn from 'wait-on';
+
+let serverProcess: ReturnType<typeof spawn>;
 
 async function globalSetup() {
   process.env.SEND_NOTIFICATIONS = '';
 
-  // Wait for the reseed server to be up
-  await waitOn({
-    resources: ['http://localhost:9999/testingOnly/reseed'],
-    timeout: 15000, // 15 seconds max
-    interval: 250,
-    headers: { accept: 'application/json' },
+  // Start test server
+  serverProcess = spawn('node', ['src/testingOnly.js'], {
+    stdio: 'inherit', // show logs
+    env: { ...process.env, NODE_ENV: 'test' },
+  });
+
+  // Wait until /testingOnly/reseed is ready
+  await new Promise<void>((resolve, reject) => {
+    waitOn(
+      {
+        resources: ['http://localhost:9999/testingOnly/reseed'],
+        timeout: 20000,
+      },
+      (err) => {
+        if (err) {
+          console.error('Timed out waiting for testing server');
+          reject(err);
+        } else {
+          console.log('Testing server is up!');
+          resolve();
+        }
+      }
+    );
   });
 }
 
