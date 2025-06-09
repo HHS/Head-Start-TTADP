50e34b9b998a68b0f004182b725852581c4b7e8d
Tom Meagher <tom.meagher@adhocteam.us>
2025-06-09 14:36:02 -0700

    syntax

--- src/routes/testingOnly/handlers.ts
+++ src/routes/testingOnly/handlers.ts
@@ -2,16 +2,16 @@ import { Request, Response } from 'express';
 import handleErrors from '../../lib/apiErrorHandler';
 import { reseed, query } from '../../../tests/utils/dbUtils';
 
-export async function reseedDB(req: Request, res: Response) {
+export const reseedDB = async (req: Request, res: Response) => {
   try {
     const result = await reseed();
     res.status(result ? 200 : 500).json(result);
   } catch (e) {
     await handleErrors(req, res, e, 'reseedDB');
   }
-}
+};
 
-export async function queryDB(req: Request, res: Response) {
+export const queryDB = async (req: Request, res: Response) => {
   try {
     if (!req.body) { throw new Error('req.body is required'); }
 
@@ -24,4 +24,4 @@ export async function queryDB(req: Request, res: Response) {
   } catch (e) {
     await handleErrors(req, res, e, 'queryDB');
   }
-}
+};
