cfcec2d9f4bec94af78b8bbfe7d8801d32f2e4bc
Tom Meagher <tom.meagher@adhocteam.us>
2025-06-09 15:54:00 -0700

    debug

--- src/routes/testingOnly/index.js
+++ src/routes/testingOnly/index.js
@@ -2,10 +2,11 @@ import express from 'express';
 import { reseedDB, queryDB } from './handlers';
 import testingOnly from '../../middleware/testingOnlyMiddleware';
 
-const router = express.Router();
-router.use(express.json({ limit: '2MB' }));
+// eslint-disable-next-line import/prefer-default-export
+const testRouter = express.Router();
+testRouter.use(express.json({ limit: '2MB' }));
 // must run outside of transaction wrapper as some of the action will run in a child process
-router.get('/reseed', testingOnly, reseedDB);
-router.post('/query', testingOnly, queryDB);
+testRouter.get('/reseed', testingOnly, reseedDB);
+testRouter.post('/query', testingOnly, queryDB);
 
-export default router;
+export default testRouter;
--- src/testingOnly.js
+++ src/testingOnly.js
@@ -2,17 +2,14 @@ import express from 'express';
 
 import {} from 'dotenv/config';
 import { auditLogger } from './logger';
-import testingRouter from './routes/testingOnly';
+import testRouter from './routes/testingOnly';
 
 const app = express();
 
-app.use('/testingOnly', testingRouter);
+app.use('/testingOnly', testRouter);
 
 app.listen(9999, '0.0.0.0', () => {
   auditLogger.info('TestingOnly listening on port 9999');
 });
 
 export default app;
-
-// // eslint-disable-next-line import/prefer-default-export
-// export const doNothing = () => {};
