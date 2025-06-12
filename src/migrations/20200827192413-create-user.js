698913d5e905e9124596c81518edfef82c96db5d
Tom Meagher <tom.meagher@adhocteam.us>
2025-06-11 18:40:37 -0700

    debug

--- src/migrations/20200827192413-create-user.js
+++ src/migrations/20200827192413-create-user.js
@@ -1,5 +1,5 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => queryInterface.createTable('Users', {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('Users', {
     id: {
       type: Sequelize.INTEGER,
       allowNull: false,
@@ -28,5 +28,5 @@ module.exports = {
       type: Sequelize.DATE,
     },
   }),
-  down: async (queryInterface) => queryInterface.dropTable('Users'),
+  down: (queryInterface) => queryInterface.dropTable('Users'),
 };
