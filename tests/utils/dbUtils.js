6416984b8b7fb2f0aca57f2d750200d63be32ee3
Tom Meagher <tom.meagher@adhocteam.us>
2025-06-16 07:44:58 -0700

    back to old method

--- tests/utils/dbUtils.js
+++ tests/utils/dbUtils.js
@@ -1,9 +1,8 @@
-import { QueryInterface, Sequelize } from 'sequelize';
-import { Umzug, SequelizeStorage, MigrationError, MigrationParams, RunnableMigration } from 'umzug';
+import { Sequelize }  from 'sequelize';
+import { Umzug, SequelizeStorage, MigrationError } from 'umzug';
+import { calledFromTestFileOrDirectory } from './testOnly';
 import { auditLogger } from '../../src/logger';
 import configs from '../../config/config';
-import fs from 'fs'
-import sequelize from 'sequelize/types/sequelize';
 
 const getDB = () => {
   const env = process.env.NODE_ENV || 'development';
@@ -24,40 +23,158 @@ const getDB = () => {
 
 const db = getDB();
 
-export async function clear() {
+const clear = async () => {
   await db.sequelize.query(`
     DROP SCHEMA public CASCADE;
     CREATE SCHEMA public;
   `);
 };
 
-export async function loadMigrations(migrationSet) {
-
-  const migrationDir = `./src/${migrationSet}`;
-  const migrations = fs.readdirSync(migrationDir).map(name => {
-    const migration = import(`../../src/${migrationSet}/${name}`)
-    return {
-      up: async (params) => await module.up(params.context, sequelize),
-      down: async (params) => await migration.down(params.context, sequelize),
-      name,
-    }
-  })
+const loadMigrations = async (migrationSet:string): Promise<void> => {
+  const migrationPattern = '*.js'; // File extension pattern for migration files
+  const migrationDir = `src/${migrationSet}/${migrationPattern}`; // path.join('./', migrationSet, migrationPattern);
 
   const umzug = new Umzug({
-    migrations,
-    context: db.sequelize.getQueryInterface(),
     storage: new SequelizeStorage({ sequelize: db.sequelize }),
-    logger: console,
+    migrations: {
+      glob: migrationDir,
+      resolve: ({ name, path, context }) => {
+        const migration = require(path);
+        return {
+            name,
+            up: async () => migration.up(context, db.Sequelize),
+            down: async () => migration.down(context, db.Sequelize),
+        };
+      },
+    },
+    context: db.sequelize.getQueryInterface(),
+    logger: { error: console.error, warn: () => {}, info: () => {}, debug: () => {} },
   });
 
+  try {
+    const migrations = await umzug.up();
+
+    auditLogger.log('info', `Successfully executed ${migrations.length} migrations.`);
+  } catch (error) {
+    if (error instanceof MigrationError) {
+      auditLogger.error('Error executing migrations:', error.cause, '\n', error);
+    }
+    auditLogger.error('Error executing migrations:', error);
+    throw error;
+  }
 }
 
-export async function reseed() {
-  await clear();
-  await loadMigrations('migrations');
-  await loadMigrations('seeders');
+export const reseed = async () => {
+  try {
+    if (calledFromTestFileOrDirectory()) {
+      await clear();
+      await loadMigrations('migrations');
+      await loadMigrations('seeders');
+      return true;
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
 };
 
-export async function query(command, options = {}) {
-  return await db.sequelize.query(command, options);
+export const query = async(command, options = {}) => {
+  try {
+    if (calledFromTestFileOrDirectory()) {
+      return await db.sequelize.query(command, options);
+    }
+    return { error: 'called from non-testing file or directory' };
+  } catch (error) {
+    return { error };
+  }
+};
+import { Sequelize }  from 'sequelize';
+import { Umzug, SequelizeStorage, MigrationError } from 'umzug';
+import { calledFromTestFileOrDirectory } from './testOnly';
+import { auditLogger } from '../../src/logger';
+import configs from '../../config/config';
+
+const getDB = () => {
+  const env = process.env.NODE_ENV || 'development';
+  const config = configs[env];
+
+  let sequelize;
+  if (config.use_env_variable) {
+    sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  } else {
+    sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }
+
+  return {
+    sequelize,
+    Sequelize,
+  };
+}
+
+const db = getDB();
+
+const clear = async () => {
+  await db.sequelize.query(`
+    DROP SCHEMA public CASCADE;
+    CREATE SCHEMA public;
+  `);
+};
+
+const loadMigrations = async (migrationSet:string): Promise<void> => {
+  const migrationPattern = '*.js'; // File extension pattern for migration files
+  const migrationDir = `src/${migrationSet}/${migrationPattern}`; // path.join('./', migrationSet, migrationPattern);
+
+  const umzug = new Umzug({
+    storage: new SequelizeStorage({ sequelize: db.sequelize }),
+    migrations: {
+      glob: migrationDir,
+      resolve: ({ name, path, context }) => {
+        const migration = require(path);
+        return {
+            name,
+            up: async () => migration.up(context, db.Sequelize),
+            down: async () => migration.down(context, db.Sequelize),
+        };
+      },
+    },
+    context: db.sequelize.getQueryInterface(),
+    logger: { error: console.error, warn: () => {}, info: () => {}, debug: () => {} },
+  });
+
+  try {
+    const migrations = await umzug.up();
+
+    auditLogger.log('info', `Successfully executed ${migrations.length} migrations.`);
+  } catch (error) {
+    if (error instanceof MigrationError) {
+      auditLogger.error('Error executing migrations:', error.cause, '\n', error);
+    }
+    auditLogger.error('Error executing migrations:', error);
+    throw error;
+  }
+}
+
+export const reseed = async () => {
+  try {
+    if (calledFromTestFileOrDirectory()) {
+      await clear();
+      await loadMigrations('migrations');
+      await loadMigrations('seeders');
+      return true;
+    }
+    return false;
+  } catch (error) {
+    return false;
+  }
+};
+
+export const query = async(command, options = {}) => {
+  try {
+    if (calledFromTestFileOrDirectory()) {
+      return await db.sequelize.query(command, options);
+    }
+    return { error: 'called from non-testing file or directory' };
+  } catch (error) {
+    return { error };
+  }
 };
