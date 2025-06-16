298d0746362d6bc3dd1f5f59a4febc968ff3e25d
Krys Wisnaskas <krystyna@adhocteam.us>
2025-06-16 16:19:39 -0400

    [TTAHUB-4090] Remove unused CDI assignment in the Admin section

--- src/services/grant.js
+++ src/services/grant.js
@@ -1,32 +1,6 @@
 import { Op } from 'sequelize';
 import { sequelize, Grant, Recipient } from '../models';
 
-export async function cdiGrants(unassigned, active) {
-  const where = [{ cdi: true }];
-
-  if (unassigned === 'true') {
-    where.push({ regionId: 13 });
-  }
-
-  if (active === 'true') {
-    where.push({ status: 'Active' });
-  }
-
-  return Grant.findAll({
-    attributes: ['id', 'cdi', 'number', 'status', 'startDate', 'endDate', 'regionId', 'recipientId'],
-    where: { [Op.and]: where },
-    include: [
-      {
-        model: Recipient,
-        as: 'recipient',
-      },
-    ],
-    order: [
-      ['id', 'ASC'],
-    ],
-  });
-}
-
 export async function statesByGrantRegion(regions) {
   const grants = await Grant.unscoped().findAll({
     attributes: [
@@ -56,16 +30,4 @@ export async function grantById(grantId) {
       id: grantId,
     },
   });
-}
-
-export async function assignCDIGrant(grant, regionId, recipientId) {
-  const updatedGrant = await grant.update({
-    regionId,
-    recipientId,
-  }, {
-    fields: ['regionId', 'recipientId'],
-    returning: true,
-    individualHooks: true,
-  });
-  return updatedGrant;
 }
