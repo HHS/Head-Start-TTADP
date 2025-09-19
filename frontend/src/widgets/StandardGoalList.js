22d5df38e731d81457b97f36e5383d714f0f517c
Adam Levin <adam.levin@adhocteam.us>
2025-09-19 16:29:41 -0400

    css fix

--- frontend/src/widgets/StandardGoalList.js
+++ frontend/src/widgets/StandardGoalList.js
@@ -18,11 +18,11 @@ export function StandardGoalsListTable({
   return (
     <Container paddingX={1} paddingY={1} loading={loading} loadingLabel="Goals list loading">
       <div className="ttahub-goal-counts-table landing inline-size-auto maxw-full margin-bottom-0">
-        <div className="display-flex flex-wrap flex-align-center">
-          <WidgetH2 classNames="padding-bottom-0 padding-3">
+        <div className="display-flex flex-wrap flex-align-center padding-3">
+          <WidgetH2 classNames="padding-0">
             {title || 'Goals categories in Activity Reports'}
           </WidgetH2>
-          <p className="usa-prose padding-x-3 padding-bottom-3 padding-top-1 margin-0">Data reflects activity starting on 09/01/2025.</p>
+          <p className="usa-prose padding-0 margin-0">Data reflects activity starting on 09/01/2025.</p>
         </div>
         <div
           className="usa-table-container--scrollable"
