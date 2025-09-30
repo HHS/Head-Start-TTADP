13d81b6bf41795cd5ba1c6851443008a932bc67a
Tom Meagher <tom.meagher@adhocteam.us>
2025-09-30 08:24:23 -0700

    adjust logging

--- src/tools/createMonitoringGoals.js
+++ src/tools/createMonitoringGoals.js
@@ -243,7 +243,7 @@ const createMonitoringGoals = async () => {
     });
   } catch (error) {
     // eslint-disable-next-line no-console
-    console.log(`Error creating monitoring: ${error.message} | Stack Trace: ${error.stack}`);
+    console.log(`Error creating monitoring: ${error} | Stack Trace: ${error.stack}`);
     auditLogger.error(`Error creating monitoring: ${error.message} | Stack Trace: ${error.stack}`);
     throw error;
   }
