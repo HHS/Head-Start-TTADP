c86659ce5d57296d185fe6fc242611f913582edf
thewatermethod@gmail.com <thewatermethod@gmail.com>
2025-10-01 13:07:25 -0400

    add new conditional session form and a UI test for it

---
+++ frontend/src/pages/SessionReportFacilitation/index.js
@@ -0,0 +1,124 @@
+import React from 'react';
+import ReactRouterPropTypes from 'react-router-prop-types';
+import { useForm } from 'react-hook-form';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
+import {
+  Button, ErrorMessage, Fieldset, FormGroup, Radio,
+} from '@trussworks/react-uswds';
+import { ErrorMessage as ReactHookFormError } from '@hookform/error-message';
+import { Helmet } from 'react-helmet';
+import useFetch from '../../hooks/useFetch';
+import Container from '../../components/Container';
+import BackLink from '../../components/BackLink';
+import IndicatesRequiredField from '../../components/IndicatesRequiredField';
+import Req from '../../components/Req';
+import { createSession } from '../../fetchers/session';
+import { eventById } from '../../fetchers/event';
+import { ROUTES } from '../../Constants';
+
+const TRAINING_REPORT_URL = '/training-reports/not-started';
+const ERROR_MESSAGE = 'Select who is providing the training';
+const INPUT_NAME = 'facilitation';
+
+export default function SessionReportFacilitation({ match }) {
+  const { params: { trainingReportId } } = match;
+  const history = useHistory();
+  const hookForm = useForm({
+    mode: 'onBlur',
+    defaultValues: {},
+  });
+
+  const { data: trainingReport, error, statusCode } = useFetch(
+    null,
+    async () => eventById(trainingReportId),
+    [trainingReportId],
+  );
+
+  if (error) {
+    history.push(`${ROUTES.SOMETHING_WENT_WRONG}/${statusCode}`);
+  }
+
+  if (!trainingReport) {
+    return 'Loading...';
+  }
+
+  const { register, handleSubmit, errors } = hookForm;
+
+  const onSubmit = async (data) => {
+    try {
+      await createSession(trainingReportId, data);
+      history.push(TRAINING_REPORT_URL);
+    } catch (err) {
+      history.push(`${ROUTES.SOMETHING_WENT_WRONG}/${statusCode}`);
+    }
+  };
+
+  const fieldError = errors[INPUT_NAME];
+
+  return (
+    <>
+      <Helmet>
+        <title>Training Report - Create a session</title>
+      </Helmet>
+      <BackLink to={TRAINING_REPORT_URL}>Back to Training Reports</BackLink>
+      <h1 className="landing margin-bottom-1">Training Report - Create a session</h1>
+      <p className="margin-0 margin-bottom-4 font-serif-md text-normal">
+        {trainingReport.data.eventId}
+        :
+        {' '}
+        { trainingReport.data.eventName }
+      </p>
+      <Container className="maxw-tablet" paddingX={4} paddingY={5}>
+        <h2 className="font-serif-xl margin-top-0 margin-bottom-1">Training facilitation</h2>
+        <IndicatesRequiredField className="margin-bottom-3" />
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <FormGroup error={fieldError}>
+            <Fieldset className="margin-bottom-3">
+              <legend className="margin-bottom-1">
+                Who is providing the training?
+                {' '}
+                <Req />
+              </legend>
+              <ReactHookFormError
+                errors={errors}
+                name={INPUT_NAME}
+                render={({ message }) => <ErrorMessage>{message}</ErrorMessage>}
+              />
+              <Radio
+                value="national_center"
+                label="National Center"
+                id={`${INPUT_NAME}-national_center`}
+                name={INPUT_NAME}
+                inputRef={register({ required: ERROR_MESSAGE })}
+                className="margin-bottom-2"
+              />
+
+              <Radio
+                value="regional_tta_staff"
+                label="Regional TTA staff"
+                id={`${INPUT_NAME}-regional_tta_staff`}
+                name={INPUT_NAME}
+                inputRef={register({ required: ERROR_MESSAGE })}
+                className="margin-bottom-2"
+              />
+              <Radio
+                value="both"
+                label="Both (National Center and Regional TTA staff)"
+                id={`${INPUT_NAME}-both`}
+                name={INPUT_NAME}
+                inputRef={register({ required: ERROR_MESSAGE })}
+              />
+            </Fieldset>
+          </FormGroup>
+          <Button type="submit">Create session</Button>
+          <Link className="usa-button usa-button--outline" to={TRAINING_REPORT_URL}>Cancel</Link>
+        </form>
+      </Container>
+    </>
+  );
+}
+
+SessionReportFacilitation.propTypes = {
+  match: ReactRouterPropTypes.match.isRequired,
+};
