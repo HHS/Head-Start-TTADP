5523ecfefc21ad5e59eb97c8703004d60a406df4
Andrew Steele <andrew.steele@adhocteam.us>
2025-11-12 14:09:54 -0500

    Fixing a missed test

--- frontend/src/pages/RegionalCommunicationLogDashboard/__tests__/index.js
+++ frontend/src/pages/RegionalCommunicationLogDashboard/__tests__/index.js
@@ -218,7 +218,7 @@ describe('RegionalCommunicationLogDashboard', () => {
     await waitFor(() => expect(screen.getByRole('button', { name: /log id\. activate to sort ascending/i })).toBeInTheDocument());
     await waitFor(() => expect(screen.getByRole('link', { name: 'R01-CL-0001' })).toBeInTheDocument());
 
-    const actions = screen.getByRole('button', { name: 'Actions for Communication Log' });
+    const actions = screen.getByRole('button', { name: 'Actions for R01-CL-0001' });
     act(() => userEvent.click(actions));
 
     await waitFor(() => expect(screen.getAllByRole('button', { name: /view/i })[0]).toBeInTheDocument());
