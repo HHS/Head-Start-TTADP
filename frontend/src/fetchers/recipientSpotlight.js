378d2f4b120999d9b48b939bb42f37d11e839c50
Adam Levin <adam.levin@adhocteam.us>
2025-12-17 13:55:12 -0500

    more logging

--- frontend/src/fetchers/recipientSpotlight.js
+++ frontend/src/fetchers/recipientSpotlight.js
@@ -9,12 +9,22 @@ export const getRecipientSpotlight = async (sortBy = 'recipientName', sortDir = 
   console.log('[recipientSpotlight.js] getRecipientSpotlight called with:', {
     sortBy, sortDir, offset, filters, limit,
   });
+  // eslint-disable-next-line no-console
+  console.trace('[recipientSpotlight.js] Call stack:');
   const limitParam = limit !== undefined && limit !== null ? `&limit=${limit}` : '';
   // eslint-disable-next-line no-console
   console.log('[recipientSpotlight.js] limitParam:', limitParam, '| limit was:', limit);
   const queryString = `?sortBy=${sortBy}&direction=${sortDir}&offset=${offset}${limitParam}${filters ? `&${filters}` : ''}`;
   // eslint-disable-next-line no-console
   console.log('[recipientSpotlight.js] Full URL:', `${recipientUrl}${queryString}`);
+
+  if (limit === 10) {
+    // eslint-disable-next-line no-console
+    console.error('[recipientSpotlight.js] ⚠️ WARNING: limit is 10! This should not happen from RegionalDashboard');
+    // eslint-disable-next-line no-console
+    console.trace('[recipientSpotlight.js] Stack trace for limit=10:');
+  }
+
   const spotlight = await get(`${recipientUrl}${queryString}`);
   return spotlight.json();
 };
