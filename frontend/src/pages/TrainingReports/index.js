02f1455632b3a1a123151a7bb720fbc10f973298
thewatermethod@gmail.com <thewatermethod@gmail.com>
2026-01-05 11:54:20 -0500

    Add alert and success messages

--- frontend/src/pages/TrainingReports/index.js
+++ frontend/src/pages/TrainingReports/index.js
@@ -35,7 +35,7 @@ const tabValues = Object.keys(TRAINING_REPORT_STATUSES_URL_PARAMS).map((status) 
 }));
 
 const MESSAGE_TEMPLATES = {
-  eventSubmitted: (eventId, dateStr) => (
+  eventSubmitted: (_sessionName, eventId, dateStr) => (
     <>
       You submitted Training Event
       {' '}
@@ -46,7 +46,35 @@ const MESSAGE_TEMPLATES = {
       {dateStr}
     </>
   ),
+  sessionCreated: (_sessionName, eventId, dateStr) => (
+    <>
+      You created a session
+      {' '}
+      for Training Event
+      {' '}
+      <Link to={`/training-report/view/${eventId.split('-').pop()}`}>{eventId}</Link>
+      {' '}
+      on
+      {' '}
+      {dateStr}
+    </>
+  ),
   sessionSubmitted: (sessionName, eventId, dateStr) => (
+    <>
+      You submitted the session
+      {' '}
+      {sessionName}
+      {' '}
+      of Training Event
+      {' '}
+      <Link to={`/training-report/view/${eventId.split('-').pop()}`}>{eventId}</Link>
+      {' '}
+      on
+      {' '}
+      {dateStr}
+    </>
+  ),
+  sessionReviewSubmitted: (sessionName, eventId, dateStr) => (
     <>
       Your review for session
       {' '}
@@ -72,15 +100,19 @@ export const evaluateMessageFromHistory = (history) => {
       return null;
     }
 
-    if (message.isSession) {
-      return MESSAGE_TEMPLATES.sessionSubmitted(
+    const { messageTemplate } = message;
+
+    if (MESSAGE_TEMPLATES[messageTemplate]) {
+      return MESSAGE_TEMPLATES[messageTemplate](
         message.sessionName,
         message.eventId,
         message.dateStr,
       );
     }
 
+    // default case
     return MESSAGE_TEMPLATES.eventSubmitted(
+      message.sessionName,
       message.eventId,
       message.dateStr,
     );
