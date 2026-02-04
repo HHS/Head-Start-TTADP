2d2e6e1d59dc7acf589c89bf37ab012b44b6201e
thewatermethod@gmail.com <thewatermethod@gmail.com>
2026-02-04 13:12:01 -0500

    Fix duplicated citation text

--- frontend/src/components/CitationDrawerContent.js
+++ frontend/src/components/CitationDrawerContent.js
@@ -1,4 +1,5 @@
 import React, { useCallback } from 'react';
+import { uniqBy } from 'lodash';
 import PropTypes from 'prop-types';
 import useFetchNoLoading from '../hooks/useFetchNoLoading';
 import { fetchCitationTextByName } from '../fetchers/citations';
@@ -7,9 +8,11 @@ export default function CitationDrawerContent({ citations }) {
   const fetcher = useCallback(() => fetchCitationTextByName(citations), [citations]);
   const { data: content } = useFetchNoLoading([], fetcher, [citations]);
 
+  const uniqueContent = uniqBy(content, 'citation');
+
   return (
     <div>
-      {content.map((standard) => (
+      {uniqueContent.map((standard) => (
         <div key={standard.citation} className="margin-bottom-3">
           <p className="text-bold usa-prose margin-0">{standard.citation}</p>
           <p className="usa-prose margin-0">{standard.text}</p>
