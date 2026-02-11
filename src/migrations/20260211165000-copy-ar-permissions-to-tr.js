e22d6654eb3a0648da461f853e888aa8491c774b
thewatermethod@gmail.com <thewatermethod@gmail.com>
2026-02-11 14:27:46 -0500

    Check to see if a user has site access before modifying them

--- src/migrations/20260211165000-copy-ar-permissions-to-tr.js
+++ src/migrations/20260211165000-copy-ar-permissions-to-tr.js
@@ -16,12 +16,18 @@ module.exports = {
       // Copy AR permissions to TR permissions for all users with AR permissions
       await queryInterface.sequelize.query(`
         WITH ar_readwrite_users as (
-            SELECT 
+            SELECT
                 u.id user_id,
                 p."regionId" region_id
             FROM "Users" u
             LEFT JOIN "Permissions" p ON u.id = p."userId"
             WHERE p."scopeId" = 3
+            AND EXISTS (
+                SELECT 1 FROM "Permissions" p2
+                WHERE p2."userId" = u.id
+                AND p2."scopeId" = 1
+                AND p2."regionId" = 14
+            )
         ) 
         INSERT INTO "Permissions" ("userId", "scopeId", "regionId", "createdAt", "updatedAt")
         SELECT user_id, 7, region_id, NOW(), NOW() FROM ar_readwrite_users
