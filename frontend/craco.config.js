ddfd3ae36ab94a2926f22605e5c1364164c72e85
Tom Meagher <tom.meagher@adhocteam.us>
2026-02-18 05:06:21 -0800

    more frontend

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -8,4 +8,34 @@ module.exports = {
       },
     },
   },
+  plugins: [
+    {
+      plugin: {
+        overrideWebpackConfig: ({ webpackConfig }) => {
+          // Silence Dart Sass legacy JS API deprecation warnings
+          // (sass-loader v12 uses the legacy API by default)
+          webpackConfig.module.rules.forEach((rule) => {
+            if (!rule.oneOf) return;
+            rule.oneOf.forEach((oneOf) => {
+              if (!oneOf.use) return;
+              oneOf.use.forEach((loader) => {
+                if (loader.loader && loader.loader.includes('sass-loader')) {
+                  Object.assign(loader, {
+                    options: {
+                      ...loader.options,
+                      sassOptions: {
+                        ...(loader.options && loader.options.sassOptions),
+                        silenceDeprecations: ['legacy-js-api'],
+                      },
+                    },
+                  });
+                }
+              });
+            });
+          });
+          return webpackConfig;
+        },
+      },
+    },
+  ],
 };
