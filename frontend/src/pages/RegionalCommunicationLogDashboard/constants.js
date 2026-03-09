1f5ced598951dfeacd933c5c8869616c87bc26f6
Andrew Steele <andrew.steele@adhocteam.us>
2026-03-09 16:26:51 -0400

    Enabling Comm Log Result Filter

--- frontend/src/pages/RegionalCommunicationLogDashboard/constants.js
+++ frontend/src/pages/RegionalCommunicationLogDashboard/constants.js
@@ -1,10 +1,12 @@
 /* eslint-disable import/prefer-default-export */
 import {
   communicationDateFilter,
+  resultFilter,
 } from '../../components/filter/communicationLogFilters';
 
 const DASHBOARD_FILTER_CONFIG = [
   communicationDateFilter,
+  resultFilter,
 ];
 
 export { DASHBOARD_FILTER_CONFIG };
