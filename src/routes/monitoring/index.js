6956a8fbef4ac1ad77e3e5a6e96f615874f8d032
thewatermethod@gmail.com <thewatermethod@gmail.com>
2026-03-18 13:29:37 -0400

    feat: Add grant number validation middleware and integrate into monitoring routes

--- src/routes/monitoring/index.js
+++ src/routes/monitoring/index.js
@@ -6,11 +6,28 @@ import {
   getTtaByReview,
 } from './handlers';
 import transactionWrapper from '../transactionWrapper';
+import { checkGrantNumberParam, checkRecipientIdParam, checkRegionIdParam } from '../../middleware/checkIdParamMiddleware';
 
 const router = express.Router();
 router.get('/:recipientId/region/:regionId/grant/:grantNumber', transactionWrapper(getMonitoringData));
-router.get('/:recipientId/region/:regionId/tta/citation', transactionWrapper(getTtaByCitation));
-router.get('/:recipientId/region/:regionId/tta/review', transactionWrapper(getTtaByReview));
-router.get('/class/:recipientId/region/:regionId/grant/:grantNumber', transactionWrapper(getClassScore));
+router.get(
+  '/:recipientId/region/:regionId/tta/citation',
+  checkRecipientIdParam,
+  checkRegionIdParam,
+  transactionWrapper(getTtaByCitation),
+);
+router.get(
+  '/:recipientId/region/:regionId/tta/review',
+  checkRecipientIdParam,
+  checkRegionIdParam,
+  transactionWrapper(getTtaByReview),
+);
+router.get(
+  '/class/:recipientId/region/:regionId/grant/:grantNumber',
+  checkRecipientIdParam,
+  checkRegionIdParam,
+  checkGrantNumberParam,
+  transactionWrapper(getClassScore),
+);
 
 export default router;
