583c4a7d7737ddb2fb77dd14e03e1c97d9ea5605
Nato <github@klasser.net>
2026-04-02 08:31:53 -0700

    Add review_name column to DeliveredReviews fact table (#3473)

---
+++ src/migrations/20260401000000-add_review_name_to_delivered_reviews.js
@@ -0,0 +1,25 @@
+const { prepMigration } = require('../lib/migration');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+
+      await queryInterface.addColumn('DeliveredReviews', 'review_name', {
+        type: Sequelize.TEXT,
+        allowNull: true,
+      }, { transaction });
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+
+      await queryInterface.removeColumn('DeliveredReviews', 'review_name', { transaction });
+    });
+  },
+};
