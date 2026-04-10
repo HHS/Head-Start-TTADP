640ae9432a9aaff251c6a12a0c2be0b3f010c935
thewatermethod@gmail.com <thewatermethod@gmail.com>
2026-04-10 14:17:45 -0400

    Use validated ID array

--- src/scopes/grantCitation/id.ts
+++ src/scopes/grantCitation/id.ts
@@ -1,9 +1,9 @@
 /* eslint-disable import/prefer-default-export */
 import { Op } from 'sequelize';
+import { validatedIdArray } from '../utils';
 
 export function withId(ids: string[]) {
-  // convert string array to number array
-  const numericIds = ids.map((id) => Number(id)).filter((id) => !Number.isNaN(id));
+  const numericIds = validatedIdArray(ids);
 
   return {
     id: {
