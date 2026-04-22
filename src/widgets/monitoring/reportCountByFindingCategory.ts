41cb66f0bd3f29246f172eb35f071a3d2e0332ea
thewatermethod@gmail.com <thewatermethod@gmail.com>
2026-04-22 10:51:31 -0400

    Add warning threshold

--- src/widgets/monitoring/reportCountByFindingCategory.ts
+++ src/widgets/monitoring/reportCountByFindingCategory.ts
@@ -5,6 +5,7 @@ import { REPORT_STATUSES } from '@ttahub/common';
 import { IScopes } from '../types';
 import db, { sequelize } from '../../models';
 import { buildContinuousMonths } from '../../scopes/utils';
+import { auditLogger } from '../../logger';
 
 const { ActivityReport } = db;
 
@@ -21,6 +22,8 @@ interface AggregatedRow {
 }
 
 const NO_CATEGORY_LABEL = 'No finding category assigned';
+
+const WARN_THRESHOLD = 1000;
 
 export default async function reportCountByFindingCategory(
   scopes: IScopes,
@@ -47,6 +50,12 @@ export default async function reportCountByFindingCategory(
 
   if (!approvedReports.length) {
     return [];
+  }
+
+  if (approvedReports.length > WARN_THRESHOLD) {
+    auditLogger.warn(`reportCountByFindingCategory: More than ${WARN_THRESHOLD} approved reports found, which may impact performance`, {
+      approvedReportCount: approvedReports.length,
+    });
   }
 
   const approvedReportIds = approvedReports.map((r) => r.id);
