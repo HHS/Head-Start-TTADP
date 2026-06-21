a8d9ab691b1a88ef05f777b74d049ebcff607ee8
thewatermethod@gmail.com <thewatermethod@gmail.com>
2026-06-21 19:06:17 -0400

    Add migration to revert known invalid deletions

---
+++ src/migrations/20260621225954-revert-bad-objective-deletions.js
@@ -0,0 +1,37 @@
+const { prepMigration } = require('../lib/migration');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.sequelize.transaction(async (transaction) => {
+      const sessionSig = __filename;
+      await prepMigration(queryInterface, transaction, sessionSig);
+
+      await queryInterface.sequelize.query(
+        `
+        UPDATE "Objectives"
+        SET "deletedAt" = NULL
+         WHERE id IN (
+          SELECT o.id FROM "Objectives" o
+            INNER JOIN "Goals" g ON o."goalId" = g.id
+            INNER JOIN "GoalTemplates" gt ON g."goalTemplateId" = gt.id
+            INNER JOIN "ActivityReportObjectives" aro ON aro."objectiveId" = o.id
+            INNER JOIN "ActivityReports" ar ON ar.id = aro."activityReportId"
+                WHERE gt."standard" IS NOT NULL
+                AND o."onAR" = true
+                AND g."createdAt" >= '2025-09-01'
+                AND g."deletedAt" IS NULL
+                AND o."deletedAt" IS NOT NULL
+                AND ar."calculatedStatus" != 'deleted'
+          );
+        
+        `,
+        { transaction }
+      );
+    });
+  },
+
+  async down() {
+    // This cannot be sensibly rolled bacl
+  },
+};
