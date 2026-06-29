9ecdde409b1f58524f53bd10109540272ce9fad5
thewatermethod@gmail.com <thewatermethod@gmail.com>
2026-06-29 10:37:37 -0400

    Update template contents tests

--- src/lib/mailer/index.test.js
+++ src/lib/mailer/index.test.js
@@ -1029,7 +1029,7 @@ describe('mailer tests', () => {
       expect(message.subject).toBe('TTA Hub weekly digest: Activity reports for approval');
       expect(message.text).toContain(`Hello ${mockNewCollaborator.name}`);
       expect(message.text).toContain('Below are your report notifications for this week.');
-      expect(message.text).toContain('These activity reports were submitted for your review:');
+      expect(message.text).toContain('These activity reports were submitted for your approval:');
       expect(message.text).toContain(`* ${mockReport.displayId}`);
       expect(message.text).toContain(reportPath);
     });
@@ -1048,7 +1048,7 @@ describe('mailer tests', () => {
         jsonTransport
       );
       const message = JSON.parse(email.message);
-      expect(message.subject).toBe('TTA Hub monthly digest: reports for a');
+      expect(message.subject).toBe('TTA Hub monthly digest: Activity reports for approval');
       expect(message.text).toContain(`Hello ${mockNewCollaborator.name}`);
       expect(message.text).toContain('Below are your report notifications for this month.');
       expect(message.text).toContain('These activity reports were submitted for your approval:');
